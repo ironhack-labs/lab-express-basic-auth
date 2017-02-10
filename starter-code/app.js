8d3f591951053f17b03edfc7bdf80e0c7e9867df
Michiel Barnaart <michielbarnaart@gmail.com>
2017-02-10 21:14:50 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,13 +4,23 @@ const logger         = require("morgan");
 const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
-const app            = express();
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
+
+const bcrypt     = require("bcrypt");
+// const MongoStore = require("connect-mongo")(session);
 // Controllers
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/basic-auth");
+mongoose.connect("mongodb://localhost:27017/auth");
 
+const authController = require('./routes/authController');
+const siteController = require('./routes/siteController');
+var index = require('./routes/index');
+
+
+const app            = express();
 // Middlewares configuration
 app.use(logger("dev"));
 
@@ -26,8 +36,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Authentication
 app.use(cookieParser());
 
-// Routes
 
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+// Routes
+app.use('/', index);
+app.use('/', authController);
+app.use('/', siteController);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error("Not Found");
