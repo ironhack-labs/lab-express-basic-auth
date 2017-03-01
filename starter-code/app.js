fb92a730059a478866d3763ca62cc0a2a7f05907
Doyen Brown <doyen.brown@gmail.com>
2017-03-01 16:59:16 -0500

    Almost Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,9 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+const session        = require("express-session");
+const MongoStore     = require('connect-mongo')(session);
+
 
 // Controllers
 
@@ -26,7 +29,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Authentication
 app.use(cookieParser());
 
+const index = require("./routes/index.js");
+const authRoutes = require("./routes/auth-routes.js");
+
 // Routes
+app.use("/", index);
+app.use("/", authRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
