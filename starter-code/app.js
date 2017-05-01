6818d9b4d6a9a62a38113b681865c69fbebdc7d3
Bjim <imbjim1@gmail.com>
2017-05-01 17:12:20 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,11 +5,19 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+
+// Routes
+const authRoute = require('./routes/auth-routes');
+const indexRoute = require('./routes/index');
+
 
 // Controllers
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/basic-auth");
+mongoose.connect("mongodb://localhost:27017/auth-lab");
 
 // Middlewares configuration
 app.use(logger("dev"));
@@ -19,6 +27,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
 // Access POST params with body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,7 +43,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Authentication
 app.use(cookieParser());
 
-// Routes
+//Routes
+app.use('/', authRoute);
+app.use('/', indexRoute);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ starter-code/routes/auth-routes.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user')
+
+// set up bcrypt
+const bcrypt = require('bcrypt')
+const bcryptSalt     = 10;
+
+
+// show form to signup
+router.get('/signup', function(req, res, next) {
+  res.render('auth/signup')
+});
+
+// process form signup
+router.post('/signup', (req, res, next) => {
+  console.log(req.body);
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne( {username: username}, (err, user) =>{
+    if (user !== null) {
+      res.render("auth/signup", {
+        errorMessage: "The username already exists"
+      });
+      return;
+    }
+
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    console.log('username: ', username)
+    console.log('password: ', hashPass)
+
+
+
+    var newUser  = new User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      console.log('save')
+      res.redirect('/')
+    })
+  })
+
+
+
+
+
+
+
+
+})
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login')
+})
+
+
+
+
+router.post('/login', (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, (err, user) => {
+      if (err || !user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+
+
+
+      if (bcrypt.compareSync(password, user.password)) {
+        console.log(req.session)
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/private");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy((err) => {
+    // cannot access session here
+    res.redirect("/login");
+  });
+})
+
+module.exports = router;
