fd027705e60188a37770428a16d7ccd21a21a7b7
salvocostahack <salvatorecostanzopa@gmail.com>
2017-07-07 14:21:18 +0200

    iteration done2

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,8 +5,14 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+const expressLayouts = require('express-ejs-layouts');
+const bcrypt         = require('bcrypt');
+// ...other code
+
+
 
 // Controllers
+const authController = require('./routes/authController');
 
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/basic-auth");
@@ -18,15 +24,18 @@ app.use(logger("dev"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(expressLayouts);
+app.set("layout", "layouts/main-layout");
 
 // Access POST params with body parser
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Authentication
 app.use(cookieParser());
 
+
 // Routes
+app.use("/", authController);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
