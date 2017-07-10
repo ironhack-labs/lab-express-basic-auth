43a4969962cbb36d0458d776e43c932a3683e7b1
Jordi Manchon <jordimanchon@me.com>
2017-07-10 07:14:02 +0200

    Iteration 1

--- starter-code/routes/auth-routes.js
+++ starter-code/routes/auth-routes.js
@@ -4,6 +4,11 @@ const User           = require("../models/user");
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
 
+function withTitle(c, title) {
+  c.title = title || 'Undefined title';
+  return c;
+}
+
 authRoutes.get("/", (req, res, next) => {
   res.render("index");
 });
@@ -23,9 +28,31 @@ authRoutes.post("/signup", (req, res, next) => {
     password: hashPass
   });
 
-  newUser.save((err) => {
-    res.redirect("../");
+  User.findOne({
+    "username": req.body.username
+  }, "username", (err, user) => {
+    if (user !== null) {
+      console.log("User exists");
+      return res.render('signup',
+      withTitle({
+        errorMessage: "Username already exists"
+      }));
+    }
+    var username = req.body.username;
+    var password = req.body.password;
+    var salt = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      username:req.body.username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      res.redirect("../");
+    });
   });
 });
+
 
 module.exports = authRoutes;
