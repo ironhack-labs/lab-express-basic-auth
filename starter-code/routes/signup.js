73f6ff8fd5efce15d1b827e38ab480761e6b98fc
ByronGBP <byrongbp@gmail.com>
2017-09-08 12:01:10 +0200

    setup signup

---
+++ starter-code/routes/signup.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const userModel = require('../model/user.js');
+
+const router = express.Router();
+
+router.get('/',(req, res, next) => {
+
+});
+
+router.post('/', (req, res, next) => {
+
+});
