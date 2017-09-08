13ce4c6379b3aca9718f8cbf8eeb27eca22ea682
lele <lele@crackberry.local>
2017-09-08 13:22:55 +0200

    setup signin

--- starter-code/routes/signin.js
+++ starter-code/routes/signin.js
@@ -1,7 +1,6 @@
 const express 	   = require("express");
 const bcrypt 	   = require("bcrypt");
-const session      = require("express-session");
-const mongoSession = require("connect-mongo")(session);
+const userModel    = require('../model/user');
 const router       = express.Router();
 
 router.get('/signin',(req, res, next) => {
@@ -9,5 +8,34 @@ router.get('/signin',(req, res, next) => {
 });
 
 
+router.post("/signin", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("signin", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  userModel.findOne({ "username": username }, (err, user) => {
+      if (err || !user) {
+        res.render("signin", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the signin in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("signin", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+});
 
 module.exports = router;
