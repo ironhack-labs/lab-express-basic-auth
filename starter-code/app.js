7d1dd455c118e35c7644b9e8392a71c835382a59
ByronGBP <byrongbp@gmail.com>
2017-09-08 11:41:14 +0200

    setup

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,6 +4,7 @@ const logger         = require("morgan");
 const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
+const bcrypt         = require("bcrypt");
 const app            = express();
 
 // Controllers
