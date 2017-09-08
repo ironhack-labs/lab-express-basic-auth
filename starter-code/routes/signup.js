5b6fdf552681c125a8df9cf4de672f7a45de7794
lele <lele@crackberry.local>
2017-09-08 12:22:15 +0200

    some spaghetti code with signup foo

--- starter-code/routes/signup.js
+++ starter-code/routes/signup.js
@@ -1,12 +1,54 @@
-const express        = require("express");
-const userModel 	 = require('../model/user');
+const express = require("express");
+const userModel = require('../model/user');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
+const bcryptSalt = 10;
+
+router.get('/', (req, res, next) => {
 
 });
 
-router.post('/', (req, res, next) => {
+router.post('/signup', (req, res, next) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    if (username === "" || password === "") {
+        res.render("/signup", {
+            errorMessage: "Indicate a username and a password to sign up"
+        });
+        return;
+    }
+
+    userModel.findOne({ "username": username }, "username", (err, user) => {
+        if (user !== null) {
+            res.render("/signup", {
+                errorMessage: "The username already exists"
+            });
+            return;
+        }
+
+        var salt = bcrypt.genSaltSync(bcryptSalt);
+        var hashPass = bcrypt.hashSync(password, salt);
+
+        var newUser = User({
+            username,
+            password: hashPass
+
+        });
+
+        newUser.save((err) => {
+            if (err) {
+                res.render("/signup", {
+                    errorMessage: "Something went wrong when signing up"
+                });
+            } else {
+                res.redirect('/');
+            }
+        });
+
+    });
 
 });
+
+module.exports = router;
