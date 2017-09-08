4065cba709802ce7ae02c7fede1a801806bc861f
Davix76 <30471850+Davix76@users.noreply.github.com>
2017-09-08 18:15:15 +0200

    primera interaccion

---
+++ starter-code/routes/auth.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const User = require('../models/User');
+const bcryptSalt = 10;
+
+
+
+
+router.post('/signup', function(req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }).then(user =>{
+    if(user){
+      res.render("auth/signup", {
+        errorMessage: "User already exists"
+      });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+    new User({
+        username: username,
+        password: hashPass
+      })
+      .save()
+      .then(() => res.redirect('/'))
+      .catch(e => next(e));
+  });
+
+});
