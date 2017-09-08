04a54e183d2d1f0999ff13e7209837bc4f401d39
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-09-08 13:37:40 +0200

    iteration 2 done. Login works

--- starter-code/routes/auth-routes.js
+++ starter-code/routes/auth-routes.js
@@ -50,7 +50,41 @@ authRoutes.post("/signup", (req, res, next) => {
   });
 });
 
+//redirect to login page
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
 
+//login a user and check if it matches
+authRoutes.post("/login", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, (err, user) => {
+      if (err || !user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+});
 
 
 
