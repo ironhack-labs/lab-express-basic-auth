d21691f03b85d2a087cd461317fa2c65ac519620
Nick Navarro <jacinto802@gmail.com>
2017-09-24 22:07:15 -0400

    dont

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,36 +1,48 @@
-const express        = require("express");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+// var users = require('./routes/users');
+var app = express();
 
-// Controllers
+const mongoose = require('mongoose');
+const session = require("express-session");
+const MongoStore = require('connect-mongo')(session);
 
-// Mongoose configuration
+const authRoutes = require('./routes/auth-routes');
+const siteRoutes = require('./routes/site-routes');
 mongoose.connect("mongodb://localhost/basic-auth");
 
-// Middlewares configuration
-app.use(logger("dev"));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// View engine configuration
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-
-// Access POST params with body parser
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Authentication
 app.use(cookieParser());
-
-// Routes
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+app.use('/', index);
+app.use('/', authRoutes);
+app.use('/', siteRoutes);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error("Not Found");
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -39,11 +51,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
