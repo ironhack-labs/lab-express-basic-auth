9bee7305f676116c3e996a18c82c85b3c0234ba8
Elo <wanang.elodie@hotmail.fr>
2017-09-29 13:20:04 +0200

    PAR - LIUDA/ELODIE - PARTLY DONE //  Just Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,14 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+// const User           = require("../models/user");
+const bcrypt = require ("bcrypt");
+const saltRounds = 10;
+const salt  = bcrypt.genSaltSync(saltRounds);
+const authRoutes = require('./routes/auth-routes');
+const session = require ("express-session");
+const MongoStore = require ("connect-mongo")(session);
+
 
 // Controllers
 
@@ -13,6 +21,14 @@ mongoose.connect("mongodb://localhost/basic-auth");
 
 // Middlewares configuration
 app.use(logger("dev"));
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // View engine configuration
 app.set("views", path.join(__dirname, "views"));
@@ -27,7 +43,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Routes
-
+app.use('/',authRoutes);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error("Not Found");
