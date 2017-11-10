b36e32d4d0ff755262368a051465ec679226d9ed
Sara Morales <saringa@gmail.com>
2017-11-10 18:02:51 +0100

    cristina & sara m.

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,15 +1,20 @@
-const express        = require("express");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const app = express();
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+const index = require('./routes/index');
+const auth = require('./routes/auth');
 
 // Controllers
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/basic-auth");
+mongoose.connect("mongodb://localhost/basic-auth-pp");
 
 // Middlewares configuration
 app.use(logger("dev"));
@@ -21,12 +26,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Access POST params with body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+
 
 // Authentication
 app.use(cookieParser());
 
 // Routes
+
+app.use("/", index);
+app.use("/auth", auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
