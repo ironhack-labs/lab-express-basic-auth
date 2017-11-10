f82c5eee959199b63186d9e598adf93e28cec99c
sdediego <sergio.dediego@outlook.com>
2017-11-10 18:17:55 +0100

    Iteracion 3 - Protected Routes

--- starter-code/routes/site-routes.js
+++ starter-code/routes/site-routes.js
@@ -5,4 +5,20 @@ siteRoutes.get("/", (req, res, next) => {
   res.render("home");
 });
 
+siteRoutes.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+siteRoutes.get("/main", (req, res, next) => {
+  res.render("main");
+});
+
+siteRoutes.get("/private", (req, res, next) => {
+  res.render("private");
+});
+
 module.exports = siteRoutes;
