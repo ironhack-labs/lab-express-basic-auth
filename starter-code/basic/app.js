f70e8b3febd780b56842c29fb3a813cbd6b68a5b
Manuel Ortega <coolhunting@MacBook-Pro-de-Manuel.local>
2017-11-10 18:18:54 +0100

    iteracion 1 completada

--- starter-code/basic/app.js
+++ starter-code/basic/app.js
@@ -5,10 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// view engine setup
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/basic/routes/index.js
+++ starter-code/basic/routes/index.js
@@ -1,9 +1,107 @@
-var express = require('express');
-var router = express.Router();
+
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/signup', (req, res) => {
+  res.render('auth/signup', { title: 'SIGNUP' });
+});
+
+
+/* GET home page. */
+router.post('/signup', (req, res) => {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username },
+    "username",
+    (err, user) => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists"
+        });
+        return;
+      }
+
+      var salt     = bcrypt.genSaltSync(bcryptSalt);
+      var hashPass = bcrypt.hashSync(password, salt);
+
+      var newUser = User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save((err) => {
+        res.redirect("/");
+      });
+    });
+});
+
+
+
+router.get('/login', (req, res) => {
+  res.render('auth/login', { title: 'LOGIN' });
+});
+
+// const bcrypt = require('bcrypt');
+// const path = require('path');
+// const debug = require('debug')('basic-auth:'+ path.basename(__filename));
+// const bcryptSalt = 10;
+
+
+// router.get('/', (req, res) => {
+//   res.render('index', { title: 'INDEXJS' });
+// });
+
+//
+// /* GET home page. */
+// router.post('/signup', (req, res) => {
+//
+//   const username = req.body.username;
+//   const password = req.body.password;
+//
+//   if (username === "" || password === "") {
+//     res.render("auth/signup", {
+//       errorMessage: "Indicate a username and a password to sign up"
+//     });
+//     return;
+//   }
+//
+//   User.findOne({ "username": username },
+//     "username",
+//     (err, user) => {
+//       if (user !== null) {
+//         res.render("auth/signup", {
+//           errorMessage: "The username already exists"
+//         });
+//         return;
+//       }
+//
+//       var salt     = bcrypt.genSaltSync(bcryptSalt);
+//       var hashPass = bcrypt.hashSync(password, salt);
+//
+//       var newUser = User({
+//         username,
+//         password: hashPass
+//       });
+//
+//       newUser.save((err) => {
+//         res.redirect("/");
+//       });
+//     });
+//
+// });
 
 module.exports = router;
