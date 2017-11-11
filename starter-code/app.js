96f9885da360451a071f59cd76f02c3f381070cc
bryan <supercrew3@gmail.com>
2017-11-11 20:48:29 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,11 +5,20 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+const auth = require('./routes/auth');
 
 // Controllers
 
 // Mongoose configuration
-mongoose.connect("mongodb://localhost/basic-auth");
+mongoose.connect("mongodb://localhost/basic-auth", {
+  keepAlive : true,
+  reconnectTries : Number.MAX_VALUE,
+  useMongoClient: true,
+});
+
 
 // Middlewares configuration
 app.use(logger("dev"));
@@ -19,6 +28,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
 // Access POST params with body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,6 +45,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Routes
+app.use('/auth',auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
