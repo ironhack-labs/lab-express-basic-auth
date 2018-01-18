7c35c3df4781d430292955de1f9e376ca1adecc5
Marco <marco238cuba@gmail.com>
2018-01-18 02:18:42 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,36 +1,55 @@
-const express        = require("express");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require("connect-mongo")(session);
 
-// Controllers
+require('./configs/db.config');
 
-// Mongoose configuration
-mongoose.connect("mongodb://localhost/basic-auth");
+var auth = require('./routes/auth.routes');
+var user = require('./routes/user.routes');
 
-// Middlewares configuration
-app.use(logger("dev"));
+var app = express();
 
-// View engine configuration
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+// view engine setup
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// Access POST params with body parser
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Authentication
 app.use(cookieParser());
+app.use(session({
+  secret: 'Super Secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 1000
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  })
+}))
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+app.use('/', auth);
+app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error("Not Found");
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -39,11 +58,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
