843acafb3fd579b70e08ff0fa3dfae9527187271
mouyaq <mouyaq@gmail.com>
2018-01-20 01:41:38 +0100

    feat: Iteration 3

--- starter-code/controllers/user.controller.js
+++ starter-code/controllers/user.controller.js
@@ -1,3 +1,11 @@
 module.exports.profile = (req, res, next) => {
     res.render('user/profile', {user: req.session.currentUser})
 }
+
+module.exports.main = (req, res, next) => {
+    res.render('user/main', {user: req.session.currentUser})
+}
+
+module.exports.private = (req, res, next) => {
+    res.render('user/private', {user: req.session.currentUser})
+}
--- starter-code/routes/auth.routes.js
+++ starter-code/routes/auth.routes.js
@@ -8,4 +8,6 @@ router.post('/signup', authController.doSignup);
 router.get('/login', authController.login);
 router.post('/login', authController.doLogin);
 
+router.get('/logout', authController.logout);
+
 module.exports = router;
