3088ec78501e79277169ac3d8159dbdcad039b45
Felix Heras <heras.felix@gmail.com>
2018-01-23 17:00:46 +0100

    feat:Last commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,14 +1,18 @@
 const express        = require("express");
 const path           = require("path");
+const favicon = require('serve-favicon');
 const logger         = require("morgan");
 const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
+//rsconst expressLayouts  = require("express-ejs-layouts");
 const mongoose       = require("mongoose");
+//const session = require('express-session');
+//const MongoStore = require("connect-mongo")(session);
 const app            = express();
-const authRoutes = require('./routes/auth-routes');
 const bcrypt         = require('bcrypt');
 // Controllers
-mongoose.Promise = global.Promise;
+const authRoutes = require('./routes/auth-routes');
+const user = require('./routes/user.routes');
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/basic-auth");
 
@@ -16,19 +20,37 @@ mongoose.connect("mongodb://localhost/basic-auth");
 app.use(logger("dev"));
 
 // View engine configuration
+app.use(expressLayouts);
+app.set('layout', 'layouts/main');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 // Access POST params with body parser
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Authentication
 app.use(cookieParser());
+app.use(session({
+   secret: 'Super Secret',
+   resave: false,
+   saveUninitialized: true,
+   cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 1000
+    },
+    store: new MongoStore({
+     mongooseConnection: mongoose.connection,
+     ttl: 24 * 60 * 60
+   })
+ }));
 
 // Routes
-app.use('/', authRoutes);
+app.use('/', auth);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/controllers/auth.controller.js
+++ starter-code/controllers/auth.controller.js
@@ -1,3 +1,72 @@
-module.exports.index = (req, res, next) => {
-  res.render('index');
-}
+module.exports.doSignup = (req, res, next) => {
+     User.findOne({ username: req.body.username })
+         .then(user => {
+             if (user != null) {
+                 res.render('auth/signup', { user: user, error: { username: 'Username already exists'} })
+             } else {
+                 user = new User(req.body);
+                 user.save()
+                     .then(() => {
+                         res.redirect('/signup');
+                     }).catch(error => {
+                         if (error instanceof mongoose.Error.ValidationError) {
+                             res.render('auth/signup', { user: user, error: error.errors })
+                         } else {
+                             next(error);
+                         }
+                     });
+             }
+         }).catch(error => next(error));
+  }
+
+  module.exports.login = (req, res, next) => {
+    console.log(req.session.currentUser);
+      res.render('auth/login');
+ }
+
+ module.exports.doLogin = (req, res, next) => {
+     const username = req.body.username;
+     const password = req.body.password;
+     if (!username || !password) {
+         res.render('auth/login', {
+             user: { username: username },
+             error: {
+                 username: username ? '' : 'Username is required',
+                 password: password ? '' : 'Password is required'
+             }
+         });
+     } else {
+         User.findOne({ username: username})
+             .then(user => {
+                 errorData = {
+                     user: { username: username },
+                     error: { password: 'Invalid username or password' }
+                 }
+                 if (user) {
+                     user.checkPassword(password)
+                         .then(match => {
+                             if (!match) {
+                                 res.render('auth/login', errorData);
+                             } else {
+                                 req.session.currentUser = user;
+                                 res.redirect('/user/profile');
+                             }
+                         })
+                         .catch(error => next(error));
+                 } else {
+                     res.render('auth/login', errorData);
+                 }
+             }).catch(error => next(error));
+     }
+
+ }
+
+ module.exports.logout = (req, res, next) => {
+     req.session.destroy(error => {
+         if (error) {
+             next(error);
+         } else {
+             res.redirect("/login");
+         }
+     });
+  }
