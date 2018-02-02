bece937f6e7962714a97f0a5a58a47832181737e
Ana Recuero <anarecsa@gmail.com>
2018-02-02 17:53:19 +0100

    Home redirect with/without session

--- starter-code/routes/auth-routes.js
+++ starter-code/routes/auth-routes.js
@@ -18,37 +18,68 @@ router.post("/signup", (req, res, next) => {
   let username = req.body.username;
   let password = req.body.password;
 
-  if (username == "" || password == ""){
-    console.log("EMPTY")
-    res.render("auth/signup",{
+  if (username == "" || password == "") {
+    console.log("EMPTY");
+    res.render("auth/signup", {
       errorMessage: "Username or password can't be empty"
     });
     return;
   }
 
-  User.findOne({"username": username},
-    "username",
-    (err, user) => {
-      if (user !==  null){
-        res.render("auth/signup",{
-          errorMessage: "The username already exists"
-        });
-        return;
-      }
-      let salt = bcrypt.genSaltSync(bcryptSalt);
-      let hashPass = bcrypt.hashSync(password, salt);
-    
-      let newUser = User({
-        username,
-        password: hashPass
+  User.findOne({ username: username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", {
+        errorMessage: "The username already exists"
       });
-    
-      newUser.save(err => {
-        res.redirect("/signup");
-      });
+      return;
+    }
+    let salt = bcrypt.genSaltSync(bcryptSalt);
+    let hashPass = bcrypt.hashSync(password, salt);
 
-    }) 
+    let newUser = User({
+      username,
+      password: hashPass
+    });
 
+    newUser.save(err => {
+      res.redirect("/signup");
+    });
+  });
 });
+
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if (username == "" || password == "") {
+    res.render("auth/signup", {
+      errorMessage: "Username or password can't be empty"
+    });
+    return;
+  }
+
+  User.findOne({ username: username }, (err, user) => {
+    if (err || !user) {
+      res.render("auth/login", {
+        errorMessage: "The username doesn't exist"
+      });
+      return;
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      req.session.currentUser = user;
+      res.redirect("/");
+    } else {
+      res.render("auth/login", {
+        errorMessage: "Incorrect password"
+      });
+    }
+  });
+});
+
+
 
 module.exports = router;
