17194cc2af24b8d3d819f56d880f3d0d329d0c57
Alvaro Martinez <alvaromartinez@MacBook-Pro-de-Alvaro.local>
2018-02-02 15:57:54 +0100

    Iteración 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,8 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+const bcrypt     = require("bcrypt");
+
 
 // Controllers
 
@@ -27,6 +29,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Routes
+const authRoutes = require('./routes/auth-routes');
+app.use('/', authRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
