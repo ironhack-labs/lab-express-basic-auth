00a13df687bceacbf99fe0c4f7928fa431df0734
Kathia Rieser <kath.medulla@gmail.com>
2018-02-02 18:19:27 +0100

    iteración 2

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -54,4 +54,45 @@ authRoutes.post("/signup", (req, res, next) => {
 });
 
 
+authRoutes.get('/login', (req, res, next) => {
+  res.render('auth/login', { title: "Login" });
+});
+
+authRoutes.post("/login", (req, res, next) => {
+  const {username,password} = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username }, (err, user) => {
+      if (err || !user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        debug(`${user.username} is now logged in`);
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+});
+
+
+authRoutes.get('/logout',(req,res) => {
+  req.session.currentUser = null;
+  res.redirect('/');
+})
+
 module.exports = authRoutes;
