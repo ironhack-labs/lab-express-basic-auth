b8a07031dbbf4d673d8ccba0d9f0476a8a629f90
davidalexteixeira <david5teixeira@gmail.com>
2018-02-04 21:18:20 +0100

    login page

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -7,6 +9,8 @@ const mongoose = require('mongoose');
 const app = express();
 const index = require('./routes/index');
 const authRoutes = require('./routes/auth-routes');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 // Controllers
 
@@ -15,6 +19,14 @@ mongoose.connect('mongodb://localhost/basic-auth');
 
 // Middlewares configuration
 app.use(logger('dev'));
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // View engine configuration
 app.set('views', path.join(__dirname, 'views'));
--- starter-code/routes/auth-routes.js
+++ starter-code/routes/auth-routes.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const authRoutes = express.Router();
 const User = require('../models/user');
@@ -6,7 +8,9 @@ authRoutes.get('/signup', (req, res, next) => {
   res.render('auth/signup');
 });
 
-// User model
+authRoutes.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
 
 // BCrypt to encrypt passwords
 const bcrypt = require('bcrypt');
@@ -25,6 +29,36 @@ authRoutes.post('/signup', (req, res, next) => {
 
   newUser.save((err) => {
     res.redirect('/');
+  });
+});
+
+authRoutes.post('/login', (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      message: 'Indicate a username and a password to sign up'
+    });
+    return;
+  }
+
+  User.findOne({ 'username': username }, (err, user) => {
+    if (err || !user) {
+      res.render('auth/login', {
+        message: "The username doesn't exist"
+      });
+      return;
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      // Save the login in the session!
+      req.session.currentUser = user;
+      res.redirect('/');
+    } else {
+      res.render('auth/login', {
+        message: 'Incorrect password'
+      });
+    }
   });
 });
 
