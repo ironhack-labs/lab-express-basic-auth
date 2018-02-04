896e68bbedfd76cc632e84c0424ffebecef417df
Rodrigo Daoud <rodaoud93@gmail.com>
2018-02-04 21:25:57 +0100

    done Iteration 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,11 @@ const logger         = require("morgan");
 const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
+const session        = require("express-session");
+const expressLayouts = require('express-ejs-layouts');
+const MongoStore     = require("connect-mongo")(session);
+const authRoutes     = require('./routes/auth-routes');
+const siteRoutes     = require('./routes/site-routes'); 
 const app            = express();
 
 // Controllers
@@ -17,11 +22,25 @@ mongoose.connect("mongodb://localhost/basic-auth", {
 
 // Middlewares configuration
 app.use(logger("dev"));
+app.use(express.static('public'));
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // View engine configuration
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+
+//Layout
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
+app.set('views', __dirname + '/views');
 
 // Access POST params with body parser
 app.use(bodyParser.json());
@@ -31,8 +50,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Routes
+app.use('/', authRoutes);
+app.use('/', siteRoutes);
 
-// catch 404 and forward to error handler
+// -- 404 and error handler
+
 // NOTE: requires a views/not-found.ejs template
 app.use(function (req, res, next) {
   res.status(404);
