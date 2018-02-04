6d553f655386a016fcb9600b6f02d4d73a6171d6
theRealSheng <sheng.huchai@gmail.com>
2018-02-05 00:54:10 +0100

    first commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,13 +1,18 @@
+
+'use strict';
+
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+const authRoutes = require('./routes/auth-route');
 
+mongoose.connect('mongodb://localhost/basic-auth');
+
+// Route
 const app = express();
 
 // view engine setup
@@ -22,8 +27,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', authRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ starter-code/routes/auth-route.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const authRoutes = express.Router();
+
+authRoutes.get('/signup', (req, res, next) => {
+  res.render('./auth/signup');
+});
+
+module.exports = authRoutes;
