95e29bf4550c5eb7cfe71bf9380e97600dd11689
Francis Calizo <francisacalizo@gmail.com>
2018-02-19 16:28:17 -0500

    complete except logout and bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,11 +5,24 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
+const session        = require("express-session");
+const MongoStore     = require("connect-mongo")(session);
 
 // Controllers
 
+// Configure Middleware to enable Sessions in Express
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
 // Routes
 const authRoutes = require('./routes/auth-routes');
+const siteRoutes = require('./routes/site-routes');
 
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/basic-auth-pairprogramming");
@@ -29,8 +42,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Authentication
 app.use(cookieParser());
 
-// Middlwarex
+// Middlware
 app.use('/', authRoutes);
+app.use('/', siteRoutes);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
