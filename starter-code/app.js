4e2a858ef822b94e72e2c54f161b9407bd154f02
Davinia Roca <daviniaroca@Davinias-MacBook-Air.local>
2018-02-19 09:48:04 -0600

    done 1 iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,36 +1,49 @@
-const express        = require("express");
-const path           = require("path");
-const logger         = require("morgan");
-const cookieParser   = require("cookie-parser");
-const bodyParser     = require("body-parser");
-const mongoose       = require("mongoose");
-const app            = express();
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const session = require("express-session");
+const MongoStore =require("connect-mongo")(session);
 
-// Controllers
 
-// Mongoose configuration
-mongoose.connect("mongodb://localhost/basic-auth");
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-// Middlewares configuration
-app.use(logger("dev"));
+var app = express();
 
-// View engine configuration
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/autentication");
 
-// Access POST params with body parser
+// //Sesiones de usuario
+// app.use(session({
+//   secret: "bliss",
+//   cookie: { maxAge: 60000 },
+//   store: new MongoStore({
+//     mongooseConnection: mongoose.connection,
+//     ttl: 24 * 60 * 60 // 1 day
+//   })
+// }));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// Authentication
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+app.use('/', index);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error("Not Found");
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -39,11 +52,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
