4de2469008661063cfc1b0e7d63099a6c8010e73
Helieber Sigesmundo <heliebersig@gmail.com>
2018-02-19 17:06:50 -0500

    Done with 3rd Iteration

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,13 +4,18 @@ const logger         = require("morgan");
 const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
+const session        = require("express-session");
+const MongoStore     = require("connect-mongo")(session);
 const app            = express();
+
 
 // Controllers
 
 
 // Routes
 const authRoutes = require('./routes/auth-routes');
+const siteRoutes = require('./routes/site-routes');
+
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/basic-auth");
 
@@ -29,8 +34,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Authentication
 app.use(cookieParser());
 
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 // Middleware
 app.use('/', authRoutes);
+app.use('/', siteRoutes);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/routes/auth-routes.js
+++ starter-code/routes/auth-routes.js
@@ -56,4 +56,73 @@ authRoutes.post("/signup", (req, res, next) =>{
     });
 });
 
+// Router to handle login form
+authRoutes.get("/login", (req, res, next) => {
+    res.render("auth/login");
+});
+
+authRoutes.post("/login", (req, res, next) => {
+    var username = req.body.username;
+    var password = req.body.password;
+  
+    if (username === "" || password === "") {
+      res.render("auth/login", {
+        errorMessage: "Please enter a username and a password to log in"
+      });
+      return;
+    }
+  
+    User.findOne({ "username": username }, (err, user) => {
+        if (err || !user) {
+          res.render("auth/login", {
+            errorMessage: "The username doesn't exist"
+          });
+          return;
+        }
+        if (bcrypt.compareSync(password, user.password)) {
+          // Save the login in the session!
+          req.session.currentUser = user;
+          res.redirect("/");
+        } else {
+          res.render("auth/login", {
+            errorMessage: "Incorrect password"
+          });
+        }
+    });
+  });
+
+  authRoutes.get("/login", (req, res, next) => {
+    res.render("auth/login");
+  });
+
+  authRoutes.post("/login", (req, res, next) => {
+    var username = req.body.username;
+    var password = req.body.password;
+  
+    if (username === "" || password === "") {
+      res.render("auth/login", {
+        errorMessage: "Indicate a username and a password to sign up"
+      });
+      return;
+    }
+  
+    User.findOne({ "username": username }, (err, user) => {
+        if (err || !user) {
+          res.render("auth/login", {
+            errorMessage: "The username doesn't exist"
+          });
+          return;
+        }
+        if (bcrypt.compareSync(password, user.password)) {
+          // Save the login in the session!
+          req.session.currentUser = user;
+          res.redirect("/");
+        } else {
+          res.render("auth/login", {
+            errorMessage: "Incorrect password"
+          });
+        }
+    });
+  });
+
 module.exports = authRoutes;
