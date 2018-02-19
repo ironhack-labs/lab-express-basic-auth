c152208013e24034fc0b5e9582c8323b7a0b3b09
Debby ermel <debbyermel@gmail.com>
2018-02-18 18:47:56 -0600

    Almost Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,34 +5,16 @@ const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
 const app            = express();
-const session        = require("express-session");
-const MongoStore     = require("connect-mongo")(session);
 
-var index = require('./routes/index');
+ //Session 
+ const session       = require("express-session");
+ const MongoStore    = require("connect-mongo")(session);
+
+ const index = require('./routes/index');
 
 
-// Controllers
-
-// Mongoose configuration
-mongoose.connect("mongodb://localhost/basic-auth");
-
-// Middlewares configuration
-app.use(logger("dev"));
-
-// View engine configuration
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
-
-// Access POST params with body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Authentication
-app.use(cookieParser());
-
-//Routes
-app.use('/', index);
+// Mongoose configuration Db
+mongoose.connect("mongodb://localhost/ironAccount");
 
 //Sesiones de usuario
 app.use(session({
@@ -45,10 +27,24 @@ app.use(session({
 }));
 
 
-// const mongoose = require("mongoose");
-//  mongoose.connect("mongodb://localhost:27017/ironAccount");//db name
+// View engine configuration
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
+// Middlewares configuration
+app.use(logger("dev"));
+
+// Access POST params with body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Authentication
+app.use(cookieParser());
+
+//Routes
+app.use('/', index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -7,3 +7,4 @@ const userSchema = new Schema({
 });
 
 module.exports = mongoose.model("User", userSchema);
+
