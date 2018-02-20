e90f8fbed3d861e3f9d0dc12188be794b22c2333
ElsoMichael <elsomichael@ymail.com>
2018-02-20 07:59:02 -0500

    initial commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,15 +4,29 @@ const logger         = require("morgan");
 const cookieParser   = require("cookie-parser");
 const bodyParser     = require("body-parser");
 const mongoose       = require("mongoose");
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const app            = express();
 
 // Controllers
+const authRoutes = require("./routes/auth-routes")
+const siteRoutes = require('./routes/site-routes');
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Mongoose configuration
 mongoose.connect("mongodb://localhost/basic-auth");
 
 // Middlewares configuration
 app.use(logger("dev"));
+
 
 // View engine configuration
 app.set("views", path.join(__dirname, "views"));
@@ -27,6 +41,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Routes
+app.use("/", authRoutes);
+app.use('/', siteRoutes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
