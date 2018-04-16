3eb940d5e38cfaf0654f6ee53e3b5e9487ed2c65
FernandoGalende <fernandogalende@gmail.com>
2018-04-16 17:24:37 +0200

    interation 3 done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.render('index',{user: req.session.currentUser});  
+});
+
+router.get('/main', (req, res) => {
+  if(req.session.currentUser){
+    res.render('main',{user: req.session.currentUser});    
+  } else{
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
