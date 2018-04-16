e277fd1db10ebae38802cae71f7f79de5e096026
Raul.dll <rdl.llave@gmail.com>
2018-04-16 19:10:39 +0200

    iteration...

---
+++ routes/auth.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+const router = express.Router();
+
+const bcryptSalt = 10;
+
+
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+
+router.post("/signup", (req, res) => {
+  let { username, password } = req.body;
+  User.findOne({ username: username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", {
+        errorMessage: "The username already exists"
+      });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save(err => {
+      res.redirect("/");
+    });
+  });
+});
+
+/* Auth: show signup form */
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+/* Auth: Check user and log it in */
+router.post("/login", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to login"
+    });
+    return;
+  }
+
+  User.findOne({ username: username }, (err, user) => {
+    if (err || !user) {
+      res.render("auth/login", {
+        errorMessage: "The username doesn't exist"
+      });
+      return;
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      // Save the login in the session!
+      req.session.currentUser = user;
+      res.redirect("/");
+    } else {
+      res.render("auth/login", {
+        errorMessage: "Incorrect password"
+      });
+    }
+  });
+});
+
+module.exports = router;
