b34ffe89fa5e305d9be98d00e47913a376c51bad
FernandoGalende <fernandogalende@gmail.com>
2018-04-16 17:02:21 +0200

    interation 2 done

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -37,4 +37,41 @@ router.post("/signup", (req, res) => {
   });
 });
 
+////--------Login-----------/////////
+router.get ("/login", (req, res)=>{
+  res.render("auth/login");
+  });
+
+router.post("/login", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ username: username }, (err, user) => {
+      if (err || !user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  });
+});
+
+
+
 module.exports = router;
