6444f058df6b10740207ea0cf4fcaa2043518cc7
JMBarcelo <jmbbazal@gmail.com>
2018-04-16 16:43:54 +0200

    Hecho sin Bonus

---
+++ basic-auth/app.js
@@ -0,0 +1,86 @@
+require("dotenv").config();
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+//const debug = require("/.log")(__filename);
+
+mongoose.Promise = Promise;
+mongoose
+  .connect(process.env.DBURL)
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+const app = express();
+
+// Middleware Setup
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+);
+
+// Express View engine setup
+
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+// default value for title local
+app.locals.title = "Basic Auth";
+
+const index = require("./routes/index");
+app.use("/", index);
+const authRoutes = require("./routes/authRoutes");
+app.use("/", authRoutes);
+const siteRoutes = require("./routes/siteRoutes");
+app.use('/', siteRoutes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = process.env.ENV === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+}); // Express middleware to handle errors
+
+module.exports = app;
