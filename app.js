0888b37366b5dfcb54a2c23a64d51fa1fcf8effb
fodon11 <francisco.odon11@gmail.com>
2018-06-19 19:55:44 -0400

    finished expect password strength display

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/lab-express-basic-auth', {useMongoClient: true})
+  .connect('mongodb://localhost/lab-basic-auth', {useMongoClient: true})
   .then(() => {
     console.log('Connected to Mongo!')
   }).catch(err => {
@@ -29,6 +31,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: {maxAge: 60000},
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  })
+}));
 
 // Express View engine setup
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,116 @@
 const express = require('express');
 const router  = express.Router();
+const Users = require('../models/users');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const {username, password} = req.body;
+
+  if (username === "" || password === ""){
+    res.render('signup', {
+      errorMessage: 'Please indicate a user and password or BE DOOMED'
+    });
+    return;
+  }
+  
+  Users.findOne({'username': username})
+    .then(user => {
+      if(user == null){
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(password, salt);
+
+        const newUser = Users({
+          username, 
+          password: hash
+        });
+
+        newUser.save()
+          .then(user => {
+            res.redirect('/');
+          })
+          .catch(err => {
+            console.log('Errors falling from the sky: ', err);
+          });
+      } else {
+        res.render('signup', {
+          errorMessage: 'This username already exists, who do you think you are??'
+        });
+      }
+    })
+    .catch(err => {
+      console.log('An error here, an error there, an error everywhere!: ', err);
+      next();
+    });
+});
+
+router.get('/yeet', (req, res, next) => {
+  res.render('yeet');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  const {username, password} = req.body;
+
+  if (username === "" || password === "") {
+    res.render('login', {
+      errorMessage: 'Please indicate a user and password to get dis'
+    });
+    return;
+  }
+  
+  Users.findOne({'username': username})
+    .then(user => {
+      if(user == null){
+        res.render('login', {
+          errorMessage: 'Username or password is not valid'
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)){
+        req.session.currentUser = user;
+        res.redirect('/yeet');
+      } else {
+        res.render('login', {
+          errorMessage: 'Username or passsword is not valid'
+        });
+      }
+      
+    })
+    .catch(err => {
+      console.log('Right user no password who you be?: ', err);
+      next();
+    });
+});
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.render('login', {
+      errorMessage: 'Please login before attempting to view this page'
+    })
+  }
+});
+
+router.get('/main', (req, res, next) => {
+  res.render('main');
+});
+
+router.get('/private', (req, res, next) => {
+  res.render('private');
 });
 
 module.exports = router;
