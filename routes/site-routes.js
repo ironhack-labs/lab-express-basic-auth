f462794ea40e42f587d4102250f290a51a48f0ec
MyrnaArredondo <tracia_ra@hotmail.com>
2018-07-07 16:30:41 -0500

    done

---
+++ routes/site-routes.js
@@ -0,0 +1,23 @@
+const express    = require("express");
+const siteRoutes = express.Router();
+
+siteRoutes.get("/", (req, res, next) => {
+  res.render("home");
+});
+
+siteRoutes.use((req, res, next) => {
+    if (req.session.currentUser) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  });
+  
+  siteRoutes.get("/main", (req, res, next) => {
+    res.render("main");
+  });
+
+  siteRoutes.get("/private", (req, res, next) => {
+    res.render("private");
+  });
+module.exports = siteRoutes;
