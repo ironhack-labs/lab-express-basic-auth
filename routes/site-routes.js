406aca5520593e52a8091308a016eb5d84a2747b
Ricardo Rea <rickirea@gmail.com>
2018-07-07 15:19:19 -0500

    Done

---
+++ routes/site-routes.js
@@ -0,0 +1,24 @@
+const express    = require("express");
+const siteRoutes = express.Router();
+
+siteRoutes.get("/", (req, res, next) => {
+  res.render("home");
+});
+
+siteRoutes.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+siteRoutes.get("/secret", (req, res, next) => {
+  res.render("secret");
+});
+
+siteRoutes.get("/main", (req, res, next) => {
+  res.render("main");
+});
+
+module.exports = siteRoutes;
