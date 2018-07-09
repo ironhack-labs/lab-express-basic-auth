e4d40c6bd626770d68c841300b5c8b79c03ee966
Paula <paulagarizcun@gmail.com>
2018-07-09 16:52:35 +0200

    I2

---
+++ basic-auth/routes/indexRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router  = express.Router();
+
+/* GET home page */
+router.get('/',(req,res) => {
+    res.render('index');
+})
+
+//router.get('/private',(req,res) => {
+//    if(req.session.currentUser){
+//        res.render('private-page');
+//    }else{
+//        res.redirect('/');
+//    }
+//})
+
+module.exports = router;
