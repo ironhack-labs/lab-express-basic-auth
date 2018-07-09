f0b1cc95641ad7ab3c6b5a3352032c516c56a145
Will Schaller <willschaller@gmail.com>
2018-07-09 19:39:59 +0200

    Add User model

---
+++ lab-express-basic-auth/models/users.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String
+})
+  ;
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
