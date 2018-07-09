a019feb6ad327476f480ba79b33c71c0cc6c8c19
Will Schaller <willschaller@gmail.com>
2018-07-09 20:11:37 +0200

    Basic signup post route

--- lab-express-basic-auth/app.js
+++ lab-express-basic-auth/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,7 +14,8 @@ const app = express();
 
 // --- Configurations
 // database
-
+const dbName = 'lab-express-basic-auth';
+mongoose.connect(`mongodb://localhost/${dbName}`);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- lab-express-basic-auth/routes/auth.js
+++ lab-express-basic-auth/routes/auth.js
@@ -5,9 +5,33 @@ const bodyParser = require('body-parser');
 const User = require('../models/users');
 
 router.get('/signup', function (req, res, next) {
-  const newUser = new User({ username: 'foo', password: 'bar' });
-  newUser.save();
   res.render('auth/signup', { title: 'Express' });
+});
+
+router.post('/signup', function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if user is logged in
+
+  // Check if username and password params have been sent
+  if (!username || !password) {
+    res.redirect('/auth/signup');
+    return;
+  }
+
+  // Check if user exists
+  User.findOne({ username: username })
+    .then(user => {
+      if (user) {
+        // Username exists
+        res.redirect('/auth/signup');
+      } else {
+        // Username doesn't exist, create the user
+        const newUser = new User({ username: username, password: password });
+        newUser.save();
+        res.render('auth/signup', { title: 'Express' });
+      }
+    });
 });
 
 router.get('/login', function (req, res, next) {
