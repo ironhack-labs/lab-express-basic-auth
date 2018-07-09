16317cd8dd61c4abc0219917aa3be650ef760f35
Antonio Esteban <aecdigitaltransformer@gmail.com>
2018-07-09 17:24:05 +0200

    primerv

---
+++ lab-express-basic-auth/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {type:String, required: true, unique:true},
+  password: {type:String, required: true},
+}, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
