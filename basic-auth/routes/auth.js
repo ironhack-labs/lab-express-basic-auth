38edd4a422f84e4aeac80308b96a40171a397017
Montse Badia <mbadia_11@hotmail.com>
2018-07-09 20:41:10 +0200

    created post method after sign up and encrypted password

--- basic-auth/routes/auth.js
+++ basic-auth/routes/auth.js
@@ -3,8 +3,51 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/users');
+
+// Bcrypt to encrypt passwords
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+
 router.get('/signup', (req, res, next) => {
   res.render('auth/signup'); // without /auth!!
+});
+
+router.post('/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === '' || password === '') {
+    res.render('auth/signup');
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (user !== null) {
+        res.render('auth/signup');
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save((err) => {
+        if (err) {
+          res.render('auth/signup');
+        } else {
+          res.redirect('/');
+        }
+      });
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
