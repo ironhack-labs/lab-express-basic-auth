339f41fa7217040a6ac22ec9736bd31b25411fbb
Verónica Reina <veroreinah@gmail.com>
2018-07-09 16:30:16 +0200

    Iteration 6 (Bonus)

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
-
+  $('#myPassword').strength({
+    strengthClass: 'strength',
+    strengthMeterClass: 'strength_meter',
+    strengthButtonClass: 'button_strength',
+    strengthButtonText: 'Show Password',
+    strengthButtonTextToggle: 'Hide Password'
+});
 }, false);
