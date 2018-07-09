a60b898ee5b34d71877fa0da88f2557eb96ffe9c
Montse Badia <mbadia_11@hotmail.com>
2018-07-09 20:21:15 +0200

    added sign up form

--- basic-auth/models/users.js
+++ basic-auth/models/users.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+  username: {type: String, unique: true},
+  password: {type: String}
 });
 
 const User = mongoose.model('User', userSchema);
--- basic-auth/routes/auth.js
+++ basic-auth/routes/auth.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/signup', (req, res, next) => {
-  res.render('/auth/signup');
+  res.render('auth/signup'); // without /auth!!
 });
 
 module.exports = router;
