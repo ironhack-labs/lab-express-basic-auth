7acd4c876a32619a4f033a37111ac96d97d5ccc3
Minyu Li <minyuli1997@gmail.com>
2018-07-09 17:26:09 +0200

    d

---
+++ p1/public/javascripts/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function ($) {
+
+  $("#myPassword").strength();
+
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  console.log('IronGenerator JS imported successfully!');
+
+}, false);
