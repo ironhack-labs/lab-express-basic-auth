ecc3e1a12951d9b386ade0fe7bed51c03e723faa
Andrea Moya <andrea@MacBook-Pro-de-Andrea.local>
2018-07-10 10:27:03 +0200

    added /page and new routes, also the post

--- lab-express-basic-auth/routes/auth-routes.js
+++ lab-express-basic-auth/routes/auth-routes.js
@@ -4,5 +4,17 @@ const authRoutes = express.Router();
 authRoutes.get('/signup', (req, res, next) => {
   res.render('auth/signup');
 });
+//  aqui creo la nueva ruta
+authRoutes.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
+//  ahora creo posts porque necesito ahora enviar información para hacer submit del form.
+authRoutes.post('/signup', (req, res, next) => {
+  res.render('auth/signup');
+});
+
+authRoutes.get('/login', (req, res, next) => {
+  res.render('auth/login');
+});
 
 module.exports = authRoutes;
