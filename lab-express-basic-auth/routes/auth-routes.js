401948f43c25fd7fcc69ba16b5c8d13bac20d67d
Andrea Moya <andrea@MacBook-Pro-de-Andrea.local>
2018-07-10 09:30:13 +0200

    second commit

--- lab-express-basic-auth/routes/auth-routes.js
+++ lab-express-basic-auth/routes/auth-routes.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const authRoutes = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+authRoutes.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
 });
 
-module.exports = router;
+module.exports = authRoutes;
