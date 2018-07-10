fff75cc993410900ef0f9023793c007edb8b151a
Will Schaller <willschaller@gmail.com>
2018-07-10 10:13:37 +0200

    Signup and login with password encryption working

--- lab-express-basic-auth/routes/auth.js
+++ lab-express-basic-auth/routes/auth.js
@@ -1,41 +1,82 @@
 const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/users');
+
+const saltRounds = 10;
 
 router.get('/signup', function (req, res, next) {
   res.render('auth/signup', { title: 'Express' });
 });
 
-router.post('/signup', function (req, res, next) {
+router.post('/signup', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   // Check if user is logged in
 
   // Check if username and password params have been sent
   if (!username || !password) {
+    console.log('username or password param not supplied');
     res.redirect('/auth/signup');
     return;
   }
 
   // Check if user exists
   User.findOne({ username: username })
-    .then(user => {
+    .then((user) => {
       if (user) {
         // Username exists
-        res.redirect('/auth/signup');
+        console.log(err);
+        return res.redirect('/auth/signup');
       } else {
         // Username doesn't exist, create the user
-        const newUser = new User({ username: username, password: password });
+        console.log('username doesnt exist. creating user');
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(password, salt);
+        const newUser = new User({ username: username, password: hash });
         newUser.save();
-        res.render('auth/signup', { title: 'Express' });
+        res.redirect('/');
       }
+    })
+    .catch(err => {
+      console.log(err);
+      next();
     });
+
+  router.get('/login', function (req, res, next) {
+    res.render('auth/login', { title: 'Express' });
+  });
 });
 
-router.get('/login', function (req, res, next) {
-  res.render('auth/login', { title: 'Express' });
+router.post('/login', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // Check if user is logged in
+  // Check if username and password params have been sent
+  if (!username || !password) {
+    console.log('username or password param not supplied');
+    res.redirect('/auth/login');
+    return;
+  }
+  // Check if user exists
+  User.findOne({ username: username })
+    .then((user) => {
+      if (!user) {
+        // User doesn't exist
+        console.log('username does not exist in db');
+        res.redirect('/auth/login');
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        console.log('log in successful');
+        res.redirect('/');
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      next();
+    });
 });
 
 module.exports = router;
