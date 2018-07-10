6e65f754ccdbd098d3d6ae67572e2fb9938a29cd
Javier Romero <javier.romero@Henkel-Backup-2.local>
2018-07-10 09:16:03 +0200

    done

---
+++ basic-auth/app.js
@@ -0,0 +1,54 @@
+'use strict';
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+
+const authRouter = require('./routes/signup');
+const usersRouter = require('./routes/users');
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/users', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', authRouter);
+app.use('/users', usersRouter);
+
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+// NOTE: requires a views/error.ejs template
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
+
+module.exports = app;
