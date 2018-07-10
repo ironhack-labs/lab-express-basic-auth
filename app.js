5581accc0b54c55062c6309e7b4dffcba0111c27
Miguel Angel Rodriguez <finkel218@gmail.com>
2018-07-10 15:41:11 -0500

    Done

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
