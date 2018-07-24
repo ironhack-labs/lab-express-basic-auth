f3f1593d1a8d6c0d8caf1533136b06c388468a88
Edu Benito <eduardbenito@icloud.com>
2018-07-24 19:50:43 +0200

    ES6

--- express-basic-auth/app.js
+++ express-basic-auth/app.js
@@ -43,7 +43,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   app.locals.currentUser = req.session.currentUser;
   next();
 });
@@ -54,12 +54,12 @@ app.use('/private', middlewareAuth.requireUser, privateRouter);
 app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
