19771723dcffa495300f89356b7b928645d80d7a
Javier Chacón <j.chacon.alvarez@gmail.com>
2018-07-27 10:14:22 +0200

    added login functionality

--- routes/auth.js
+++ routes/auth.js
@@ -33,4 +33,36 @@ router.post('/signup', (req, res, next) => {
   }
 });
 
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    rest.render('login', { message: 'no empty fields' });
+  } else {
+    User.findOne({ username })
+      .then((user) => {
+        if (!user) {
+          res.render('login', { message: 'incorrect username or password' });
+        } else if (bcrypt.compareSync(password, user.password)) {
+          req.session.currentUser = user;
+          res.render('login', { message: 'logged in successfully' });
+        } else {
+          res.render('login', { message: 'incorrect username or password' });
+        }
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+});
+
+router.post('/logout', (req, res, next) => {
+  delete session.currentUser;
+  res.redirect('login');
+});
+
 module.exports = router;
