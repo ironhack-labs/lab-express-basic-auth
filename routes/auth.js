5e19511dfffa534aefe80fd728721f254ad4fae5
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-08-11 11:56:08 +0200

    WIP

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const User = require('../models/user');
+const User = require("../models/user");
 
 // Iteration 1: Create a sign up form.
 
@@ -48,5 +48,57 @@ router.get("/success", (req, res, next) => {
   res.render("success");
 });
 
-module.exports = router;
+//Iteration 2: Login
 
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.render('login', { message: "Please fill all the required fields"});
+  } else{
+    User.findOne( {username} )
+    .then(user => {
+      if (user) {
+        if(bcrypt.compareSync(password, user.password)) {
+          res.redirect('/')
+        }
+      } else {
+        res.redirect('/auth/login');
+      }
+    })
+  }
+});
+
+//   if (!username || !password) {
+//     res.render("login", { message: "Please fill all the required fields" });
+//   } else {
+//     User.findOne({ username }).then(user => {
+//       if (user) {
+//         res.render("signup", {
+//           message:
+//             "User already exists in our database, pick a different username"
+//         });
+//       } else {
+//         const salt = bcrypt.genSaltSync(saltRounds);
+//         const hashedPassword = bcrypt.hashSync(password, salt);
+//         const newUser = new User({
+//           name: name,
+//           username: username,
+//           password: hashedPassword
+//         });
+//         newUser
+//           .save()
+//           .then(user => {
+//             res.redirect("success");
+//           })
+//           .catch(error => {
+//             console.log(error);
+//           });
+//       }
+//     });
+//   }
+// });
+module.exports = router;
