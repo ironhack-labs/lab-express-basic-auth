35c1f45fccbeaf6205785e5a7de18056267f1e6f
Emmanuelle Jaldorau <e.jaldorau@gmail.com>
2018-09-05 18:20:44 +0200

    done

---
+++ starter_code/routes/auth-router.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router  = express.Router();
+const bcrypt = require('bcrypt');
+const User= require("../models/user-model.js");
+
+
+router.get('/signup', (req, res, next) => {
+  res.render('auth-views/signup-form.hbs');
+});
+
+
+router.post('/process-signup', (req, res,next) => {
+    const { username, originalPassword } = req.body;
+    const encryptedPassword = bcrypt.hashSync(originalPassword, 10); 
+
+    User.create({ username, encryptedPassword })
+    .then(userDoc =>{
+        res.redirect("/");
+    })
+    .catch(err => next(err));
+});
+
+router.get('/login', (req, res, next) => {
+    res.render("auth-views/login-form.hbs");
+});
+
+router.post('/process-login', (req, res, next) => {
+    const { username, originalPassword } = req.body;
+    
+    User.findOne( {username: {$eq: username}})
+    .then( userDoc => {
+      if (!userDoc) {
+        res.redirect("/login");
+        return;
+      }
+      
+      const { encryptedPassword } = userDoc;
+
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        res.redirect("/login");
+         return;
+       }
+       
+         res.redirect("/");        
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
