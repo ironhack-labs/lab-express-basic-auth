9925f1b00971350146257b00dafdd46f7c4e198d
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-10 14:17:37 -0400

    Done

--- lav-express-auth/public/javascripts/script.js
+++ lav-express-auth/public/javascripts/script.js
@@ -1,5 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+$(document).ready(function ($) {
 
-  console.log('IronGenerator JS imported successfully!');
+    $('#passwordSignup').strength({
+        strengthClass: 'strength',
+        strengthMeterClass: 'strength_meter',
+        strengthButtonClass: 'button_strength',
+        strengthButtonText: 'Show password',
+        strengthButtonTextToggle: 'Hide Password'
+    });
 
-}, false);
+});
