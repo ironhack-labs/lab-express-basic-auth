eecdc105f4b739696989eef3d0dd17735c40e719
Ilham Moutaharik <ilham@prescrypto.com>
2018-09-17 13:36:26 -0500

    Casi!

---
+++ basicAuth/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  username:{
+    type:String,
+    unique:true
+  },
+  password:String
+},{
+  timestamps:{
+    createdAt : 'created_at',
+    updatedAt: 'updated_at'
+  }
+})
+
+module.exports = mongoose.model('user', userSchema)
