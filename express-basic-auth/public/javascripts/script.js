d6e8579d4592c6354ae1540a4bc6466ef8e75187
cyril.attie <attiecyril@gmail.com>
2018-09-17 13:29:47 +0200

    BONUS - front-end validation and password strength measurement

--- express-basic-auth/public/javascripts/script.js
+++ express-basic-auth/public/javascripts/script.js
@@ -3,3 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+$(document).ready(function ($) {
+  $('#myPassword').strength({
+    strengthClass: 'strength',
+    strengthMeterClass: 'strength_meter',
+    strengthButtonClass: 'button_strength',
+    strengthButtonText: 'Show password',
+    strengthButtonTextToggle: 'Hide Password'
+});     
+});
