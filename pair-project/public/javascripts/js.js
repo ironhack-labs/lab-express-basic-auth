a94116b6682ed3e82a4738e045599d492da18e77
Jhon Scarzo <jhon753951@gmail.com>
2018-09-17 16:19:14 +0200

    Todo listo hasta el primer bonus

---
+++ pair-project/public/javascripts/js.js
@@ -0,0 +1,23 @@
+$(document).ready(function ($) {
+
+    $('#signPass').strength({
+        strengthClass: 'strength',
+        strengthMeterClass: 'strength_meter',
+        strengthButtonClass: 'button_strength',
+        strengthButtonText: 'Show password',
+        strengthButtonTextToggle: 'Hide Password'
+    });
+
+});
+
+$(document).ready(function ($) {
+
+    $('#logPass').strength({
+        strengthClass: 'strength',
+        strengthMeterClass: 'strength_meter',
+        strengthButtonClass: 'button_strength',
+        strengthButtonText: 'Show password',
+        strengthButtonTextToggle: 'Hide Password'
+    });
+
+});
