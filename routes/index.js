285ff02a078c840f8d664ec4ffdc98c7e2e02ab5
TeoDiaz <tdiazsan@gmail.com>
2018-09-17 13:42:58 +0200

    login form

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,16 @@ router.get("/singUp", (req, res, next) => {
 
 router.post("/newUser", (req, res, next) => {
   const { user, password } = req.body;
-  const salt     = bcrypt.genSaltSync(bcryptSalt);
+  if (user === "" || password === "") {
+    res.render("singUp", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+ 
+  const salt = bcrypt.genSaltSync(bcryptSalt);
   const hashPass = bcrypt.hashSync(password, salt);
-  const newUser = new Login({ user, password:hashPass });
+  const newUser = new Login({ user, password: hashPass });
   newUser
     .save()
     .then(() => {
@@ -27,6 +34,42 @@ router.post("/newUser", (req, res, next) => {
     .catch(err => {
       console.log(err);
     });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res, next) => {
+  const { user, password } = req.body;
+
+  if (user === "" || password === "") {
+    res.render("login", {
+      errorMessage: "Indicate a username and a password to log in"
+    });
+    return;
+  }
+
+  Login.findOne({ "user": user})
+  .then(user => {
+      if (!user) {
+        res.render("login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
 module.exports = router;
