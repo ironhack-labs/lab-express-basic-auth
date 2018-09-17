958a30f752dc596b803aab7ff22b02e3eb42d24b
agdwm <almudenaguerraswm@gmail.com>
2018-09-17 12:05:28 +0200

    Iteration 1 done - Sign Up

--- basic-auth/routes/auth-routes.js
+++ basic-auth/routes/auth-routes.js
@@ -2,32 +2,70 @@ const express = require("express");
 const authRoutes = express.Router();
 
 // User model
-const User           = require("../models/user");
+const User = require("../models/user");
 
 // BCrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 authRoutes.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
+	res.render("auth/signup");
 });
 
 authRoutes.post("/signup", (req, res, next) => {
 	const username = req.body.username;
 	const password = req.body.password;
-	const salt     = bcrypt.genSaltSync(bcryptSalt);
+	const salt = bcrypt.genSaltSync(bcryptSalt);
 	const hashPass = bcrypt.hashSync(password, salt);
-  
-	const newUser  = User({
-	  username,
-	  password: hashPass
+
+	const newUser = User({
+		username,
+		password: hashPass
 	});
-  
-	newUser.save()
-	.then(user => {
-	  res.redirect("/");
-	})
-  });
+
+	if (username === "" || password === "") {
+		res.render("auth/signup", {
+			errorMessage: "Indicate a username and a password to sign up"
+		});
+		return;
+	} else {
+		User.findOne({"username": username})
+			.then(user => {
+				if (user !== null) {
+					res.render("auth/signup", {
+						errorMessage: "The username already exists"
+					});
+					return;
+				}
+
+				// const salt     = bcrypt.genSaltSync(bcryptSalt);
+				// const hashPass = bcrypt.hashSync(password, salt);
+
+				// const newUser = User({
+				//   username,
+				//   password: hashPass
+				// });
+
+				newUser.save()
+					.then(user => {
+						res.redirect("/");
+					})
+					.catch(error => {
+						next(error)
+					})
+			})
+			.catch(error => {
+				next(error)
+			})
+	}
+
+
+
+	// newUser.save()
+	// 	.then(user => {
+	// 		res.redirect("/");
+	// 	})
+});
 
 
 
