69d4b4448dd0b4c9808862e12e81c33f233607e2
agdwm <almudenaguerraswm@gmail.com>
2018-09-17 12:28:00 +0200

    Iteration 2 done - Login

--- basic-auth/app.js
+++ basic-auth/app.js
@@ -1,23 +1,29 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+// Routes
+const authRoutes = require('./routes/auth-routes');
+const router = require('./routes/site-routes');
 
 mongoose
-  .connect('mongodb://localhost/basic-auth', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+	.connect('mongodb://localhost/basic-auth', {
+		useNewUrlParser: true
+	})
+	.then(x => {
+		console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+	})
+	.catch(err => {
+		console.error('Error connecting to mongo', err)
+	});
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -27,17 +33,29 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	sourceMap: true
 }));
-      
+
+app.use(session({
+	secret: "basic-auth-secret",
+	cookie: {
+		maxAge: 60000
+	},
+	store: new MongoStore({
+		mongooseConnection: mongoose.connection,
+		ttl: 24 * 60 * 60 // 1 day
+	})
+}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -50,11 +68,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
-const index = require('./routes/index');
-app.use('/', index);
-const authRoutes = require('./routes/auth-routes');
+// Routes
 app.use('/', authRoutes);
-
+app.use('/', router);
 
 module.exports = app;
---
+++ basic-auth/routes/site-routes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.use((req, res, next) => {
+	if (req.session.currentUser) {
+		next();
+	} else {
+		res.redirect("/login");
+	}
+});
+
+
+router.get("/", (req, res, next) => {
+	res.render("home");
+});
+
+router.get("/secret", (req, res, next) => {
+	res.render("secret");
+});
+
+
+module.exports = router;
