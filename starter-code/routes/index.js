bdabdb16b8e19f719350c7b80786784081cc21f4
FonRiego <fonriego@gmail.com>
2018-09-17 13:20:53 +0200

    Iteration 1.1

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,38 +1,46 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 const User = require("../models/user");
 
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
 router.get("/signup", (req, res, next) => {
-  res.render("./signup")
+  res.render("./signup");
 });
 
 router.post("/signup", (req, res, next) => {
-  const {username, password} = req.body;
+  const { username, password } = req.body;
   if (username === "" || password === "") {
     res.render("./signup", {
       errorMessage: "Indicate a username and a password to sign up"
     });
     return;
-    } else {
-    let newUser = new User ({username, password})
-    newUser.save()
-    .then (() => {
+  }
+  User.findOne({ username: username }).then(user => {
+    if (user !== null) {
+      res.render("./signup", {
+        errorMessage: "User Name already exists"
+      });
+      return;
+    }
+  });
+  let newUser = new User({ username, password });
+  newUser
+    .save()
+    .then(() => {
       res.render("./index", {
         successMessage: "User created"
       });
     })
-    .catch((e) => {
-      console.log(e)
-    })
-    };
+    .catch(e => {
+      console.log(e);
+    });
 });
 
 module.exports = router;
