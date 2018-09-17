600f1c6df95a3c8d51ba8e90dc87af8d19b3c5ec
FonRiego <fonriego@gmail.com>
2018-09-17 17:14:37 +0200

    Iteration Done

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
+$(document).ready(function($) {
+  $("#password").strength({
+    strengthClass: "strength",
+    strengthMeterClass: "strength_meter",
+    strengthButtonClass: "button_strength",
+    strengthButtonText: "Show Password",
+    strengthButtonTextToggle: "Hide Password"
+  });
 
-}, false);
+});
