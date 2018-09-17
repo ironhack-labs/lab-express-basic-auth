3cc3ef709897c92d24184cc0e7a27a9e3f72e34e
Evenstarw1 <lauracorrallopez@gmail.com>
2018-09-17 12:29:02 +0200

    Primera iteracción

---
+++ password_project/basic-auth/routes/auth-routes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const authRoutes = express.Router();
+
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+// User model
+const User           = require("../models/user");
+
+// BCrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+
+authRoutes.post("/signup", (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+  
+    const newUser  = User({
+      username,
+      password: hashPass
+    });
+  
+    newUser.save()
+    .then(user => {
+      res.redirect("/");
+    })
+});
+
+
+module.exports = authRoutes;
