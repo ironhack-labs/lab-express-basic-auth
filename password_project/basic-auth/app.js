7f5152a3de06c46e0754eebc69a647882c28f2c2
Evenstarw1 <lauracorrallopez@gmail.com>
2018-09-17 13:59:42 +0200

    Second commit

--- password_project/basic-auth/app.js
+++ password_project/basic-auth/app.js
@@ -8,7 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
   .connect('mongodb://localhost/basic-auth', {useNewUrlParser: true})
@@ -29,6 +30,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Express View engine setup
 
@@ -51,10 +61,13 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-// const index = require('./routes/index');
-// app.use('/', index);
+// Routes
+const index = require ('./routes/index');
 const authRoutes = require('./routes/auth-routes');
+const router = require('./routes/site-routes');
+// Routes
+app.use('/', index)
 app.use('/', authRoutes);
-
+app.use('/', router);
 
 module.exports = app;
--- password_project/basic-auth/routes/auth-routes.js
+++ password_project/basic-auth/routes/auth-routes.js
@@ -6,27 +6,91 @@ authRoutes.get("/signup", (req, res, next) => {
 });
 
 // User model
-const User           = require("../models/user");
+const User = require("../models/user");
 
 // BCrypt to encrypt passwords
-const bcrypt         = require("bcrypt");
-const bcryptSalt     = 10;
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
 authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists"
+        });
+        return;
+      }
+
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
+
+        const newUser = User({
+          username,
+          password: hashPass
+        });
+
+        newUser.save()
+          .then(user => {
+            res.redirect("/");
+          })
+          .catch(error => {
+            next(error)
+          })
+      }
+    });
+
+  authRoutes.get("/login", (req, res, next) => {
+    res.render("auth/login");
+  });
+
+  authRoutes.post("/login", (req, res, next) => {
     const username = req.body.username;
     const password = req.body.password;
-    const salt     = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
-  
-    const newUser  = User({
-      username,
-      password: hashPass
+
+    if (username === "" || password === "") {
+      res.render("auth/login", {
+        errorMessage: "Indicate a username and a password to sign up"
+      });
+      return;
+    }
+
+
+    authRoutes.get("/logout", (req, res, next) => {
+      req.session.destroy((err) => {
+        // cannot access session here
+        res.redirect("/login");
+      });
     });
-  
-    newUser.save()
-    .then(user => {
-      res.redirect("/");
-    })
+
+});
 });
 
 
