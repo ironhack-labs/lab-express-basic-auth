eff5e529b4fc28280ecf84107afea8b0d49cc5a2
FonRiego <fonriego@gmail.com>
2018-09-17 13:08:32 +0200

    Iteration 1

---
+++ starter-code/routes/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router  = express.Router();
+const User = require("../models/user");
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("./signup")
+});
+
+router.post("/signup", (req, res, next) => {
+  const {username, password} = req.body;
+  if (username === "" || password === "") {
+    res.render("./signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+    } else {
+    let newUser = new User ({username, password})
+    newUser.save()
+    .then (() => {
+      res.render("./index", {
+        successMessage: "User created"
+      });
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+    };
+});
+
+module.exports = router;
