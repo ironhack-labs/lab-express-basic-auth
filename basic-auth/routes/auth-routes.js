83a9dd9362656ec36acebf01642526ec92e15026
RicardoAlonzo <alonzoricardo6@gmail.com>
2018-09-17 13:31:43 +0200

    LOGGIN VALIDATION DONE

--- basic-auth/routes/auth-routes.js
+++ basic-auth/routes/auth-routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const User = require('../models/user')
 
 /*--------------------------------*/
-const bcrypt     = require("bcrypt");
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
 //const plainPassword1 = "HelloWorld";
@@ -21,46 +21,86 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/signup', (req,res,next) => {
+router.get('/signup', (req, res, next) => {
   res.render('auth/signup')
 })
 
-router.post('/register', (req,res,next) => {
+
+router.post('/register', (req, res, next) => {
   const username = req.body.Username;
   const password = req.body.Password;
   const salt = bcrypt.genSaltSync(saltRounds);
   const hashPass = bcrypt.hashSync(password, salt);
-  
-  if ( username === '' || password === '') {
+
+  if (username === '' || password === '') {
     res.render("auth/signup", {
       errorMessage: "Indicate a username and a password to sign up"
     });
     return;
   }
 
-  User.findOne({ 'username': username})
-  .then(user => {
-    if(user !== null) {
-      res.render("auth/signup", {
-        errorMessage: "The username already exists"
+  User.findOne({ 'username': username })
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists"
+        });
+        return;
+      }
+
+      const newUser = new User({
+        username: req.body.Username,
+        password: hashPass
       });
-      return;
-    }
-    
-    const newUser = new User({
-      username: req.body.Username,
-      password: hashPass
-    });
-    
-    newUser.save()
-    .then(() => {
-      console.log('entra lalalalalal');
-      res.redirect('/')
+
+      newUser.save()
+        .then(() => {
+          res.redirect('/login')
+        })
     })
-  })
     .catch(err => next(err))
-    
 })
+
+
+router.get('/login', (req, res, next) => {
+  res.render('auth/login')
+})
+
+router.post('/login', (req, res, next) => {
+  const username = req.body.Username;
+  const password = req.body.Password;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  if (username === '' || password === '') {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to login"
+    });
+    return;
+  }
+
+  User.findOne({ 'username': username })
+    .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username does not exists"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        })
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
+});
 
 
 
