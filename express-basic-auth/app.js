2e148f510c73830033609e68bacc14556683b6f0
cyril.attie <attiecyril@gmail.com>
2018-09-17 12:32:18 +0200

    Iteration 2 - Login

--- express-basic-auth/app.js
+++ express-basic-auth/app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 
 mongoose
@@ -23,6 +25,15 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -49,8 +60,10 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
 const authRoutes = require('./routes/auth-routes');
+const router = require('./routes/site-routes');
+// Routes
 app.use('/', authRoutes);
+app.use('/', router);
 
 module.exports = app;
--- express-basic-auth/routes/auth-routes.js
+++ express-basic-auth/routes/auth-routes.js
@@ -3,6 +3,10 @@ const authRoutes  = express.Router();
 
 const User = require("../models/user");
 
+// BCrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
 /* GET home page */
 authRoutes.get('/', (req, res, next) => {
   res.render('index');
@@ -52,6 +56,47 @@ authRoutes.post("/signup", (req, res, next) => {
       next(error)
     })
 });
+
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+authRoutes.post("/login", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+  .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        console.log(req.session )
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error)
+  })
+});
+
+
 
 
 module.exports = authRoutes;
