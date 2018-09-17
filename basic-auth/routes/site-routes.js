070f7ed7fd36ab7b2aa7759a3cc92b7fd94167b6
yamioliva <yamioliva06@gmail.com>
2018-09-17 13:49:41 +0200

    Done

---
+++ basic-auth/routes/site-routes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.render("home");
+});
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
+});
+
+module.exports = router;
