ef3ea19885bd14110af896c17a362cd608d60f16
Pedro Cruz <pedro.cruz@blackinntech.com>
2018-10-12 21:46:35 -0500

    Finished

---
+++ basic-auth/models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
