cfe355ac745a04da9d90290dd622e361dc284019
Johanna <johanna.skuhra@gmail.com>
2018-11-06 08:29:41 +0100

    done

---
+++ starter-code/routes/auth.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+router.get("/", (req, res, next) => {
+    res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+        username,
+        password: hashPass
+    });
+
+    newUser.save()
+        .then(user => {
+            res.redirect("/");
+        })
+});
+
+module.exports = router;
