c02188c9ac14aae778f61250e77cb2e97d829074
Sonia Ruiz <yune.vk@gmail.com>
2018-11-16 17:46:13 +0100

    Bonus - Frontend validations

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -2,4 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   console.log('IronGenerator JS imported successfully!');
 
+
+  document.querySelector('form').onsubmit = function(e) {
+    let user = document.querySelector('input[name="user"]').value;
+    let pass = document.querySelector('input[name="password"]').value;
+
+    if (user === "" || pass === "") {
+      document.querySelector('div').innerHTML = "Indicate a username and a password to sign up";
+      e.preventDefault();
+    }
+
+    e.preventDefault();
+  }
+
 }, false);
