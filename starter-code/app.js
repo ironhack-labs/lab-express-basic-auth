98122e109a7ffb40671ab54e580fc974a5504b50
Eddie Ruiz <ruizeduardo21@gmail.com>
2018-11-16 16:39:29 +0100

    first step done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,15 +8,16 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const User         = require('./models/User');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect('mongodb://localhost/starter-code', { useNewUrlParser: true })
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
+  .catch((err) => {
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
@@ -35,9 +36,9 @@ app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  sourceMap: true
+  sourceMap: true,
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -45,14 +46,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
 const index = require('./routes/index');
+
 app.use('/', index);
+
+app.listen(3000);
 
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const User = require('../models/User');
+
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.post('/', (req, res, next) => {
+  const userName = req.body.user;
+  const password = req.body.password;
+  console.log(userName);
+  console.log(password);
+});
+
 
 module.exports = router;
