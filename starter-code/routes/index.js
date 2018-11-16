b354de6d90eb4cc2fd94ac77eacd71f2b4aedb4f
Zeswen <zeswentyrael@gmail.com>
2018-11-16 13:44:33 +0100

    Iteration 1

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const bodyParser   = require('body-parser');
+const bcrypt = require('bcrypt');
+
 const router  = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/signup', (req, res, next) => res.render('signup'));
+
+router.post('/signup', (req, res, next) => {
+  const saltRounds = 5;
+
+  const myUser = req.body.username;
+
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+  console.log(hashedPasword);
+});
+
+router.get('/login', (req, res, next) => res.render('login'));
 
 module.exports = router;
