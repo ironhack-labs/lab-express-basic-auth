24ce1f78e2eae2592195cb0d98f230490b19140c
sitobusnan <sito@citytonight.es>
2018-11-16 17:49:27 +0100

    it 1 ok

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,61 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const User = require("../models/user");
+const app = express();
+const MongoStore = require('connect-mongo')(session);
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+
+router.post('/login', (req, res, next) => {
+  User.findOne({
+    user: req.body.user,
+  }).then((found) => {
+    const matches = bcrypt.compareSync(req.body.password, found.password);
+
+    if (matches) {
+      req.session.inSession = true;
+      req.session.user = req.body.user;
+
+      res.redirect('secret');
+    } else {
+      req.session.inSession = false;
+      res.redirect('login');
+    }
+  });
+});
+
+router.get('/create', (req, res, next) => {
+  res.render('create');
+});
+
+router.post('/create', (req, res) => {
+  const saltRounds = 5;
+
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+  const newUser  = User({
+    user: req.body.user,
+    password: hashedPassword
+  });
+
+  newUser.save()
+  .then(user => {
+    res.redirect("/");
+  })
+  .catch(error => {
+    console.log(error);
+  })
 });
 
 module.exports = router;
