f23280bd4dfd25a120610445ca0b82ab07c0c512
Yared Santiago <yaredmyers@gmail.com>
2018-11-16 18:50:19 +0100

    login conseguido

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
+router.get('/', (req, res, next) => {
+  res.render('success');
+});
 
 router.get("/login", (req, res, next) => {
   res.render("auth/login");
@@ -33,5 +36,48 @@ router.post('/signup', (req, res) => {
     res.redirect('/auth/login')
   })
 })
+
+router.post("/login", (req, res, next) => {
+  console.log("hola")
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log(username, password)
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+  .then(user => {
+      if (!user) {
+        console.log("123")
+
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        console.log("456")
+
+        // Save the login in the session!
+        // req.session.newUser = user;
+        res.redirect("/");
+      } else {
+        console.log("789")
+
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error)
+  })
+});
 
 module.exports = router;
