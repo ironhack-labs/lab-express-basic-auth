5edfe52236532c41e01983a6da9b772ac21b9956
RuddyLotar <ruddylotar@gmail.com>
2018-11-16 13:43:14 +0100

    iteration 1

---
+++ starter-code/bin/seeds.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const User = require('../model/user');
+
+
+
+mongoose
+.connect('mongodb://localhost/usersLogin', {useNewUrlParser: true})
+.then(x => {
+  console.log(`Connected to Mongo!`)
+})
+.catch(err => {
+  console.error('Error connecting to mongo', err)
+});
+
+const users = [
+
+{
+  user : 'admin',
+  password: 'password'
+}
+
+];
+
+User.create(users, (err) => {
+  if (err) { throw(err) }
+  console.log(users.length);
+  mongoose.connection.close()
+});
