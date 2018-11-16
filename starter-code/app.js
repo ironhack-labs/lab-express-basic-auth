1de7738a49d94d7637bf5f57f7e1e67320759048
Eduardo Gandía Fernández <eduardogandiafernandez@MacBook-Pro-de-Eduardo.local>
2018-11-16 17:55:36 +0100

    Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,8 +10,9 @@ const logger       = require('morgan');
 const path         = require('path');
 
 
+
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/paswordproyect', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
-
+const express = require("express");
+const session = require("express-session");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const genericUser = new User();
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.post("/", (req, res, next) => {
+  const saltRounds = 6;
+
+  genericUser.user = req.body.user;
+  
+  if (req.body.user === "" || req.body.password === "") {
+    console.log("error vacio");
+  } else {
+    
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    genericUser.password = hash;
+  
+    genericUser.save().then(user => {
+      // req.session.inSession = true
+      res.render("main", { user: user.user });
+    });
+  }
 });
 
 module.exports = router;
