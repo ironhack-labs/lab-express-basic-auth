0d98a16e8578652461c1584b87fbe604399ba2a4
David Diez <ddiezr@gmail.com>
2018-11-16 18:43:12 +0100

    Exercised Finished. No css style

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,22 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
 
 
 mongoose
-  .connect('mongodb://localhost/users', {useNewUrlParser: true})
+  .connect('mongodb://localhost/users', {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -24,20 +29,34 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
+
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: {
+    maxAge: 60000,
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60, // 1 day
+  }),
+}));
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,41 +1,88 @@
 const express = require('express');
 const router = express.Router();
-//const MongoStore = require("connect-mongo")(session);
-const bodyParser = require('body-parser');
 const bcrypt = require("bcrypt");
 const User = require("../models/user");
 const genericUser = new User();
 
+// router.use((req, res, next) => {
+// 	if (req.session.currentUser) {
+// 		next();
+// 	} else {
+// 		res.redirect("/login");
+// 	}
+// });
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
 });
+/* GET login page */
 router.get('/login', (req, res, next) => {
-  res.render('login');
+    res.render('login');
 });
 
-router.get('/createUser', (req, res, next) => {
-  //   res.render('index');
+router.get('/main', (req, res) => {
+  if (req.session.inSession) {
+    const sessionData = { ...req.session};
+    console.log(sessionData);
+    res.render('main', {sessionData});
+  } else {
+    res.render('error',{e});
+  }
 });
 
-router.post('/createUser', function (req, res) {
+router.get('/private', (req, res) => {
+  if (req.session.inSession) {
+    res.render('private');
+  } else {
+    res.render('error',{e});
+  }
+});
+
+// Check if users are in mongo with correct pwd
+router.post('/login', (req, res, next) => {
+    console.log( req.session);
+    User.findOne({
+        user: req.body.user
+    }).then((found) => {
+        const matches = bcrypt.compareSync(req.body.password, found.password)
+        if (matches) {
+            req.session.inSession = true;
+            req.session.user = req.body.user;
+            res.redirect('/main')
+        } else {
+            req.session.inSession = false;
+            res.redirect('/login')
+        }
+    })
+})
+
+
+
+
+// Generate new users with form data
+router.post('/createUser', (req, res, next) => {
     console.log(req.body.user)
     console.log(req.body.password)
-  const saltRounds = 5;
-  if (req.body.user === '' || req.body.password === '') {
-    res.redirect('/');
-  } else {
-    genericUser.user = req.body.user;
-    const salt = bcrypt.genSaltSync(saltRounds);
-    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-    genericUser.password = hashedPassword
-    //Save the new user created in the form with the schema User
-    genericUser.save().then(x => { 
-      console.log(`User ${x.user} has been saved on database mongo`)
-      res.render('login');
-    })
-  }
+    const saltRounds = 5;
+    if (req.body.user === '' || req.body.password === '') {
+        let e = 'Write one user'
+        res.render('error', {
+            e
+    });
+
+    } else {
+        genericUser.user = req.body.user;
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+        genericUser.password = hashedPassword
+        //Save the new user created in the form with the schema User
+        genericUser.save().then(x => {
+            req.session.inSession = true;
+            console.log(`User ${x.user} has been saved on database mongo`)
+            res.redirect('/login');
+        })
+    }
 
 })
 
