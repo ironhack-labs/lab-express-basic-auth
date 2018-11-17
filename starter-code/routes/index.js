8c0ad483d8ec53a58ca2ea43b98acd33be1c6bc2
cgandiaf <c.gandia.f@gmail.com>
2018-11-17 20:42:14 +0100

    Done con manejo de errores

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -32,84 +32,86 @@ router.post('/', (req, res, next) => {
 
   let bodyUser = req.body.user;
   let bodyPassword = req.body.password;
+  User.username = req.body.user;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+  User.password = hashedPassword;
 
-  if (bodyUser.length=== 0 || bodyPassword.length=== 0 ) {
+  //Campos vacios
+  if (bodyUser === '' || bodyPassword === '') {
     res.render("signUpError", {
       errorMessage: "Ninguno de los campos debe estar empty"
     });
     return
   }
-  
-  User.username = req.body.user;
 
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-
-  User.password = hashedPassword;
-
-
-
-
-  User.create({ userName: User.username, password: User.password })
-    .then((x) => {
-      console.log('Usuario creado');
-      res.redirect('/login');
-    },
-      err => {
-        next(err)
-        res.render("signup", {
-          errorMessage: "Error al conectar con Mongo"
-        });
-        return;
+  //Usuario ya registrada
+  User.findOne({ userName: req.body.user })
+    .then(found => {
+      res.render("signUpError", {
+        errorMessage: "El usuario ya tiene cuenta"
       });
+    })
+    .catch(() => {
+      User.create({ userName: User.username, password: User.password })
+        .then((x) => {
+          console.log('Usuario creado');
+          res.redirect('/login');
+        },
+          err => {
+            next(err)
+            res.render("signup", {
+              errorMessage: "Error al conectar con Mongo"
+            });
+            return;
+          });
+    });
 });
 
 
 //LOGIN
 router.post('/login', (req, res, next) => {
-console.log('entra en el post');
-    User.findOne({ userName: req.body.user})
+  User.findOne({ userName: req.body.user })
     .then(found => {
-      console.log("xxxxxxx")
-      console.log(found)
-        const matches = bcrypt.compareSync(req.body.password, found.password)
-        if (matches) {
-          console.log('hay match');
-            req.session.inSession = true
-            req.session.user = req.body.user
-            res.redirect('/main')
-        } else {
-            req.session.inSession = false
-            res.redirect('/')
-        }
+      const matches = bcrypt.compareSync(req.body.password, found.password)
+      if (matches) {
+        req.session.inSession = true
+        req.session.user = req.body.user
+        res.redirect('/main')
+      } else {
+        req.session.inSession = false
+        res.redirect('/')
+      }
     })
     .catch();
-  
+
 });
 
 
 
 router.get('/main', function (req, res) {
   if (req.session.inSession) {
-      let sessionData = { ...req.session
-      }
-      res.render('main', {
-          sessionData
-      })
+    let sessionData = {
+      ...req.session
+    }
+    res.render('main', {
+      sessionData
+    })
   } else {
-      res.render('404')
+    res.render('404')
   }
 })
 
 router.get('/private', function (req, res) {
   if (req.session.inSession) {
-      let sessionData = { ...req.session
-      }
-      res.render('private', {
-          sessionData
-      })
+    let sessionData = {
+      ...req.session
+    }
+    res.render('private', {
+      sessionData
+    })
   } else {
-      res.render('404')
+    res.render('404')
   }
 })
 
