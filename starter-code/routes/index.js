5b408cfd7a26a4b852a3f873943195b32222b2dd
cgandiaf <c.gandia.f@gmail.com>
2018-11-17 20:22:57 +0100

    Done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -15,14 +15,57 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-router.get('/profile', (req, res, next) => {
-  res.render('profile');
+router.get('/main', (req, res, next) => {
+  res.render('main');
 });
 
 router.get('/login', (req, res, next) => {
   res.render('login');
 });
 
+router.get('/private', (req, res, next) => {
+  res.render('private');
+});
+
+//SIGN UP
+router.post('/', (req, res, next) => {
+
+  let bodyUser = req.body.user;
+  let bodyPassword = req.body.password;
+
+  if (bodyUser.length=== 0 || bodyPassword.length=== 0 ) {
+    res.render("signUpError", {
+      errorMessage: "Ninguno de los campos debe estar empty"
+    });
+    return
+  }
+  
+  User.username = req.body.user;
+
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+  User.password = hashedPassword;
+
+
+
+
+  User.create({ userName: User.username, password: User.password })
+    .then((x) => {
+      console.log('Usuario creado');
+      res.redirect('/login');
+    },
+      err => {
+        next(err)
+        res.render("signup", {
+          errorMessage: "Error al conectar con Mongo"
+        });
+        return;
+      });
+});
+
+
+//LOGIN
 router.post('/login', (req, res, next) => {
 console.log('entra en el post');
     User.findOne({ userName: req.body.user})
@@ -34,7 +77,7 @@ console.log('entra en el post');
           console.log('hay match');
             req.session.inSession = true
             req.session.user = req.body.user
-            res.redirect('/profile')
+            res.redirect('/main')
         } else {
             req.session.inSession = false
             res.redirect('/')
@@ -44,36 +87,31 @@ console.log('entra en el post');
   
 });
 
-router.post('/', (req, res, next) => {
 
-  let bodyUser = req.body.user;
-  let bodyPassword = req.body.password;
 
-  if (bodyUser.length=== 0 || bodyPassword.length=== 0 ) {
-    console.log("Ninguno de los campos puede estar vacio")
-    return
+router.get('/main', function (req, res) {
+  if (req.session.inSession) {
+      let sessionData = { ...req.session
+      }
+      res.render('main', {
+          sessionData
+      })
+  } else {
+      res.render('404')
   }
-  
-  User.username = req.body.user;
+})
 
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
-
-  User.password = hashedPassword;
-
-  User.create({ userName: User.username, password: User.password })
-    .then((x) => {
-      console.log('Usuario creado');
-      res.render('profile')
-      
-    },
-      err => {
-        console.log(`Error en login pringao ${err}`)
-        next(err)
-      });
-});
-
-
+router.get('/private', function (req, res) {
+  if (req.session.inSession) {
+      let sessionData = { ...req.session
+      }
+      res.render('private', {
+          sessionData
+      })
+  } else {
+      res.render('404')
+  }
+})
 
 
 module.exports = router;
