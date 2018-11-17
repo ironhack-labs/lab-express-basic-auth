cfea4c60032727b20cc018a6e6d7e611780ea28e
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-11-17 12:30:40 +0100

    PP done!

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  console.log($('.strength_meter'));
+
+  if($('#myPassword').val() === '') {
+    $('.strength_meter div').hide();
+  } 
+
+  $('#myPassword').strength({
+    strengthClass: 'strength',
+    strengthMeterClass: 'strength_meter',
+    strengthButtonClass: 'button_strength',
+    strengthButtonText: 'Show Password',
+    strengthButtonTextToggle: 'Hide Password'
+});  
 
 }, false);
