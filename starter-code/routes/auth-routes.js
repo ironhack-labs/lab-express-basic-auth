5c74fa3c77c01585db33614bc194577c7b86e732
AaronReina <aaron.ivan.reina@gmail.com>
2018-11-18 21:52:00 +0100

    Done

---
+++ starter-code/routes/auth-routes.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const authRoutes = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const bcryptSalt = 10;
+
+/* GET home page */
+authRoutes.get("/", (req, res, next) => {
+  res.render("index");
+});
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+authRoutes.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+  User.findOne({ username: username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("auth/signup", {
+        errorMessage: "The username already exists"
+      });
+      return;
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save(err => {
+      res.redirect("/");
+    });
+  });
+});
+authRoutes.post("/login", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ username: username }, (err, user) => {
+    if (err || !user) {
+      res.render("auth/login", {
+        errorMessage: "The username doesn't exist"
+      });
+      return;
+    }
+    if (bcrypt.compareSync(password, user.password)) {
+      // Save the login in the session!
+      req.session.currentUser = user;
+      res.redirect("/");
+    } else {
+      res.render("auth/login", {
+        errorMessage: "Incorrect password"
+      });
+    }
+  });
+});
+
+module.exports = authRoutes;
