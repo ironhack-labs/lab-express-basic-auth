2946a9f5a59b97d235c47a2ca552f3111981ce1e
aemartos <aemartos@gmail.com>
2018-11-18 12:55:38 +0100

    Minor changes

--- starter-code/routes/authRouter.js
+++ starter-code/routes/authRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
-const bcryptSalt = 10;
 
 
 /* GET home page */
@@ -15,10 +14,11 @@ router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
+
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  const salt = bcrypt.genSaltSync(bcryptSalt);
+  const salt = bcrypt.genSaltSync(10);
   const hashPass = bcrypt.hashSync(password, salt);
 
   const newUser = User({
