b229c69f942a4abc0413797018e237a153a3f04d
Leticia Olábarri <leticiaolabarri@yahoo.es>
2018-11-19 16:58:39 +0100

    done

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
 
-  console.log('IronGenerator JS imported successfully!');
+    var password = document.getElementById("myPassword");
 
-}, false);
+    password.addEventListener("input", function(event) {
+      if (password.validity.typeMismatch) {
+        password.setCustomValidity("Your password needs a minimun length of 6 characters!");
+      } else {
+        password.setCustomValidity("");
+      }
+    });
+
+    var username = document.getElementById("username");
+
+    username.addEventListener("input", function(event) {
+      if (username.validity.typeMismatch) {
+        username.setCustomValidity("Only lower case letters, please!");
+      } else {
+        username.setCustomValidity("");
+      }
+    });
+
+    $(document).ready(function($) {
+      $("#myPassword").strength({
+        strengthClass: "strength",
+        strengthMeterClass: "strength_meter",
+        strengthButtonClass: "button_strength",
+        strengthButtonText: "Show password",
+        strengthButtonTextToggle: "Hide Password"
+      });
+    });
+  },
+  false
+);
