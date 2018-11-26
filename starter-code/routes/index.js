06a4256dacc60a9f9f9b7ea25dd33651134aff02
MontseOrtiz <montse_2908@hotmail.com>
2018-11-25 19:39:00 -0600

    MEX DE MONTSE ORTIZ

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
+
+const isLogged = (req, res, next) => {
+  const user = req.session.currentUser;
+  if (!user) return res.redirect('/auth/login');
+  else next();
+}
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/profile', isLogged, (req, res) => {
+  const user = req.session.currentUser;
+  res.render('profile', user);
+})
+
+router.get('/main', isLogged, (req, res) => {
+  res.render('main');
+});
+
+router.get('/private', isLogged, (req, res) => {
+  res.render('private');
+});
+
+
 
 module.exports = router;
