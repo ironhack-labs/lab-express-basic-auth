5c4b3b6d6717e00b6e1893f74be4ce49681300b7
Roberto <roberto.m.aragon@gmail.com>
2019-01-21 18:22:49 +0100

    bonus

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+  
+  $('#myPassword').strength({
+    strengthClass: 'strength',
+    strengthMeterClass: 'strength_meter',
+    strengthButtonClass: 'button_strength',
+    strengthButtonText: 'Show password',
+    strengthButtonTextToggle: 'Hide Password'
+});     
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -7,3 +7,5 @@ router.get('/', (req, res, next) => {
 });
 
 module.exports = router;
+
+
