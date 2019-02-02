7124202a00601065f4ea06c0e667fe1155a3faf8
André Sebastian <a.sebastian@posteo.de>
2019-02-02 23:21:01 +0100

    add authentication (cookies/sessions)

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require("express-session");
+const MongoStore   = require("connect-mongo")(session);
 
 
 mongoose
@@ -29,6 +31,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "this is my little secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Express View engine setup
 
@@ -51,8 +61,10 @@ app.locals.title = 'Auth';
 
 
 
-const router = require('./routes/auth');
-app.use('/', router);
+app.use('/', require('./routes/auth'));
+// app.use('/', require('./routes/auth-routes'));
+app.use('/', require('./routes/site-routes'));
+
 
 
 module.exports = app;
--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const router = express.Router();
+const express     = require("express");
+const router      = express.Router();
 
 // User model
 const User           = require("../models/user");
@@ -17,9 +17,9 @@ const Recaptcha = require('express-recaptcha').Recaptcha;
 const recaptcha = new Recaptcha('6LdSro4UAAAAAFV9fNPEBExJkXIHY0_OjqBkQ3O7', '6LdSro4UAAAAAMFpuj58J1IRC6ZkaB0WCMnr5vfE');
 
 //Routes
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
+// router.get("/", (req, res, next) => {
+//   res.render("home");
+// });
 
 router.get("/signup", recaptcha.middleware.render, (req, res, next) => {
   res.render("auth/signup", { captcha:res.recaptcha });
@@ -79,6 +79,51 @@ router.post("/signup", recaptcha.middleware.verify, (req, res, next) => {
     });
 
   }
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+  .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error)
+  });
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    // cannot access session here
+    res.redirect("/login");
+  });
 });
 
 module.exports = router;
