cdeb1e34c1d65d70749b7e17fd674f9595315cee
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-02-03 14:08:51 +0100

    the system checks if the username already exists and if the required fields are empty or not

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -10,18 +10,38 @@ router.get("/signup", (req, res) => {
   res.render('auth/signup');
 })
 
-router.post("/signup", (req, res) => {
+router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
-  const salt = bcrypt.genSaltSync(bcryptSalt);
-  const hashPwd = bcrypt.hashSync(password, salt);
 
-  User.create({
-    username,
-    password: hashPwd
-  })
-    .then(() => { res.redirect("/") })
-    .catch(error => { console.log(error) })
+
+  if (username == "" || password == "") {
+    res.render("auth/signup", {
+      errorMessage: "Please type your username and password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "This username already exists"
+        })
+        return
+      }
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPwd = bcrypt.hashSync(password, salt);
+
+      User.create({
+        username,
+        password: hashPwd
+      })
+        .then(() => { res.redirect("/") })
+        .catch(error => { console.log(error) })
+    })
+    .catch(error => { next(error) })
+
 })
 
 module.exports = router
