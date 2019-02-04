876df41903ab164940d2f4af5c218cd00ef3b0ae
Maryssael <guilherme.maryssael@gmail.com>
2019-02-04 09:38:41 -0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/basic-auth', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -30,6 +31,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
@@ -47,7 +57,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Lab Express - Basic Auth';
 
 
 
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,99 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const bcryptSalt = 10;
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.render('signup', {
+      errorMessage: 'Fields can\'t be blank'
+    });
+    return;
+  }
+
+  User.findOne({ username: username })
+    .then((user) => {
+      if (user !== null) {
+        res.render('signup', {
+          errorMessage: 'Username is taken'
+        });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashedPass = bcrypt.hashSync(password, salt);
+
+      User.create({ username, password: hashedPass })
+        .then(() => {
+          res.redirect('/');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.render('login', {
+      errorMessage: 'Fields can\'t be blank'
+    });
+    return;
+  }
+
+  User.findOne({ username: username })
+    .then((user) => {
+      if (!user) {
+        res.render('login', {
+          errorMessage: 'Username doesn\'t exist'
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect('/');
+      } else {
+        res.render('login', { errorMessage: 'Incorrect password' });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+});
+
+router.get('/main', (req, res, next) => {
+  res.render('main');
+});
+
+router.get('/private', (req, res, next) => {
+  res.render('private');
 });
 
 module.exports = router;
