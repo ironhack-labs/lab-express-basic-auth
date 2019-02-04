88ab6a78b34bbf543db6ade8637e4ba61263864e
j0nl1 <jonlitech@gmail.com>
2019-02-04 11:20:19 +0100

    Little Fix

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,72 +1,79 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const userSchema = require("../models/user");
-const checker = require("../src/js/fieldsVerification")
-const bcrypt = require("../src/js/bcrypt")
-const encriptPassword = bcrypt.encriptPassword
-const checkPassword = bcrypt.checkPassword
-const checkFields = checker.checkFields
-const checkUser = checker.checkUser
-
+const checker = require("../src/js/fieldsVerification");
+const bcrypt = require("../src/js/bcrypt");
+const encriptPassword = bcrypt.encriptPassword;
+const checkPassword = bcrypt.checkPassword;
+const checkFields = checker.checkFields;
+const checkUser = checker.checkUser;
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.get('/signup', (req, res, next) => {
-  res.render("signup")
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
 });
 
 router.post("/signup", (req, res, next) => {
-  console.log(checkUser(req.body.username))
   if (checkFields(req.body.username, req.body.password)) {
     res.render("signup", {
       errorMessage: "Indicate a username and a password to sign up"
-    })
-  } else if (checkUser(req.body.username)) {
-    res.render("signup", {
-      errorMessage: "The username already exists!"
-    })
+    });
+    return;
   } else {
-    userSchema
-      .create({ username: req.body.username, password: encriptPassword(req.body.password) })
-      .then(done => {
-        console.log(done)
-      })
-      .catch(err => console.log("An error ocurred:", err))
+    checkUser(req.body.username).then(promise => {
+      if (promise) {
+        res.render("signup", {
+          errorMessage: "The username already exists!"
+        });
+        return;
+      }
+      userSchema
+        .create({
+          username: req.body.username,
+          password: encriptPassword(req.body.password)
+        })
+        .then(() => {
+          console.log(`User ${req.body.username} was saved in the database`);
+          res.redirect("/");
+        })
+        .catch(err => console.log("An error ocurred:", err));
+    });
   }
-})
+});
 
 router.get("/login", (req, res, next) => {
-  res.render("login")
-})
+  res.render("login");
+});
 
 router.post("/login", (req, res, next) => {
   if (checkFields(req.body.username, req.body.password)) {
     res.render("signup", {
       errorMessage: "Indicate a username and a password to sign up"
-    })
+    });
   } else {
     userSchema
       .findOne({ username: req.body.username })
       .then(user => {
-        if(!user) {
+        if (!user) {
           res.render("login", {
             errorMessage: "The username doesn't exist"
-          })
-          return
+          });
+          return;
         }
-        if(checkPassword(req.body.password, user.password)) {
-          req.session.currentUser = user
-          res.redirect("/")
+        if (checkPassword(req.body.password, user.password)) {
+          req.session.currentUser = user;
+          res.redirect("/");
         } else {
           res.render("login", {
             errorMessage: "Incorrect Password"
-          })
+          });
         }
       })
-      .catch(err => console.log("An error ocurred:", err))
+      .catch(err => console.log("An error ocurred:", err));
   }
 
   router.use((req, res, next) => {
@@ -76,15 +83,14 @@ router.post("/login", (req, res, next) => {
       res.redirect("/login");
     }
   });
-  
+
   router.get("/main", (req, res, next) => {
     res.render("main");
   });
 
   router.get("/logout", (req, res, next) => {
-    req.session.destroy(() => res.redirect("/login"))
-  })
-
-})
+    req.session.destroy(() => res.redirect("/login"));
+  });
+});
 
 module.exports = router;
--- starter-code/src/js/fieldsVerification.js
+++ starter-code/src/js/fieldsVerification.js
@@ -1,18 +1,16 @@
-const userSchema = require("../../models/user")
+const userSchema = require("../../models/user");
 
 const checkUser = username => {
-    //todo
-return userSchema.findOne({ "username": username })
-    .then(user => {
-        return user !== null ? true : false
-    })
-}
+  return userSchema.findOne({ username: username }).then(user => {
+    return user !== null ? true : false;
+  });
+};
 
 const checkFields = (username, password) => {
-    return (username === "" || password === "") ? true : false
-}
+  return username === "" || password === "" ? true : false;
+};
 
 module.exports = {
-    checkFields,
-    checkUser
-}
+  checkFields,
+  checkUser
+};
