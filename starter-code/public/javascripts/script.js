a8ddfb7c072f586ea6673ed1800f0e2e41f0bc69
Samuel <bouaroua.samuel@gmail.com>
2019-02-08 00:18:10 +0100

    sign up form validation

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+	console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+
+$(document).ready(function () {
+
+	$('#userPassword').pwstrength({
+		ui: {
+			showVerdictsInsideProgressBar: true
+		}
+	});
+
+	$('#signup-form').validate({
+		rules: {
+			name: {
+				minlength: 3,
+				required: true
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			password: {
+				minlength: 6,
+				required: true
+			}
+		},
+		highlight: function (element) {
+			$(element).closest('.form-control').removeClass('is-valid').addClass('is-invalid');
+		},
+		success: function (element) {
+			element.text('OK!').addClass('valid')
+				.closest('.form-control').removeClass('is-invalid').addClass('is-valid');
+		}
+	});
+});
