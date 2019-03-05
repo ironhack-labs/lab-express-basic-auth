45bae75c52d18e5de051604117496167a078eead
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos.local>
2019-03-05 09:42:06 +0100

    done better

--- starter-code/routes/site-routes.js
+++ starter-code/routes/site-routes.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const Router = express.Router();
 
-Router.get("/", (req, res, next) => res.render("main"));
+Router.get("/", (req, res, next) => res.render("home"));
+
+Router.use((req, res, next) => {
+    if (req.session.currentUser) {
+      next()
+    } else {                       
+      res.redirect("/login");     
+    }                             
+  })                              
+  
+  Router.get("/main", (req, res, next) => res.render("main"));
 
 Router.use((req, res, next) => {
     if (req.session.currentUser) {
