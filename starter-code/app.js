0bf1f8571682ec4f59a36431ea2b756ed50f54c9
joseocamar <joseocamar@gmail.com>
2019-03-05 07:57:12 +0100

    Done Donette

--- starter-code/app.js
+++ starter-code/app.js
@@ -9,9 +9,12 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
+const session      = require("express-session");
+const MongoStore   = require("connect-mongo")(session);
+
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -24,18 +27,28 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-// Middleware Setup
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
 
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
+}));
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  resave: true,
+  saveUninitialized: true
 }));
       
 
@@ -46,13 +59,17 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 
-// default value for title local
+
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
+const authr = require('./routes/auth-routes');
+app.use('/', authr);
 
-const index = require('./routes/index');
-app.use('/', index);
+const siter = require('./routes/site-routes');
+app.use('/', siter);
+
+
 
 
 module.exports = app;
