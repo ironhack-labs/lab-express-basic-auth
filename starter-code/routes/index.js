baa2d13cdbda86eac1e0d660ac2849d094219e9d
marianaLz <marianaglp15@gmail.com>
2019-03-30 01:49:38 -0600

    Done (?)

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const isLoggedIn = (req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/auth/login");
+  }
+};
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", isLoggedIn, (req, res, next) => {
+  res.render("index");
+});
+
+router.get('/main', isLoggedIn, (req, res, next) => {
+  res.render('main');
+});
+
+router.get('/private', isLoggedIn, (req, res, next) => {
+  res.render('private')
 });
 
 module.exports = router;
