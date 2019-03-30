a4046866dcc41c261a067edd29fa090c84f347b4
lockeas16 <lockeas16@gmail.com>
2019-03-29 22:23:03 -0600

    1era iteracion finalizada, trabajando en la segunda

--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    user: {
+    username: {
       type: String,
       required: true,
       unique: true
--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
-emptyCredentials = (req, res, next) => {
+// middleware function to validate credentials
+const emptyCredentials = (req, res, next) => {
   let { username, password } = req.body;
-  console.log(req.url.includes("login"));
   if (!username)
     res.render("auth/login-signup", {
       err: "Username is empty!",
@@ -20,6 +20,35 @@ emptyCredentials = (req, res, next) => {
   next();
 };
 
+const authenticate = (req, res, next) => {
+  let { username, password } = req.body;
+  User.findOne({ username })
+    .then(user => {
+      // user doesn't exists
+      if (!user)
+        res.render("auth/login-signup", {
+          err: "User doesn't exists, check your username",
+          login: req.url.includes("login")
+        });
+      if (bcrypt.compareSync(password, user.password)) {
+        // store cookie
+        req.session.currentUser = user;
+        next();
+      } else {
+        res.render("auth/login-signup", {
+          err: "Incorrect password",
+          login: req.url.includes("login")
+        });
+      }
+    })
+    .catch(err => {
+      res.render("auth/login-signup", {
+        err: `An error has occurred during login, please try later`,
+        login: false
+      });
+    });
+};
+
 router.get("/login", (req, res) => {
   // Aqui no se considera el slash en la ruta
   res.render("auth/login-signup", { login: true });
@@ -30,18 +59,35 @@ router.get("/signup", (req, res) => {
   res.render("auth/login-signup", { login: false });
 });
 
-router.post("/login", emptyCredentials, (req, res, next) => {
-  let { username, password } = req.body;
-  User.findOne({ username })
-    .then()
-    .catch();
+router.post("/login", emptyCredentials, authenticate, (req, res, next) => {
+  res.render("private");
 });
 
 router.post("/signup", emptyCredentials, (req, res, next) => {
   let { username, password } = req.body;
-  User.findOne({ username })
-    .then()
-    .catch();
+  const salt = 10;
+  const bsalt = bcrypt.genSaltSync(salt);
+  password = bcrypt.hashSync(password, bsalt);
+  User.create({ username, password })
+    .then(() => {
+      res.redirect("/auth/login");
+    })
+    .catch(err => {
+      let { code } = err;
+      console.log(`Error en proceso de signup`);
+      console.log(err);
+      // duplicated key
+      if (code === 11000)
+        res.render("auth/login-signup", {
+          err: `Username ${username} already exists, try another one`,
+          login: false
+        });
+      else
+        res.render("auth/login-signup", {
+          err: `An error has occurred, please try later`,
+          login: false
+        });
+    });
 });
 
 module.exports = router;
