84f73c96bed330e9e45bced9b65826d3ae16c56b
Raúl Contreras <raulc98@gmail.com>
2019-04-12 18:55:50 +0200

    Finished signup section

--- starter-code/public/javascripts/signup.js
+++ starter-code/public/javascripts/signup.js
@@ -2,6 +2,7 @@
   const showPassword = document.querySelector('#show-password');
   const hidePassword = document.querySelector('#hide-password');
   const password = document.querySelector('#password');
+  const passwordStrength = document.querySelector('#password-strength');
   
   function togglePassword(){
   
@@ -16,6 +17,39 @@
     }
   }
 
+  function controlStrength(){
+    console.log('Changed')
+    let rawPassword = password.value;
+
+
+    let specialChar = /[\$@!%*#?&]+/.test(rawPassword);
+    let upperChar = /[A-Z]+/.test(rawPassword);
+    let lowerChar = /[a-z]+/.test(rawPassword);
+    let digitChar = /[\d]+/.test(rawPassword);
+
+    let count = specialChar + upperChar + lowerChar + digitChar;
+    switch(count){
+      case 0:
+      case 1:
+        passwordStrength.innerText = 'weak';
+        passwordStrength.style.backgroundColor = 'hsl(0, 50%, 50%)';
+        passwordStrength.style.color = 'white';
+      break;
+      case 2:
+      case 3:
+        passwordStrength.innerText = 'normal';
+        passwordStrength.style.backgroundColor = 'hsl(20, 75%, 50%)';
+        passwordStrength.style.color = 'black';
+      break;
+      case 4:
+        passwordStrength.innerText = 'strong';
+        passwordStrength.style.backgroundColor = 'hsl(210, 50%, 50%)';
+        passwordStrength.style.color = 'white';
+      break;
+    }
+  }
+
   showPassword.onclick = togglePassword;
   hidePassword.onclick = togglePassword;
+  password.onkeyup = controlStrength;
 })();
