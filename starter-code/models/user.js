958ab18d9155cae3b678a277eb8000059d352b5a
Francisco Vaca <fcovacarh@gmail.com>
2019-04-12 17:21:45 +0200

    Done first iteration, some of the second iteration

---
+++ starter-code/models/user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, unique: true },
+  password: { type: String }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  let {username, password} = req.body;
+  if(username !== "" && password !== ""){
+    password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    User.create({username, password}, {new: true})
+    .then((user) => res.render('/', user));
+  }
+  res.render('signup');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  let {username, password} = req.body;
+  res.render('login');
+});
+
 
 module.exports = router;
