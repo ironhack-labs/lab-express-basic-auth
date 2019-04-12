638e964ee7abb796cb982856f7a874f7c9d34806
Raúl Contreras <raulc98@gmail.com>
2019-04-12 17:04:52 +0200

    Created login and signup views

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+
 
 module.exports = router;
