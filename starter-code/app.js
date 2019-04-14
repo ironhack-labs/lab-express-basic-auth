5f3203118377b4af0c46ca6b6ac9b6aeadcbf6d3
gplocanto <gplocanto@gmail.com>
2019-04-14 20:51:26 +0200

    structure 100%

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,14 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const bcrypt       = require("bcrypt")
+const session      = require("express-session");
+const MongoStore   = require("connect-mongo")(session);
+
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/Users', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -29,6 +33,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Express View engine setup
 
@@ -47,7 +59,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Tony's delivery service";
 
 
 
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,101 @@
 const express = require('express');
+const bcrypt = require("bcrypt");
+const User = require("../models/user")
 const router  = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  if (req.session.currentUser) 
+  {
+    let username = req.session.currentUser.username;
+    res.render('index', {name: username});
+  }
+  else {res.render('index');}
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+
+  let username = req.body.username;
+  let password = req.body.password;
+
+  if(Error) {res.render("signup", {errorMessage: `Username ${username} already exists`})}
+  
+  if(username.length < 4 || (password.length > 8 && password.length < 4))
+  {
+    res.render("signup", {
+      errorMessage: "Please enter both, username (min 4 chars) and password (min 4 - max 8) to sign up."
+    })
+    return;
+  }
+
+  let saltRounds = 5;
+  let salt = bcrypt.genSaltSync(saltRounds)
+  let encryptedPwd = bcrypt.hashSync(password, salt)
+
+  Promise.resolve()
+    .then(() => User.create({ username: username, password: encryptedPwd }))
+      .then(newUser => console.log(`User added`))
+      .catch(err => console.log("An error happened:", err))
+    .then(res.render("login"))
+  })
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  let {username, password} = req.body
+
+  User
+    .findOne({ username: username })
+    .then((userData) => 
+    {
+      if (!userData) {
+        res.render("login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+
+      const isAuthorized = bcrypt.compareSync(password, userData.password)
+
+      if(isAuthorized)
+      {
+        req.session.currentUser = userData;
+        res.redirect("/main");
+      }
+      else 
+      {
+        res.render("login", {errorMessage: "Incorrect password"})
+      }
+    })
+});
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/main", (req, res, next) => {
+  let username = req.session.currentUser.username;
+  res.render("main", {name: username});
+});
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    res.redirect("/login");
+  });
 });
 
 module.exports = router;
