093de57cf5876df38f6e61810624096b06beba66
albertofragoso <hola@albertofragoso.com>
2019-04-21 22:49:16 -0500

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,12 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require('express-session')
+const MongoStore   = require('connect-mongo')(session)
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -23,6 +25,17 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+)
 
 // Middleware Setup
 app.use(logger('dev'));
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,73 @@
-const express = require('express');
-const router  = express.Router();
+require('dotenv').config()
+
+const router = require('express').Router();
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/signup', (req, res, next) => {
+  const config = {
+    title: 'Sign up',
+    action: '/signup',
+    button: 'Sign up!'
+  }
+  res.render('auth/signup', config)
+})
+
+router.post('/signup', (req, res, next) => {
+  const salt = bcrypt.genSaltSync(Number(process.env.ROUNDS))
+  User.create({ 
+    ...req.body,
+    password: bcrypt.hashSync(req.body.password, salt)
+  })
+    .then(user => res.send(user))
+    .catch(err => {
+      if(String(err.errmsg).includes('E11000 duplicate key')) res.render('auth/signup', config ,{err: 'Sorry, this user already exists :(', title: 'Sign up', action: '/signup',
+      button: 'Sign up!'})
+      if(err) res.render('auth/signup', {err: 'Sorry, user cannot be empty :(', title: 'Sign up', action: '/signup',
+      button: 'Sign up!'})
+      console.log(err)
+    })
+})
+
+router.get('/login', (req, res, next) => {
+  const config = {
+    title: 'Login',
+    action: '/login',
+    button: 'Login!'
+  }
+  res.render('auth/signup', config)
+})
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body
+  User.findOne({ username })
+    .then(user => {
+      if(!bcrypt.compareSync(password, user.password))
+        return res.render('auth/signup', {err: 'Access denied!', title: 'Login', action: '/login', button: 'Login!'})
+      req.session.currentUser = user
+      req.app.locals.loggedUser = user
+      res.redirect('private')
+    })
+    .catch(err => console.log(err))
+})
+
+router.get('/private', isLogged, (req, res, next) => {
+  res.render('private')
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy()
+  res.redirect('/private')
+})
+
+function isLogged(req, res, next) {
+  if(!req.session.currentUser) return res.redirect('/login')
+  next()
+}
 
 module.exports = router;
