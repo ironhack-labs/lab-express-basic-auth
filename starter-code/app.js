85a3dbb2c3e7550baae54ded195bac0fe908be13
VictoriaPl <victoriapp15@gmail.com>
2019-04-22 08:50:23 -0500

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,7 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 mongoose
   .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
@@ -23,6 +24,17 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+
+app.use(
+  session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+)
 
 // Middleware Setup
 app.use(logger('dev'));
