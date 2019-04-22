5255b844dfe3cd7bfa7c0f4c5a2bd9f5594f03c5
pipepico <fpr29.10@gmail.com>
2019-04-21 23:36:37 -0500

    init

--- starter-code/app.js
+++ starter-code/app.js
@@ -52,6 +52,7 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 const index = require('./routes/index');
+
 app.use('/', index);
 
 
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,76 @@
+require('dotenv').config()
+
 const express = require('express');
 const router  = express.Router();
+const User    = require('../models/User')
+const bcrypt  = require('bcrypt')
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/signup', (req,res, next) => {
+  const config = {
+    action: '/signup',
+    button: 'Signup'
+  }
+  res.render('auth/signup', config)
+})
+
+router.post('/signup', (req,res, next) => {
+  const salt = bcrypt.genSaltSync(Number(process.env.ROUNDS))
+  User.create({
+    ...req.body,
+    password: bcrypt.hashSync(req.body.password, salt)
+  })
+    .then(user => {
+      res.send(user)
+    })
+    .catch(err => {
+      if (String(err.errmsg).includes('E1100 duplicate key')) {
+        res.render('auth/signup', {err: 'User already exists'})
+      } else{
+        res.render('auth/signup', err)
+      }
+    })
+})
+
+router.get('/login', (req, res, next) => {
+  const config ={
+    action: '/login',
+    button: 'Login'
+  }
+  res.render('auth/signup', config)
+})
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body
+  User.findOne({ username })
+    .then(user => {
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.send('Sorry, your password/user is wrong')
+      }
+      req.session.currentUser   = user
+      req.app.locals.loggedUser = user
+      res.redirect('/private')
+    })
+    .catch(err => res.send(err))
+})
+
+router.get('/private', isLogged, (req, res, next) => {
+  res.render('private')
+})
+
+router.get('/logout', (req, res, next) => {
+  req.session.destroy()
+  res.redirect('/private')
+})
+
+function isLogged(req, res, next ) {
+  if (!req.session.currentUser){
+    return res.redirect('/login')
+  }
+  next()
+}
 
 module.exports = router;
