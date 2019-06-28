e1ba01ff50b7c0a809e568578d44c009b12e46c8
Francho Garcia <franchog79@gmail.com>
2019-06-28 17:41:05 +0200

    First commit

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const bcrypt = require("bcrypt")
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
+    res.render('index');
 });
+
+router.get("/signup", (req, res) => {
+    res.render("signup")
+})
+
+router.get("/login", (req, res) => {
+    res.render("login")
+})
 
 module.exports = router;
