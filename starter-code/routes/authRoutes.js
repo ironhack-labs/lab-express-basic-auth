e81b279a363fe0907901dfe04896ec2060d9dc5c
mariana-gamino <mariianagamino@gmail.com>
2019-06-30 23:28:10 -0500

    Rutas terminadas. Revisar error en base de datos

--- starter-code/routes/authRoutes.js
+++ starter-code/routes/authRoutes.js
@@ -6,5 +6,11 @@ router.get('/signup', getSignUp)
 router.post('/signup', postSignUp)
 router.get('/login', getLogin)
 router.post('/login', postLogin)
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    // can't access session here
+    res.redirect("/auth/login")
+  })
+})
 
 module.exports = router
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const router  = express.Router();
 
+function checksession (req, res, next) {
+  if(req.session.currentUser){
+    next()
+  } else {
+    res.redirect('/auth/login')
+  }
+}
+
 /* GET home page (Sign up page)*/
 router.get('/', (req, res, next) => {
-  //res.render('index');
-  res.redirect('/auth/signup')
+  res.render('index');
+  //res.redirect('/auth/signup')
 })
 
 /* GET menu page after login */
