051a6f19fac599a94ee64cd5bd1ec7614f913bbe
Aroldo Bettega <aroldo.bettega@gmail.com>
2019-07-08 13:55:04 -0300

    cone

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,3 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+$(document).ready(function ($) {
+
+});
+
+$('#password').keyup(function(s) {
+  console.log($("#password").val())
+  if ($("#password").val().length > 5) {
+    $("#password").strength();
+  }
+})
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,96 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+
+const User           = require("../models/user");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/main', (req, res, next) => {
+  res.render('main');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const salt     = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+
+  if (username === "" || password === "") {
+    res.render("signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+  User.create({
+    username,
+    password: hashPass
+  })
+  .then(() => {
+    res.redirect("/");
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("login", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ "username": theUsername })
+  .then(user => {
+      if (!user) {
+        res.render("login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
+});
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) { // <== if there's user in the session (user is logged in)
+    next(); // ==> go to the next route ---
+  } else {                          //    |
+    res.redirect("/login");         //    |
+  }                                 //    |
+}); // ------------------------------------                                
+//     | 
+//     V
+router.get("/secret", (req, res, next) => {
+  res.render("secret");
 });
 
 module.exports = router;
