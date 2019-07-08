2e61f9e19674e5691e376e2394b8ec93424da35d
Layza Floriano <layzafloriano@gmail.com>
2019-07-08 14:37:45 -0300

    inicial commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session    = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 
 mongoose
@@ -42,6 +44,7 @@ app.use(require('node-sass-middleware')({
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
@@ -49,6 +52,15 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 
 const index = require('./routes/index');
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,97 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const User = require('../models/users');
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/main', (req, res, next) => {
+  res.render('main');
+});
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+  const user = req.body.user;
+  const password = req.body.password;
+
+  if (user === '' || password === '') {
+    res.render('signup', {
+      errorMessage: 'Indicate a username and a password to sign up'
+    });
+    return;
+  }
+  User.findOne({ user: user })
+    .then(username => {
+      if (username !== null) {
+        res.render('signup', {
+          errorMessage: 'The username already exists!'
+        });
+        return;
+      }
+
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      User.create({
+        user: user,
+        password: hashPass
+      })
+        .then(() => {
+          res.redirect('/');
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+/* ----------Login-------- */
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  const theUsername = req.body.user;
+  const thePassword = req.body.password;
+
+  if (theUsername === '' || thePassword === '') {
+    res.render('login', {
+      errorMessage: 'Please enter both, username and password to sign up.'
+    });
+    return;
+  }
+
+  User.findOne({ user: theUsername })
+    .then((username) => {
+      if (!username) {
+        res.render('login', {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, username.password)) {
+        // Save the login in the session!
+        req.session.currentUser = username;
+        res.redirect('/');
+        // console.log('login: ', username);
+      } else {
+        res.render('login', {
+          errorMessage: 'Incorrect password'
+        });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
