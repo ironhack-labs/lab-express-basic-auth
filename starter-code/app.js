0507b807a1fa36c221ea26def1e5970c5ee5100f
Dan Gil <dan.nek0.bass@gmail.com>
2019-09-08 00:05:00 -0500

    All done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,22 +1,23 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/starter-code', { useNewUrlParser: true })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
@@ -29,30 +30,38 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    },
+    secret: process.env.SECRET,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60
+    })
+  })
+);
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true
+  })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
+app.locals.title = 'Basic Auth';
 
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,60 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/signup', (req, res, next) => {
+  res.render('signup', { registered: false });
+});
+
+router.post('/signup', async (req, res, next) => {
+  const salt = await bcrypt.genSalt(5);
+  const password = await bcrypt.hash(req.body.password, salt);
+  const user = await User.create({
+    username: req.body.username,
+    password
+  });
+  res.render('signup', { registered: true, username: user.username });
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    res.render('login', { err: 'User not found' });
+  } else {
+    if (await bcrypt.compare(password, user.password)) {
+      req.session.loggedUser = user;
+      res.redirect('/main');
+    } else {
+      res.render('login', { err: 'Invalid Password' });
+    }
+  }
+});
+
+router.get('/main', isLoggedIn, (req, res, next) => {
+  res.render('main');
+});
+
+router.get('/private', isLoggedIn, (req, res, next) => {
+  res.render('private');
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.session.loggedUser) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
 
 module.exports = router;
