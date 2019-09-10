d82be67ff1a4ffef8d29e1b3147379ea45acd258
Imanol Sanchez <you@example.com>
2019-09-10 10:31:28 -0500

    done

---
+++ starter-code/models/User.js
@@ -0,0 +1,21 @@
+const { model, Schema } = require('mongoose')
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+module.exports = model('User', userSchema)
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,60 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
-module.exports = router;
+router.get('/signup', (req, res, next) => {
+  res.render('signup', { registered: false })
+})
+
+router.post('/signup', async (req, res, next) => {
+  const salt = await bcrypt.genSaltSync(7)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const user = await User.create({
+    username: req.body.username,
+    password: hashedPassword
+  })
+  res.render('signup', { registered: true, username: user.username })
+})
+
+router.get('/login', (req, res, next) => {
+  res.render('login')
+})
+
+router.post('/login', async (req, res, next) => {
+  const { username, password } = req.body
+  const user = await User.findOne({ username })
+  if (!user) {
+    res.render('login', { err: "User doesn't exist" })
+  }
+  if (bcrypt.compareSync(password, user.password)) {
+    req.session.loggedUser = user
+    req.app.locals.loggedUser = user
+    res.redirect('/main')
+  } else {
+    res.render('login', { err: 'Incorrect user' })
+  }
+})
+
+let userLogged = (req, res, next) => {
+  if (req.session.loggedUser) {
+    next()
+  } else {
+    res.redirect('/login')
+  }
+}
+
+router.get('/main', userLogged, (req, res, next) => {
+  res.render('main')
+})
+
+router.get('/private', userLogged, (req, res, next) => {
+  res.render('private')
+})
+
+module.exports = router
