b9c18c3c60fdbbca232cc1e2cd95b10189afe5ad
Augusta Raposo <gutaraposomarques@gmail.com>
2019-09-13 15:18:07 +0200

    update

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,23 +1,36 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  .connect("mongodb://localhost:27017/dogLovers", {
+    useNewUrlParser: true
+  })
+  .then(() => {
+    console.log("I AM CONNECTED");
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.log("Something went wrong");
   });
+
+
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -27,17 +40,19 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(cookieParser());
 
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -53,6 +68,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+
 
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.get("/register", (req, res, next) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res, next) => {
+  res.send("ok");
+});
+
+
 
 module.exports = router;
