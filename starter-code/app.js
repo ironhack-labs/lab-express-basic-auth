04dcea2be4c91895f56af49a73c2613f91b7a58d
“Natalia <natgarea@gmail.com>
2019-09-13 23:21:23 +0200

    iterations + bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,6 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require("express-session");
+const MongoStore   = require("connect-mongo")(session);
 
 
 mongoose
@@ -24,35 +26,38 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-// Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-// Express View engine setup
 
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
-// default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
-
-
 
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,125 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Users = require("../models/Users");
+const bcrypt = require("bcrypt");
 
-/* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.get("/signup", (req, res, next) => {
+  if (req.query.error) {
+    if (req.query.error === "empty") {
+      res.render("signup", {
+        error: "🚨 The provided username and/or password were empty"
+      });
+    }
+
+    if (req.query.error === "user-exists") {
+      res.render("signup", {
+        error: "🚨 The provided username already exists"
+      });
+    }
+  } else {
+    res.render("signup");
+  }
+});
+
+router.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username.length === 0 || password.length === 0) {
+    res.redirect("/signup?error=empty");
+  }
+
+  Users.findOne({ username: username }).then(foundUserData => {
+    if (foundUserData === null) {
+      const saltRounds = 5;
+
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const encryptedPassword = bcrypt.hashSync(password, salt);
+
+      Users.create({ username: username, password: encryptedPassword }).then(
+        createdUserData => {
+          res.json({ userCreated: true, createdUserData });
+        }
+      );
+    } else {
+      res.redirect("/signup?error=user-exists");
+    }
+  });
+});
+
+router.get("/login", (req, res, next) => {
+  if (req.query.error) {
+    if (req.query.error === "empty") {
+      res.render("login", {
+        error: "🚨 The provided username and/or password were empty"
+      });
+    }
+
+    if (req.query.error === "user-doesnot-exist") {
+      res.render("login", { error: "🚨 The provided username does not exist" });
+    }
+
+    if (req.query.error === "wrong-password") {
+      res.render("login", { error: "🚨 The provided password is incorrect" });
+    }
+  } else {
+    res.render("login");
+  }
+});
+
+router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username.length === 0 || password.length === 0) {
+    res.redirect("/login?error=empty");
+  }
+
+  Users.findOne({ username: username }).then(foundUserData => {
+    if (foundUserData === null) {
+      res.redirect("/login?error=user-doesnot-exist");
+    } else {
+      const bcrypt = require("bcrypt");
+      const hashedPassword = foundUserData.password;
+
+      if (bcrypt.compareSync(password, hashedPassword)) {
+        req.session.user = foundUserData._id;
+        res.redirect("/main");
+      } else {
+        res.redirect("/login?error=wrong-password");
+      }
+    }
+  });
+});
+
+router.get("/public", (req, res) => {
+  res.render("public");
+});
+
+router.get("/main", (req, res) => {
+  if (req.session.user) {
+    Users.findById(req.session.user).then(res.render("main"));
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/private", (req, res) => {
+  if (req.session.user) {
+    Users.findById(req.session.user).then(res.render("private"));
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy(err => {
+    res.redirect("/login");
+  });
 });
 
 module.exports = router;
