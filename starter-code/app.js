e6c219ceadfa8aac6ae5201ca4beeab903c743fe
Ester-Martinez <martinez.ester@gmail.com>
2019-09-14 15:35:06 +0200

    sin bonus

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const bodyParser   = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/Ironhack0819', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -37,7 +39,19 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+   
+app.use(
+  session({
+    resave: true,
+    saveUninitialized: true,
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -47,7 +61,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Basic Authenticator';
 
 
 
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,5 +1,118 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const Users = require("../models/Users");
+const bcrypt = require("bcrypt");
+
+router.get("/signup", (req, res, next) => {
+  if (req.query.error) {
+    if (req.query.error === "empty") {
+      res.render("signup", {
+        error: "The provided username and/or password were empty"
+      });
+    }
+
+    if (req.query.error === "user-exists") {
+      res.render("signup", { error: "The provided username already exists" });
+    }
+  } else {
+    res.render("signup");
+  }
+});
+
+router.post("/signup", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username.length === 0 || password.length === 0) {
+    res.redirect("/signup?error=empty");
+  }
+
+  Users.findOne({ username: username }).then(foundUserData => {
+    if (foundUserData === null) {
+      const saltRounds = 5;
+
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const encryptedPassword = bcrypt.hashSync(password, salt);
+
+      Users.create({ username: username, password: encryptedPassword }).then(
+        createdUserData => {
+          res.json({ userCreated: true, createdUserData });
+        }
+      );
+    } else {
+      res.redirect("/signup?error=user-exists");
+    }
+  });
+});
+
+router.get("/login", (req, res, next) => {
+  if (req.query.error) {
+    if (req.query.error === "empty") {
+      res.render("login", {
+        error: "The provided username and/or password were empty"
+      });
+    }
+
+    if (req.query.error === "user-doesnot-exist") {
+      res.render("login", { error: "The provided username does not exist" });
+    }
+
+    if (req.query.error === "wrong-password") {
+      res.render("login", { error: "The provided password is incorrect" });
+    }
+  } else {
+    res.render("login");
+  }
+});
+
+router.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username.length === 0 || password.length === 0) {
+    res.redirect("/login?error=empty");
+  }
+
+  Users.findOne({ username: username }).then(foundUserData => {
+    if (foundUserData === null) {
+      res.redirect("/login?error=user-doesnot-exist");
+    } else {
+      const bcrypt = require("bcrypt");
+      const hashedPassword = foundUserData.password;
+
+      if (bcrypt.compareSync(password, hashedPassword)) {
+        req.session.user = foundUserData._id;
+        res.redirect("/main");
+      } else {
+        res.redirect("/login?error=wrong-password");
+      }
+    }
+  });
+});
+
+router.get("/main", (req, res) => {
+  if (req.session.user) {
+    Users.findById(req.session.user).then(yourInfo => {
+      res.render("main", {yourInfo: yourInfo});
+    })
+  } else {
+    res.redirect("/login");
+  }
+});
+router.get("/private", (req, res) => {
+  if (req.session.user) {
+    Users.findById(req.session.user).then(yourInfo => {
+      res.render("private", {yourInfo: yourInfo});
+    })
+  } else {
+    res.redirect("/login");
+  }
+});
+router.get("/logout", (req, res) => {
+  req.session.destroy(err => {
+    res.redirect("/login");
+  });
+});
 
 /* GET home page */
 router.get('/', (req, res, next) => {
