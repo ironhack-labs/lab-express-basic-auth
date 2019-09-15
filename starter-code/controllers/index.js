9d2d2c35dc586f110985d9e422d8567520905c5d
Larry Nobles <larrynobles@Larrys-MacBook-Pro.local>
2019-09-15 05:44:39 -0500

    done

---
+++ starter-code/controllers/index.js
@@ -0,0 +1,142 @@
+const express = require('express');
+const router  = express.Router();
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+
+exports.showSignup = (req, res, next) => {
+  res.render('auth/signup')
+}
+
+exports.makeSignup = async (req, res, next) => {
+  const { username, password } = req.body;
+  const salt = bcrypt.genSaltSync(7);
+  const hashedPassword = bcrypt.hashSync(password, salt);
+  const user = await User.create({ username, password: hashedPassword });
+  res.redirect('/login');
+}
+
+exports.showLogin = (req, res, next) => {
+  res.render('auth/login')
+}
+
+exports.makeLogin = async (req, res, next) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) {
+    res.render('auth/login', { err: "User doesn't exist" });
+  }
+  if (bcrypt.compareSync(password, user.password)) {
+    req.session.loggedUser = user;
+    req.app.locals.loggedUser = user;
+    res.redirect('/profile');
+  } else {
+    res.render('auth/login', { err: 'Your password is password, well at least you would be closer' });
+  }
+}
+
+exports.access = (req, res, next) => {
+  const { loggedUser } = req.app.locals;
+  if(loggedUser){
+    res.render('auth/profile', loggedUser);
+  }
+  res.render('auth/login', { err: 'Status Matters try again' });
+}
+
+exports.accessMain = (req, res, next) => {
+  const { loggedUser } = req.app.locals;
+  if(loggedUser){
+    res.render('auth/profile', loggedUser);
+  }
+  res.render('auth/main', { err: 'Ooops' });
+}
+
+exports.accessPrivate = (req, res, next) => {
+  const { loggedUser } = req.app.locals;
+  if(loggedUser){
+    res.render('auth/profile', loggedUser);
+  }
+  res.render('auth/private', { err: 'No peaking!' });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router  = express.Router();
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const {showSignup, makeSignup, showLogin, makeLogin, access, accessMain, accessPrivate} = require('../controllers/index')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+/*Signup Router, '/signup refers to the action in views' and showSignup refers to the controller in index.js*/
+
+ router.get('/signup', showSignup)
+ router.post('/signup', makeSignup)
+
+ /* Log in */
+ router.get('/login', showLogin)
+ router.post('/login', makeLogin)
+ 
+ /* Access/Profile, Main & Private */
+ router.get('/profile', access)
+ router.get('/main', accessMain)
+ router.get('/private', accessPrivate)
+ 
+ /* Function Checker */
+ function isLoggedIn(req, res, next) {
+   if (req.session.loggedUser) {
+     next();
+   } else {
+     res.redirect('/login');
+   }
+ }
 
 module.exports = router;
