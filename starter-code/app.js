7b4f35bcf2ec65708de6cd88ecaa7c89b06a55e3
Antonioadv08 <antonioadv08@gmail.com>
2019-09-15 23:07:55 +0200

    [MAD PP]Antonio Diez

--- starter-code/app.js
+++ starter-code/app.js
@@ -54,5 +54,6 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+app.listen(3000)
 
 module.exports = app;
---
+++ starter-code/models/users.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+mongoose
+  .connect('mongodb://localhost/usuarios', { useNewUrlParser: true })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const schemaName = new Schema(
+  {
+    username: String,
+    password: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Model = mongoose.model("Users", schemaName);
+module.exports = Model;  
