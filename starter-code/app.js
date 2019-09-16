c722659bd7f12bfa5f0caaa6bc9a196170d635da
rashier <rashier13@gmail.com>
2019-09-16 07:35:14 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -12,7 +12,7 @@ const session    = require("express-session");
 const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/basic-authentication', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
