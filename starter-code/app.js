4e5bc10fb4a1edab553c9cd819d31f8c877e183d
Felipe Padua <felipepadua@Mariannas-MBP.localdomain>
2019-09-30 18:21:53 -0300

    done v1

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/express-basic-auth', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -38,6 +39,14 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
       
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,105 @@
 const express = require('express');
 const router  = express.Router();
+const User           = require("../models/user");
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("index", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ "username": theUsername })
+  .then(user => {
+      if (!user) {
+        res.render("index", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/private");
+      } else {
+        res.render("index", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
+});
+
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const { username, password } = req.body
+  const salt     = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+  if (username === "" || password === "") {
+    res.render("signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+  
+  User.findOne({ "username": username })
+  .then(user => {
+  if (user !== null) {
+      res.render("signup", {
+        errorMessage: "The username already exists!"
+      });
+      return;
+    }
+
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    User.create({
+      username,
+      password: hashPass
+    })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  })
+  .catch(error => {
+    next(error);
+  })
+});
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next(); 
+  } else {                        
+    res.redirect("/");        
+  }                             
+});
+
+router.get("/main", (req, res, next) => {
+  res.render("main");
+});
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
+});
 module.exports = router;
