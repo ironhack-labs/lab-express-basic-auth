764f9c738e47e5ca7bb825090809d23c44fcc1ee
merrymustard <merryymustard@gmail.com>
2019-11-15 17:48:06 -0600

    1-2 compleated

---
+++ starter-code/controllers/authUser.js
@@ -0,0 +1,105 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User')
+
+exports.signupView = (req, res) => {
+    res.render("auth/signup");
+};
+
+exports.signupProcess = (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    if (email === "" || password === "") {
+        res.render("auth/signup", {
+            errorMessage: "Correo y contraseña requeridos"
+        });
+    }
+
+
+
+
+    //check if name is not repeated
+
+    User.findOne({
+            email
+        })
+        .then(user => {
+            if (!user) {
+                //TODO registrar
+                // 1. configurar el salt
+                const salt = bcrypt.genSaltSync(10);
+                const hashPassword = bcrypt.hashSync(password, salt);
+                User.create({
+                        email,
+                        password: hashPassword
+                    })
+                    .then(() => {
+                        res.redirect("/");
+                    })
+                    .catch(err => console.error(err));
+            } else {
+                //notificar que el email está en uso
+                res.render("auth/signup", {
+                    errorMessage: "Email en uso"
+                });
+            }
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+
+exports.loginView = (req, res) => {
+    res.render('auth/login');
+}
+
+
+
+
+exports.loginProcess = async (req, res) => {
+    // TODO ...
+    // 1. extraemos la información de req.body
+    const {
+        email,
+        password
+    } = req.body;
+    // 2. verificamos que no nos envien campos vacios
+    if (email === "" || password === "") {
+        res.render("auth/login", {
+            errorMessage: "Error: Missing fields"
+        });
+    }
+    // 3. Verificar si el correo está registrado, si lo está intentamos logear: verificamos si las contraseñas coinciden
+    const user = await User.findOne({
+        email
+    });
+    if (!user) {
+        res.render("auth/login", {
+            errorMessage: "Error: Email doesn't exists"
+        });
+    }
+
+    // 4. verificar si la contraseña es correcta
+    if (
+        bcrypt.compareSync(
+            password,
+            /* contraseña desde el formulario */
+            user.password
+            /* contraseña hasheada almacenada en el registro del usuario */
+        )
+    ) {
+        res.redirect("/sesion-open");
+    } else {
+        //todo error, contraseña incorrecta
+        res.render("auth/login", {
+            errorMessage: "Error: Incorrect Password"
+        });
+    }
+};
+
+exports.userSesion = (req, res) => {
+    res.render('sesion-open');
+}
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+
+
+const {
+  signupView,
+  signupProcess,
+  loginView,
+  loginProcess,
+  userSesion
+
+} = require("../controllers/authUser");
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.get('/signup', signupView);
+router.post('/signup', signupProcess);
+router.get('/login', loginView);
+router.post('/login', loginProcess);
+router.get('/sesion-open', userSesion);
+
+
 
 module.exports = router;
