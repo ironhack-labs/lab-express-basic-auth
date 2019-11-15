32165fffdb1d3a5afca32fdcc7d1bc5c53d87ded
Luis Zarza <luiszarza@Luiss-MacBook-Pro.local>
2019-11-15 19:19:59 +0100

    Done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,22 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/Usuarios-App', {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -27,17 +32,24 @@ const app = express();
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
-// Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: {
+    maxAge: 60000
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
 }));
-      
+
+
+// Express View engine setup
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -49,10 +61,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
-const index = require('./routes/index');
-app.use('/', index);
+app.use('/', require('./routes/index'));
 
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,112 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require('bcrypt')
+
+const bcryptSalt = 10;
+const User = require('../models/User.models')
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+
+router.get('/signup', (req, res) => res.render('auth/signUp'));
+router.post('/signup', (req, res) => {
+const {
+  username,
+  password
+} = req.body
+
+if (!username || !password) {
+  res.render('auth/signup', {
+    errorMessage: 'ERROR: Rellena los dos campos'
+  })
+  return
+}
+User.findOne({
+    "username": username
+  })
+  .then(user => {
+    if (user) {
+      res.render("auth/signup", {
+        errorMessage: "El nombre de usuario ya existe"
+      })
+      return
+    }
+    
+    if (password.length < 8){
+      res.render('auth/signup', {
+        errorMessage: 'ERROR: la contraseña es muy corta'
+      }) 
+      return
+    }
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+    User.create({
+        username,
+        password: hashPass
+      })
+      .then(() => res.redirect("/"))
+      .catch(error => console.log(error))
+  })
+  .catch(error => {
+    console.log(error)
+  })
+})
+
+
+//LOG IN
+
+router.get('/login', (req, res) => res.render('auth/logIn'));
+router.post("/login", (req, res, next) => {
+
+  const {
+    username,
+    password
+  } = req.body
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Rellena ambos campos."
+    })
+    return
+  }
+
+  User.findOne({
+      "username": username
+    })
+    .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "El nombre de Usuraio no existe."
+        })
+        return
+      }
+
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user; // El objeto request dispone de una propiedad .session donde guardar el .currenntUser
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Contraseña Incorrecta"
+        })
+      }
+    })
+    .catch(error => console.log('error', error))
+})
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => res.redirect("/login"));
 });
+
+router.use((req, res, next) => {
+  req.session.currentUser ? next() : res.redirect("/login")
+})
+
+router.get('/', (req, res, next) => res.render('index',{
+  user: req.session.currentUser
+}));
+
+router.get('/private', (req, res) => res.render('auth/privatePage', {
+  user: req.session.currentUser
+}));
 
 module.exports = router;
