2cded5445e31b60f377e5e2d6d852aec1e1001c0
María Simó <maria@coponstudio.es>
2019-11-17 00:35:22 +0100

    Bonus 2

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,3 +3,19 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+
+
+$(document).ready(function($) {
+    
+  const password = $('#myPassword')
+  console.log(password)
+  password.strength({
+    strengthClass: 'input is-primary is-large strength',
+    strengthMeterClass: 'strength_meter',
+    strengthButtonClass: 'button_strength',
+    strengthButtonText: 'Show password',
+    strengthButtonTextToggle: 'Hide Password'
+  });	
+  
+  });
