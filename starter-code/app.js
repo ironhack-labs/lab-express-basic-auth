8c257a7a8e3a7cd4741a2b3ae34f5d8e9c323d7e
Javier Agudo <javieragudo3d@gmail.com>
2019-11-17 11:47:52 +0100

    Iteration 1 DONE - SIGN UP

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const bcrypt       = require("bcrypt");
+const User         = require("./models/user");
+const bcryptSalt   = 10;
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/LoginDB', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -31,12 +34,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+//-------DA ERROR------
+// app.use(require('node-sass-middleware')({
+//   src:  path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   sourceMap: true
+// }));
       
 
 app.set('views', path.join(__dirname, 'views'));
@@ -47,7 +50,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Sistema Sign up / Log in';
 
 
 
@@ -55,4 +58,10 @@ const index = require('./routes/index');
 app.use('/', index);
 
 
+
+
 module.exports = app;
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,59 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt       = require("bcrypt");
+const User         = require("../models/user");
+const bcryptSalt   = 10;
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // const salt     = bcrypt.genSaltSync(bcryptSalt);
+  // const hashPass = bcrypt.hashSync(password, salt);
+
+  if (username === "" || password === "") {
+    res.render("signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+  User.findOne({ "username": username })
+  .then(user => {
+    if (user !== null) {
+        res.render("signup", {
+          errorMessage: "The username already exists!"
+        });
+        return;
+      }
+  
+      const salt     = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+  
+      User.create({
+        username,
+        password: hashPass
+      })
+      .then(() => {
+        res.render ("signup",{createUser: "Cuenta de Usuario creada"})
+        // res.redirect("/");
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  })
+  .catch(error => {
+    next(error);
+  })
 });
 
 module.exports = router;
