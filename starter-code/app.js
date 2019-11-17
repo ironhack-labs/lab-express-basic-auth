d1f95b8381fe931c284ef11aa75f9da033902cc5
Javier Agudo <javieragudo3d@gmail.com>
2019-11-17 13:05:26 +0100

    DONE SIGNUP LOGIN & PATH AUTEN

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,8 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-const bcrypt       = require("bcrypt");
-const User         = require("./models/user");
-const bcryptSalt   = 10;
-
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
   .connect('mongodb://localhost/LoginDB', {useNewUrlParser: true})
@@ -32,6 +30,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Express View engine setup
 //-------DA ERROR------
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -13,8 +13,12 @@ router.get("/signup", (req, res, next) => {
   res.render("signup");
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
 
 
+//SIGN UP-----------------------------------
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -54,6 +58,66 @@ router.post("/signup", (req, res, next) => {
   .catch(error => {
     next(error);
   })
+});
+
+//LOGIN-------------------------------------
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("login", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ "username": theUsername })
+  .then(user => {
+      if (!user) {
+        res.render("login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
+});
+
+//SESSION PATH-------------------
+router.use((req, res, next) => {
+  if (req.session.currentUser) { 
+    next();
+  } else {                          
+    res.redirect("/login");         
+  }                                
+});                            
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
+});
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) { 
+    next();
+  } else {                          
+    res.redirect("/login");         
+  }                                
+});                            
+
+router.get("/main", (req, res, next) => {
+  res.render("main");
 });
 
 module.exports = router;
