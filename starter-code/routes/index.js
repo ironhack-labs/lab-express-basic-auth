2fec3b23f90acdd8efb8ee8b1f4634849a378b5e
Erik Galiana Farell <erikgalianafarell@MacBook-Pro-de-Erik.local>
2019-11-19 22:01:54 +0100

    login almost done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -2,9 +2,26 @@ const express = require('express');
 const router  = express.Router();
 
 const authRouter = require('./auth');
+const loginRouter = require('./login');
+
+/*
+const logoutRouter = require('./logout');
+const signupRouter = require('./signup');
+*/
 
 // *  '/auth'
 router.use('/auth', authRouter);
+
+// *  '/login'
+router.use('/login', loginRouter);
+/*
+// *  '/logout'
+router.use('/logout', logoutRouter);
+
+// *  '/signup'
+router.use('/signup', signupRouter);*/
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => {
