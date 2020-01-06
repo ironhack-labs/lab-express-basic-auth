00c6b2c3d9a4ce28e9d8fce764f25454fdd77d99
henricasi <henriquecmandruzato@gmail.com>
2020-01-06 17:07:01 -0300

    Finish iteration 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,14 +1,15 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
   .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
@@ -44,10 +45,18 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
+// authentication middleware
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Basic Auth Lab';
 
 
 
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,112 @@
 const express = require('express');
 const router  = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
-/* GET home page */
+// home page
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+// signup page
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup');
+})
+
+// signup POST (user create)
+router.post("/signup", (req, res, next) => {
+  const {username, password} = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "The fields can't be empty"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+  .then(user => {
+    if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "Username already in use"
+        });
+        return;
+      }
+  
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+  
+      User.create({
+        username,
+        password: hashPass
+      })
+      .then(() => {
+        // aqui quis adicionar uma mensagem. o contra desse método é que a url permanece /signup
+        res.render("index", {message: "Account created!"});
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  })
+  .catch(error => {
+    next(error);
+  })
+});
+
+// login page
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+// login POST (user login)
+router.post("/login", (req, res, next) => {
+  const {username, password} = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both username and password to log in"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+  .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "User doesn't exist"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
+});
+
+// restricted routes
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/main", (req, res, next) => {
+  res.render("main");
+});
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
 });
 
 module.exports = router;
