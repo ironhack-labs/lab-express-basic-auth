6ab8affc6ddc31d98f1a2b815b4cfc7ba3f18d5f
Juliane Arruda <juliane_ch.arruda@live.com>
2020-01-07 01:04:05 -0300

    interaction 3 completed

--- starter-code/routes/site.js
+++ starter-code/routes/site.js
@@ -5,4 +5,37 @@ router.get("/", (req, res, next) => {
   res.render("home");
 });
 
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) { // <== if there's user in the session (user is logged in)
+    next(); // ==> go to the next route ---
+  } else {                          //    |
+    res.redirect("/login");         //    |
+  }                                 //    |
+}); // ------------------------------------                                
+//     | 
+//     V
+router.get("/main", (req, res, next) => {
+  res.render("main");
+});
+
+
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    res.redirect("/login");
+  });
+});
+
 module.exports = router;
