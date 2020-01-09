bc8b8d841fff8f912da3f2c263ed067c24848fa1
Biermann, Dirk <biermannd@mediamarktsaturn.com>
2020-01-09 17:11:57 +0100

    bonus

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,3 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
+
+$(document).ready(function($) {
+  $('#myPassword').strength({
+              strengthClass: 'strength',
+              strengthMeterClass: 'strength_meter',
+              strengthButtonClass: 'button_strength',
+              strengthButtonText: 'Show Password',
+              strengthButtonTextToggle: 'Hide Password'
+          });
+});
+
+
