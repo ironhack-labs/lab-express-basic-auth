5344ec660cb2d2237816708b740975848ef4d1de
Iván Herranz <ihcuesta@gmail.com>
2020-02-02 14:34:34 +0100

    DONE

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -6,7 +6,7 @@ const User = require("../model/user");
 const bcrypt         = require("bcrypt");
 const bcryptSalt     = 10;
 
-
+// SIGN UP
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
@@ -65,6 +65,55 @@ User.findOne({ "username": username })
     console.log(error);
   })
 });
+
+
+// LOG IN
+router.get("/login", (req, res, next) => {
+    res.render("auth/login");
+  });
+
+  router.post("/login", (req, res, next) => {
+    const theUsername = req.body.username;
+    const thePassword = req.body.password;
+  
+    // Check if user or password are empty
+    if (theUsername === "" || thePassword === "") {
+      res.render("auth/login", {
+        errorMessage: "Please enter both, username and password to sign up."
+      });
+      return;
+    }
+  
+    // Check if the user exists and the password is correct
+    User.findOne({ "username": theUsername })
+    .then(user => {
+        if (!user) {
+          res.render("auth/login", {
+            errorMessage: "The username doesn't exist."
+          });
+          return;
+        }
+        if (bcrypt.compareSync(thePassword, user.password)) {
+          // Save the login in the session!
+          req.session.currentUser = user;
+          res.redirect("/");
+        } else {
+          res.render("auth/login", {
+            errorMessage: "Incorrect password"
+          });
+        }
+    })
+    .catch(error => {
+      next(error);
+    })
+  });
+
+  router.get("/logout", (req, res, next) => {
+    req.session.destroy((err) => {
+      // cannot access session here
+      res.redirect("/login");
+    });
+  });
 
 
 
---
+++ starter-code/views/main.hbs
@@ -0,0 +1,1 @@
+<h1>MAIN PAGE</h1>
