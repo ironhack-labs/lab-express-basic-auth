38ede68f088612668720e02632db18fe3929ba78
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-02-03 21:23:21 +0100

    routes start

--- starter-code/routes/authRouter.js
+++ starter-code/routes/authRouter.js
@@ -4,21 +4,21 @@ const Users = require("../models/User");
 
 // Show the list celebrity in celebrity/index
 router.get("/", async (req, res, next) => {
-  res.render("login/");
+  res.render("auth/login");
 });
 
-// Create: Submit & Process form data
-router.post("/", async (req, res, next) => {
-  const { username, password } = req.body;
-  const addUser = new Users({ username, password }); //Crear nueva entrada
-  try {
-    await addUser.save(); //Guardar objeto .save()
-    res.redirect("/");
-  } catch (err) {
-    console.log("este es el error", err);
-    res.render("login/");
-    next();
-  }
-});
+// // Create: Submit & Process form data
+// router.post("/", async (req, res, next) => {
+//   const { username, password } = req.body;
+//   const addUser = new Users({ username, password }); //Crear nueva entrada
+//   try {
+//     await addUser.save(); //Guardar objeto .save()
+//     res.redirect("/");
+//   } catch (err) {
+//     console.log("este es el error", err);
+//     res.render("login/");
+//     next();
+//   }
+// });
 
 module.exports = router;
