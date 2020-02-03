f299958a975043a97952d43b1e3b6b5f7e095614
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-02-03 18:17:11 +0100

    seed

--- starter-code/seed/seed.js
+++ starter-code/seed/seed.js
@@ -1,12 +1,12 @@
 const withDbConnection = require("../withDbConnection");
-const ssss = require("../models/users");
+const Users = require("../models/Users");
 
-const dataCel = [
+const dataUsers = [
   { name: "Adam Samler", occupation: "Actor", catchPhrase: "Mola la leche" },
 ];
 
 withDbConnection(async () => {
   //await Celebrity.collection.drop();
-  await ssss.deleteMany();
-  await ssss.create(dataCel);
+  await Users.deleteMany();
+  await Users.create(dataUsers);
 });
