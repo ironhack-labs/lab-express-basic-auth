80e3207ce795718c83fcd4b81a1daa459c206dbd
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-03 18:00:22 +0100

    Create router for /auth and add views

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect(process.env.DBURL, {useNewUrlParser: true,  useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -53,6 +53,9 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const auth = require('./routes/auth');
+app.use('/auth', auth);
 
 
 module.exports = app;
---
+++ starter-code/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router  = express.Router();
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('auth/register');
+});
+
+module.exports = router;
