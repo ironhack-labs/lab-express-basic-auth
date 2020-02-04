c0d8be5a77f76d25b8ced2903227b22297aab119
J. A. Hernández <joseanher81@gmail.com>
2020-02-04 19:58:38 +0100

    Sign up implemented

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -16,7 +16,7 @@ router.get("/login", (req, res, next) => {
   res.render("auth/login");
 });
 
-router.post("/", (req, res, next) => {
+router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
@@ -54,6 +54,40 @@ router.post("/", (req, res, next) => {
     .catch(error => {
       next(error);
     });
+});
+
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ "username": theUsername })
+  .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
 });
 
 module.exports = router;
