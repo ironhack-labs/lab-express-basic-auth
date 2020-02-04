0f31bbcc117aabefb2eb4e2af99ced7d7a719852
Pilar <p.garciacampo@gmail.com>
2020-02-04 20:09:41 +0100

    Add iteration 3

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -8,4 +8,28 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+//protected routes
+router.get("/main", (req, res, next) => {
+  res.render("main");
+});
+
+router.get("/private", (req, res, next) => {
+  res.render("private");
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy(err => {
+    // cannot access session here
+    res.redirect("/login");
+  });
+});
+
 module.exports = router;
