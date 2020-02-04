3008558092a45faa31de82e61a4ef905c3792eed
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-02-04 22:58:23 +0100

    jQuery Strength.js

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -6,6 +6,14 @@ document.addEventListener(
   false
 );
 
+window.onload = function() {
+  let submit = document.querySelector("form button");
+  submit.addEventListener("click", () => {
+    let inputs = document.querySelectorAll("form input:invalid");
+    if (inputs.length > 0) $(".auth-form").effect("shake");
+  });
+};
+
 $("#password").strength({
   strengthClass: "strength",
   strengthMeterClass: "strength_meter",
