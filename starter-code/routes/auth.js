db5787cb17d391a72e5ab5cd12ab760bda0036fa
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-05 18:26:40 +0100

    Update strength conditions

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -18,16 +18,17 @@ router.post("/register", isLoggedOut(), async (req, res, next) => {
   } else {
     try {
       const existingUser = await User.findOne({ username });
-      if (!existingUser && strength(password) >= 2) {
+      if (!existingUser && strength(password) >= 3) {
         const newUser = await User.create({
           username,
           password: hashPassword(password)
         });
+        console.log(strength(password));
         return res.redirect("/");
-      } else if (strength(password) < 2) {
+      } else if (strength(password) < 3) {
         return res.render("auth/register", {
           errorMessage:
-            "Create a password with mixed case, character and number (minimun 8 characters)"
+            "Create a password with mixed case, special character and number (minimun 8 characters and no repeated letters)"
         });
       } else {
         return res.render("auth/register", {
@@ -54,13 +55,13 @@ router.post("/login", isLoggedOut(), async (req, res, next) => {
     try {
       const existingUser = await User.findOne({ username });
       if (!existingUser) {
-        return res.render("/auth/login", {
-          errorMessage: "The username doesn`t exist"
+        return res.render("auth/login", {
+          errorMessage: "Incorrect combination of user and password"
         });
       } else {
         if (!checkHashed(password, existingUser.password)) {
           return res.render("auth/login", {
-            errorMessage: "Password missmatch"
+            errorMessage: "Incorrect combination of user and password"
           });
         } else {
           req.session.currentUser = existingUser;
