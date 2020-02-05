41d460a1b0531b89ba8935f36e3973d091982a3a
vctorChamizo <victor.chamizo96@gmail.com>
2020-02-05 13:58:27 +0100

    Add bcrypt logic

--- starter-code/models/User.js
+++ starter-code/models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongooseSchema (
+const userSchema = new mongoose.Schema (
   {
     username: {
       type: String,
--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -1,20 +1,31 @@
 const express = require("express");
+const User = require("../models/User");
+const bcrypt = requiere("bcrypt")
 const router = express.Router();
+
+const saltRounds = 10;
+const salt = bcrypt.genSaltSync(saltRounds);
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
-router.post("/signup", (req, res, next) => {
-    console.log(req.body) // debug
+router.post("/signup", async (req, res, next) => {
+  const { username, password } = req.body;
+  const hashPassword = bcrypt.hashSync(password, salt);
+
+  try {
+    const user = await User.create({ username, password: hashPassword });
+    res.render("index", { user });
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.get("/login", (req, res, next) => {
   res.render("auth/login", {});
 });
 
-router.post("/login", (req, res, next) => {
-    console.log(req.body) // debug
-});
+router.post("/login", (req, res, next) => {});
 
 module.exports = router;
