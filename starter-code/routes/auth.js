c8f5401cba61225ee4b9d1f2697cb2e7e301e222
Carlos Mateo <cmc2k5@gmail.com>
2020-02-05 01:46:32 +0100

    Requires user model + defines /login POST

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
@@ -13,8 +14,26 @@ router.get("/login", (req, res, next) => {
   res.render("auth/login", {});
 });
 
-router.post("/login", (req, res, next) => {
-    console.log(req.body) // debug
+router.post("/login", async (req, res, next) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+
+    // if user doesn't exist
+    if (!existingUser) {
+        console.log("MESSAGE: User does not exist");
+        return res.redirect("/auth/login");
+    }
+
+    // if password missmatch
+    if (!checkHashed(password, existingUser.password)) {
+        console.log("MESSAGE: Password missmatch");
+        return res.redirect("/auth/login");
+    }
+
+    // Successful login
+    console.log(`User ${existingUser.email}, has logged in succesfully`);
+    req.session.currentUser = existingUser;
+    return res.redirect("/");
 });
 
 module.exports = router;
