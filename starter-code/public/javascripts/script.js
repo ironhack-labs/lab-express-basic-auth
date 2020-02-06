21aff684e727a91fa1a03a6adf861a44b14792e2
militroncho7 <victor@MacBook-Air-de-Victor.local>
2020-02-06 20:52:44 +0100

    Exercise Done

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
+/* document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+      console.log("IronGenerator JS imported successfully!");
+    },
+    false
+  );
 
-  console.log('IronGenerator JS imported successfully!');
+  $(document).ready
+  $("#password").strength({
+    strengthClass: "strength",
+    strengthMeterClass: "strength_meter",
+    strengthButtonClass: "button_strength",
+    strengthButtonText: "",
+    strengthButtonTextToggle: ""
+  });
 
-}, false);
+  console.log("script.JS terminado"); */
+
+  document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+      console.log("IronGenerator JS imported successfully!");
+    },
+    false
+  );
+  $(document).ready(function($) {
+    $("#myPassword").strength();
+  });
