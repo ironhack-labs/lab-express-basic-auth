33dc458b0f2bb8d1e39fb97893cd4da82c05a7c4
vctorChamizo <victor.chamizo96@gmail.com>
2020-02-06 23:30:23 +0100

    Merge successful

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -2,11 +2,7 @@ const express = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const router = express.Router();
-const User = require("../models/User");
 const { hashPassword, checkHashedPassword } = require("../lib/hashing");
-
-const saltRounds = 10;
-const salt = bcrypt.genSaltSync(saltRounds);
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
@@ -16,27 +12,21 @@ router.post("/signup", async (req, res, next) => {
   const { username, password } = req.body;
   try {
     const user = await User.findOne({ username });
-    if (user) {
-      res.render("auth/signup", {
-        errorMessage: "User already exists! Please, try again."
-      });
-    } else {
-      const hashPassword = bcrypt.hashSync(password, salt);
-      await User.create({ username, password: hashPassword });
-      res.redirect("/");
+    if (!user) {
+      const hash = hashPassword(password);
+      await User.create({ username, password: hash });
+      return res.redirect("/");
     }
+    res.render("auth/signup", {
+      errorMessage: "User already exists! Please, try again."
+    });
   } catch (e) {
     next(e);
   }
 });
 
 router.get("/login", (req, res, next) => {
-
-  // If user already logged-in
-  if (req.session.currentUser) {
-      return res.redirect('/');
-  }
-
+  if (req.session.currentUser) return res.redirect("/");
   res.render("auth/login", {});
 });
 
@@ -44,27 +34,18 @@ router.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
   const existingUser = await User.findOne({ username });
 
-  // If user doesn't exist
-  if (!existingUser) {
-    console.log("MESSAGE: User does not exist");
-    return res.redirect("/auth/login");
-  }
+  if (!existingUser || !checkHashedPassword(password, existingUser.password))
+    return res.render("/auth/login", {
+      errorMessage: "Username/password incorrect! Please, try again."
+    });
 
-  // If password missmatch
-  if (!checkHashedPassword(password, existingUser.password)) {
-    console.log("MESSAGE: Password missmatch");
-    return res.redirect("/auth/login");
-  }
-
-  // Successful login
-  console.log(`User ${existingUser.username}, has logged in succesfully`);
   req.session.currentUser = existingUser;
   return res.redirect("/");
 });
 
 router.get("/logout", async (req, res, next) => {
-    req.session.destroy();
-    res.redirect("/");
+  req.session.destroy();
+  res.redirect("/");
 });
 
 module.exports = router;
