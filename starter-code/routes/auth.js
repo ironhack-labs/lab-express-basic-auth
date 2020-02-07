a0c122aefbe761dcb45ae44c10c05b6ff951f0a0
Javier Lopez <jlopezro@gmail.com>
2020-02-07 20:57:16 +0100

    Iteration 2 done

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -38,7 +38,7 @@ router.post("/signup", (req, res, next) => {
         password: hashPass
       })
         .then(() => {
-          res.redirect("/");
+          res.redirect("/login");
         })
         .catch(error => {
           console.log(error);
@@ -47,6 +47,51 @@ router.post("/signup", (req, res, next) => {
     .catch(error => {
       next(error);
     });
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", {
+   })
+});
+// router.post("/login", (req, res, next) => {
+//   const username = req.body.username;
+//   const password = req.body.password;
+//   const salt = bcrypt.genSaltSync(bcryptSalt);
+//   const hashPass = bcrypt.hashSync(password, salt);
+// });
+
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ "username": theUsername })
+  .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
 });
 
 module.exports = router;
