c038e2f59e910bfeed85a69abaedeabcf21f7311
davidvrl <veral.david@gmail.com>
2020-02-07 18:44:30 +0100

    done

---
+++ starter-code/routes/index.routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+router.use((req, res, next) =>
+  req.session.currentUser ? next() : res.redirect("/login")
+);
+
+router.get("/main", (req, res) => res.render("main"));
+router.get("/private", (req, res) => res.render("private"));
+
+module.exports = router;
