0ea7023c594e75668d227e795b86299b59205488
CrisSugar <aguacero18@hotmail.com>
2020-02-07 18:06:36 +0100

    interation 2

---
+++ starter-code/Models/Users.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const schemaName = new Schema(
+  {
+    username: { type: String, unique: true },
+    password: String
+  },
+  { timestamps: true }
+);
+
+const Model = mongoose.model("Users", schemaName);
+module.exports = Model;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,104 @@
 const express = require('express');
 const router  = express.Router();
+const bcrypt = require("bcrypt");
+const Users = require("../Models/Users");
+
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+// router.get('/', (req, res, next) => {
+//   res.render('index');
+// });
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
 });
+
+
+router.post("/create-user", (req, res, next) => {
+  if (req.body.username.trim() === "" || req.body.password.trim() === "") {
+    res.json({
+      error: true,
+      msg: "Username or password are empty"
+    });
+
+    return;
+  }
+
+  Users.findOne({ username: req.body.username }).then((foundUser) => {
+    if (foundUser) {
+      res.json({
+        error: true,
+        msg: "Username already taken"
+      });
+
+      return;
+    } else {
+      const saltRounds = 10;
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hash = bcrypt.hashSync(req.body.password, salt);
+
+      // res.json({
+      //   username: req.body.username,
+      //   password: req.body.password,
+      //   hashedPassword: hash
+      // });
+
+      Users.create({
+        username: req.body.username,
+        password: hash,
+        role: "admin"
+      }).then(() => {
+        res.json({
+          userCreated: true,
+          timestamp: new Date()
+        });
+      });
+    }
+  });
+});
+
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+
+router.post("/log-user", (req, res, next) => {
+  if (req.body.username.trim() === "" || req.body.password.trim() === "") {
+    res.json({
+      error: true,
+      msg: "Username or password are empty"
+    });
+
+    return;
+  }
+
+  Users.findOne({ username: req.body.username }).then((foundUser) => {
+    if (foundUser) {
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        // user is ok and password is ok
+        res.json({
+          error: false,
+          msg: "Logged in, yay!",
+          when: new Date()
+        });
+      } else {
+        // user is ok but password is not ok
+        res.json({
+          error: true,
+          msg: "Password is not ok"
+        });
+      }
+    } else {
+      res.json({
+        error: true,
+        msg: "User not found"
+      });
+    }
+  });
+});
+
+
 
 module.exports = router;
