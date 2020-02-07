c1ad38c5f32e56179e26d297d9ab0cbc3b36ab1c
Luis Pena <luisp.buzon@gmail.com>
2020-02-07 17:34:31 +0100

    Done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+
+router.use((req, res, next) => req.session.currentUser ? next() : res.redirect("/login"))
+
+router.get("/main", (req, res) => res.render("main"));
+router.get("/private", (req, res) => res.render("private"));
 
 module.exports = router;
