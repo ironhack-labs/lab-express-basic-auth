529282c62f468d049b950d697fc5f93c99ec04c5
Juan Carlos <jcarlosriego@gmail.com>
2020-02-07 16:43:38 +0100

    Added home page

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/signuplab', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -53,6 +53,5 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router  = express.Router();
 
-/* GET home page */
+
 router.get('/', (req, res, next) => {
   res.render('index');
 });
+
+router.get('/signup', (req, res, next) => {
+  
+  res.render('signup');
+});
+
+
 
 module.exports = router;
