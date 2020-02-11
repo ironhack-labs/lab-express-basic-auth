64e6747ee8a08d5da70e810874fb35a0fd278908
Stefan Berk <stefan@k2ff.de#>
2020-02-11 16:59:54 +0100

    private routes added

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -3,8 +3,29 @@ const router  = express.Router();
 const signUpRouter = require('./sign-up');
 const loginRouter = require('./log-in');
 
+
+
+//open routes
 router.use(["/sign-up", "/signup"], signUpRouter);
 router.use(["/log-in", "/login"], loginRouter);
+
+// AUTHENTICATION CHECKER
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next(); 
+  } 	
+  else {  
+    res.redirect("/login"); 
+  }  
+});	
+
+//private routes
+router.use("/main", (req, res, next) => {
+  res.render('../views/main')
+})
+router.use("/private", (req, res, next) => {
+  res.render('../views/private')
+})
 
 /* GET home page */
 router.get('/', (req, res, next) => {
