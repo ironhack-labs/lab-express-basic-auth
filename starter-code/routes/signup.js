6a9b58c221aa549fd7893dab5c9a62c9eb7df12e
Jorge Berrizbeitia <jorgeberrizbeitia@gmail.com>
2020-02-11 16:26:33 +0100

    initial commit

---
+++ starter-code/routes/signup.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var signupRouter = express.Router();
+const User = require("./../models/User");
+
+//GET /login
+signupRouter.get("/", (req, res) => {
+    res.render("signup");
+});
+
+module.exports = signupRouter;
+
+// signupRouter.post("/", (req, res, next) => {
+//     const { username, password } = req.body;
+
+//     if ( password === "" || username === "") {
+//         res.render("signup", {
+//             errorMessage: "Username and Password are required"
+//         });
+//         return;
+//     } 
+//     User.findOne({ username })
+//     .then(user => {
+//         if (user) {
+//             res.render("/login")
+//         }
+//     })
+// })
