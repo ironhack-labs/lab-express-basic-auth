bc995dc1d62f5ca4b0007f44b292f0d937c2f764
Javier Varela <jvarela@strands.com>
2020-03-09 23:06:17 +0100

    Iteration 1 - Sign Up

--- starter-code/app.js
+++ starter-code/app.js
@@ -49,7 +49,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
+const router = require('./routes/auth');
+app.use('/', router);
 
 const index = require('./routes/index');
 app.use('/', index);
---
+++ starter-code/routes/auth.js
@@ -0,0 +1,52 @@
+// routes/auth.js
+
+const express = require("express");
+const router = express.Router();
+// User model
+const User           = require("../models/user");
+// BCrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const salt     = bcrypt.genSaltSync(bcryptSalt);
+  const hashPass = bcrypt.hashSync(password, salt);
+  
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up"
+    });
+    return;
+  }
+
+  User.findOne({ "username": username })
+    .then(user => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists!"
+        });
+        return;
+      }
+      User.create({
+        username,
+        password: hashPass
+      })
+        .then(() => {
+          res.redirect("/index");
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    })
+    .catch(error => {
+      next(error);
+    })
+});
+
+module.exports = router;
