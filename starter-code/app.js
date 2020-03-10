bdcb474c9647b23d5f014b587da0d24155e6c59e
Baenas <dbg94dani@gmail.com>
2020-03-10 18:10:41 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/starter-code', { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -23,6 +25,16 @@ const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60 * 1000 }, // 60 seconds
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    resave: true,
+    saveUninitialized: false,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Middleware Setup
 app.use(logger('dev'));
@@ -33,11 +45,11 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -47,7 +59,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Baenas Lab Login and Signin';
 
 
 
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,82 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const User = require('../models/user')
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 
-/* GET home page */
+
+
 router.get('/', (req, res, next) => {
-  res.render('index');
+  if (req.session.currentUser) {
+
+    res.redirect('/main')
+  } else {
+    res.redirect('/login')
+
+  }
+
+});
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+router.get('/sign', (req, res, next) => {
+  res.render('sign');
+});
+router.post('/sign', (req, res, next) => {
+  const { user, pass
+  } = req.body;
+
+  if (user === "" || pass === "") {
+    res.render('sign', { error: "Campos vacios" });
+  } else {
+
+    User.findOne({ username: user }).then((userdata) => {
+
+      if (userdata) {
+        res.render('sign', { error: "Usuario no disponible" });
+      } else {
+
+        User.insertMany({ username: user, password: pass }).then(() => {
+
+          res.render('login', { error: "Registro completado, inicia sesion" })
+        })
+      }
+    })
+  }
+});
+router.post('/login', (req, res, next) => {
+  const { user, pass
+  } = req.body;
+
+  if (user === "" || pass === "") {
+    res.render('login', { error: "Campos vacios" });
+  } else {
+
+    User.findOne({ username: user, password: pass }).then((datalogin) => {
+
+      if (datalogin) {
+        req.session.currentUser = datalogin;
+        res.redirect('/main')
+      } else {
+
+        res.render('login', { error: "Error al introducir datos" })
+      }
+
+    })
+  }
+});
+
+router.get('/main', (req, res, next) => {
+  console.log(req.session)
+  res.render('home');
+
+});
+router.get('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+
+    res.redirect('/');
+
+  })
 });
 
 module.exports = router;
