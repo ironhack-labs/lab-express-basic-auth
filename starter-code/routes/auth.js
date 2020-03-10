ab2ca012a0d8be52ab94aa0c237e77f8ea3cffdd
Javier Varela <jvarela@strands.com>
2020-03-10 15:40:07 +0100

    Iteration 2 - Login

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -12,6 +12,10 @@ router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
@@ -47,6 +51,40 @@ router.post("/signup", (req, res, next) => {
     .catch(error => {
       next(error);
     })
+});
+
+router.post("/login", (req, res, next) => {
+  const theUsername = req.body.username;
+  const thePassword = req.body.password;
+
+  if (theUsername === "" || thePassword === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to sign up."
+    });
+    return;
+  }
+
+  User.findOne({ "username": theUsername })
+  .then(user => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "The username doesn't exist."
+        });
+        return;
+      }
+      if (bcrypt.compareSync(thePassword, user.password)) {
+        // Save the login in the session!
+        req.session.currentUser = user;
+        res.redirect("/index");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Incorrect password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
 });
 
 module.exports = router;
