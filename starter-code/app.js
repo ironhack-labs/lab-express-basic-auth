75b0837c1e049634012fbafcb5ca524bfaa75eed
Michael Mueller <michael@miniguide.co>
2020-03-14 22:30:53 +0100

    Iteration 1

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/user-db', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error('Error connecting to mongo', err);
   });
 
 const app_name = require('./package.json').name;
@@ -32,27 +31,23 @@ app.use(cookieParser());
 
 // Express View engine setup
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true,
+  }),
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
+app.locals.title = 'Lab Express Basic Auth';
 
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,44 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+let error = null;
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.post('/', (req, res, next) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+
+  User.findOne({
+    username
+  })
+  .then((user) => {
+    console.log(`Response is `, JSON.stringify(user));
+    if (user) {
+      console.log(`Inside if with response ${user}`);
+      res.render('index', { error: `Username ${user} already exist foo` });
+    } else {
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      console.log('Creating user', username);
+      User.create({
+        username,
+        password: hashPass
+      })
+      .then(() => {
+        error = null;
+        res.redirect("/");
+      })
+      .catch(error => next(error))
+    }
+  })
+  .catch(error => {
+    next(error);
+  })
 });
 
 module.exports = router;
