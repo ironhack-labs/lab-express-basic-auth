07fa2a2685353a133175a3d1954231a9d75b1973
Raul Marchan Catala <chan@Rauls-iMac.local>
2020-03-17 00:08:08 +0100

    [BCN-WDFT-RAUL]-done3

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+    console.log('IronGenerator JS imported successfully!');
 
 }, false);
+$(document).ready(function($) {
+
+    $('#myPassword').strength({
+        strengthClass: 'strength',
+        strengthMeterClass: 'strength_meter',
+        strengthButtonClass: 'button_strength',
+        strengthButtonText: 'Show Password',
+        strengthButtonTextToggle: 'Hide Password'
+    });
+
+});
