1b9a965e5dffdd590dd1abf19a59397b1cbf0ce9
Raul Marchan Catala <chan@Rauls-iMac.local>
2020-03-16 21:08:38 +0100

    [BCN-WDFT-RAUL]-done2

--- starter-code/routes/auth.js
+++ starter-code/routes/auth.js
@@ -102,7 +102,7 @@ router.post('/login', (req, res, next) => {
 router.get("/logout", (req, res, next) => {
     req.session.destroy(err => {
         // cannot access session here
-        res.redirect("/login");
+        res.redirect("/");
     });
 });
 
