b36c28684b709278347fb3c353f6d43ef89489da
felixvanriessen <felixvanriessengurung@gmail.com>
2020-03-17 16:14:52 +0100

    added logout function and media page

--- starter-code/routes/login.js
+++ starter-code/routes/login.js
@@ -19,3 +19,4 @@ router.post('/', (req,res)=>{
 })
 
 module.exports = router;
+
--- starter-code/routes/main.js
+++ starter-code/routes/main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 
+//check if user is logged in
 router.use((req,res,next)=>{
     if (req.session.currentUser){
       next()
@@ -9,6 +10,16 @@ router.use((req,res,next)=>{
 
 router.get('/main', (req,res)=>{
     res.render('main')
+})
+
+router.get('/media', (req,res)=>{
+    res.render('private')
+})
+
+router.get('/logout', (req,res)=>{
+    req.session.destroy(()=>{
+        res.redirect('/login')
+    })
 })
 
 module.exports = router;
