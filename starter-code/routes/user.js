9dac4863964f3773dc79837ad9af0133b4006ab7
miranda <miranda@LAPTOP-R0JVELH6.localdomain>
2020-03-18 15:44:04 +0100

    hashed passwords using bcrypt

--- starter-code/routes/user.js
+++ starter-code/routes/user.js
@@ -1,77 +1,76 @@
-const express = require("express");
-const router = express.Router(); // Router is like a lightweight app. You can use it exactly the same way except you can't listen on it. 
-const User = require("../models/User");
+const express = require('express');
+const router = express.Router(); // Router is like a lightweight app. You can use it exactly the same way except you can't listen on it.
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-router.get("/signup", (req, res) => { //url 
-  res.render("user/signup.hbs"); //filename
-})
+router.get('/signup', (req, res) => {
+  //url
+  res.render('user/signup.hbs'); //filename
+});
 
-router.post("/signup", (req, res) => {
+router.post('/signup', (req, res, next) => {
   // const username = req.body.username;
   // const password = req.body.password;
-  const {
-    username,
-    password
-  } = req.body;
-  User
-    .create({
-      username: username,
-      password: password
-    })
-    .then((user) => {
-      res.redirect("/login"); // instruct the browser to make a get request to /user/login (http://localhost:3000 is implicit)
-    })
-    .catch((err) => {
-      res.send("user not created", err);
-    })
-})
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10, function(err, hash) {
+    if (err) next('hashing error');
+    else {
+      User.create({
+        username: username,
+        password: hash,
+      })
+        .then(user => {
+          res.redirect('/login'); // instruct the browser to make a get request to /user/login (http://localhost:3000 is implicit)
+        })
+        .catch(err => {
+          res.send('user not created', err);
+        });
+    }
+  });
+});
 
+router.get('/login', (req, res) => {
+  res.render('user/login.hbs');
+});
 
-router.get("/login", (req, res) => {
-  res.render("user/login.hbs");
-})
-
-router.post("/login", (req, res, next) => {
-  const {
-    username,
-    password
-  } = req.body; // short hand notation for the uncommented code below
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body; // short hand notation for the uncommented code below
   // const username = req.body.username;
   // const password = req.body.password;
   User.findOne({
-      username // short hand notation for {username: username}
+    username, // short hand notation for {username: username}
+  })
+    .then(user => {
+      if (!user) res.send('invalid credentials.');
+      bcrypt.compare(password, user.password, function(err, correctPassword) {
+        if (err) next('hash compare error');
+        else if (!correctPassword) res.send('invalid credentials.');
+        else {
+          req.session.currentUser = user;
+          res.redirect('/profile');
+        }
+      });
     })
-    .then((user) => {
-      if (!user) res.send("invalid credentials.")
-      else if (user.password !== password) res.send("invalid credentials.");
-      else {
-        // log the user in by starting a session;
-        // redirect the user to a section of the site that is normally protected
-        req.session.currentUser = user;
-        res.redirect("/profile");
-      }
-    })
-    .catch((err) => {
-      next("login is a complete shitshow")
-    })
-})
+    .catch(err => {
+      next('login is a complete shitshow');
+    });
+});
 
-router.get("/profile", (req, res) => { //url 
-  res.render("user/profile.hbs"); //filename
-})
+router.get('/profile', (req, res) => {
+  //url
+  res.render('user/profile.hbs'); //filename
+});
 
-router.get("/main", (req, res) => {
-  res.render("main.hbs");
-})
+router.get('/main', (req, res) => {
+  res.render('main.hbs');
+});
 
-router.get("/private", (req, res) => {
-  res.render("private.hbs");
-})
+router.get('/private', (req, res) => {
+  res.render('private.hbs');
+});
 
-
-router.get("/index", (req, res) => {
-  res.render("index.hbs");
-})
-
+router.get('/index', (req, res) => {
+  res.render('index.hbs');
+});
 
 module.exports = router;
