75bd0ddb617770c705909f8bc0e042d2eb1fe488
Emma-Solano <solano.c.emmanuel@gmail.com>
2020-04-17 15:14:13 -0500

    parcial

---
+++ starter-code/controllers/auth.js
@@ -0,0 +1,3 @@
+const bcrypt = require("bcrypt")
+const User = require("../models/User")
+
---
+++ starter-code/models/User.js
@@ -0,0 +1,8 @@
+const {Schema, model} = require("mongoose")
+const userSchema = new Schema({
+ email: String, password: String  
+}, 
+    { timestamps: true}
+) 
+
+module.exports = model("User", userSchema)
