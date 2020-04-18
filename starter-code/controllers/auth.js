baf68ced038a7685c987c381e9fa3d8ac20f977b
donovanftl <donovan_torreslan@outlook.com>
2020-04-18 13:48:50 -0500

    iteracion 3

--- starter-code/controllers/auth.js
+++ starter-code/controllers/auth.js
@@ -10,19 +10,19 @@ exports.signupProcess = async (req, res) => {
   const { username, password } = req.body
   //2. verificar que recibi la informacion correcta (no me mandaron algo vacio kbrones)
   if (username === '' || password === '') {
-    res.render('auth/signup', {
+    return res.render('auth/signup', {
       error: 'No seas cabron, dame la info que te pedi',
     })
-    return
+    
   }
   //3. Verificar si existe un usuario con ese correo
   const userInDB = await User.findOne({ username })
 
-  if (userInDB) {
-    res.render('auth/signup', {
+  if (userInDB !== null) {
+    return res.render('auth/signup', {
       error: 'ya te  registrastes wey, no te acuerdas ?... si no fuiste tu ya te jakiaron compa',
     })
-    return
+    
   }
   //4. Hashear la contrasena
   //4.1 Generar el salt
