a48a789a0c7cf4d9a2d57aa46481c9a1bfe392b1
Emma-Solano <solano.c.emmanuel@gmail.com>
2020-04-18 17:27:35 -0500

    added cat image

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -6,6 +6,10 @@ router.get('/', (req, res, next) => {
   res.render('index')
 })
 
+router.get('/main', (req, res, next) => {
+  res.render('main')
+})
+
 const { signupView, signupProcess, loginView, loginProcess } = require('../controllers/auth')
 
 router.get('/signup', signupView)
@@ -14,6 +18,6 @@ router.post('/signup', signupProcess)
 router.get('/login', loginView)
 router.post('/login', loginProcess)
 
-router.get('main', mainView)
+router.get('main')
 
 module.exports = router
