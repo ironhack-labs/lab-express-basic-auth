3f62df64911eb9814561ed8e2a9517ede79d0ed8
Maxime Paolone <paolonemax@gmail.com>
2020-04-21 15:27:29 +0200

    Iteration 1 - Sign Up

---
+++ starter-code/routes/auth.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("auth/signup", {
+      errorMessage: "Indicate a username and a password to sign up",
+    });
+    return;
+  }
+  User.findOne({ username: username })
+    .then((user) => {
+      if (user !== null) {
+        res.render("auth/signup", {
+          errorMessage: "The username already exists!",
+        });
+        return;
+      }
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+      User.create({
+        username,
+        password: hashPass,
+      })
+        .then(() => {
+          res.redirect("/");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    })
+    .catch((err) => console.log(`An error ocurred: ${err}`));
+});
+module.exports = router;
