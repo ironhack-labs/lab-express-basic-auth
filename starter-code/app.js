83849a6dcbf86a91031dd9e180e35a271f893a5a
Chantel Taki <chanteltaki@gmail.com>
2020-04-22 13:24:38 -0400

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const bcrypt       = require('bcrypt');
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/auth-lab', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -49,10 +52,29 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: { maxAge: 60000000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60, // 1 day
+  }),
+}));
 
 
-const index = require('./routes/index');
-app.use('/', index);
 
+app.use((req, res, next) => {
+
+  if (req.session.currentUser) {
+   res.locals.isLoggedIn = true;
+  } else {
+    res.locals.isLoggedIn = false;
+  }
+  next();
+});
+
+// routes
+app.use('/', require('./routes/authRoutes'));
+app.use('/', require('./routes/siteRoutes'));
 
 module.exports = app;
---
+++ starter-code/routes/authRoutes.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+// SIGNUP
+router.get('/signup', (req, res, next) => {
+  res.render('auth/signup.hbs');
+});
+
+router.post('/signup', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.render('auth/signup.hbs', {
+      errorMessage: 'Indicate a username and a password to sign up',
+    });
+  }
+
+  User.findOne({ username: username })
+    .then((user) => {
+      if (user !== null) {
+        res.render('auth/signup.hbs', {
+          errorMessage: 'The username already exists!',
+        });
+      } else {
+        const bcryptSalt = 10;
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
+        const newUser = { username, password: hashPass };
+        User.create(newUser)
+          .then(() => res.redirect('/'))
+          .catch((err) => console.log(err));
+      }
+    })
+    .catch((err) => next(err));
+});
+
+// LOGIN
+router.get('/login', (req, res, next) => {
+  res.render('auth/login.hbs');
+});
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.render('auth/login.hbs', {
+      errorMessage: 'Please enter a username and password to sign up.',
+    });
+  }
+
+  User.findOne({ username: username })
+    .then((user) => {
+      if (!user) {
+        res.render('auth/login.hbs', {
+          errorMessage: 'Invalid credentials',
+        });
+      }
+      if (bcrypt.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        res.redirect('/');
+      } else {
+        res.render('auth/login.hbs', {
+          errorMessage: 'Invalid credentials',
+        });
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// LOGOUT
+router.get('/logout', (req, res, next) => {
+  req.session.destroy((err) => {
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
