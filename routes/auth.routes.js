9b89f1f6e314597ec42e9ce9b1545b6861abebf4
Antoine Rolland <antoine-rolland@hotmail.fr>
2020-06-09 13:23:46 +0200

    Bonus : Front end validation + password validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,6 +18,14 @@ router.post('/signup', (req, res, next) => {
         });
         return;
       }
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+        res
+        .status(500)
+        .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+        return;
+    }
+    
     bcrypt.genSalt(saltRounds)
     .then(salt => (bcrypt.hash(password,salt)))
     .then(hashedPassword => {
