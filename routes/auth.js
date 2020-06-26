0548cf1285f320e9f2b5013a13f0d9a2f07fabdb
SoonWoo Kwon <soonk@outlook.com>
2020-06-26 20:05:29 +0200

    done

---
+++ routes/auth.js
@@ -0,0 +1,90 @@
+// routes/auth.js
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/signup", (req, res, next) => {
+    res.render("auth/signup");
+});
+
+const User = require("../models/User.model");
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+router.post("/signup", (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+
+    if (username.length === 0 || password.length === 0) {
+        res.render('auth/signup', {
+            errorMsg: 'Please fill in the fields.'
+        })
+        return
+    }
+
+    if (password.length < 2) {
+        res.render('auth/signup', {
+            errorMsg: 'Password too short'
+        })
+        return
+    }
+
+    User.create({
+            username,
+            password: hashPass,
+        })
+        .then(() => {
+            console.log("added new user named ", username);
+            res.redirect("/");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+
+});
+
+router.get("/login", (req, res, next) => {
+    res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+    const theUsername = req.body.username;
+    const thePassword = req.body.password;
+
+    if (theUsername === "" || thePassword === "") {
+        res.render("auth/login", {
+            errorMessage: "Please enter both, username and password to login."
+        });
+        return;
+    }
+
+    User.findOne({
+            "username": theUsername
+        })
+        .then(user => {
+            if (!user) {
+                res.render("auth/login", {
+                    errorMessage: "The username doesn't exist."
+                });
+                return;
+            }
+            if (bcrypt.compareSync(thePassword, user.password)) {
+                req.session.currentUser = user;
+                res.redirect("/");
+            } else {
+                res.render("auth/login", {
+                    errorMessage: "Incorrect password"
+                });
+            }
+        })
+        .catch(error => {
+            next(error);
+        })
+});
+
+module.exports = router;
---
+++ routes/site-routes.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+ 
+router.get("/", (req, res, next) => {
+  res.render("home");
+});
+ 
+router.use((req, res, next) => {
+    if (req.session.currentUser) { 
+      next(); 
+    } else {                          
+      res.redirect("/login");         
+    }                                
+  });                             
+  
+  router.get("/main", (req, res, next) => {
+    res.render("protected/main");
+  });
+  
+  router.get("/private", (req, res, next) => {
+    res.render("protected/private");
+  });
+  
+  router.get("/logout", (req, res, next) => {
+    req.session.destroy((err) => {
+     
+      res.redirect("/login");
+    });
+  });
+  
+module.exports = router;
