b18f6cc33ea829472e1e2475bc3d9d330d725e48
carlosziegler <carlos.ziegler@gmail.com>
2020-07-01 19:51:15 +0200

    display name in Private

--- routes/private.routes.js
+++ routes/private.routes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 /* GET home page */
 router.get('/private', (req, res, next) => {
     const user = req.session.user
+    console.log(user)
     if (!user) return res.redirect('/login')
-    return res.render('private')
+    return res.render('private', { user })
 });
 
 module.exports = router;
