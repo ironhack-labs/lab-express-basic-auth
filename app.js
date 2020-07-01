c3fc8964a61df189e6617dd7ac1aff8f993cf578
vmayrock <verena.mayrock@gmail.com>
2020-07-01 22:58:29 +0200

    done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+const bcrypt=require('bcrypt');
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -22,6 +23,28 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const session=require('express-session'); 
+const MongoStore = require('connect-mongo')(session);
+
+app.use(
+    session({
+        secret: "basic-auth-secret", 
+        cookie: {maxAge: 24*60*60}, 
+        store: new MongoStore({
+            mongooseConnection: mongoose.connection, 
+            ttl: 24*60*60
+        })
+    })
+); 
+
+app.use(
+    require('node-sass-middleware')({
+        src: path.join(__dirname, "public"), 
+        dest: path.join(__dirname, "public"), 
+        sourceMap: true
+    })
+); 
 
 // Express View engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,103 @@
 const express = require('express');
+const app=require('../app')
 const router = express.Router();
+const User=require('../models/User.model'); 
+const bcrypt=require('bcrypt');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// show login page
+router.get('/login', (req, res) => {
+    res.render('login')
+}); 
+// show signup page
+router.get('/signup', (req, res) => {
+    res.render('signup')
+}); 
+
+// middleware to check if user is logged in 
+const loginCheck=()=> {
+    return (req, res, next) => {
+        if (req.session.user) {
+            next(); 
+        } else {
+            res.redirect('/login'); 
+        }
+    }
+}; 
+
+router.get('/private', loginCheck(), (req, res) => {
+    console.log('this is a cookie:', req.cookies); 
+    console.log('this is the user:', req.session.user); 
+    res.render('private'); 
+}); 
+
+// what happens when signing up
+router.post('/signup', (req, res) => {
+    const {username, password} = req.body; 
+    if (password < 3) {
+        res.render('signup', {message: 'Your password should contain at least 3 characters.'}); 
+        return; 
+    } if (username==='') {
+        res.render('signup', {message: 'Please provide a username.'});
+        return; 
+    }
+    // check if user is already in the database
+    User.findOne({username: username})
+    .then(found => {
+        if (found !== null) {
+            res.render('signup', {message: 'This name is already taken.'});
+        } else {
+            // otherwise create the user
+            const salt =bcrypt.genSaltSync(); 
+            const hash=bcrypt.hashSync(password, salt)
+            User.create({username: username, password: hash})
+            .then(dbUser => {
+                req.session.user=dbUser; 
+                res.redirect('/private')
+            })
+            .catch(err => {
+            next(err); 
+            })
+        }
+    })
+}); 
+
+
+router.post('/login', (req, res) => {
+    const {username, password} = req.body; 
+    if (password.length < 3) {
+        res.render('login', {message: 'This is not a valid password.'}); 
+        return; 
+    } if (username ==="") {
+        res.render('login', {message: 'Please enter your username.'}); 
+        return; 
+    } 
+    User.findOne({username: username})
+    .then(dbUser => {
+        if (dbUser===null) {
+            res.render('login', {message: 'Invalid credentials.'}); 
+            return; 
+        }
+        if (bcrypt.compareSync(password, dbUser.password)) {
+            req.session.user=dbUser; 
+            res.redirect('/private'); 
+        } else  {
+            res.render('login', {message: 'Invalid credentials.'})
+        }
+    })
+}); 
+
+router.get('/logout', (req, res, next) => {
+    req.session.destroy(err => {
+        if (err) {
+            next(err); 
+        } else {
+            res.redirect('/'); 
+        }
+    })
+}); 
+
 
 module.exports = router;
