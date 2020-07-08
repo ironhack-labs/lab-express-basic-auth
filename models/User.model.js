5824cd834c2b71c3dbb1c5b6eb2e2b11375deb5e
Pumpel <romanbaos@gmail.com>
2020-07-08 20:58:29 +0200

    user model created

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,20 @@
-// User model here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true,'username is required'],
+        unique: [true,'username must be unique'],
+        minlength: [3,'username must be at least 3 characters long']
+    },
+    password: {
+        type: String,
+        required: [true,'password required'],
+        minlength: [3, 'password must be at least 3 characters long']
+    }
+})
+
+const User = mongoose.model('User',userSchema)
+model.exports = User;
+
