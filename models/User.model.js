f10e888dc2c17fca65267101a7e30d00b1ff7308
fermarinsanchez <fermarinsanchez@yahoo.es>
2020-07-09 08:14:42 +0200

    working on first iterarion

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,18 @@
 // User model here
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password :{
+        type: String,
+        minlength: 4,
+        maxlength: 10
+    } 
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/signin', (req, res, next) => res.render('createUser'))
 
 module.exports = router;
