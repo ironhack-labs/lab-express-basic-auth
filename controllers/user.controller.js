105b63c27db91259799bed721e49306c86baf3a6
Irma Blanco <irblancofdz@gmail.com>
2020-07-11 13:17:56 +0200

    feat: Add user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,29 @@
+const User = require('../models/user.model');
+
+module.exports.index = (req, res) => {
+  res.render('index');
+};
+
+module.exports.login = (req, res) => {
+  res.render('login');
+};
+module.exports.signup = (req, res) => {
+  res.render('signup');
+};
+
+module.exports.createUser = (req, res) => {
+  const newUser = new User(req.body);
+
+  newUser.save()
+    .then(user => {
+      res.render('users/profile', {
+        user
+      });
+    })
+    .catch((errors) => {
+      res.render('users/signup', {
+        errors,
+        user: req.body
+      })
+    });
+}
