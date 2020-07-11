34100dfcd27d2aa627966473d73712ae236602a9
Irma Blanco <irblancofdz@gmail.com>
2020-07-11 13:17:32 +0200

    refactor

--- routes/signup.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next) => res.render('signup'));
-router.post('/new-user', (req, res, next) => {
-  console.log('post', req.body);
-});
-
-module.exports = router;
