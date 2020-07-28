bb19e1b7416ccc9ea7ad82a42e3c934e9f56472f
Mariana Mazzariello <mfmazzariello@gmail.com>
2020-07-28 09:34:42 +0200

    Finally version

--- routes/index.js
+++ routes/index.js
@@ -18,10 +18,10 @@ router.get("/main", (req, res, next) => res.render("main"));
 router.get("/private", (req, res, next) => res.render("private"));
 
 
-// router.get("/logout", function (req, res, next) {
-//   req.session.destroy((err) => {
-//     res.redirect("/login");
-//   })
-// });
+router.get("/logout", function (req, res, next) {
+  req.session.destroy((err) => {
+    res.redirect("/login");
+  })
+});
 
 module.exports = router;
