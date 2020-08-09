d6e2041cd0a53c1b6a86bb523ed7067ba8bb1a16
Kevin Hernandez <kevin.hernandez001@gmail.com>
2020-08-09 13:21:38 -0400

    iteration 1

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,24 @@
-// User model here
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Username is required.'],
+      trim: true,
+      unique: true
+    },
+    passwordHash: { type: String, required: [true, 'Password is required.'] }
+  },
+  {
+    timestamps: true
+  }
+);
+
+// const User = model('User', userSchema);
+// module.exports = User;
+
+// User: model name
+// users: collection
+module.exports = model('User', userSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
+const bcryptjs = require('bcrypt');
+
+const saltRounds = 10;
+
+const User = require('../models/User.model');
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+/* GET home page */
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+});
+
+/* POST sign up page */
+router.post('/signup', (req, res, next) => {
+
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.render('signup', { errorMessage: 'All fields are mandatory!' });
+        return;
+    }
+
+    bcryptjs
+        .genSalt(saltRounds)
+        .then(salt => bcryptjs.hash(password, salt))
+        .then(hashedPassword => {
+
+            return User.create({
+                username,
+                passwordHash: hashedPassword
+            });
+        })
+        .then(userDoc => console.log(userDoc))
+        .catch(err => console.log(err));
+
+        res.redirect('/');
+});
+
+/*GET login page */
+router.get('/login', (req, res, next) => {
+    res.render('login');
+})
 
 module.exports = router;
