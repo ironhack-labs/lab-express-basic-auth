e1fef81cbde6842ce67b0adcac271eea62005236
Arianna Valle <ariannamarbella@gmail.com>
2020-08-09 10:38:30 -0400

    Complete iteration 1: Sign Up

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-const index = require('./routes/index.routes');
-app.use('/', index);
+app.use('/', require('./routes/index.routes'));
+app.use('/auth', require('./routes/auth-routes/auth.routes'));
 
 module.exports = app;
