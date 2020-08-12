6369e39c4a99eef9e8a77147d4cc419a38642166
Matthew Howard <mattalanhoward@gmail.com>
2020-08-12 20:53:04 +0200

    Iteration 1

--- app.js
+++ app.js
@@ -34,5 +34,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index.routes');
 app.use('/', index);
+const authRouter = require('./routes/auth.routes')
+app.use('/', authRouter)
+
 
 module.exports = app;
