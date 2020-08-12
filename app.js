f31b2f33bdde3e80354bd9a82473ed0e39d6e632
alejanglez <64974849+alejanglez@users.noreply.github.com>
2020-08-12 17:35:53 +0200

    IT1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
 const express = require('express');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
@@ -17,6 +18,7 @@ const app = express();
 // require database configuration
 require('./configs/db.config');
 
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -28,11 +30,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index.routes');
+const authRouter = require('./routes/auth.routes');
 app.use('/', index);
+app.use('/', authRouter);
+
+require('./configs/session.config')(app);
+
+// Catch missing routes and forward to error handler
+app.use((req, res, next) => next(createError(404)));
+
+// Catch all error handler
+app.use((error, req, res) => {
+  // Set error information, with stack only available in development
+  res.locals.message = error.message;
+  res.locals.error = req.app.get('env') === 'development' ? error : {};
+
+  // render the error page
+  res.status(error.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
