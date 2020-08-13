643f89bfde28dd26e0635b5517ab7badf72b7eef
Nuno Pacheco <nunorpacheco@gmail.com>
2020-08-13 17:13:53 +0100

    IT 1 DONE

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,22 @@
 // User model here
+
+const mongoose = require("mongoose")
+ const { Schema, model } = mongoose;
+
+  const userSchema = new Schema(
+     {
+         username: {
+             type: String,
+             required: true,
+             trim: true,
+             unique: true
+         },
+         passwordHash: {
+             type: String,
+             required: true,
+             trim: true
+         }
+     }
+ )
+
+  module.exports = model("User", userSchema) 
---
+++ routes/auth-routes/auth.routes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+ const router = express.Router();
+ const bcryptjs = require("bcryptjs");
+ const saltRounds = 10;
+ const User = require("../../models/User.model");
+
+  router.get("/signup", (req, res, next) => {
+ 	res.render("auth-views/signup");
+ });
+
+  router.post("/signup", (req, res, next) => {
+ 	const { username, password } = req.body;
+
+  	if (!username || !password) {
+ 		res.render("auth-views/signup", {
+ 			errorMessage: "All fields are mandatory.",
+ 		});
+ 	}
+
+  	bcryptjs
+ 		.genSalt(saltRounds)
+ 		.then((salt) => bcryptjs.hash(password, salt))
+ 		.then((hashedPassword) => {
+ 			console.log(hashedPassword);
+
+  		 	User.create({
+ 				username,
+ 				passwordHash: hashedPassword,
+ 			});
+ 			res.redirect('/auth/signup');
+ 		})
+ 		.then((userDoc) => console.log(userDoc))
+ 		.catch((err) => console.log(err));
+ });
+
+  router.get('/login', (req,res,next) => {
+ 	res.render('auth-views/login')
+ })
+
+  module.exports = router;
