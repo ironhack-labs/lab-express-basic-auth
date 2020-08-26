f9a08271408812a9da7a3cf1b4862455c5b3b4ec
ViktorJT <viktor.jensentorp@hyperisland.se>
2020-08-26 15:30:08 +0200

    iteration 1 done

---
+++ routes/auth.routes.js
@@ -0,0 +1,38 @@
+const { Router } = require('express');
+const router = new Router();
+const mongoose = require('mongoose');
+
+const User = require('../models/User.model');
+
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+// .get() route ==> to display the signup form to users
+
+router.get('/signup', (_, res) => res.render('auth/signup'));
+
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+// .post() route ==> to process form data
+
+router.post('/signup', (req, res, next) => {
+
+  const { username, password } = req.body;
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        username,
+        password: hashedPassword
+      });
+    })
+    .then(userFromDB => {
+      console.log(`Newly created user is: `, userFromDB);
+      res.redirect('/userProfile');
+    })
+    .catch(err => next(error))
+})
+
+module.exports = router;
