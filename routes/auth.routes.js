0a60202935cbef309dfe3b619f63f299c4fb5bbe
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-26 18:25:47 +0200

    login stuff

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -48,4 +48,30 @@ const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
     })
   });
   router.get('/userProfile', (req, res) => res.render('users/user-profile')); 
+  // .get() route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+// .post() login route ==> to process form data
+router.post('/login', (req, res, next) => {
+    const { email, password } = req.body;
+   
+    if (email === '' || password === '') {
+      res.render('auth/login', {
+        errorMessage: 'Please enter both, email and password to login.'
+      });
+      return;
+    }
+   
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+          res.render('users/user-profile', { user });
+        } else {
+          res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
+  });
 module.exports = router;
