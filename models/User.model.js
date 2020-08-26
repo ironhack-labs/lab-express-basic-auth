0f1ba91f0ad9dd8ebd4585709f24df8bdfdb8017
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-26 18:15:45 +0200

    adding extra validation

--- models/User.model.js
+++ models/User.model.js
@@ -12,6 +12,7 @@ const userSchema = new Schema(
     email: {
       type: String,
       required: [true, 'Email is required.'],
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
       unique: true,
       lowercase: true,
       trim: true
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,6 +9,10 @@ router.get('/signup', (req, res) => res.render('auth/signup'));
 router.post('/signup', (req, res) => {
     //console.log('The form data: ', req.body);
     const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+        return;
+      }
     bcryptjs
     .genSalt(saltRounds)
     .then(salt => bcryptjs.hash(password,salt))
@@ -21,8 +25,20 @@ router.post('/signup', (req, res) => {
     })
     .then(userFromDB => {
         console.log(`Newly created user is: `,userFromDB);
+        res.redirect('/userProfile');
     })
-    .catch(error => (error));
+    .catch(error => (error) => {
+        if (error instanceof mongoose.Error.ValidationError) {
+            res.status(500).render('auth/signup', { errorMessage: error.message });
+          } else if (error.code === 11000) {
+            res.status(500).render('auth/signup', {
+               errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+            });
+          } 
+        else {
+            next(error);
+        }
+    })
   });
- 
+  router.get('/userProfile', (req, res) => res.render('users/user-profile')); 
 module.exports = router;
