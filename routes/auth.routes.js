7a388d58c96b9adb2152fde019f05d5fa7a8e35a
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-26 18:42:57 +0200

    logout done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -79,4 +79,10 @@ router.post('/login', (req, res, next) => {
       })
       .catch(error => next(error));
   });
+
+  router.post('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+  });
+
 module.exports = router;
