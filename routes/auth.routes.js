a4fde56908a8d39d17d1bf22d58f62b4b0e5b186
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-26 16:49:54 +0200

    adding correctly bcrypt for new users so passwords don't appear

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,28 @@
 const { Router } = require('express');
 const router = new Router();
+const User = require('../models/User.model');
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
 // .get() route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
 // .post() route ==> to process form data
-router.post('/signup', (req, res, next) => {
-    console.log('The form data: ', req.body);
+router.post('/signup', (req, res) => {
+    //console.log('The form data: ', req.body);
+    const { username, email, password } = req.body;
+    bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password,salt))
+    .then(hashedPassword => {
+        return User.create({
+            username,
+            email,
+            passwordHash: hashedPassword
+        });
+    })
+    .then(userFromDB => {
+        console.log(`Newly created user is: `,userFromDB);
+    })
+    .catch(error => (error));
   });
  
 module.exports = router;
