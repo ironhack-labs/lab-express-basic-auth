c5366fa2e8289c0beaccb24340acb3b1204ec97a
Alex Anthopoulos <zephoxcodese@gmail.com>
2020-08-26 15:34:02 +0200

    added post route but no model imported yet

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,5 +3,8 @@ const router = new Router();
 // .get() route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
 // .post() route ==> to process form data
+router.post('/signup', (req, res, next) => {
+    console.log('The form data: ', req.body);
+  });
  
 module.exports = router;
