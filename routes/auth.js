3246bcdf9aecdcde122fd866a8f666aa8c8e13d9
Kenouly Lo Van <kenouly.lovan@gmail.com>
2020-09-10 21:16:54 +0200

    done

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,10 @@ router.get('/signup', (req, res, next) => {
 })
 
 router.post('/signup', (req, res, next) => {
-    const {username, password} = req.body
+    const {
+        username,
+        password
+    } = req.body
 
     bcrypt.hash(password, 10)
         .then(hashedPassword => {
@@ -17,12 +20,39 @@ router.post('/signup', (req, res, next) => {
                 password: hashedPassword
             })
         })
-        .then( () => {
+        .then(() => {
             res.send('User created')
         })
         .catch(e => {
             next(e)
         })
 })
+
+router.get('/login', (req, res, next) => {
+    res.render('login')
+})
+
+router.post('/login', (req, res, next) => {
+    const {
+        username,
+        password
+    } = req.body
+
+    User.findOne({
+            username
+        })
+        .then((user) => {
+            console.log(user)
+            bcrypt.compare(password, user.password)
+                .then((result) => {
+                    console.log(result)
+                })
+        })
+        .catch(e => {
+            next(e)
+        })
+})
+
+
 
 module.exports = router
