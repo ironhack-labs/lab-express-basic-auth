e8a1f6a9e0c5ffc25a161d52aeaeb0db2380e8fd
Rubem Diogo <rubemdiogo@gmail.com>
2020-09-17 11:09:46 -0400

    primeira interacao

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,37 @@
 // User model here
+const { Schema, model } = require("mongoose");
+
+// modelo de usuario em branco
+
+const userSchema = new Schema(
+  {
+    // campo de nome do usuario
+    username: {
+      type: String,
+      trim: true,
+      require: [true, "Username is required..."],
+      unique: trues,
+    },
+
+    // campo email
+    email: {
+      type: String,
+      require: [true, " Email is required..."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    //campo de senha
+    passwordHash: {
+      type: String,
+      require: [true, " Password is required"],
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
