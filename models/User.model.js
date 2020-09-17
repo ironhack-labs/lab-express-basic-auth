2be871e872be62fca28613fb971b8c015c3464b7
yordancurty <yordancurty@gmail.com>
2020-09-16 23:30:58 -0300

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,44 @@
 // User model here
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username :{
+            type: String,
+            trim: true,
+            // Sintaxe de mensagem de erro customizada pra quando a regra do Schema não for satisfeita
+            required: [true, 'Username is required.'],
+            unique: true
+        },
+
+        email : {
+            type : String,
+            required : [true, 'Email is required'],
+            unique: true,
+            lowercase : true,
+            trim: true
+        },
+
+        passwordHash : {
+            type: String,
+            required : [true, "Password is required"]
+        }
+    },
+    {
+        timestamp: true
+    }
+);
+
+module.exports = model('User', userSchema);
+
+
+
+
+
+
+
+
+
+
+
+
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require("../models/User.model")
+const saltRounds = 10
+const bcrypt = require("bcryptjs")
 
-/* GET home page */
+/* GET home page */ 
 router.get('/', (req, res, next) => res.render('index'));
+
+// GET sign up page
+router.get('/signup', (req, res) => res.render('auth/signup'))
+
+router.post('/signup', async (req, res) => {
+    console.log(req.body)
+    const {username, email, password} = req.body
+    try {
+        const salt = await bcrypt.genSalt(saltRounds)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        console.log('Hashed password => ', hashedPassword);
+
+        const result = await UserModel.create({ username, email, passwordHash: hashedPassword });
+
+        res.redirect('/');
+
+        console.log(result);
+    } catch (err) {
+        console.error(err);
+    }
+})
+
+
+
+
+
+
+
+
 
 module.exports = router;
