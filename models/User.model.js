026a1a11da48b0288e163ddd9cc24d6e45c2fc26
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-09-16 23:45:43 -0300

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,14 @@
+// const { Mongoose } = require("mongoose")
+
 // User model here
+const {Schema, model} = require("mongoose");
+
+const UserSchema = new Schema ({
+    username: {type: String, unique: true, trim: true,  required: true},
+    
+    passwordHash: {type: String, required: [true, 'Password is required.']},
+  },
+  {timestamps:true}
+  );
+
+module.exports = model('User', UserSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/User.model")
+
+
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/signup', (req, res) => res.render('../views/auth/signup'));
+
+router.post('/signup', async (req, res) => {
+    console.log(req.body);
+
+    const {username, password} = req.body;
+
+    try {
+        const salt = await bcryptjs.genSalt(saltRounds);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        
+        console.log('Hashed password => ', hashedPassword);
+
+        const result = await User.create({username, passwordHash: hashedPassword})
+
+        res.redirect('/signup');
+        console.log(result);
+
+    } catch (err) {
+        console.error(err);
+    }
+});
+
 
 module.exports = router;
