638b49c27046bee0d261d084cad0eecc1cc6fe10
Arthur Farneze <arthurfarneze@gmail.com>
2020-09-16 21:17:04 -0300

    signup done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,34 @@
 // User model here
+// const { Schema, model } = require('mongoose');
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Username is required"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    passwordHash: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// module.exports("User", userSchema);
+
+module.exports = mongoose.model("User", userSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const saltRounds = 15;
+const User = require("../models/User.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/signup", (req, res) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const result = await User.create({
+      username,
+      email,
+      passwordHash: hashedPassword,
+    });
+    console.log("Result: " + result);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
