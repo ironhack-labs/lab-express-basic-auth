88a2e8721c4c88f15794f9abae5a1b2691b97f12
Cristina Castro <cristinacastro87@gmail.com>
2020-10-26 17:34:29 +0100

    done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
-
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
@@ -29,10 +30,24 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    resave: true,
+    saveUninitialized: false
+  }));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index.routes');
 app.use('/', index);
 
+const auth =  require('./routes/auth');
+app.use('/', auth);  
+
+  
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('home'));
+
+ router.get("/secret", function (req, res, next) {
+  res.render("secret");
+}); 
 
 module.exports = router;
