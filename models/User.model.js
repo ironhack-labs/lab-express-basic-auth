cd65dd1eb0b69fa8ca2be2412bb8bbedc24dddea
Oscar M <empanaisimo@gmail.com>
2020-10-26 14:27:00 +0000

    Project Set

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,11 @@
 // User model here
+const mongoose = require('mongose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    mail: { type: String, require: true },
+    password: { type: String, require: true }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
