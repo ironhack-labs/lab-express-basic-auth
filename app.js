a4b6f8bd65cc43803c1ccf595fbeefbe43aff838
Xavier Caparros Gelabert <xcaparros@hotmail.com>
2020-10-26 17:25:16 +0100

    done

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const favicon = require('serve-favicon');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
 const logger = require('morgan');
 const path = require('path');
 
@@ -32,7 +34,20 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-const index = require('./routes/index.routes');
-app.use('/', index);
 
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    resave: true,
+    saveUninitialized: false
+  }));
+
+const index = require('./routes/index.routes');
+const auth = require('./routes/auth')
+app.use('/', index);
+app.use('/', auth);
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("home");
+});
+
+router.use(['/private', '/main'], (req, res, next) => {
+  // if hay un usuario en sesión (si está logged in)
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+});
+
+router.get("/private", function (req, res, next) {
+  res.render("private");
+});
+
+router.get("/main", function (req, res, next) {
+    res.render("main");
+  });
 
 module.exports = router;
