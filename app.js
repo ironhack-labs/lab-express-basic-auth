e8cc04f76beeff2306c54c3639381f858577791b
Alex <alexgarcia1771@outlook.com>
2020-10-26 20:00:29 +0100

    ALEXANDRE WEBBCN

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -32,7 +34,32 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    resave: true,
+    saveUninitialized: false
+  }));
+
 const index = require('./routes/index.routes');
 app.use('/', index);
+
+var authRouter = require('./routes/auth');
+app.use('/', authRouter);
+
+// app.use(session({
+//     secret: "basic-auth-secret",
+//     cookie: { maxAge: 60000 },
+//     store: new MongoStore({
+//       mongooseConnection: mongoose.connection,
+//       ttl: 24 * 60 * 60 // 1 day
+//     }),
+//     resave: true,
+//     saveUninitialized: false
+//   }));
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
+const withAuth = require('../helpers/middleware')
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+
+// router.use((req, res, next) => {
+//     // if hay un usuario en sesión (si está logged in)
+//     if (req.session.currentUser) {
+//       next();
+//     } else {
+//       res.redirect("/login");
+//     }
+//   });
+
+//   router.get("/secret", function (req, res, next) {
+//     res.render("secret");
+//   });
 module.exports = router;
