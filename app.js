a33ef4f0f6bb6d83c67ee39514cf648083a243ea
marcurto <mcurtogilabert@gmail.com>
2020-10-26 19:29:39 +0100

    done

--- app.js
+++ app.js
@@ -9,10 +9,31 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session)
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app = express();
+
+// Conectem Mongoose
+mongoose
+  .connect("mongodb://localhost/basic-auth-lab", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
+
+  
+
+ const app = express();
 
 // require database configuration
 require('./configs/db.config');
@@ -29,10 +50,28 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    resave: true,
+    saveUninitialized: false
+  }));
+  
+ 
+
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
+
 const index = require('./routes/index.routes');
+const authRouter = require('./routes/auth');
+app.use('/', authRouter);
 app.use('/', index);
+
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,37 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", function (req, res, next) {
+  res.render("home");
+});
+
+
+
+  router.use((req, res, next) => {
+    // if hay un usuario en sesión (si está logged in)
+    if (req.session.currentUser) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  });
+  
+  router.get("/main", function (req, res, next) {
+    res.render("main");
+  });
+
+
+  router.use((req, res, next) => {
+    // if hay un usuario en sesión (si está logged in)
+    if (req.session.currentUser) {
+      next();
+    } else {
+      res.redirect("/login");
+    }
+  });
+  
+  router.get("/private", function (req, res, next) {
+    res.render("private");
+  });
 
 module.exports = router;
