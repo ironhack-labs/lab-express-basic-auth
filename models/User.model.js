97d05f74a90db5e30d2e6f1f661c660fffd93a27
Ivan Rubio <ivanrubioangeles@gmail.comn>
2020-11-06 17:37:47 -0600

    created model

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,29 @@
 // User model here
+const { Schema, model } = require("mongoose")
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+module.exports = model("User", userSchema)
