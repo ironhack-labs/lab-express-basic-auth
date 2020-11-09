4d5672a9a01b93315c2bd46619af967ca2502a71
Leonardo Campuzano Magallanes <leonardocampuzano43@gmail.com>
2020-11-09 02:31:51 -0600

    LAb Express basic Auth

--- app.js
+++ app.js
@@ -1,38 +1,69 @@
-require('dotenv').config();
+require("dotenv").config()
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+const express = require("express")
+const favicon = require("serve-favicon")
+const hbs = require("hbs")
+const mongoose = require("mongoose")
+const logger = require("morgan")
+const path = require("path")
+const session = require("express-session")
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+mongoose
+  .connect("mongodb://localhost/express-auth-basic", { useNewUrlParser: true, useUnifiedTopology : true })
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err)
+  })
 
-const app = express();
+const app_name = require("./package.json").name
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+)
 
-// require database configuration
-require('./configs/db.config');
+const app = express()
 
 // Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(logger("dev"))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+  session({
+    secret: "weyuglqwe",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  })
+)
 
+//const session = require("./configs/session")
+session(app)
 // Express View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+app.use(
+  require("node-sass-middleware")({
+    src: path.join(__dirname, "public"),
+    dest: path.join(__dirname, "public"),
+    sourceMap: true
+  })
+)
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "hbs")
+app.use(express.static(path.join(__dirname, "public")))
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")))
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Lab Express Basic Auth"
 
-const index = require('./routes/index.routes');
-app.use('/', index);
+const index = require("./routes/index.routes")
+////const auth = require("./routes/auth")
 
-module.exports = app;
+app.use("/", index)
+//app.use("/", auth)
+
+module.exports = app
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,111 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt")
+const { genSaltSync } = require("bcrypt")
+const User = require("../models/User.model")
+let isLoggedIn = false
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => {
+  console.log(req.session)
+  if (req.session.count) {
+    req.session.count += 1
+  } else {
+    req.session.count = 1
+  }
+
+  console.log(req.session.count)
+  res.render("index")
+})
+
+router.get("/signup", (req, res) => {
+  res.render("auth/signup")
+})
+
+router.post("/signup", async (req, res) => {
+  // 1. Tomar la informacion del form
+  const { username, password } = req.body
+  // 2. evaluar si nos enviaron campos vacios
+  if (username === "" ||  password === "") {
+    // 2.1 si es asi, enviar un mensaje de error
+    return res.render("auth/signup", { error: "Missing fields" })
+  } else {
+    // Buscamos un user cuyo correo sea el que nos proveen desde el form
+    const user = await User.findOne({ username })
+
+    // si la busqueda tiene resultado, mostramos el mensaje de error
+    if (user) {
+      return res.render("auth/signup", { error: "something went wrong" })
+    }
+    // 3. hashear la contrase~a
+    const salt = bcrypt.genSaltSync(12)
+    const hashpwd = bcrypt.hashSync(password, salt)
+    // 3.1 Si nos dieron  la informacion correcta, podemos guardar al usuario en la db
+    await User.create({
+      username,
+      password: hashpwd
+    })
+    // 5. responder al usuario de alguna forma (redirect('/profile'))
+    res.redirect("/profile")
+  }
+})
+
+router.get("/login", (req, res) => {
+    isLoggedIn = true
+  res.render("auth/login")
+})
+
+router.post("/login", async (req, res) => {
+  // res.send(req.body)
+  // 1. tomamos la informacion del formulario
+  const { username, password } = req.body
+  // 2. evaluar si la informacion esta completa
+  if (username === "" || password === "") {
+    res.render("auth/login", { error: "Missing fields" })
+  }
+  // 3. buscamos si hay un usuario con el correo que nos enviaron
+  const user = await User.findOne({ username })
+  // 3.1 si no hay usuario notificar el error
+  if (!user) {
+    res.render("auth/login", { error: "something went wrong" })
+  }
+
+  // 4. si existe el usuario en la base de datos, comparamos la contrase~a de ese usuario, con la que llego del form
+  if (bcrypt.compareSync(password, user.password)) {
+    // 4.1 si coinciden renderizamos profile con el usuario
+    // res.render("auth/profile", user)
+    delete user.password
+    req.session.currentUser = user
+    res.redirect("/profile")
+  } else {
+    // 4.2 si no coinciden, hacemos render del form con el error
+    res.render("auth/login", { error: "something went wrong" })
+  }
+})
+
+router.get("/profile", (req, res) => {
+  res.render("auth/profile", { user: req.session.currentUser })
+})
+
+router.get("/main", (req, res) => {
+    if(isLoggedIn == true){
+        res.render("auth/main", { user: req.session.currentUser })
+    }else{
+        res.render('not-available')
+    }
+  
+})
+router.get("/private", (req, res) => {
+   if(isLoggedIn == true){
+        res.render("auth/private", { user: req.session.currentUser })
+    }else{
+        res.render("not-available")
+    }
+})
+
+router.get("/logout", (req, res) => {
+  req.session.destroy()
+  res.redirect("/")
+})
 
 module.exports = router;
