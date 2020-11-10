35ff66f406ab0656669b9408b003cdb92684bd8a
phil-legal-tech <philtu1287@gmail.com>
2020-11-10 22:04:05 +0100

    iteration 1 done

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,15 @@
 // User model here
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    username: { type: String, required: true, unigue: true },
+    passwordHash: { type: String, required: true },
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('User', UserSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User.model');
+
+
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;                  //delay preventing brute force attacs - it tooks 10 sec to get a response
+
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/signUp', (req, res, next) => {
+    res.render('signUp');
+});
+
+router.post('/signUp', (req, res, next) => {
+    console.log(req.body);
+
+    const salt = bcrypt.genSaltSync(10);
+    const pwHash = bcrypt.hashSync(req.body.password, salt);
+
+    User.create({ username: req.body.username, passwordHash: pwHash })
+
+        .then(() => res.redirect('signUp'))
+
+});
+
 module.exports = router;
+
