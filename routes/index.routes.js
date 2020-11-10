c091f7480fd04c98cd4eded154346da73ac711f8
Alicja Slupska <ala.slupska@gmail.com>
2020-11-10 22:10:09 +0100

    Iteration 0 and 1 done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+// Iteration 1
+const User = require('../models/User.model');
+const bcrypt = require('bcryptjs');
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+// Iteration 1
+router.get('/signup', (req, res, next) => {
+    res.render('signup');
+});
+
+
+router.post('/signup', (req, res, next) => {
+
+    const salt = bcrypt.genSaltSync(10);
+    const pwHash = bcrypt.hashSync(req.body.password, salt);
+
+    User.create({ username: req.body.username, passwordHash: pwHash }).then(() => {
+        res.redirect('/')
+    });
+
+});
+
+
+// Iteration 2
 
 module.exports = router;
