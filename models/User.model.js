e2a72d8d5ff935d4ebc8a602ce2960297c039d57
Ute Wiemer <upwiemer@gmail.com>
2020-11-10 21:32:35 +0100

    0&1

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,15 @@
 // User model here
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: String,
+    password: String,
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('User', userSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcryptjs')
+
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+
+
+router.get('/signup', (req, res, next) => {
+    res.render('signup')
+})
+
+router.post('/signup', (req, res, next) => {
+    console.log(req.body)
+    const salt = bcrypt.genSaltSync(10);
+    const pwHash = bcrypt.hashSync(req.body.password, salt);
+    User.create({ username: req.body.username, password: pwHash }).then(() => {
+        res.redirect('/')
+    })
+});
 
 module.exports = router;
