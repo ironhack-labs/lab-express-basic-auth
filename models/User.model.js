6ea8d7a1b68913468298369fc2d8fefb6f022ece
Rich Alossi <alossix@gmail.com>
2020-11-11 12:11:06 +0100

    Iteration 1 initial commit

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,23 @@
-// User model here
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required."],
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: true,
+    trim: true,
+  },
+  passwordHash: {
+    type: String,
+    required: [true, "Password is required."],
+  },
+  timestamps: true,
+});
+
+module.exports = model("User", userSchema);
