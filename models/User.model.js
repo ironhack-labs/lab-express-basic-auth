bbdfeebb55ea74a69d9f00b95eebc0e0bde15a9e
Abbie <abbie@summers.gg>
2020-11-11 21:25:52 +0100

    Initial commit. Iteration 1 complete.

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,17 @@
 // User model here
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: [true, 'Please add username'],
+            unique: true
+        },
+        passwordHash: {
+            type: String,
+            required: [true, 'Password is required.']
+        }
+    }
+)
+module.exports = model("User", userSchema)
