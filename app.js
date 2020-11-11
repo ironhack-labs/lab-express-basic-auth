7f61f08c1cb30bfb05593fe3d4520d1cbc69cba3
sophiepa <sophiepa@hotmail.com>
2020-11-11 12:59:06 +0100

    iteration 1 done

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+
 // require database configuration
 require('./configs/db.config');
 
@@ -33,6 +34,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index.routes');
+const authRouter = require('./routes/auth.routes');
 app.use('/', index);
+app.use('/', authRouter);
 
 module.exports = app;
