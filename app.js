a970b5538f5855550b120dff690bd1754c8e38ef
Nina van den Braak <ninavandenbraak@gmail.com>
2020-11-11 12:59:04 +0100

    update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
+const bcrypt = require("bcrypt");
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,12 +10,31 @@ const saltRounds = 10;
 
 router.get('/sign-up', (req, res, next) => res.render('auth/signup'));
 
-// router.post('/sign-up', (res, req, next) => {
-//     const {
-//         username,
-//         passwordHash
-//     } = req.body
-// })
+router.post('/sign-up', (req, res, next) => {
+    const {
+        username,
+        password
+    } = req.body;
 
+    bcrypt
+        .genSalt(saltRounds)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hashedPassword => {
+            return User.create({
+                    username,
+                    passwordHash: hashedPassword
+                })
+                .then(newUserDB => {
+                    console.log(`Newly created user: ${newUserDB}`);
+                    res.redirect('/profile');
+                })
+                .catch(error => next(error));
+        })
+        .catch(error => next(error));
+});
+
+router.get('/profile', (req, res, next) => {
+    res.render('users/profile')
+});
 
 module.exports = router;
