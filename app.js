eb8f9b87fdaf8e7875f4bbf52f32b7dae1cd6fb2
phil-legal-tech <philtu1287@gmail.com>
2020-11-12 22:04:29 +0100

    done

--- app.js
+++ app.js
@@ -1,38 +1,73 @@
 require('dotenv').config();
-
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const path = require('path');
 const express = require('express');
+const createError = require('http-errors');
+const logger = require('morgan');
 const favicon = require('serve-favicon');
+
+const cookieParser = require('cookie-parser');
 const hbs = require('hbs');
 const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+
+// we added
+const session = require('express-session');
+
+// we added
+const MongoStore = require('connect-mongo')(session);
+
+// Set up the database
+require('./configs/db.config');
+
+// Routers
+const indexRouter = require('./routes/index.routes');
+
+const app = express();
+
+// we added
+app.use(
+  session({
+    secret: 'doesnt-matter',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 * 60 }, // 1 hour
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+    })
+  })
+);
+
+// Express View engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
-const app = express();
+// Routes middleware
+app.use('/', indexRouter);
 
-// require database configuration
-require('./configs/db.config');
+// Catch missing routes and forward to error handler
+app.use((req, res, next) => next(createError(404)));
 
-// Middleware Setup
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Catch all error handler
+app.use((error, req, res) => {
+  // Set error information, with stack only available in development
+  res.locals.message = error.message;
+  res.locals.error = req.app.get('env') === 'development' ? error : {};
 
-// Express View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-const index = require('./routes/index.routes');
-app.use('/', index);
+  // render the error page
+  res.status(error.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,8 +8,17 @@ const saltRounds = 10;                  //delay preventing brute force attacs - 
 
 
 
+
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/', (req, res, next) => {
+    if (req.session.user) {
+        res.render('index', { username: req.session.user.username })
+    } else {
+        res.render('index')
+    }
+});
 
 router.get('/signUp', (req, res, next) => {
     res.render('signUp');
@@ -25,6 +34,61 @@ router.post('/signUp', (req, res, next) => {
 
         .then(() => res.redirect('signUp'))
 
+});
+
+router.get('/login', (req, res) => res.render('login'));
+
+
+router.post('/login', (req, res) => {
+    console.log('SESSION =====> ', req.session); // req.session === {}
+
+    // find the user by their username
+    User.findOne({ username: req.body.username }).then((user) => {
+
+        if (!user) {
+            // this user does not exist
+            res.render('login', { errorMessage: 'username does not exist' })
+        } else {
+
+            // check if the password is correct
+            if (bcrypt.compareSync(req.body.passwordHash, user.passwordHash)) {
+                req.session.user = user
+                // res.send('password correct - logged in')
+                res.redirect('/protected')
+            } else {
+                res.render('login', { errorMessage: 'password wrong' })
+            }
+
+        }
+
+    })
+
+})
+
+router.post('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+
+router.get('/main', (req, res) => {
+    res.render('main')
+});
+
+
+router.get('/protected', (req, res) => {
+
+    if (!req.session.user) {
+        res.redirect('/login')
+    } else {
+        res.render('main')
+        // res.send('main')
+        
+    }
+
+})
+
+router.get('/private', (req, res) => {
+    res.render('private')
 });
 
 module.exports = router;
