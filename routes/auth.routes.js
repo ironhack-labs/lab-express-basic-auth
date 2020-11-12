d040963bd458065595913c9800a07df56acde701
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-12 12:44:16 +0100

    latest changes to log in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -49,6 +49,30 @@ router.post("/signup", (req, res, next) => {
 
 router.get('/login', (req, res) => res.render('auth/login'));
 
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+ 
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({username })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other username.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render('users/user-profile', { user });
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
 router.get("/userProfile", (req, res) => res.render("users/user-profile"));
 
 module.exports = router;
