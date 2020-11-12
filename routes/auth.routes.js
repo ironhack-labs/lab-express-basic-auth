4419f07f9ba814f0f64dab9b973a18ef4ac98dad
Emma Northridge <emmanorthridge1@gmail.com>
2020-11-12 11:25:24 +0100

    Added validation to signup

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,6 +10,11 @@ const saltRounds = 10;
 router.post("/signup", (req, res, next) => {
   const { username, password } = req.body;
 
+  if (!username || !password) {
+    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username and password.' });
+    return;
+  }
+
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
@@ -21,7 +26,7 @@ router.post("/signup", (req, res, next) => {
     })
     .then((userFromDB) => {
       console.log("Newly created user is: ", userFromDB);
-      res.redirect("/");
+      res.redirect("/userProfile");
     })
     .catch((error) => next(error));
 });
