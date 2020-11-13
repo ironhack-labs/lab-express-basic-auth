9f224691c8a8eb5d715bcbe8ebcea7c52b8a5b05
Christian Ares <christianaresvillan@gmail.com>
2020-11-13 18:50:53 +0100

    Iteration 2 Done

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
@@ -30,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Hola';
 
 const index = require('./routes/index.routes');
 app.use('/', index);
