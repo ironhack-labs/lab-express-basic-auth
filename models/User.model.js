887a9ebd94501f0ea0ec2643df8a4da04de26b25
Jesus Rua <jesus.rua.pareja@gmail.com>
2020-11-13 18:50:36 +0100

    ITERATION 1 & 2

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,14 @@
 // User model here
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = new Schema ({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+})
+
+//Crear un juego a partir del modelo primer argumento nombre de la coleccion que va a crear y segundo el schema
+const User = mongoose.model("User", userSchema)
+
+//Para poder usarlo en app.js
+module.exports = User
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,92 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt       = require("bcrypt")
+const session    = require("express-session")
+const mongoose     = require('mongoose');
+const MongoStore = require("connect-mongo")(session)
+
+const User = require("../models/User.model.js")
+
+//Configuracion de cookies
+
+router.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 
+    })
+  }));
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
 module.exports = router;
+
+router.get("/sign-up", (req, res, next)=>{
+    res.render("signup")
+})
+
+router.post("/sign-up",(req, res, next)=>{
+
+    
+    const {username, password} = req.body
+
+    User.findOne({username: username})
+    .then((result)=>{
+        if(!result) {
+            bcrypt.genSalt(10)
+            .then((salt)=>{
+                bcrypt.hash(password, salt)
+                .then((hashedPassword)=>{
+        
+                    let hashedUser = {username: username, password: ""}
+                    hashedUser.password = hashedPassword
+        
+                    User.create(hashedUser)
+                    .then((result)=>{
+                        res.redirect("/")
+                    })
+                })
+            })
+            .catch((err)=>{
+                console.log(err)
+                res.render(err)
+            })
+        } else {
+            res.render("login", {errorMessage: "Este usuario ya existe. ¿Querías hacer Log In?"})
+        }
+    })
+})
+
+router.get("/log-in", (req, res, next)=>{
+    res.render("login")
+})
+
+router.post("/log-in", (req, res, next)=>{
+    //console.log(req.body)
+    const {username, password} = req.body
+
+    User.findOne({username: username})
+    .then((result)=>{
+        if(!result) {
+            console.log("El usuario no existe")
+            res.render("login", {errorMessage: "Este usuario no existe."} )
+        } else {
+            //Comparar la contraseña para ver si es correcta con BCRYPT
+            bcrypt.compare(password, result.password)
+            .then((resultFromBcrypt)=>{
+                if(resultFromBcrypt) {
+                    req.session.currentUser = username
+                    console.log(req.session)
+                    res.redirect("/")
+                    //req.session.destroy(logout)
+                } else {
+                    res.render("login", {errorMessage: "Contraseña incorrecta. Por favor vuelva a intentarlo"})
+                }
+            })
+        }
+    })
+})
