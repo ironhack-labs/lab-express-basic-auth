244669f10af36e3a5ef49e04dfd738e6d676ada3
albacardona <alba.cardonajimenez@gmail.com>
2020-11-13 19:05:25 +0100

    done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,81 @@
-const express = require('express');
-const router = express.Router();
+const express    = require('express');
+const router     = express.Router();
+const bcrypt     = require('bcrypt')
+const session    = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const mongoose   = require ('mongoose')
+
+const User = require('../models/User.model')
+
+router.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+}));
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/sign-up', (req, res, next) => {
+    res.render('signup')
+})
+
+router.post('/sign-up', (req, res, next) => {
+
+    const {username, password} = req.body
+
+    User.findOne({username: username})
+    .then((result) => {
+        if(!result) {
+            bcrypt.genSalt(10)
+            .then((salt) => {
+                bcrypt.hash(password, salt)
+                .then((hashedPassword) => {
+                    const hashedUser = {username: username, password: hashedPassword} //se puede poner {email, password: ''} porque el key y el value son iguales
+                    User.create(hashedUser)
+                    .then(() => {
+                        res.redirect('/')
+                    })
+                })
+            })
+            .catch((error) => {
+                res.send(error)
+            })
+        
+        } else {
+            res.render('login', {errorMessage: 'This user already exists.'})
+        }
+    })
+})
+
+router.get('/log-in', (req, res, next) => {
+    res.render('login')
+})
+
+router.post('/log-in', (req, res, next) => {
+    const {username, password} = req.body
+
+    User.findOne({username: username})
+    .then ((result) => {
+        if(!result) {
+            res.render('login', {errorMessage: `This user doesn't exist`})
+        } else {
+            bcrypt.compare(password, result.password)
+            .then((resultFromBcrypt) => {
+                if(resultFromBcrypt) {
+                    req.session.currentUser = username
+                    res.redirect('/')
+                } else {
+                    res.render('login', {errorMessage: 'Wrong password, please try again.'})
+                }
+            })
+        }
+    })
+})
 
 module.exports = router;
