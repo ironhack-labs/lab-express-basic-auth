6ae33f867b1402ef58ffd9187c04b03623422073
gitferreira <iniferre96@gmail.com>
2020-11-16 19:54:57 +0100

    1st Commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+//Constants
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
@@ -8,10 +8,10 @@ const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
-
+const session    = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 const app = express();
 
 // require database configuration
@@ -23,6 +23,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//Por qué no recuerda la sesión???
+app.use(session({
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 999990000 },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    })
+  }));
+
 // Express View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -30,9 +40,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'The Authentificator';
 
 const index = require('./routes/index.routes');
 app.use('/', index);
+
+
+
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,144 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require("bcrypt")
+const bcryptSalt = 10
+const mongoose = require("mongoose")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
+const User = require("../models/User.model")
 
+router.use(session(
+	{
+    secret: "basic-auth-secret",
+    cookie: { maxAge: 60000 },
+    saveUninitialized: true,
+    resave: true,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 
+    })
+  }));
+
+  
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/sign-up", (req, res, next)=>{
+	res.render("signUp")
+})
+
+router.post("/sign-up", (req, res, next)=>{
+	const username = req.body.username;
+    const password = req.body.password;
+    const salt     = bcrypt.genSaltSync(bcryptSalt);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    if (username === "" || password === "") {
+        res.render("signUp", {
+          errorMessage: "Type in both email and password"
+        });
+        return;
+      }   
+
+    User.findOne({ "username": username })
+      .then(user=>{
+
+        if (user !== null) {
+
+            res.render("signUp", {
+
+              errorMessage: "Someone was faster and got that username before!"
+
+            });
+            return;
+          }
+
+          const salt     = bcrypt.genSaltSync(bcryptSalt);
+          const hashedPassword = bcrypt.hashSync(password, salt);
+
+          User.create({ username, password: hashedPassword})
+          
+          .then(() => {
+
+            res.redirect("/");
+
+          })
+          .catch(error => {
+
+            console.log(err);
+            res.render(err)
+          })
+      })
+      .catch(error => {
+        next(error);
+      })    
+});
+
+
+router.get("/log-in", (req, res, next) => {
+    res.render("logIn");
+  });
+
+router.post("/log-in", (req, res, next) => {
+
+  const userUsername = req.body.username;
+  const userPassword = req.body.password;
+
+  
+  if (userUsername === "" || userPassword === "") {
+
+    res.render("logIn", {
+      errorMessage: "Type in both email and password"
+    });
+    return;
+  }
+
+  User.findOne({ "username": userUsername })
+  .then(user => {
+
+      if (!user) {
+        res.render("logIn", {
+          errorMessage: "Nope, wrong username"
+        });
+        return;
+      }
+      if (bcrypt.compareSync(userPassword, user.password)) {
+        
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("logIn", {
+          errorMessage: "Nope, try again your password"
+        });
+      }
+  })
+  .catch(error => {
+    next(error);
+  })
+});
+
+
+router.use((req, res, next) => {
+
+    if (req.session.currentUser) { 
+
+    next()
+    
+	  console.log(session.currentUser) 
+    } else {                          
+      res.redirect("/log-in");         
+    }                                 
+  });                               
+     
+  router.get("/private", (req, res, next) => {
+    res.render("private");
+  });
+
+  router.get("/main", (req, res, next) => {
+    res.render("main");
+  });
+
+
+
 
 module.exports = router;
