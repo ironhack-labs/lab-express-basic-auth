a058c6f65e677e0f6e9c5a15a3962a76fff2b206
RaquelRB <raquelrobles95@gmail.com>
2020-11-16 14:50:27 +0100

    Done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -10,14 +10,14 @@ const User = require('../models/User.model.js')
 
 router.use(session({
     secret: "basic-auth-secret",
-    cookie: { maxAge: 60000 },
+    // cookie: { maxAge: 60000 },
     saveUninitialized: true,
     resave: true,
     store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60 // 1 day
+        mongooseConnection: mongoose.connection,
+        ttl: 24 * 60 * 60 // 1 day
     })
-  }));
+}));
 
 
 /* GET home page */
@@ -38,10 +38,9 @@ router.post('/sign-up', (req, res, next) => {
                     .then((salt) => {
                         bcrypt.hash(password, salt)
                             .then((hashedPassword) => {
-                                const hashedUser = { username: username, password: hashedPassword } //si name=value en form podriamos poner solo "email"
-
+                                const hashedUser = { username: username, password: hashedPassword }
                                 User.create(hashedUser)
-                                    .then((result) => {
+                                    .then(() => {
                                         res.redirect('/')
                                     })
                             })
@@ -72,16 +71,37 @@ router.post('/log-in', (req, res, next) => {
             } else {
                 bcrypt.compare(password, result.password)
                     .then((resultFromBcrypt) => {
-                        if(resultFromBcrypt){
+                        if (resultFromBcrypt) {
                             req.session.currentUser = username
                             console.log(req.session)
                             res.redirect("/")
-                        }else{
-                            res.render('logIn', {errorMessage: 'Contraseña incorrecta. Por favor, vuelva a intentarlo'})
+                        } else {
+                            res.render('logIn', { errorMessage: 'Contraseña incorrecta. Por favor, vuelva a intentarlo' })
                         }
                     })
             }
         })
+})
+
+router.get('/log-out', (req, res, next) => {
+    req.session.destroy()
+    res.redirect('/')
+})
+
+router.use((req, res, next) => {
+    if (req.session.currentUser) {
+        next();
+    } else {
+        res.redirect('/log-in')
+    }
+})
+
+router.get('/main', (req, res, next) => {
+    res.render('main')
+})
+
+router.get('/private', (req, res, next) => {
+    res.render('private')
 })
 
 module.exports = router;
