6931cbb040fd4d456fd92d1058ab9b0f0447496d
Adrian Casas <adrian.casas.fernandez@gmail.com>
2020-11-16 14:09:58 +0000

    code cleanup

--- routes/auth.js
+++ routes/auth.js
@@ -95,40 +95,6 @@ router.post("/login", (req, res, next) => {
   })
 });
 
-router.post("/login", (req, res, next) => {
-  const theUsername = req.body.username;
-  const thePassword = req.body.password;
- 
-  if (theUsername === "" || thePassword === "") {
-    res.render("auth/login", {
-      errorMessage: "Please enter both, username and password to sign up."
-    });
-    return;
-  }
- 
-  User.findOne({ "username": theUsername })
-  .then(user => {
-      if (!user) {
-        res.render("auth/login", {
-          errorMessage: "The username doesn't exist."
-        });
-        return;
-      }
-      if (bcrypt.compareSync(thePassword, user.password)) {
-        // Save the login in the session!
-        req.session.currentUser = user;
-        res.redirect("/");
-      } else {
-        res.render("auth/login", {
-          errorMessage: "Incorrect password"
-        });
-      }
-  })
-  .catch(error => {
-    next(error);
-  })
-});
-
 
 //prevent the users who are not logged from accessing main and private pages
 router.use((req, res, next) => {
