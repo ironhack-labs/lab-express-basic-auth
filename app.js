7420265ff25c78d83d146e313c5c169cfebf7212
Sergio <srosduran@gmail.com>
2020-11-16 14:47:16 +0100

    it3 done

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(cookieParser());
 // Express View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + "/views/partials");
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -79,4 +79,25 @@ router.post('/log-in', (req, res, next)=>{
         })
 });
 
+router.get('/log-out', (req, res, next)=>{
+    req.session.destroy();
+    res.redirect('/');
+})
+
+router.use((req, res, next) => {
+    if (req.session.currentUser) { // <== if there's user in the session (user is logged in)
+      next(); // ==> go to the next route ---
+    } else {                          
+      res.redirect("/log-in");         
+    }                                 
+});
+
+router.get('/main', (req, res, next)=>{
+    res.render('main');
+});
+
+router.get('/private', (req, res, next)=>{
+    res.render('private');
+});
+
 module.exports = router;
