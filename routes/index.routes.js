4197d0ec2de7dd90ed75e493abb739cf710e41ef
Thrashskin <jmontegonz@gmail.com>
2020-11-30 07:53:47 +0100

    Iteration 3 complete

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res, next) => res.render('home'));
+
+router.get('/main', (req, res, next) => {
+    let activeUser = false;
+    
+    if (req.session.currentUser){ 
+        activeUser = true;
+    }
+    res.render('main', {activeUser});
+});
+
+router.get('/private', (req, res, next) => {
+    let activeUser = false;
+    
+    if (req.session.currentUser){ 
+        activeUser = true;
+    }
+    res.render('private', {activeUser});
+});
+
+router.get('/private', (req, res, next) => res.render('private'));
 
 module.exports = router;
