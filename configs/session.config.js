ada90ecba0cc12715714e73b444bed1eaa727237
ThSchweers <th.schweers@googlemail.com>
2020-12-03 18:18:11 +0100

    Iteration 1-done, iteration 2 more broken than before, need to go through all steps a second time

---
+++ configs/session.config.js
@@ -0,0 +1,25 @@
+// configs/session.config.js
+
+// require session
+const session = require('express-session');
+
+// ADDED: require mongostore
+const MongoStore = require('connect-mongo')(session);
+
+// ADDED: require mongoose
+const mongoose = require('mongoose');
+
+module.exports = app => {
+  app.use(
+    session({
+      secret: process.env.SESS_SECRET,
+      resave: false,
+      saveUninitialized: true,
+      cookie: { maxAge: 60000 },
+      store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+      })
+    })
+  );
+};
