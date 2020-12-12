2429024bc6df03873c0f8315203f1337cb524ede
ayline <ayline1695@hotmail.com>
2020-12-12 13:19:48 +0100

    I2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,10 +5,9 @@ const saltRounds = 10;
 
 const User = require('../models/User.model')
 
-// .get() route ==> to display the signup form to users
+
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
-// .post() route ==> to process form data
 router.post('/signup', (req, res, next) => {
     const { username, email, password } = req.body;
    
@@ -17,12 +16,8 @@ router.post('/signup', (req, res, next) => {
       .then(salt => bcryptjs.hash(password, salt))
       .then(hashedPassword => {
         return User.create({
-          // username: username
           username,
           email,
-          // passwordHash => this is the key from the User model
-          //     ^
-          //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
           passwordHash: hashedPassword
         });
       })
@@ -32,28 +27,47 @@ router.post('/signup', (req, res, next) => {
     })
       .catch(error => next(error));
   });
-  router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
 // login
 router.get('/login', (req, res) => res.render('auth/login'));
-router.post("/login", async (req, res) => {
-    try {
-        const { password, email } = req.body;
-        const hasMissingCredential = !password || !email;
-    if (hasMissingCredential) {
-      return res.send("credentials missing");
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+    const { email, password } = req.body;
+   
+    if (email === '' || password === '') {
+      res.render('auth/login', {
+        errorMessage: 'Please enter both, email and password to login.'
+      });
+      return;
     }
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.send("user does not exist");
-      }
-      const verifyPassword = await bcryptjs.compare(password, user.hashPassword);
-      if (!verifyPassword) {
-        return res.send("wrong credentials");
-      }
-      return res.send("login succesfull");
-    } catch (err) {
-      console.log(err);
-    }
-  })
+   
+    User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+          //res.render('users/user-profile', { user });
+          req.session.currentUser = user;
+          res.redirect('/userProfile');
+        } else {
+          res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
+  });
+
+ // userprofile
+ router.get('/userProfile', (req,res) =>{
+   res.render('users/user-profile', { userInSession: req.session.currentUser})
+ })
+
+ //logout
+ router.post('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+
 
   module.exports = router;
