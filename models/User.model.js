673bd3e62020457fa334512d97d885e038f41bdf
carmen valencia <carmenpilar1990@gmail.com>
2020-12-12 14:40:46 +0100

    iteracion2

--- models/User.model.js
+++ models/User.model.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require('mongoose');
  
+const emailRegex = /^\S+@\S+\.\S+$/;
+
 const userSchema = new Schema(
   {
     username: {
@@ -13,7 +15,8 @@ const userSchema = new Schema(
       required: [true, 'Email is required.'],
       unique: true,
       lowercase: true,
-      trim: true
+      trim: true,
+      match: [emailRegex, "Please use a valid email address"],
     },
     passwordHash: {
       type: String,
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -12,7 +12,13 @@ router.get('/signup', (req, res) => res.render('auth/signup'));
 
 router.post('/signup', (req, res, next) => {
     const { username, email, password } = req.body;
-    
+
+    if (!username || !email || !password) {
+        res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+        return;
+      }
+
+
     bcryptjs
       .genSalt(saltRounds)
       .then(salt => bcryptjs.hash(password, salt))
@@ -29,7 +35,44 @@ router.post('/signup', (req, res, next) => {
     })
     .catch(error => next(error));
 });
-router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+
+//login
+router.get('/login', (req, res) => res.render('auth/login'));
+
+
+router.post('/login', (req, res, next) => {
+    console.log('SESSION =====> ', req.session);
+    const { email, password } = req.body;
+
+    if (email === '' || password === '') {
+        res.render('auth/login', {
+          errorMessage: 'Please enter both, email and password to login.'
+        });
+        return;
+      }
+      User.findOne({ email })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+          req.session.currentUser = user;
+            res.redirect ('/userProfile');
+        } else {
+          res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
+  });
+
+  
+  router.get('/userProfile', (req, res) => {
+      console.log(req.session.currentUser)
+    res.render('users/user-profile', { userInSession: req.session.currentUser });
+  });
+
+
 module.exports = router;
 
 
