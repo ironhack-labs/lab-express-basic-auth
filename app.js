74d002dbc828d2f6a22bdd9aecb1cd921b2ed77c
Aleksandra Bošković <sandraboskovic@hotmail.com>
2021-01-18 23:09:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const favicon = require('serve-favicon');
@@ -19,8 +18,8 @@ require('./configs/db.config');
 
 // Middleware Setup
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
