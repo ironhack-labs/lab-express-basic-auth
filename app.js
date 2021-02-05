ecc5397cb6a5424f0401ce0b703f29a3004413b2
Discordante <diegocerratos@gmail.com>
2021-02-05 09:47:40 +0100

    Iteration-0 done

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'Express - Basic-Authentication';
 
 const index = require('./routes/index.routes');
 app.use('/', index);
--- configs/db.config.js
+++ configs/db.config.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 mongoose
-  .connect('mongodb://localhost/express-basic-auth-dev', {
+  .connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then(x =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
   .catch(err => console.error('Error connecting to mongo', err));
+
+  process.on('SIGINT', () => {
+    mongoose.connection.close()
+      .then(() => console.log('Mongoose default connection disconnected through app termination'))
+      .catch(error => console.log('Error disconnecting from the database', error))
+      .finally(() => process.exit());
+}) 
