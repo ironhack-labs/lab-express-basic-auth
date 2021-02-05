6f8fbbd5e90f0debc6a08687fa0302ef6dae8f21
Barbara Diaz <barbarad91@gmail.com>
2021-02-05 16:08:54 +0100

    User model created

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,22 @@
 // User model here
+
+const moongose = require("mongoose");
+
+const userSchema = new moongose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  { timestamp: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
