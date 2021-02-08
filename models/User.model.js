e89e4b19b0a803161915b7ebca9ac951c83ac697
rgarsan <rgarsan84@gmail.com>
2021-02-08 21:53:39 +0100

    iteration 2

--- models/User.model.js
+++ models/User.model.js
@@ -1,43 +1,38 @@
-// User model here
-const mongoose = require("mongoose")
-const { Schema, model } = require('mongoose');
-const bcrypt = require ('bcrypt')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const EMAIL_PATTERN = /^(([^<>()[\]\\.,;:\s@']+(\.[^<>()[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const PASSWORD_PATTERN = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
-
-const SALT_ROUNDS = 10
- 
-const userSchema = new Schema(
-  {
+const SALT_ROUNDS = 11
+const userSchema = new mongoose.Schema({
     email: {
-      type: String,
-      trim: true,
-      required: [true, 'email is required.'],
-      unique: true,
-      lowercase:true,
-      match: [EMAIL_PATTERN,'Email invalid']
+        type: String,
+        required: 'Es necesario que introduzcas un correo electrónico',
+        unique: true,
+        lowercase: true,
+        trim: true
     },
-   
     password: {
-      type: String,
-      required: [true, 'Password is required.'],
-      match :[PASSWORD_PATTERN,'Your password must contain at least: 1 uppercase, 1 lowercase, 1 number and 8 characters']
-    },
-  });
-
-  userSchema.pre('save', function (next) {
-    const user = this
-    if (user.isModified('password')) {
-        bcrypt.hash(user.password, SALT_ROUNDS)
-            .then(hashPassword => {
-                user.password = hashPassword
+        type: String,
+        required: 'La contraseña es requerida',
+        unique: true,
+    }
+})
+userSchema.pre('save', function(next) {
+    //user es igual a this
+    if (this.isModified('password')) {
+        bcrypt.hash(this.password, SALT_ROUNDS)
+            .then(hash => {
+                this.password = hash
                 next()
-        })
+            })
     } else {
         next()
     }
 })
+const User = mongoose.model('User', userSchema)
+module.exports = User;
 
-const User = mongoose.model("User", userSchema)
- 
-module.exports = model('User', userSchema);
+
+
+
+
