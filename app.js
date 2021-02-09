a16979883d5091b0663e46de8b2fb839fdf691fc
Eduardo García <edugarciasb16@gmail.com>
2021-02-09 22:11:03 +0100

    Iteration 1 done

--- app.js
+++ app.js
@@ -29,9 +29,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+/*app.locals.title = 'Express - Generated with IronGenerator';*/
+app.locals.title = 'Express -- Basic Auth';
 
 const index = require('./routes/index.routes');
+const auth = require('./routes/auth.routes')
 app.use('/', index);
+app.use('/', auth)
 
 module.exports = app;
--- configs/db.config.js
+++ configs/db.config.js
@@ -8,3 +8,11 @@ mongoose
   })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
+
+process.on('SIGINT', () => {
+  mongoose.connection
+    .close()
+    .then(() => console.log("Successfully disconnected from the DB"))
+    .catch((e) => console.log("Error disconnected from the DB", e))
+    .finally(() => process.exit())
+})
