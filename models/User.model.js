ce71dbcc077f02ca2ee595b10ad8a16a8b02246a
Mati Weber <mati.weber@gmail.com>
2021-02-09 15:51:06 -0300

    finish with bonus

--- models/User.model.js
+++ models/User.model.js
@@ -1,20 +1,11 @@
-// User model here
-const mongoose = require ("mongoose");
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema; 
 
-const userSchema = new Schema(
-    {
-        username: {type: String, required:true, unique:true},
-        password: String
-    },
-    {
-        timestamps: {
-            createdAt: "created_at",
-            updatedAt: "updated_at"
-        }
-    }
-);
+const userInfo = new Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true}
+})
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userInfo)
 
-module.exports = User;
+module.exports = User; 
