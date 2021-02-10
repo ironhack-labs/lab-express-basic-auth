c98677fecb490ce3fd36935915f5b821549d9b45
orianemgn <mgnoriane@gmail.com>
2021-02-10 14:47:19 +0100

    Iteration 1 not working

--- app.js
+++ app.js
@@ -34,4 +34,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index.routes');
 app.use('/', index);
 
+// go to the signup
+//const auth = require('./routes/auth'); 
+//app.use('/', auth); 
+
 module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const User = require ('../models/User.model');
+const bcrypt = require('bcrypt');
+
+//sigup 
+router.get("/signup", (req, res, next) => {
+    res.render("signup");
+});
+
+// the signup form post to this route
+router.post("/signup", (req, res) => {
+    //get username and password
+    const {username, password} = req.body; 
+    if (password.length <8) {
+        res.render('signup', {message: 'Your password has to be 8 chars min'});
+        return
+    }
+    if (username === ''){
+        res.render('signup', {message: 'Your usernama cannot be empty'});
+        return
+    }
+    //check if the username already exists 
+    User.findOne({username: username})
+    .the(userFromDB =>{
+        if (userFromDB !== null) {
+            res.render(signup, {message: 'Username already taken'});
+        } else {
+            // all validation passed - > we can create a new user in the database with a hashed password
+            // create salt and hash
+            const salt = bcrypt.genSaltSync();
+            const hash = bcrypt.hashSync(password, salt);
+            //create tthe user in the DB 
+            User.create({username: username, password: hash})
+            .then(userFromDB =>{
+                //then redirect to login page 
+                res.redirect('/'); 
+            })
+        }
+    })
+    .catch(err =>{
+        console.log(err);
+    })
+})
+
+
+module.exports = router; 
