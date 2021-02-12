a58921e65237bd1618b406675e7f0637c48c77d2
Hugo Copca <copcal79@gmail.com>
2021-02-12 16:14:44 -0600

    Proyecto creo que terminado

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://copca:<pumitas12345>@cluster0.1kl7i.mongodb.net/basicAuth"
 mongoose
-  .connect('mongodb://localhost/express-basic-auth-dev', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+    .connect('mongodb://localhost/express-basic-auth-dev', {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch(err => console.error('Error connecting to mongo', err));
