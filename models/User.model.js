a19a7e77065aaf3ac4363776a70b03d01d32b953
michaloumen <michelle.lmendonca@gmail.com>
2021-03-26 16:32:07 -0300

    Iteração 1

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,12 @@
 // User model here
+/* const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: { type: String },
+  password: { type: String },
+  email: { type: String },
+});
+
+const user = model("User.model", userSchema);
+module.exports = User.model;
+ */
