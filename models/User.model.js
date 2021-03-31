fa71d8285b23801d81b5cf967659b20ab4d7de15
lhfdv <47636364+lhfdv@users.noreply.github.com>
2021-03-30 21:21:05 -0300

    init commit

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,20 @@
 // User model here
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+    {   
+        username:{
+            type: String,
+            trim: true,
+            required: [ true ],
+            unique: true
+        },
+      password: {
+      type: String,
+      required: [ true ],
+    }
+  },
+);
+
+module.exports = model('User', userSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User.model');
+const bcryptjs = require('bcryptjs');
+const mongoose = require('mongoose');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('/signup', (req, res) =>{
+    res.render('./signup')
+})
 
 module.exports = router;
