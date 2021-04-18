6bb51f7f67163daa5e22d7b1f93346f9010f77a0
Jorge <jorgeberrizbeitia@gmail.com>
2021-04-18 18:25:52 +0200

    added ironlauncher structure

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-require('dotenv').config();
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require('dotenv/config');
 
-const cookieParser = require('cookie-parser');
+// ℹ️ Connects to the database
+require('./db');
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
 const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+// Handles the handlebars
+// https://www.npmjs.com/package/hbs
+const hbs = require('hbs');
 
 const app = express();
 
-// require database configuration
-require('./configs/db.config');
-
-// Middleware Setup
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// Express View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+// ℹ️ This function is getting exported from the config folder. It runs most middlewares
+require('./config')(app);
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+const projectName = 'lab-express-basic-auth';
+const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-const index = require('./routes/index.routes');
+app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+
+// 👇 Start handling routes here
+const index = require('./routes/index');
 app.use('/', index);
 
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require('./error-handling')(app);
+
 module.exports = app;
+
