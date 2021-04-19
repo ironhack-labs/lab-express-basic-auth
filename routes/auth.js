17ac8d1d673c5dffaffef7f990b5ecab7730ba2b
triana <trianaheinz@gmail.com>
2021-04-19 16:10:25 +0200

    iteration 1 done

---
+++ routes/auth.js
@@ -0,0 +1,38 @@
+const express= require('express');
+const User =  require('../models/User.model')
+const bcrypt = require('bcryptjs');
+const router= express.Router();
+const saltRound = 10;
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+})
+
+router.post('/signup', (req , res , next) => {
+  const { username, password } = req.body;
+
+    if(!username || !password){
+  res.render('signup', { errorMessage: 'Username and password are required'});
+}
+
+User.findOne({username})
+.then(user =>{
+  if(user){
+    res.render('signup', {errorMessage: "User already exists"});
+  }
+
+  const salt = bcrypt.genSaltSync(saltRound);
+  const hashPassword = bcrypt.hashSync(password, salt);
+
+  User.create ({username, password: hashPassword})
+  .then (() =>{
+    res.render('index');
+  })
+  .catch((error)=> next (error))
+
+})
+})
+
+
+
+
