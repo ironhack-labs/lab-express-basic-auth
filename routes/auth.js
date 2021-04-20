a3a287dbfc13a10d545f8e6d9249452cc0f440fb
mhsngr <markus.heisinger@gmail.com>
2021-04-20 17:43:41 +0200

    Iteration 1

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
+
+router.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+router.post('/signup', (req, res, next) => {
+    const { username, password } = req.body;
+    if (password.length < 8) {
+        res.render('signup', { message: 'Your password has to be at least 8 characters long' });
+        return
+    }
+    if (username === '') {
+        res.render('signup', { message: 'Your username cannot be empty' });
+        return
+    }
+    User.findOne({ username: username })
+        .then(userFromDB => {
+            if (userFromDB !== null) res.render('signup', { message: 'This username is already taken' });
+            else {
+                const salt = bcrypt.genSaltSync();
+                const hash = bcrypt.hashSync(password, salt);
+                User.create({ username: username, password: hash })
+                    .then(createdUser => {
+                        res.redirect('/');
+                    })
+            }
+        })
+});
+
+module.exports = router;
