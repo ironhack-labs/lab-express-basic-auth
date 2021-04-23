478beeb427368e531bc23b44f3c5d2b2b157e1a1
jamal-nasser <nasserr.jamal@gmail.com>
2021-04-23 14:36:29 +0200

    Removed some comments

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,11 +2,6 @@ const express = require('express');
 
 const router = express.Router();
 
-// router.get('/profile', (req, res) => {
-//   const { user } = req.session;
-//   res.status(200).render("auth/profile", user);
-// });
-
 router.get('/main', (req, res, next) => res.render('auth/main'));
 router.get('/private', (req, res, next) => res.render('auth/private'));
 
