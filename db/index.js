61344a8b4545fa6f9c8f6a462f1edd46fa9fb043
Mohamud Issa <mohamud.issa@ymail.com>
2021-06-03 21:35:02 +0200

    done

--- db/index.js
+++ db/index.js
@@ -5,10 +5,9 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth";
 
 mongoose
-  .connect(MONGO_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,23 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true
+const userSchema = new Schema(
+  { 
+    username: {     
+        type: String,
+        unique: [true, 'that username has already been taken, pick a unique Username'],
+        trim: true
+    },
+    passwordHash: {
+      type: String,
+      unique: [true,'that email has already been registered'],
+      trim: true
+    },  
   },
-  password: String
-});
+  {
+    timestamps: true
+  },
+);
 
 const User = model("User", userSchema);
 
