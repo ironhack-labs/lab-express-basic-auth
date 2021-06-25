5eacfc0198a9ef890a9ada6e51ca734a981a0bf5
jose Zhu <josezhu123@gmail.com>
2021-06-25 17:58:22 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,74 @@
-const router = require("express").Router();
+const router = require("express").Router()
+const bcrypt = require('bcrypt')
+const app = require('../app')
+
+const User = require('./../models/User.model')
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
-});
+router.get("/", (req, res, next) => { res.render("index");});
+
+router.get("/signup-page", (req, res) => res.render("auth/signup-page"))
+
+router.post("/signup-page", (req,res) => {
+  const { username, pwd } = req.body
+  //res.send(req.body)
+
+  User.findOne({ username})
+  .then(user => {
+    if(user) {
+      res.render('auth/signup-page', {errorMessage: 'Usuario ya registrado'})
+      return
+    }
+
+    const bcryptSalt = 10
+    const salt = bcrypt.genSaltSync(bcryptSalt)
+    const hashPass = bcrypt.hashSync(pwd, salt)
+
+    User
+      .create({ username, password: hashPass})
+      .then(() => res.redirect('/'))
+      .catch(err => console.log(err))
+  })
+  .catch(err => console.log(err))
+})
+
+router.get('/login-page', (req,res) => res.render('auth/login-page'))
+
+router.post('/login-page', (req, res) => {
+  const { username, pwd } = req.body
+  //res.send(req.body)
+
+  User
+  .findOne({ username })
+  .then(user => {
+    if (!user) {
+      res.render('auth/login-page', {errorMessage: 'Usuario Incorrecto' })
+      return
+    }
+
+    if (bcrypt.compareSync(pwd, user.password) === false) {
+      res.render('auth/login-page', {errorMessage: ' Contraseña Incorrecta' })
+      return
+    }
+    req.session.currentUser = user
+    res.redirect('/user-profile')
+  })
+  .catch(err => console.log(err))
+})
+
+router.get("/user-profile", (req, res, next) => { res.render("user/user-profile");});
+
+router.use((req, res, next) => req.session.currentUser ? next() : res.render('auth/login-page', { errorMessage: 'Desautorizado' }))
+
+router.get('/private', (req, res) => {
+  const loggedUser = req.session.currentUser
+  res.render('user/private', loggedUser)
+})
+
+router.get('/main', (req, res) => {
+  const loggedUser = req.session.currentUser
+  res.render('user/main', loggedUser)
+})
+
 
 module.exports = router;
