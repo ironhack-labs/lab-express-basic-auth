5b4f6046717680895bd266d1ef2066fa07d5e509
Luca TC <lucatuveri@outlook.it>
2021-06-25 18:13:11 +0200

    Lab solved

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
-
+// require("./config/session.config")(app) //session de usuario
 // default value for title local
 const projectName = 'lab-express-basic-auth';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
