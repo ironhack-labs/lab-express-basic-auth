a75c3b100a0db5f7259f4f2f416a5fadc31567ec
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-29 23:06:36 +0200

    server on 3200

--- models/User.model.js
+++ models/User.model.js
@@ -4,9 +4,10 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
+    required: true,
     unique: true
   },
-  password: String
+  password: {type: String, required: true}
 });
 
 const User = model("User", userSchema);
