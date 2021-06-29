d0b9b5c71ba6e5127da8cfbee26af69eeef68900
kintrp <philipp.kintrup@googlemail.com>
2021-06-29 21:44:20 +0200

    still iteration one

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,54 @@
 const router = require("express").Router();
+const bcrypt = require('bcrypt');
+const User = require('./../models/User.model')
+const app = require("../app")
 
-/* GET home page */
+/* Route home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+/* Route register page */
+router.get('/register', (req, res)=>{
+  res.render('register')
+})
+
+router.post('/register', (req, res)=>{
+  const {username, password} = req.body
+  const bcryptSalt = 10
+  const salt = bcrypt.genSaltSync(bcryptSalt)
+  const hashPass = bcrypt.hashSync(password, salt)
+
+  User
+    .create({username:username, password:hashPass})
+    .then((user) => res.redirect('/login'))
+    .catch(err => console.log(err))
+})
+
+/* Route login page */
+router.get('/login', (req, res)=>{
+  res.render('login')
+})
+
+router.post('/login', (req, res) => {
+
+  const { username, password } = req.body
+
+  User.findOne({ username })
+    .then(user => {
+      if (bcrypt.compareSync(password, user.password) === false) {
+        res.render('auth/login-page', { errorMessage: 'Wrong password' })
+        return
+      } else {
+        req.session.currentUser = user
+        res.render('/profile')
+      }
+    })
+})
+
+/* Route profile */
+router.get('/profile', (req, res)=>{
+  res.render('profile')
+})
 
 module.exports = router;
