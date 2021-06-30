81c037abea956dae2b0506396ef44b1b243430c9
Irina <ifalko@protonmail.com>
2021-06-30 16:52:15 +0200

    Login page added

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,32 @@
 const router = require("express").Router();
 const User = require("../models/User.model");
-const bcrypt = require("bcrypt");
+const bcryptjs = require("bcrypt");
+const saltRounds = 10;
 
 router.get("/signup", (req, res, next) => {
   res.render("signup");
 });
+
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+
+router.post("/signup", (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  const salt = bcryptjs.genSaltSync();
+  const hash = bcryptjs.hashSync(password, salt);
+  console.log(hash);
+  User.create({ username: username, password: hash })
+    .then((createdUser) => {
+      console.log(createdUser);
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/login", (req, res) => res.render("login"));
 
 module.exports = router;
