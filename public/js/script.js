1fc53dfb412f2bab3063c1684313598c16e1bb67
alexandreoliv <alexandrebo@gmail.com>
2021-06-30 18:41:50 +0200

    done

--- public/js/script.js
+++ public/js/script.js
@@ -11,6 +11,7 @@ document.querySelector('#password').addEventListener('keyup', e => {
     document.querySelector('span').style.visibility = 'visible';
     document.querySelector('button').removeAttribute('disabled');
   } else {
+    console.log(e.target.value.length);
     document.querySelector('span').style.visibility = 'hidden';
     document.querySelector('button').setAttribute('disabled', true);
   }
