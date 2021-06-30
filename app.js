69446da3a3ee9895a2d757f90cad91aae6b6f6bb
dechilds1@gmail.com <dechilds1@gmail.com>
2021-06-30 22:56:35 +0200

    Completed Lab Express Basic Auth

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
@@ -18,11 +21,38 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
 // default value for title local
 const projectName = 'lab-express-basic-auth';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+
+// MIDDLEWARE
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+
+// SESSION MIDDLEWARE
+// Checks incoming request: if there is a cookie, and if cookie has valid session id
+app.use(
+  session({
+    secret: 'DannyBytes',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000
+    },
+    store: MongoStore.create({
+      mongoUrl: 'mongodb://localhost/auth-demo'
+    })
+  })
+);
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
@@ -30,6 +60,9 @@ app.use('/', index);
 
 const authRouter = require('./routes/auth.router');
 app.use('/auth', authRouter);
+
+const protectedRouter = require('./routes/protected.router');
+app.use('/protected', protectedRouter);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- routes/auth.router.js
+++ routes/auth.router.js
@@ -3,18 +3,18 @@ const authRouter = express.Router();
 const User = require("../models/User.model");
 
 const bcrypt = require("bcrypt");
-const saltRounds = 16;
+const saltRounds = 8;
 
 const zxcvbn = require("zxcvbn");
 
 
 authRouter.get("/signup", (req, res) => {
-    res.render("auth-views/signup-form");
+    res.render("auth-views/signup-form")
 });
   
 // POST    '/auth/signup'
 authRouter.post("/signup", (req, res, next) => {
-
+   
     const { username, password } = req.body;
 
     if (username === "" || password === "") {
@@ -30,20 +30,21 @@ authRouter.post("/signup", (req, res, next) => {
     // console.log("passwordStrenth :>> ", passwordStrength);
     if (passwordStrength < 3) {
         res.render("auth-views/signup-form", {
-        errorMessage: zxcvbn(password).feedback.warning,
+        errorMessage: "Password is too weak",
         });
         return;
     }
 
     User.findOne({ username })
         .then((userObj) => {
+            
         if (userObj) {
             res.render("auth-views/signup-form", {
             errorMessage: `Username ${username} is already taken.`,
             });
             return;
         } else {
-            
+           
             const salt = bcrypt.genSaltSync(saltRounds);
             const hashedPassword = bcrypt.hashSync(password, salt);
 
@@ -61,5 +62,50 @@ authRouter.post("/signup", (req, res, next) => {
         })
         .catch((err) => next(err));
 });
+
+authRouter.get("/login", (req, res) => {
+    res.render("auth-views/login-form")
+})
+
+authRouter.post("/login", (req, res)=>{
+    const {username, password} = req.body
+  
+    if (username === "" || password === "") {
+      res.render("auth-views/login-form", { errorMessage: "Username and Password are required." });
+      return;
+    }
+  
+    User.findOne({username})
+    .then(user=>{
+           if (!user) {
+            res.render("auth-views/login-form", { errorMessage: "Input invalid" });
+          } 
+            else {
+                const encryptedPassword = user.password;
+                const passwordCorrect = bcrypt.compareSync(password, encryptedPassword);
+         
+                if(passwordCorrect){
+                    console.log("You are logged in!")
+                    req.session.currentUser = user;
+                    console.log(user)
+                    res.redirect("/")
+                } else {
+                    res.render("auth-views/login-form", { errorMessage: "Name OR pwd is incorrect" });
+                }
+            }
+    })
+  
+})
+
+authRouter.get('/logout', (req, res)=>{
+    req.session.destroy(err =>{
+      if(err){
+        res.render("error", { message: "Something went wrong! Yikes!" });
+      }else{
+        console.log("You have successfully logged out!")
+        res.redirect('/')
+      }
+    })
+  })
 
 module.exports = authRouter;
