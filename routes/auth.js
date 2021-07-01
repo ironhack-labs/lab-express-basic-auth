d863b86adc9c3432e418ab3978ddf4d45ce04d44
kintrp <philipp.kintrup@googlemail.com>
2021-07-01 14:50:54 +0200

    Ahmed

---
+++ routes/auth.js
@@ -0,0 +1,85 @@
+const router = require("express").Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
+
+router.get('/signup', (req, res, next) => {
+	res.render('signUp');
+});
+
+router.get('/login', (req, res, next) => {
+	res.render('login');
+});
+
+// Signup
+
+router.post('/signup', (req, res)=> {
+	console.log('Hello World', req.body);
+	const {username, password} = req.body;
+
+	if (password.length <8) {
+		res.render('signUp', { message: 'Minimum password length: 8 chars' });
+		return;
+	}
+	if (username.length=== 0) {
+		res.render('signUp', {message: 'Username cannot be empty'});
+		return;
+	}
+
+	User.findOne({ username: username })
+		.then(userFromDB => {
+			console.log('userFromDB', userFromDB);
+			if (userFromDB !== null) {
+				res.render('signUp', {message: 'Username has already been taken.'})
+				return;
+			
+			} else {
+				const salt = bcrypt.genSaltSync();
+				const hash = bcrypt.hashSync(password,salt);
+				console.log('hash',hash);
+				User.create({username: username, password: hash})
+					.then(createdUser => {
+						console.log('createdUser', createdUser);
+						res.redirect('/login');	
+					})
+					.catch(err=>{
+						next(err);
+					})
+			}
+		})
+})
+
+// Login
+
+router.post('/login', (req, res, next)=> {
+	const { username, password } = req.body;
+
+	User.findOne ({ username : useranme })
+		.then(userFromDB => {
+			if (userFromDB === 0) {
+				res.render('login', {message: 'Invalid credentials'});
+				return;
+			}
+			if (bcrypt.compareSync(password,userFromDB.password)) {
+				req.session.user = userFromDB;
+				res.redirect('/profile');
+			} else {
+				res.render('login', {message: 'Invalid credentials'});
+				return;
+			}
+		})
+})
+
+// Logout
+
+router.get('/logout', (req, res, next) => {
+	req.session.destroy(err=>{
+		if (err) {
+			next(err);
+		} else {
+			res.redirect('/');
+		}
+	})
+})
+
+
+module.exports = router; 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,35 @@
 const router = require("express").Router();
 
+// create a middleware to check if the user is logged in
+const loginCheck = () => {
+  return (req, res, next) => {
+    // is there a logged in user
+    if (req.session.user) {
+      // proceed as intended
+      next();
+    } else {
+      // there is no user logged in
+      // we redirect to /login
+      res.redirect('/login');
+    }
+  }
+}
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/profile', loginCheck(), (req, res, next) => {
+  // this is how you access a cookie
+  console.log('this is the cookie: ', req.cookies)
+  // this is how you can set a cookie
+  res.cookie('myCookie', 'hello world');
+  // this is how you delete a cookie
+  res.clearCookie('myCookie');
+  const loggedInUser = req.session.user
+  res.render('profile', { user: loggedInUser });
+});
+
 
 module.exports = router;
