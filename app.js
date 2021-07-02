85ff295e7bde0d5263c3491bc87cca5326f69045
Mangonando <juan.f.ramirezv@gmail.com>
2021-07-02 18:38:56 +0200

    lab done

--- app.js
+++ app.js
@@ -18,6 +18,23 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const DB_URL = process.env.ATLAS_CONNECTION;
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		// for how long is a user automatically logged in 
+		cookie: { maxAge: 1000 * 60 * 60 * 24 },
+		saveUninitialized: false,
+		resave: true,
+		store: MongoStore.create({
+			mongoUrl: DB_URL
+		})
+	})
+)
+
 // default value for title local
 const projectName = 'lab-express-basic-auth';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
@@ -27,6 +44,9 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const auth = require("./routes/auth");
+app.use("/", auth);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
