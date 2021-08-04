0f2827e6c2bcdd09bf3596bb85739e7254970669
claradrojas <drc.clara@gmail.com>
2021-08-04 22:01:22 +0200

    iteration 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,6 +12,19 @@ router.get("/signup", (req, res) => res.render("auth/signup"));
 router.post("/signup", (req, res, next) => {
   const { username, password } = req.body;
 
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
+
+  if (!username || !password) {
+    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+    return;
+  }
+
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
@@ -23,11 +36,53 @@ router.post("/signup", (req, res, next) => {
     })
     .then((userFromDB) => {
         console.log(userFromDB),
-        res.redirect('/userProfile')})
-    .catch((error) => next(error));
+        res.redirect(`/userProfile?username=${userFromDB.username}`)})
+        .catch(error => {
+          if (error instanceof mongoose.Error.ValidationError) {
+            res.status(500).render('auth/signup', { errorMessage: error.message });
+          } else if (error.code === 11000) {
+            res.status(500).render('auth/signup', {
+               errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+            });
+          } else {
+            next(error);
+          }
+        });
 });
 
-router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+router.get('/login', (req, res) => res.render('auth/login'))
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+ 
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other name.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render('users/user-profile', { user });
+
+        req.session.currentUser = user;
+        res.redirect('/userProfile');
+
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
+router.get('/userProfile', (req, res) => {
+  res.render('users/user-profile', { userInSession: req.session.currentUser });
+});
 
 
 module.exports = router;
