77a81fb95eda53595e3743c4963c295a84d1124c
Presenter <hendrik.wermser@ironhack.com>
2021-08-11 01:16:46 +0200

    fixes relative paths, also fixes MONGOURI and just ignores the session secret

--- routes/signup.js
+++ routes/signup.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../../models/User.model");
+const User = require("../models/User.model");
 const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
 
