f7ca5eb7ede5a2d24fcc14be2945b094b5b2cc93
Fede <fedemarinasruiz@gmail.com>
2021-09-03 17:38:45 +0200

    [PP MAD]Fede y Marta

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+
 require('dotenv/config');
 
-// ℹ️ Connects to the database
+
 require('./db');
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
+
 const express = require('express');
 
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
+
 const hbs = require('hbs');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
+
 require('./config')(app);
 
-// default value for title local
+
 const projectName = 'lab-express-basic-auth';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
-// 👇 Start handling routes here
+require('./config/session.config')(app)
+
 const index = require('./routes/index');
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,83 @@
 const router = require("express").Router();
+const bcrypt = require('bcryptjs')
+const User = require("../models/User.model")
 
 /* GET home page */
+
+
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/registro", (req, res, next) => {
+  res.render("sign-up")
+})
+router.post("/registro", (req, res) => {
+
+  const { username, userPwd } = req.body
+
+  if (userPwd.length === 0) {
+    res.render('sign-up')
+    return
+  }
+  if (username.length === 0) {
+    res.render('sign-up')
+    return
+  }
+  User
+    .findOne({ username })
+    .then(user => {
+      if (user) {
+        res.render('sign-up')
+        return
+      }
+      const bcryptSalt = 10
+      const salt = bcrypt.genSaltSync(bcryptSalt)
+      const hashPass = bcrypt.hashSync(userPwd, salt)
+
+      User
+        .create({ username, password: hashPass })
+        .then(res.redirect("/"))
+        .catch(err => console.log(err))
+    })
+    .catch(err => console.log(err))
+})
+
+router.get("/log-in", (req, res) => res.render("log-in"))
+router.post("/log-in", (req, res) => {
+
+  const { username, userPwd } = req.body
+
+  if (userPwd.length === 0 || username.length === 0) {
+    res.render("sign-up")
+    return
+  }
+
+  User
+    .findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render("log-in")
+        return
+      }
+      if (bcrypt.compareSync(userPwd, user.password) === false) {
+        res.render("log-in")
+      }
+
+      req.session.currentUser = user
+      res.redirect('/perfil')
+
+    })
+    .catch(err => console.log(err))
+})
+
+router.use((req, res, next) => {
+  req.session.currentUser ? next() : res.render('log-in')
+})
+router.get('/perfil', (req, res) => {
+  res.render('profile')
+})
+
+
 
 module.exports = router;
