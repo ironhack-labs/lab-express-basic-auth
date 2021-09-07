3062318619e1a16683bb71cd77dff779c0bcf691
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-09-07 18:29:13 +0200

    add signup page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
 
 module.exports = router;
