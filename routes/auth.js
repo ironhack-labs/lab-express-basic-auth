ed75fd2a847d1a66e70bc8972f08e36fa1446ef4
Rahaf <r.abualhassan@gmail.com>
2021-09-08 17:20:07 +0200

    Make sure users use strong passwords

--- routes/auth.js
+++ routes/auth.js
@@ -23,6 +23,17 @@ router.post("/signup", (req, res, next) => {
     return;
   }
 
+  // make sure passwords are strong
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
+
+
+
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
@@ -39,11 +50,7 @@ router.post("/signup", (req, res, next) => {
     })
     .catch(error => {
       if (error instanceof mongoose.Error.ValidationError) {
-        res.status(500).render('auth/signup', { errorMessage: error.message });
-      } else if (error.code === 11000) {
-        res.status(500).render('auth/signup', {
-           errorMessage: 'Username and email need to be unique. Either username or email is already used.'
-        });
+        res.status(500).render('auth/signup', { errorMessage: 'Username and email need to be unique. Either username or email is already used.' });
       } else {
         next(error);
       }
