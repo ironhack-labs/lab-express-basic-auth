cdeda992f263b0257613db1bbf16556deac206f3
lauraAlabau <arlaura3@gmail.com>
2021-09-08 18:29:19 +0200

    media iteracion 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,7 +9,7 @@ const User = require('../models/User.model');
 /**********************************************************************/
 /**********************************************************************/
 
-/* FORM INICIO */
+/* FORM HOME */
 // GET route ==> to display the signup form
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
@@ -17,7 +17,14 @@ router.get('/signup', (req, res) => res.render('auth/signup'));
 router.post('/signup', (req, res, next) => {
   
     const { username, password } = req.body;
-   
+
+    // Start BONUS
+    if (!username || !password) {
+      res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username and password.' });
+      return;
+    }
+   // End BONUS
+
     bcryptjs
       .genSalt(saltRounds)
       .then(salt => bcryptjs.hash(password, salt))
@@ -41,5 +48,43 @@ router.post('/signup', (req, res, next) => {
 router.get('/userProfile', (req, res) => {
     res.render('user-profile.hbs')
 })  
+
+
+
+
+/**********************************************************************/
+//////////// L O G I N ///////////
+ 
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+
+// POST login route ==> to process form data
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+ 
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, user and password to login.'
+    });
+    return;
+  }
+ 
+  User
+    .findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'El usuario no existe' });
+        return;
+      }
+      else if (bcryptjs.compareSync(password, user.password) === false) {
+        res.render('auth/login', { errorMsg: "Contraseña incorrecta" })
+      } else {
+        res.redirect('/userProfile')
+      }
+    })
+    .catch(error => next(error));
+}); 
+
+
 
 module.exports = router;
