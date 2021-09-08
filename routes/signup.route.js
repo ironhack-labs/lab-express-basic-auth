f91d9eb6b147333e42d6a2061b5adc09877ac789
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-08 14:36:08 +0200

    iteration 1-2

--- routes/signup.route.js
+++ routes/signup.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 // Import user
 const User = require('../models/User.model');
+// Import mongoose
+const mongoose = require('mongoose');
 
 // require bcryptJS
 const bcryptjs = require('bcryptjs');
@@ -18,6 +20,24 @@ router.get('/signup', (req, res) => res.render('auth/signup.hbs'));
 // Signup POST ------------------------------------------------------------
 router.post('/signup', (req, res, next) => {
 	const { username, email, password } = req.body;
+
+	// show error if no email or no pass or no name
+	if (!username || !email || !password) {
+		res.render('auth/signup', {
+			errorMessage: 'All fields are mandatory. Please provide your username, email and password.'
+		});
+		return;
+	}
+
+	// make sure passwords are strong:
+	const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+	if (!regex.test(password)) {
+		res.status(500).render('auth/signup', {
+			errorMessage:
+				'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.'
+		});
+		return;
+	}
 
 	bcryptjs
 		.genSalt(saltRounds)
@@ -37,7 +57,17 @@ router.post('/signup', (req, res, next) => {
 			console.log('Newly created user is: ', userFromDB);
 			res.redirect('/userProfile');
 		})
-		.catch((error) => next(error));
+		.catch((error) => {
+			if (error instanceof mongoose.Error.ValidationError) {
+				res.status(500).render('auth/signup', { errorMessage: error.message });
+			} else if (error.code === 11000) {
+				res.status(500).render('auth/signup', {
+					errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+				});
+			} else {
+				next(error);
+			}
+		}); // close .catch()
 });
 
 router.get('/userProfile', (req, res) => res.render('users/user-profile'));
