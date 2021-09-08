f40992124f8255a7f197f40c583bab8e9b261698
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-08 14:43:53 +0200

    iteration 2 completed

--- routes/signup.route.js
+++ routes/signup.route.js
@@ -71,5 +71,47 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+// ---------------------------------------------------------------------------------
+// LOGIN
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+// ---------------------------------------------------------------------------------
+// POST route ==> to display the login form to users
+router.post('/login', (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (email === '' || password === '') {
+		res.render('auth/login', {
+			errorMessage: 'Please enter both, email and password to login.'
+		});
+		return;
+	}
+
+	User.findOne({ email }) // <== check if there's user with the provided email
+		.then((user) => {
+			// <== "user" here is just a placeholder and represents the response from the DB
+			if (!user) {
+				// <== if there's no user with provided email, notify the user who is trying to login
+				res.render('auth/login', {
+					errorMessage: 'Email is not registered. Try with other email.'
+				});
+				return;
+			} else if (bcryptjs.compareSync(password, user.passwordHash)) {
+				// if there's a user, compare provided password
+				// with the hashed password saved in the database
+				// if the two passwords match, render the user-profile.hbs and
+				//                   pass the user object to this view
+				//                                 |
+				//                                 V
+				res.render('users/user-profile', { user });
+			} else {
+				// if the two passwords DON'T match, render the login form again
+				// and send the error message to the user
+				res.render('auth/login', { errorMessage: 'Incorrect password.' });
+			}
+		})
+		.catch((error) => next(error));
+});
+// ---------------------------------------------------------------------------------
 
 module.exports = router;
