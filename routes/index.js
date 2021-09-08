df59e1f432e7af806ea801ac46fe295146845961
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-09-08 11:55:55 +0200

    add profile

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.get('/profile', (req, res, next) => {
+  const loggedInUser = req.session.user;
+  console.log(loggedInUser)
+  res.render('profile', { user : loggedInUser })
+})
+
 module.exports = router;
