e9a7a6f7c2d452f2ee58d72b3d9c2a40ea6ad8f3
Rahaf <r.abualhassan@gmail.com>
2021-09-08 12:33:35 +0200

    reorganized files

--- basic-auth/public/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("basic-auth JS imported successfully!");
-  },
-  false
-);
--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,29 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+// TODO: Please make sure you edit the user model to 
+//whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    unique: true,
+    required: [true, 'Username is required.']
   },
-  password: String
+  password: {
+    type: String,
+    required: [true, 'Password is required.']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required.'],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  {
+    timestamps: true
+  }
 });
 
 const User = model("User", userSchema);
 
-module.exports = User;
+module.exports = model('User', userSchema);
