190aa5c036c18c4afd3493b73c55a2158a139bf4
HannesCho <63042736+HannesCho@users.noreply.github.com>
2021-09-08 12:09:50 +0200

    add main

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,24 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.get('/profile', (req, res, next) => {
+const loginCheck = () => {
+  return (req, res, next) => {
+    if (req.session.user) {
+      next();
+    } else {
+      res.redirect('/login');
+    }
+  }
+}
+
+router.get('/profile', loginCheck(), (req, res, next) => {
   const loggedInUser = req.session.user;
   console.log(loggedInUser)
   res.render('profile', { user : loggedInUser })
+})
+
+router.get('/main', loginCheck(), (req, res, next) => {
+  res.render('main')
 })
 
 module.exports = router;
