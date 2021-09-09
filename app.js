67622bc397d011859cf62cbb68ea1da34ecffb83
simplePac <rober_alicante@hotmail.es>
2021-09-09 09:04:31 +0200

    WD082021 - Roberto Lorente
    Lab terminado. Tendré que volver a verme la teoría, lógicamente. Muchísimas cosas que creo que se pueden resumir en un copia y pega si se quiere hacer un sistema de Logueo de ususarios

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 const app = express();
-
+require('./config/session.config')(app)
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
@@ -24,10 +24,35 @@ const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerC
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+ 
+const plainPassword1 = 'HelloWorld';
+const plainPassword2 = 'helloworld';
+ 
+const salt = bcrypt.genSaltSync(saltRounds);
+ 
+console.log(`Salt => ${salt}`);
+ 
+const hash1 = bcrypt.hashSync(plainPassword1, salt);
+const hash2 = bcrypt.hashSync(plainPassword2, salt);
+ 
+const verifyPass1 = bcrypt.compareSync(plainPassword1, hash1);
+const verifyPass2 = bcrypt.compareSync('some wrong password', hash2);
+ 
+console.log(`Hash 1: ${hash1}`);
+console.log(`Hash 2: ${hash2}`);
+console.log('----------------------------------------');
+console.log(`Is plainPassword1 corresponding to the created hash1: ${verifyPass1}`);
+console.log(`Is plainPassword2 corresponding to the created hash2: ${verifyPass2}`);
+
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
 
+
+const authRouter = require('./routes/auth.routes'); // <== has to be added
+app.use('/', authRouter); // <== has to be added
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
--- models/User.model.js
+++ models/User.model.js
@@ -4,9 +4,14 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    unique: true,
   },
-  password: String
+  email: String,
+
+  passwordHash: {
+    type: String,
+    required: [true, "Password is required."],
+  },
 });
 
 const User = model("User", userSchema);
