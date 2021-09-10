e8628eb741274feb23b546c7f606111067a57c06
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-10 19:21:26 +0200

    commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,78 @@
+const { Router } = require("express");
+const router = new Router();
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
+const User = require("../models/User.model");
+const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
+
+//SIGNUP
+router.get("/signup", isLoggedOut, (req, res) => res.render("auth/signup"));
+
+router.post("/signup", (req, res, next) => {
+  //console.log(req.body);
+  const { username, email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, email and password to login.",
+    });
+    return;
+  }
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        username: username,
+        email: email,
+        passwordHash: hashedPassword,
+      }).then((userFromDB) => {
+        console.log("Newly created user is: ", userFromDB);
+        res.redirect("/userProfile");
+      });
+    })
+    .catch((error) => next(error));
+});
+
+//LOGIN
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Email is not registered. Try with other email.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render("users/user-profile", { user });
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+//UserProfile
+router.get("/userProfile", isLoggedIn, (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+//LOGOUT
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
