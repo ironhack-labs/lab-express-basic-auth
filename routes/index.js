260be14d953d562a111ef658e6fa491e5bcf7cbf
Miguel Munoz Estelles <mmestelles@gmail.com>
2021-11-12 17:08:45 +0100

    done

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,15 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+// MIDDLEWARE DETECTOR DE SESIÓN
+router.use((req, res, next) => {
+  console.log(req.session)
+  req.session.currentUser ? next() : res.render('main', { errorMessage: 'Necesitas estar logeado para ver esta página' })
+})
+
+//RUTAS PROTEGIDAS
+router.get("/private", (req, res) => {
+  res.render("private", req.session.currentUser)
+})
+
 module.exports = router;
