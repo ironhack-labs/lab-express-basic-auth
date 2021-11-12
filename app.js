00693716ad9a0efad82b2ac70fb6eb4091dfad81
Carlos Jiménez <charly.design.c@gmail.com>
2021-11-12 18:01:09 +0100

    mk1

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const hbs = require('hbs');
 
 const app = express();
 
+require("./config/session.config")(app)  //4. Configuración de sesión
+require("./config")(app);
+
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
@@ -25,11 +28,18 @@ const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerC
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
+
+const index = require("./routes/index");
+app.use("/", index);
+require("./error-handling")(app);
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
+
 
 module.exports = app;
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const router = require("express").Router();
 
 /* GET home page */
+
+
+
+
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+
+// MIDDLEWARE DETECTOR DE SESIÓN
+router.use((req, res, next) => {
+  console.log(req.session)
+  req.session.currentUser ? next() : res.render('main', { errorMessage: 'Necesitas estar logeado para ver esta página' })
+});
+
+
+router.get('/private', (req, res, next) => {
+  //res.send("has accedido a la zona SUPERPRIVADA")
+  res.render("private")
+});
+
+
 
 module.exports = router;
