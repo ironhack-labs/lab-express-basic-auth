6cd309a8ca17437dbcbd3c9842221e859204f0ec
Anna Morozova <a.morozova.nrw@gmail.com>
2021-11-17 13:40:41 +0100

    iteration 1 - done

--- app.js
+++ app.js
@@ -28,6 +28,9 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+const authRouter = require('./routes/auth.routes');
+app.use('/', authRouter);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
