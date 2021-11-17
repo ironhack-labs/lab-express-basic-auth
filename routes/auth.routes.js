9f79af9f7e021e01b6c601691a7f1f27461bae04
Anna Morozova <a.morozova.nrw@gmail.com>
2021-11-17 17:14:06 +0100

    updated

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -11,6 +11,14 @@ const User = require('../models/User.model');
 // GET route ==> to display the signup form to users
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+router.get('/login', (req, res) => res.render('auth/login'));
+
+router.get('/main', (req, res, next) => {
+    res.render('main')
+})
+
 // POST route ==> to process form data
 router.post('/signup', (req, res, next) => {
     // console.log("The form data: ", req.body);
@@ -36,7 +44,67 @@ router.post('/signup', (req, res, next) => {
         .catch(error => next(error));
 });
 
-router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+// middleware to protect a route
+const loginCheck = () => {
+    return (req, res, next) => {
+        // check for a logged in user
+        if (req.session.user) {
+            next()
+        } else {
+            res.redirect('/login')
+        }
+    }
+}
+
+router.get('/private', loginCheck(), (req, res, next) => {
+    res.render('private')
+})
+
+router.get('/profile', loginCheck(), async (req, res, next) => {
+    // set a cookie if you want
+    //res.cookie('theCookie', 'hello node')
+    //(console.log('this is the cookie', req.cookies))
+    // clear the cookie
+    //res.clearCookie('theCookie')
+    // retrieve logged in user from session
+    const loggedInUser = req.session.user
+    console.log('LOGGEDINUSER', loggedInUser)
+    res.render('profile', { user: loggedInUser })
+})
+
+router.post('/login', (req, res, next) => {
+    const { username, password } = req.body;
+
+    if (username === '' || password === '') {
+        res.render('auth/login', {
+            errorMessage: 'Please enter both, username and password to login.'
+        });
+        return;
+    }
+
+    User.findOne({ username })
+        .then(user => {
+            if (!user) {
+                res.render('auth/login', { errorMessage: 'User is not registered. Try with other user.' });
+                return;
+            } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+                res.render('users/user-profile', { user });
+            } else {
+                res.render('auth/login', { errorMessage: 'Incorrect password.' });
+            }
+        })
+        .catch(error => next(error));
+});
+
+router.get('/logout', (req, res, next) => {
+    req.session.destroy(err => {
+        if (err) {
+            next(err)
+        } else {
+            res.redirect('/')
+        }
+    })
+})
 
 module.exports = router;
 
