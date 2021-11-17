24a41ace779c8ca6b208031d0adf4981de7a48db
Sillas Victor <Sillas_2@hotmail.com>
2021-11-17 18:27:05 +0000

    final edits

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
+const isLoggedIn = require("../middleware/isLoggedin");
 const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  // Check if the incoming request has a valid cookie/session
+  let userIsLoggedIn = false;
+  if (req.session.user) {
+    userIsLoggedIn = true;
+  }
+
+  res.render("index", { userIsLoggedIn: userIsLoggedIn });
 });
 
 module.exports = router;
