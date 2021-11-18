3c813b4859fe22ea396bcfa71149b66d8e640faf
Anne Dantas <annedantas@gmail.com>
2021-11-18 09:22:32 +0000

    adding details

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,30 @@ const userLoggedIn = require("./../middleware/login-confirmation")
 /* GET home page */
 router.get("/", (req, res, next) => {
   let userLoggedIn = false;
-  if (req.session.user){
+  const userInfo = req.session.user;
+  if (userInfo){
     userLoggedIn = true;
   }
-  res.render("index", { userLoggedIn: userLoggedIn });
+  res.render("index", { userLoggedIn: userLoggedIn, userInfo });
 });
 
 router.get("/main", userLoggedIn, (req, res) => {
   let userLoggedIn = false;
-  if (req.session.user){
+  const userInfo = req.session.user;
+  console.log(userInfo);
+  if (userInfo){
     userLoggedIn = true;
   }
-  res.render("main-page", { userLoggedIn: userLoggedIn });
+  res.render("main-page", { userLoggedIn: userLoggedIn, userInfo });
 })
 
 router.get("/private", userLoggedIn, (req, res) => {
   let userLoggedIn = false;
-  if (req.session.user){
+  const userInfo = req.session.user;
+  if (userInfo){
     userLoggedIn = true;
   }
-  res.render("secret-page", { userLoggedIn: userLoggedIn });
+  res.render("secret-page", { userLoggedIn: userLoggedIn, userInfo });
 })
 
 module.exports = router;
