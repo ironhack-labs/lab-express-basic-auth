73d4b41a80d1ed381e727d82101a8c90848ace37
Michael Seitz <michael.s.seitz@gmail.com>
2021-11-23 20:27:06 +0100

    created rudimentary routes

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,13 @@ const router = require("express").Router();
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+router.get("/login", (req, res, next) => {
+  res.render("login");
+});
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+
 
 module.exports = router;
