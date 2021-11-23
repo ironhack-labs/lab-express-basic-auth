b04354ca1c12bba881f70becc4d7581d692d3911
Michael Seitz <michael.s.seitz@gmail.com>
2021-11-23 21:02:07 +0100

    a new user can be created with a hashed passwort

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User.model")
+const bcryptjs = require("bcryptjs")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -15,7 +16,14 @@ router.get("/signup", (req, res, next) => {
 
 router.post("/signup", async (req, res, next) => {
   const {username, password} = req.body
-  await User.create({"username": username, "password": password})
+  
+  // hashing password
+  const saltRounds = 10
+  const salt = await bcryptjs.genSalt(saltRounds)
+  hashedPasswort = await bcryptjs.hash(password, salt)
+
+  //create user with hashed password
+  await User.create({"username": username, "password": hashedPasswort})
   res.render("login");
 });
 router.post("/login", async (req, res, next) => {
