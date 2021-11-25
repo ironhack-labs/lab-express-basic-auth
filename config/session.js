7e71c7710bdcf0396ac537770c593383f5699e4f
GeorgeLR87 <jorgelrgarduno@gmail.com>
2021-11-25 17:52:41 -0600

    actualizacion sessions

--- config/session.js
+++ config/session.js
@@ -2,22 +2,26 @@
 const session = require("express-session")
 const MongoStore = require("connect-mongo")
 
+// 2. GESTIÓN DE SESIÓN
 const sessionManager = (app) => {
 
 	app.set("trust proxy", 1)
-
+	
 	app.use(session({
-		secret: "HOLAMUNDO", 
+		secret: process.env.SESSION,
 		resave: true,
 		saveUninitialized: false,
-		cookie: { 
-			maxAge: 86400000  
+		cookie: {
+			httpOnly: true,
+			maxAge: 86400000
 		},
 		store: MongoStore.create({
 			mongoUrl: process.env.MONGODB_URI
 		})
 	}))
+
 }
+
 
 // 3. EXPORTACIÓN
 module.exports = sessionManager
