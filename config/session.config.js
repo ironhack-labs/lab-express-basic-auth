390d6a21fd5ce5a48eac565cf8af8c555a80ec6f
Michael Seitz <michael.s.seitz@gmail.com>
2021-11-28 18:36:34 +0100

    only verfied user will now get a cookie

--- config/session.config.js
+++ config/session.config.js
@@ -19,12 +19,12 @@ module.exports = app => {
     session({
       secret: process.env.SESS_SECRET,
       resave: true,
-      saveUninitialized: true,
+      saveUninitialized: false,
       cookie: {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 60000 // 60 * 1000 ms === 1 min
+        maxAge: 600000 // 60 * 1000 ms === 1 min
       },
       store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth"
