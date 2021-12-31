6a00b01456e6e49f2a8d232fcddff54131f474cb
carloscordon82 <carloscordon82@gmail.com>
2021-12-31 13:19:00 -0500

    Done

--- config/index.js
+++ config/index.js
@@ -1,9 +1,12 @@
 // We reuse this import in order to have access to the `body` property in requests
 const express = require("express");
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth";
 
 // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
 // https://www.npmjs.com/package/morgan
 const logger = require("morgan");
+const session = require("express-session");
 
 // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
 // https://www.npmjs.com/package/cookie-parser
@@ -16,7 +19,7 @@ const favicon = require("serve-favicon");
 // ℹ️ global package used to `normalize` paths amongst different operating systems
 // https://www.npmjs.com/package/path
 const path = require("path");
-
+const MongoStore = require("connect-mongo");
 // Middleware configuration
 module.exports = (app) => {
   // In development environment the app logs
@@ -26,7 +29,6 @@ module.exports = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
-
   // Normalizes the path to the views folder
   app.set("views", path.join(__dirname, "..", "views"));
   // Sets the view engine to handlebars
@@ -34,6 +36,18 @@ module.exports = (app) => {
   // Handles access to the public folder
   app.use(express.static(path.join(__dirname, "..", "public")));
 
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || "super hyper secret key",
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({
+        mongoUrl: MONGO_URI,
+      }),
+    })
+  );
   // Handles access to the favicon
-  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+  app.use(
+    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
+  );
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,137 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const isLoggedIn = require("../middleware/isLoggedIn");
+const isLoggedOut = require("../middleware/isLoggedOut");
+const saltRounds = 10;
 
-/* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", isLoggedIn, (req, res, next) => {
+  res.redirect("/private");
+});
+
+router.get("/main", isLoggedIn, (req, res, next) => {
+  res.render("main", { user: req.session.user.username });
+});
+
+router.get("/private", isLoggedIn, (req, res, next) => {
+  res.render("private", { user: req.session.user.username });
+});
+
+router.get("/login", isLoggedOut, (req, res, next) => {
+  if (!req.session.user) {
+    res.render("login");
+  } else {
+    res.render("index", { user: req.session.user.username });
+  }
+});
+
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username) {
+    return res
+      .status(400)
+      .render("login", { errorMessage: "Please provide your username." });
+  }
+
+  if (password.length < 8) {
+    return res.status(400).render("login", {
+      errorMessage: "Your password needs to be at least 8 characters long.",
+    });
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(400)
+          .render("login", { errorMessage: "Wrong credentials." });
+      }
+
+      bcrypt.compare(password, user.password).then((isSamePassword) => {
+        if (!isSamePassword) {
+          return res
+            .status(400)
+            .render("login", { errorMessage: "Wrong credentials." });
+        }
+        console.log("BEFORE", req.session.user);
+        req.session.user = user;
+        console.log("AFTER", req.session.user);
+        return res.redirect("/");
+      });
+    })
+
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  const { username, password } = req.body;
+  console.log("NOOOO");
+  if (!username) {
+    return res
+      .status(400)
+      .render("signup", { errorMessage: "Please provide your username." });
+  }
+
+  if (password.length < 8) {
+    return res.status(400).render("signup", {
+      errorMessage: "Your password needs to be at least 8 characters long.",
+    });
+  }
+
+  User.findOne({ username }).then((found) => {
+    if (found) {
+      return res
+        .status(400)
+        .render("signup", { errorMessage: "Username already taken." });
+    }
+
+    return bcrypt
+      .genSalt(saltRounds)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hashedPassword) => {
+        return User.create({
+          username,
+          password: hashedPassword,
+        });
+      })
+      .then((user) => {
+        req.session.user = user;
+        res.redirect("/");
+      })
+      .catch((error) => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          return res
+            .status(400)
+            .render("signup", { errorMessage: error.message });
+        }
+        if (error.code === 11000) {
+          return res.status(400).render("signup", {
+            errorMessage:
+              "Username need to be unique. The username you chose is already in use.",
+          });
+        }
+        return res
+          .status(500)
+          .render("signup", { errorMessage: error.message });
+      });
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).render("logout", { errorMessage: err.message });
+    }
+    res.redirect("login");
+  });
 });
 
 module.exports = router;
