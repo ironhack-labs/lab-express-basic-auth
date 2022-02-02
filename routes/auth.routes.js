700cb417fad52890746de3d5af1f9c6949c28c33
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-02 09:24:57 +0100

    Solved Iteration 1

---
+++ routes/auth.routes.js
@@ -0,0 +1,39 @@
+// routes/auth.routes.js
+
+const { Router } = require('express');
+const router = new Router();
+
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+const User = require('../models/User.model');
+
+// GET route to display the signup form to users:
+router.get('/signup', (req, res) => res.render('auth/signup'));
+
+// POST route to process form data:
+router.post('/signup', (req, res, next) => {
+    const { username, password } = req.body;
+   
+    bcryptjs
+      .genSalt(saltRounds)
+      .then(salt => bcryptjs.hash(password, salt))
+      .then(hashedPassword => {
+        return User.create({
+          username,
+          // passwordHash => this is the key from the User model
+          //     ^
+          //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+          passwordHash: hashedPassword
+        });
+      })
+      .then(userFromDB => {
+        res.redirect('/userProfile');
+      })
+      .catch(error => next(error));
+});
+
+// GET route to user proile page:
+router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+module.exports = router;
