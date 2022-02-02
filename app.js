d27615f460470033bcced372d63f50d0501250a5
KimSteenBekke <Kim.Steen.Bekke@lego.com>
2022-02-02 10:23:09 +0100

    done

--- app.js
+++ app.js
@@ -28,8 +28,39 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+const authRouter = require('./routes/auth.routes'); 
+app.use('/', authRouter);
+// ...
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+const plainPassword1 = "HelloWorld";
+const plainPassword2 = "helloworld";
+
+const salt = bcrypt.genSaltSync(saltRounds);
+
+console.log(`Salt => ${salt}`);
+
+const hash1 = bcrypt.hashSync(plainPassword1, salt);
+const hash2 = bcrypt.hashSync(plainPassword2, salt);
+
+const verifyPass1 = bcrypt.compareSync(plainPassword1, hash1);
+const verifyPass2 = bcrypt.compareSync("some wrong password", hash2);
+
+console.log(`Hash 1: ${hash1}`);
+console.log(`Hash 2: ${hash2}`);
+console.log("----------------------------------------");
+console.log(
+  `Is plainPassword1 corresponding to the created hash1: ${verifyPass1}`
+);
+console.log(
+  `Is plainPassword2 corresponding to the created hash2: ${verifyPass2}`
+);
+
 
 module.exports = app;
 
