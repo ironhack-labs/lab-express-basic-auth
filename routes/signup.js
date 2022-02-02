b3ff41349d911c2f294c7854f73c330130261645
Clara Villa <Clara.Villa@lego.com>
2022-02-02 10:27:54 +0000

    add a check for user already existing

--- routes/signup.js
+++ routes/signup.js
@@ -30,3 +30,39 @@ router.post("/sign-up", (req, res, next) => {
 });
 
 module.exports = router;
+
+// TO CHECK IF THE USER ALREADY EXIST
+/*
+router.post("/sign-up", (req, res, next) => {
+  const { username, password } = req.body;
+  User.find({ username: username })
+    .then((foundUser) => {
+      console.log(foundUser);
+      if (foundUser.length !== 0) {
+        console.log("existing user " + foundUser);
+        res.redirect("/");
+      } else {
+        bcrypt
+          .genSalt(10)
+          .then((salt) => {
+            return bcrypt.hash(password, salt);
+          })
+          .then((hashedpassword) => {
+            return User.create({ username, password: hashedpassword });
+          })
+          .then(() => {
+            res.redirect("/");
+          })
+          .catch((error) => {
+            console.log(error);
+            next(error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+
+
+*/
