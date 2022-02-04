94e13b6aea97e92b68061f6bd0f9220ea3783300
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-02-04 16:34:55 +0100

    set up project like we saw in class

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 require('dotenv/config');
 
 // ℹ️ Connects to the database
-require('./db');
+require('./config/db.config.js');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
@@ -15,21 +15,54 @@ const hbs = require('hbs');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+// ℹ️ Middlewares
 
-// default value for title local
-const projectName = 'lab-express-basic-auth';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
+// https://www.npmjs.com/package/morgan
+const logger = require("morgan");
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
+// https://www.npmjs.com/package/cookie-parser
+const cookieParser = require("cookie-parser");
+
+// ℹ️ Serves a custom favicon on each request
+// https://www.npmjs.com/package/serve-favicon
+const favicon = require("serve-favicon");
+
+// Middleware configuration
+
+// In development environment the app logs
+app.use(logger("dev"));
+// To have access to `body` property in the request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// Normalizes the path to the views folder
+app.set("views", (__dirname, "..", "views"));
+// Sets the view engine to handlebars
+app.set("view engine", "hbs");
+// Handles access to the public folder
+app.use(express.static((__dirname, "..", "public")));
+
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const routes = require('./config/routes.config.js');
+app.use('/', routes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require('./error-handling')(app);
+app.use((req, res, next) => res.status(404).render('not-found'));
+
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err);
+  if (!res.headersSent) {
+    res.status(500).render('error');
+  }
+});
+
+// Connecting to server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
 
 module.exports = app;
-
--- config/db.config.js
+++ config/db.config.js
@@ -8,10 +8,18 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGO_URI, { useNewUrlParser: true })
   .then((x) => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
+    process.exit(0);
   });
+
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected on app termination');
+    process.exit(0);
+  });
+});
