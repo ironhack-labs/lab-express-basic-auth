a4df5d34ecbc9737794acf771a4f215941699139
Isa Gutierrez <isagr_kio@hotmail.com>
2022-02-05 01:46:20 +0100

    Basic structure changed.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 require('dotenv/config');
 
 // ℹ️ Connects to the database
-require('./db');
+require('./config/db.config');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
@@ -12,24 +12,44 @@ const express = require('express');
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
-
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
+// https://www.npmjs.com/package/morgan
+const logger = require("morgan");
 
-// default value for title local
-const projectName = 'lab-express-basic-auth';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
+// https://www.npmjs.com/package/cookie-parser
+const cookieParser = require("cookie-parser");
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+// ℹ️ Serves a custom favicon on each request
+// https://www.npmjs.com/package/serve-favicon
+const favicon = require("serve-favicon");
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const routes = require('./config/routes.config');
+app.use('/', routes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
-module.exports = app;
+// In development environment the app logs
+app.use(logger("dev"));
+// To have access to `body` property in the request
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// Normalizes the path to the views folder
+app.set("views", (__dirname, "..", "views"));
+// Sets the view engine to handlebars
+app.set("view engine", "hbs");
+// Handles access to the public folder
+app.use(express.static((__dirname, "..", "public")));
 
+// Connecting to server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
+
+module.exports = app;
