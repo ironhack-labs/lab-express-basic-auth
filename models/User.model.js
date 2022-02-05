2aee3c15c2c86c23b41b552cb7f55cd777846b37
joaonacho <joaonacho@gmail.com>
2022-02-05 14:44:08 +0000

    log in working. WIP sessions

--- models/User.model.js
+++ models/User.model.js
@@ -3,10 +3,15 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
+    required: [true, "Username is mandatory!"],
     type: String,
     unique: true,
+    trim: true,
   },
-  password: String,
+  password: {
+    required: [true, "You forgot to insert the password"],
+    type: String,
+  },
 });
 
 const User = model("User", userSchema);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -6,6 +6,8 @@ const User = require("../models/User.model");
 //Bcryptjs
 const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
+//Mongoose
+const mongoose = require("mongoose");
 
 //Routes
 //Loading Sign Up page
@@ -14,8 +16,26 @@ router.get("/signup", (req, res) => {
 });
 
 //Sign Up form
-router.post("/signup", (req, res) => {
+router.post("/signup", (req, res, next) => {
   const { username, password } = req.body;
+
+  //all fields are required
+  if (!username || !password) {
+    res.render("signup/signup", {
+      errorMessage: "All fields are mandatory!",
+    });
+    return;
+  }
+
+  //validating strenght of the password
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render("signup/signup", {
+      errorMessage:
+        "Password needs to have at least 6 characters and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
 
   bcryptjs
     .genSalt(saltRounds)
@@ -24,16 +44,56 @@ router.post("/signup", (req, res) => {
       return User.create({ username, password: hashPassword });
     })
     .then((createdUser) => {
-      // console.log(`New user -> ${createdUser}`);
       res.redirect("users/user-page");
     })
     .catch((error) => {
-      console.log(`there was an error signing up ${error}`);
-      //   res.redirect("sign-up/sign-up");
+      if (error instanceof mongoose.Error.ValidationError) {
+        res
+          .status(500)
+          .render("signup/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render("signup/signup", {
+          errorMessage:
+            "Username already registered. Please try using another username.",
+        });
+      } else {
+        next(error);
+      }
     });
 });
 
 //Loading User page
 router.get("/users/user-page", (req, res) => res.render("users/user-page"));
+
+//Log in
+//Loading Log in page
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//Log in form
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.render("login", {
+      errorMessage: "Sorry, you forgot to fill all the fields. Try again",
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("login", { errorMessage: "Username is not registered" });
+        return;
+      } else if (bcryptjs.compareSync(password, user.password)) {
+        res.render("users/user-page", { user });
+      } else {
+        res.render("login", { errorMessage: "Incorrect password" });
+      }
+    })
+    .catch((error) => next(error));
+});
 
 module.exports = router;
