1f67e887a9690623501420f533d7dea30f50b2e1
Juan Carlos Saldaña Herrero <jcarlossaldae@gmail.com>
2022-02-06 01:19:42 +0100

    iterations 1 and 2

--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,37 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs')
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const PASSWORD_PATTERN = /^.{8,}$/i
+const SALT_ROUNDS = 10
+
 const userSchema = new Schema({
   username: {
+    unique: true, 
     type: String,
-    unique: true
+    required: [true, 'Name is required']
   },
-  password: String
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    match: [PASSWORD_PATTERN, 'Password must contain at least 8 chars!']
+  }
 });
 
-const User = model("User", userSchema);
+userSchema.pre('save', function(next) {
+  const user = this;
 
-module.exports = User;
+  if (user.isModified('password')) {
+    bcrypt.hash(user.password, SALT_ROUNDS)
+      .then((hash) => {
+        user.password = hash
+        next()
+      })
+      .catch(err => next(err))
+  } else {
+    next()
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,71 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs/dist/bcrypt");
+const mongoose = require('mongoose');
+const User = require('../models/User.model')
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/register", (req, res, next) => {
+  res.render("auth/register");
+});
+
+router.post("/register", (req, res, next) => {
+  const user = { username, password } = req.body;
+
+  const renderWithErrors = (errors) => {
+    res.render('auth/register', {
+      errors: errors,
+      user: user
+    })
+  }
+
+  User.findOne({ username: username })
+    .then((userFound) => {
+      if (userFound) {
+        renderWithErrors({ email: 'Email already in use!' })
+      } else {
+        return User.create({ username, password }).then(() => res.redirect('/'))
+      }
+    })
+    .catch(err => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        renderWithErrors(err.errors)
+      } else {
+        next(err)
+      }
+    })
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
+
+router.post("/login", (req, res, next) => {
+  const user = { username, password } = req.body;
+
+  const renderWithErrors = (errors) => {
+    res.render('auth/login', {
+      errors: errors,
+      user: user
+    })
+  }
+
+  User.findOne({ username: username })
+    .then(user => {
+      if (!user) {
+        renderWithErrors({ username: 'User doesn\'t exist' })
+        return
+      } else if (bcrypt.compareSync(password, user.password)) {
+        res.send('Logged in!')
+      } else {
+        renderWithErrors({ password: 'Incorrect password' })
+      }
+    })
+    .catch(err => next(err))
+});
+
 
 module.exports = router;
