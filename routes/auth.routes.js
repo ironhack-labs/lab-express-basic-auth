8eb8401500b1829ed4cdd370a165b8be355b60e7
Mario Diaz <mariodzls@gmail.com>
2022-02-07 19:05:47 +0100

    Hecho

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -24,5 +24,33 @@ router.post("/registro", (req, res, next)=> {
     .catch(error => next(error))
 })
 
+router.get("/iniciar", (req, res, next) => res.render("register/init"))
+
+router.post("/iniciar", (req, res, next)=> {
+  const { username, password } = req.body
+
+  if (username.length === 0 || password.length === 0) {
+    res.render('register/init', { errorMessage: 'Por favor, rellena todos los campos' })
+    return
+  }
+
+  User
+    .findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render('register/init', { errorMessage: 'Nombre de usuario no registrado en la Base de Datos' })
+        return
+      } else if (bcryptjs.compareSync(password, user.password) === false) {
+        res.render('register/init', { errorMessage: 'La contraseña es incorrecta' })
+        return
+      } else {
+        req.session.currentUser = user
+        console.log('El objeto de EXPRESS-SESSION', req.session)
+        res.redirect('/perfil')
+      }
+    })
+})
+
+
 
 module.exports = router
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,19 @@ const res = require("express/lib/response")
 const bcryptjs = require('bcryptjs')
 
 const router = require("express").Router()
+const { isLoggedIn } = require('./../middleware/route-guard')
+router.get("/perfil", (req, res, next) => {
 
-router.get("/perfil", (req, res, next) => res.render("profile/profile"))
+    console.log(req.session.currentUser)
+    res.render("../views/profile/profile", {user: req.session.currentUser})
+})
+
+router.get('/principal', isLoggedIn, (req, res, next) => {       
+    res.render('../views/profile/main')
+})
+
+router.get('/privado', isLoggedIn, (req, res, next) => {       
+    res.render('../views/profile/private')
+})
 
 module.exports = router;
