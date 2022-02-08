2b90513d66d72feb928bc51bcb189b8e79cc317e
Hibaber <hiba.berber@gmail.com>
2022-02-08 09:03:52 +0100

    lab done

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,8 +3,12 @@ const router = require('express').Router()
 
 const {isLoggedIn} = require('../middleware/route-guard')
 
-router.get('/inicio-sesion', isLoggedIn, (req, res, next) => {
-    res.render('private', {user: req.session.currentUser})
+router.get('/main', isLoggedIn, (req, res, next) => {
+    res.render('user/main-page', {user: req.session.currentUser})
+})
+
+router.get('/private', isLoggedIn, (req, res, next) => {
+    res.render('user/private-page', {user: req.session.currentUser})
 })
 
 module.exports = router;
