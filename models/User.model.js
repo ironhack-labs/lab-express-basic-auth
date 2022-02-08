a889bdde1de0b5a71fe3774383c624ff70ab1683
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-02-08 18:50:32 +0000

    Ana Modesto WDPT NOV21
    Iteration 1, 2 and 3 (3 incomplete)

--- models/User.model.js
+++ models/User.model.js
@@ -4,11 +4,32 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    unique: true,
+    required: [true, 'Username is required.'],
+    trim: true,
   },
-  password: String
-});
+  passwordHash: {
+    type: String,
+    required: [true, 'Password is required.']
+  },  
+  email: {
+    type: String,
+    required: [true, 'Email is required.'],
+    // this match will disqualify all the emails with accidental empty spaces, missing dots in front of (.)com and the ones with no domain at all
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  },
+  
+  {
+    timestamps: true
+  }
+);
 
 const User = model("User", userSchema);
 
 module.exports = User;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.post('/', (req,res,next) =>{
+  const {username, password} = req.body;
+
+  User.create({username, password})
+  .then(() => {
+      res.redirect('/');
+  })
+  .catch(error => {res.render('layout.hbs'); next(error)});
 });
 
 module.exports = router;
