d33e555a298f9996e0531f7276287e60a8926dbe
denisesenguel <denisesenguel@gmail.com>
2022-02-10 16:54:07 +0100

    add protected routes /main and /private

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const router = require("express").Router();
+const isLoggedIn = require("../middleware/route-protection");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/main", isLoggedIn, (req, res) => {
+  res.render("main");
+});
+
+router.get("/private", isLoggedIn, (req, res) => {
+  res.render("private");
 });
 
 module.exports = router;
