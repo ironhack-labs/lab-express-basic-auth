b6a49c28d19eea16d08476fbc938ce480ed3e8de
danrie19 <nataliriera19@gmail.com>
2022-04-02 00:15:39 -0400

    done

---
+++ routes/auth.routes.js
@@ -0,0 +1,67 @@
+const router = require('express').Router();
+const User = require("../models/User.model")
+const bcryptjs = require("bcryptjs");
+
+router.get("/createUser", (req, res, next) => {
+    res.render("auth/signup");
+});
+
+
+router.post("/createUser", async (req, res, next) => {
+    const {username, password, ...rest} = req.body
+    try {
+        const salt = bcryptjs.genSaltSync(10)
+        const newPassword = bcryptjs.hashSync(password, salt)
+
+        const user = await User.create({
+            username,
+            password: newPassword
+        })
+
+        res.redirect(`/profile/${user._id}`)
+
+    } catch (error) {
+        console.log("error:", error)
+        res.send("Error")
+    }
+
+})
+
+router.post("/login", async(req, res, next) => {
+    //destructuring
+    const {email, username, password, ...rest} = req.body
+    try{
+        User.findOne({ email })
+            .then(user => {
+                if(!user){
+                    res.render('auth/login', {
+                        errorMessage: "Invalid email/password. Verify you are using the correct ones."
+                    })
+                } else if (bcryptjs.compareSync(password, user.password)) {
+                    req.session.currentUser = user;
+                    console.log('CURRENT USER =============>',req.session.currentUser)
+                    res.redirect(`/userProfile`)
+                } else {
+                    res.render('auth/login', {
+                        errorMessage: "Invalid email/password. Verify you are using the correct one."
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+    }catch(error){
+        console.log("error:",error)
+        res.send("Error amigos!!!")
+    }
+
+})
+
+
+router.get('/userProfile', (req, res) => {
+    res.render('user-profile', { userInSession: req.session.currentUser });
+});
+
+
+module.exports = router;
