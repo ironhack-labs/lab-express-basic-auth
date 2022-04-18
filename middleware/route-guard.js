554fb4351020e988177f8d9c1542973ed934d2b0
Clara Pardo <clara.pardo@hotmail.com>
2022-04-18 19:17:52 +0200

    done

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -1,7 +1,7 @@
 // ----- Check if its Loged IN
 
 const isLoggedIn = (req, res, next) => {
-    !req.session.currentUser ? res.render('auth/login', { errorMessage: 'Desautorizado' }): next()
+    !req.session.currentUser ? res.render('auth/login', { errorMessage: 'Desautorizado' }) : next()
 }
 
 module.exports = isLoggedIn
