73e856a9d82c6b0bef293e78e2f57da5db5669f4
lokzar <motart2505@gmail.com>
2022-05-09 22:08:49 -0500

    done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,15 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const User = require('../models/User.model')
+const isLoggedIn = require('../middleware/isLoggedIn')
+const isLoggedOut = require('../middleware/isLoggedOut')
 
 
-router.get("/auth/signup",(req,res,next)=>{
+router.get("/auth/signup", isLoggedOut,(req,res,next)=>{
     res.render("auth/signup")
 })
 
-router.post("/auth/signup",(req,res,next)=>{
+router.post("/auth/signup",isLoggedOut,(req,res,next)=>{
     const {password,username}=req.body //esos nombres vienen de lo que manda signup.hbs
     if(!username||!password){
         res.render("auth/signup", {errorMessage:"You must fill all the fields!"})
@@ -22,8 +24,6 @@ router.post("/auth/signup",(req,res,next)=>{
         return;
     }
 
-
-
     const saltRounds=12
 const salt = bcrypt.genSaltSync(saltRounds)
 const newPassword = bcrypt.hashSync(password,salt);
@@ -36,21 +36,22 @@ User.create(req.body)
     .catch(err=>{console.log("Error en crate: ", err)})
 })
 
-router.get("/auth/login", (req, res) => {
+router.get("/auth/login",isLoggedOut, (req, res) => {
     res.render("auth/login")
 })
 
-router.post("/auth/login",(req,res)=>{
+router.post("/auth/login", isLoggedOut,(req,res)=>{
     const {username,password}=req.body
     //console.log(username,password)
     User.findOne({username})
     .then(findedUser=>{
         //console.log(findedUser)
+        req.session.currentUser=findedUser
         if(!findedUser){
             res.render("auth/login", { errorMessage: "This username does not exist" })
                 return
         } else if(bcrypt.compareSync(password,findedUser.password)){
-            res.render('users/user-profile', { findedUser })
+            res.render('users/user-profile', { currentUser:findedUser })
         }else{
             res.render("auth/login",{errorMessage:'Incorrect Password.'})
         }
@@ -59,6 +60,15 @@ router.post("/auth/login",(req,res)=>{
     .catch(console.log)
 })
 
+router.post("/auth/logout", isLoggedIn, (req,res)=>{
+    req.session.destroy(err => {
+        if (err) {
+            console.log("entre al error")
+            next(err)
+        }
+        res.redirect('/');
+    });
+})
 
 
 module.exports=router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,30 @@
 const router = require("express").Router()
 const User = require('../models/User.model')
+const isLoggedIn = require('../middleware/isLoggedIn')
+const isLoggedOut = require('../middleware/isLoggedOut')
 
 router.get("/userWelcome", (req, res) => {
         res.render("users/user-welcome")
 })
 
 router.get("/userProfile", (req, res) => {
-    res.render("users/user-profile")
+    res.render("users/user-profile", {currentUser: req.sessionUser})
 })
+
+router.get("/main",isLoggedIn, (req, res) => {
+const isLoggedIn = req.session?.currentUser ? true :false
+        res.render("users/main", {isLoggedIn})
+})
+
+router.get("/private",isLoggedIn, (req, res) => {
+    const isLoggedIn = req.session?.currentUser ? true :false
+    const {_id}=req.session.currentUser
+        User.findById(_id)
+        .then(userFound=>{
+            console.log(userFound)
+            res.render("users/private", {userFound,isLoggedIn})
+        })
+    })
+
 
 module.exports = router
