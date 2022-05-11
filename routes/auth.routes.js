7a2a6a401a7b5ee4fa77910f9e21710d0244ba1a
Chloé Faurie <chloe.faurie@gmail.com>
2022-05-11 15:11:52 +0100

    iteration 1 done

---
+++ routes/auth.routes.js
@@ -0,0 +1,41 @@
+// routes/auth.routes.js
+
+const { Router } = require("express");
+const router = new Router();
+
+const User = require("../models/User.model");
+
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
+
+router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+
+// GET route ==> to display the signup form to users
+router.get("/signup", (req, res) => res.render("auth/signup"));
+
+// POST route ==> to process form data
+router.post("/signup", (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      return User.create({
+        // username: username
+        username,
+        email,
+        // passwordHash => this is the key from the User model
+        //     ^
+        //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+        passwordHash: hashedPassword,
+      });
+    })
+    .then((userFromDB) => {
+      // console.log('Newly created user is: ', userFromDB);
+      res.redirect("/userProfile");
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = router;
