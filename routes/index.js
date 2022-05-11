ef18ded0967057a5b1219383a1ff88cf2286d1db
Mariana Oliveira <marianalucas018@gmail.com>
2022-05-11 18:02:33 +0100

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
+const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", { user: req.session.currentUser });
+});
+
+router.get("/profile", (req, res, next) => {
+  res.render("profile", { user: req.session.currentUser });
+});
+
+router.get("/main", isLoggedIn, (req, res, next) => {
+  res.render("main");
+});
+
+router.get("/private", isLoggedIn, (req, res, next) => {
+  res.render("private");
 });
 
 module.exports = router;
