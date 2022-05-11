78c0361fa7222d30dc04f29b78cedd5fd7ace273
Chloé Faurie <chloe.faurie@gmail.com>
2022-05-11 17:08:20 +0100

    iteration 2 done

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require("./config")(app);
 
+// use session here:
+require("./config/session.config")(app);
+
 // default value for title local
 const projectName = "lab-express-basic-auth";
 const capitalized = (string) =>
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -8,7 +8,10 @@ const User = require("../models/User.model");
 const bcryptjs = require("bcryptjs");
 const saltRounds = 10;
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get("/userProfile", (req, res) => {
+  console.log(req.session.currentUser);
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
 
 // GET route ==> to display the signup form to users
 router.get("/signup", (req, res) => res.render("auth/signup"));
@@ -33,7 +36,40 @@ router.post("/signup", (req, res, next) => {
     })
     .then((userFromDB) => {
       // console.log('Newly created user is: ', userFromDB);
-      res.redirect("/userProfile");
+      res.redirect("/login");
+    })
+    .catch((error) => next(error));
+});
+
+// GET route ==> to display the login form to users
+router.get("/login", (req, res) => res.render("auth/login"));
+
+// POST login route ==> to process form data
+router.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+  //console.log("SESSION =====> ", req.session);
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "username is not registered. Try with other username.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        req.session.currentUser = user;
+        res.render("users/user-profile", {
+          userInSession: req.session.currentUser,
+        });
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
     })
     .catch((error) => next(error));
 });
