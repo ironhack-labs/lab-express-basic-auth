c777cd89e432e0f5249daec76df8e5d10b83f93b
Jessica Fernandez <jferasef@gmail.com>
2022-05-17 16:46:00 +0200

    logout sign in

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -22,8 +22,40 @@ router.post("/signup", (req, res, next) => {
     })
     .then((userFromDB) => {
       console.log("Newly created user is: ", userFromDB);
+      res.redirect("/userProfile");
     })
     .catch((error) => next(error));
+});
+
+router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+
+router.get("/signin", isLoggedOut, (req, res) => res.render("auth/signin"));
+
+router.post("/signin", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+  const { username, password } = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/signin", {
+      errorMessage: "Please enter both, username and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/signin", { errorMessage: "User not found" });
+        return;
+      } else if (bcryptjs.compareSync(password, user.password)) {
+        req.session.currentUser = user;
+        req.app.locals.currentUser = user;
+        res.render("user/user-profile", { user });
+      } else {
+        res.render("auth/signin", { errorMessage: "Incorrect password" });
+      }
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
