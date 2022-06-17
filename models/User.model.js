4bc1e3c599ae1db462e39a27c369e9a0f50be7f5
Vale Quiroga <valentinqramos@outlook.com>
2022-06-17 16:24:30 -0500

    Iteration 1

--- models/User.model.js
+++ models/User.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
   username: {type: String, unique: true,},
-  password: {type:String,required:true}
+  passwordHash: {type:String,required:true}
 }, {timestamps:true});
 
 const User = model("User", userSchema);
--- routes/signup.routes.js
+++ routes/signup.routes.js
@@ -7,4 +7,30 @@ router.get('/signup',(req, res, next) => {
   res.render('../views/signup.hbs')
 })
 
+router.post('/signup',(req, res, next) => {
+  const {username , password} = req.body
+
+  bcryptjs
+    .genSalt(saltRound)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPswd => {
+      return User.create({
+        username,
+        passwordHash: hashedPswd
+      })
+    })
+    .then(() => {
+      console.log('a new user was created')
+      res.redirect('/user')
+    })
+    .catch(error =>{
+      console.log(error)
+      next()
+    })
+})
+
+router.get('/user',(req, res, next) =>{
+  res.render('../views/user.hbs')
+})
+
 module.exports = router
