52941f478f72df2b55f5c0d1743cb7281a8db3c8
Natalia Buzila <nataliabuzila@gmail.com>
2022-06-18 11:02:16 +0300

    done lab

--- models/User.model.js
+++ models/User.model.js
@@ -4,9 +4,13 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
+    required: [true, 'Username is required.'],
     unique: true
   },
-  password: String
+  password: {
+    type: String,
+    required: [true, 'Password is required.']
+  }
 });
 
 const User = model("User", userSchema);
