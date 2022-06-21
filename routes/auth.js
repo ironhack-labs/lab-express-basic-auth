2a02e15e161349922d5f851e95100e25290c7ac5
zarahsara <sarahabbasi25@aol.com>
2022-06-21 16:25:35 +0100

    Iteration 2 | Login

--- routes/auth.js
+++ routes/auth.js
@@ -13,12 +13,60 @@ router.get("/signup", (req, res) => {
   const { username, password } = req.body;
   bcrypt.genSalt(saltRounds)
     .then(salt => bcrypt.hash(password, salt))
-    .then(hashedPassword => User.Login.create({username, password}))
+    .then(hashedPassword => User.create({username, password: hashedPassword}))
     .then((user) => {
      console.log(user)
     })
     .catch((err) => next(err));
-});
-     
   
-module.exports = router;
+router.get("/profile", (req, res) => {
+  res.render("users/users-profile");
+
+  router.post("/signup",(req, res, next) => {
+  const { username, password } = req.body;
+
+  
+  if (!username ||!password) {
+    res.render('auth/signup', {
+      errorMessage:
+      "enter your username, and password',
+    });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(400).render('auth/signup', {
+      errorMessage:
+        'The password must contain uppercase and lowercase letter, digits and be at least 6 characters long',
+    });
+    return;
+  }
+
+  router.get('/login', (req, res) => {
+    res.render('auth/signin');
+  });
+  
+  router.get('/profile', (req, res) => {
+    res.render('users/user-profile', { user: req.session.currentUser });
+  });
+  
+  router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+  
+    
+    if (!email || !password) {
+      res.render('auth/signin', {
+        errorMessage: 'Please fill all the fields',
+      });
+      return;
+    }
+  
+  
+    router.post('/logout', (req, res) => {
+      req.session.destroy((err) => {
+        res.redirect('/login');
+      });
+    });
+    
+    module.exports = router;
