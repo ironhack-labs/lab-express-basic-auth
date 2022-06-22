19c51e7d26bca04f661a0ab76a942ea9a4910437
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-06-22 20:25:42 +0200

    Iteration 2 done

--- config/session.config.js
+++ config/session.config.js
@@ -28,7 +28,6 @@ module.exports = (app) => {
       }, // ADDED code below !!!
       store: MongoStore.create({
         mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth"
-
         // ttl => time to live
         // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
       })
