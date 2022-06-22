8129d22c59719a3f823ccece54f20b5847775561
Tanvi Gurjar <tanvi.monitors@gmail.com>
2022-06-22 20:25:00 +0200

    fixed it, now able to see sessions in mongodb

--- config/session.config.js
+++ config/session.config.js
@@ -27,7 +27,7 @@ module.exports = (app) => {
         maxAge: 60000 // 60 * 1000 ms === 1 min
       }, // ADDED code below !!!
       store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/basic-auth"
+        mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth"
 
         // ttl => time to live
         // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
