b79a1b3aee04458459ae7afb8d9a9f70a2893d36
SamPetherick1898 <fuentessamuelignacio@gmail.com>
2022-07-07 12:55:07 -0500

    Tarea perfecta

--- models/User.model.js
+++ models/User.model.js
@@ -2,11 +2,16 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: {
+  usuario: {
     type: String,
-    unique: true
+    unique: true,
+    required: true,
+    minLength: 4
   },
-  password: String
+  contrasena: {
+    type: String,
+    required: true,
+  }
 });
 
 const User = model("User", userSchema);
