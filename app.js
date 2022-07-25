a9e0b49089d74f11595af5b817123a1888bab7cd
Randolph Roy <randolphroy@me.com>
2022-07-25 19:07:48 -0400

    RMT-WEBDEV-JUN2022-RANDOLPH ROY

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const hbs = require('hbs');
 
 const app = express();
 
+require('./config/session.config')(app);
+
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
@@ -27,6 +29,9 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const authRoutes = require('./routes/auth.routes'); 
+app.use('/', authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- models/User.model.js
+++ models/User.model.js
@@ -4,9 +4,14 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
+    trim: true,
+    required: [true, 'Username is required.'],
     unique: true
   },
-  password: String
+  password: {
+    type: String,
+  required: [true, 'please enter a password']
+  }
 });
 
 const User = model("User", userSchema);
