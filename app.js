5f3792596cd2ef78ec2b5ae099450d50b9dd26cc
Pavel Popov <pcveykos@gmail.com>
2022-09-07 12:19:20 +0200

    Add some more logging in functionality

--- app.js
+++ app.js
@@ -24,6 +24,23 @@ const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerC
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
+//Session configuration :)
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {maxAge: (1000 * 60 * 60 * 24)},
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI
+        })
+    })
+)
+// end of session config
+
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.post("/signup", (req,res,next) => {
   const {username, password} = req.body
   // hashing the password :0
   async function regUser() {
+    
     // make the salt
     const salt = await bcryptjs.genSalt();
     // salt the pass
@@ -28,7 +29,53 @@ router.post("/signup", (req,res,next) => {
     // redirect to wherever we want
     res.redirect('/')
   }
+  // async function validateEntries() {
+  //   const userFound = await User.findOne({username:username})
+  //   let message = ""
+  //   if (userFound !== undefined) {
+  //     message.push('Username already taken.')
+  //   }
+  //   if (password.length < 5) {
+  //     message.push('Password too short, must be at least 6 characters.')
+  //   }
+  //   if (message.length > 0) {
+  //     res.redirect("/signup/") // try not redirect but render
+  //   } else{
+  //     regUser();
+  //   }
+  // }
   regUser();
+})
+
+// logging in functionality
+
+router.get("/login", (req,res,next) => {
+  res.render("login")
+})
+
+router.post("/login", (req,res,next) => {
+  const {username, password} = req.body;
+  async function logIn() {
+    const userToLog = await User.findOne({username:username});
+    if (userToLog === null) {
+      //username is incorrect therefore userToLog is null instead of user object
+      res.render('login', {message:'Incorrect username'})
+      return
+    } else {
+      //username is correct and we continue with session creation;
+      if (bcryptjs.compareSync(password, userToLog.password)) {
+        // req.session is the key we get from express-session
+        // every route will pass through the session middleware because we put it in app.js
+        // .user is important to remember: we put this var name, its not fixed
+        req.session.user = userToLog;
+        res.redirect('/profile')
+      } else {
+        res.render('login', {message: 'Incorrect password'})
+        return
+      }
+    }
+  }
+  logIn();
 })
 
 module.exports = router;
