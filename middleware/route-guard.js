f495d90193d7007da31f248001f0fd18556af0ae
Rodrigo Gutierrez Pacheco <rodrigogutierrezpacheco@gmail.com>
2022-10-13 12:30:53 -0500

    done

---
+++ middleware/route-guard.js
@@ -0,0 +1,23 @@
+const isLoggedIn = (req, res, next) => {
+	if (!req.session.currentUser) {
+		return res.redirect('/auth/login');
+	}
+	next();
+};
+ 
+// if an already logged in user tries to access the login page it
+// redirects the user to the home page
+const isLoggedOut = (req, res, next) => {
+	if (req.session.currentUser) {
+		return res.redirect('/');
+	}
+	next();
+};
+const private = (req,res, next)=>{
+if (!req.session.currentUser){
+	return res.redirect("/")
+}
+next();
+}
+
+module.exports = {isLoggedIn,isLoggedOut, private};
