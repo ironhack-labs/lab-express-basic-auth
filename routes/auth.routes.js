001edfe0dbc64d0da7bb5bc227f46a74cdd8f4c4
goncaloarsenio <goncalo.arsenio92@gmail.com>
2022-11-02 18:02:42 +0000

    done, I think

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -99,6 +99,7 @@ router.post('/signup', async (req, res, next) => {
     });
   });
 
-
+  router.get('/main', isLoggedOut, (req, res) => res.render('main'));
+  router.get('/private', isLoggedIn, (req, res) => res.render('private'));
 
 module.exports = router;
