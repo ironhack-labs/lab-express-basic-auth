2e6519a5d9093d051dd6ddc3496c45e52746847d
Andrea Leonetti <ciroandrealeonetti@gmail.com>
2022-11-02 21:09:48 +0000

    couldn't solve it

--- models/User.model.js
+++ models/User.model.js
@@ -4,10 +4,17 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    unique: true
+    unique: true,
+   
   },
-  password: String
-});
+  password: String,
+  
+},
+{
+  timestamps: true,
+}
+
+);
 
 const User = model("User", userSchema);
 
