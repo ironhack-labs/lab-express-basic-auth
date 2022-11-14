9685a24db47c61c7cd01fdf55d973fa52b67deb3
Larisa Mancebo <larisa.mancebo@gmail.com>
2022-11-14 14:45:52 +0100

    .

--- models/User.model.js
+++ models/User.model.js
@@ -1,9 +1,13 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcryptjs')
+const SALT_ROUNDS = 10
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
   username: {
     type: String,
+    trim: true,
+    required: false,
     unique: true
   },
   password: String
