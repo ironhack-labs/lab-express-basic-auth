1e2472e9a13b4eaae647b4f64e9ad856bff4c513
Maksym Rusnak <rusnak.maksym05@gmail.com>
2022-11-15 18:32:26 -0500

    iteration1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,50 @@
 const router = require("express").Router();
 
+const bcryptjs = require('bcryptjs');
+
+const User = require('../models/User.model');
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get('/signup', (req, res, nex) => {
+  res.render('signup.hbs');
+});
+
+router.post('/signup', (req, res, nex) => {
+  console.log(req.body);
+
+  if (!req.body.username || !req.body.password) {
+      res.send('cant have empty fields');
+      return;
+  }
+
+User.findOne({username: req.body.username}) 
+.then(foundUser => {
+  if(foundUser) {
+    res.send('sorry user already exists')
+    return;
+  }
+
+  const myHashedPassword = bcryptjs.hashSync(req.body.password)
+  
+  return User.create({
+      username: req.body.username,
+      password: myHashedPassword
+  })
+})
+  .then((newUser) => {
+      console.log('new user created', newUser);
+      res.send(newUser);
+      res.render('signup.hbs')
+  })
+  .catch(err => {
+      console.log(err);
+      res.send(err);
+  })
+});
+
 
 module.exports = router;
