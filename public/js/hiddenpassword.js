5616a449b38380cd0a9611c97e11437c3401bfdb
Ervin Varga <vargaervinerno0@gmail.comgit>
2022-11-17 18:00:33 +0100

    show password woks

--- public/js/hiddenpassword.js
+++ public/js/hiddenpassword.js
@@ -1,8 +1,9 @@
 function myFunction() {
-    var x = document.querySelector("myInput");
+    var x = document.querySelector("#myInput");
     if (x.type === "password") {
       x.type = "text";
     } else {
       x.type = "password";
     }
   }
+
