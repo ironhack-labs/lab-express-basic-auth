f513cae9b6733cb735d2c651be6d11a0574e8891
cursedxp <anilozsoy@gmail.com>
2022-11-19 14:39:50 +0100

    Iteration 1 | Sign Up

---
+++ routes/signup.routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const User = require("../models/User.model");
+//Setup of bcryp
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+/* GET signup page */
+router.get("/", (req, res, next) => {
+  res.render("./auth/signup");
+});
+router.post("/", async (req, res, next) => {
+  const { userName, password } = req.body;
+
+  const passwordHash = await bcrypt.hash(password, saltRounds);
+
+  User.create({ username: userName, password: passwordHash }).then((data) => {
+    console.log("👶 New User Created");
+  });
+  res.render("./auth/signup");
+});
+
+module.exports = router;
