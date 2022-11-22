9a7f656c6def1f5d34359dbb44679a5e268ddbba
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-11-22 15:09:49 +0000

    iteration 1

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,6 +3,10 @@ const router = new Router();
 
 router.get('/signup', (req, res) => res.render('auth/signup'));
  
+router.post('/signup', (req, res, next) => {
+    console.log('The form data: ', req.body);
+  });
+   
  
 
 module.exports = router;
