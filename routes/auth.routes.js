85a2d82c2321117d42ed519f57713893fbd81c58
Arcadio REYES <arcadioreyesph@gmail.com>
2022-12-30 15:49:07 +0100

    Iteration 1 done

---
+++ routes/auth.routes.js
@@ -0,0 +1,38 @@
+const { Router } = require("express");
+const router = new Router();
+const User = require("../models/User.model");
+
+const bcryptjs = require("bcryptjs");
+const saltRounds = 10;
+
+// GET route ==> to display the signup form to users
+router.get("/signup", (req, res) => res.render("auth/signup"));
+
+// POST route ==> to process form data
+router.post("/signup", (req, res, next) => {
+  //   console.log("The form data: ", req.body);
+
+  const { username, email, password } = req.body;
+
+  bcryptjs
+    .genSalt(saltRounds)
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
+      console.log(`Password hash: ${hashedPassword}`);
+      return User.create({
+        username,
+        email,
+        passwordHash: hashedPassword,
+      });
+    })
+    .then((userFromDB) => {
+      //   console.log("Newly created user is: ", userFromDB);
+      res.redirect("/userProfile");
+    })
+    .catch((error) => next(error));
+});
+
+// Prodile page route
+router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+
+module.exports = router;
