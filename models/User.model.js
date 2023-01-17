9a8f3d889617793ca8427aff8d836a35569ed2d4
Philip Butland <Philip_Butland@yahoo.com>
2023-01-17 18:14:48 +0100

    Still not final. Ran out of time

--- models/User.model.js
+++ models/User.model.js
@@ -2,9 +2,12 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
+  // username: {
+  //   type: String,
+  //   unique: true
+  // },
   username: {
-    type: String,
-    unique: true
+    type: String
   },
   encryptedPassword: String
 });
--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,63 +1,88 @@
-const router = require("express").Router()
-const { genSalt } = require("bcrypt")
-const bcrypt = require("bcryptjs")
-const User = require("../models/User.model")
-const saltRounds = 10
+const router = require("express").Router();
+// const { genSalt } = require("bcrypt")
+const bcrypt = require("bcryptjs");
+const User = require("../models/User.model");
+const saltRounds = 10;
+const mongoose = require("mongoose");
 
-console.log("*** LOGIN.ROUTES STARTED ****")
+router.get("/signup", (req, res) => {
+  console.log(req.session);
+  data = { userInSession: req.session };
+  res.render("login/signup", data);
+});
 
-router.get("/signup",(req,res)=>{
-    res.render("login/signup")
-})
+router.get("/login", (req, res) => {
+  res.render("login/login");
+});
 
-router.get('/mainpage', (req, res, next) => {
-  res.render('mainpage')
-}) 
-router.get('/private', (req, res, next) => {
-  res.render('private')
-})
+// router.get('/mainpage', (req, res, next) => {
+//   res.render('mainpage')
+// })
+// router.get('/private', (req, res, next) => {
+//   res.render('private')
+// })
 
+router.post("/signup", (req, res) => {
+  console.log(req.body);
+  const { username, password } = req.body;
 
-
-router.post("/signup",(req,res)=>{
-    console.log(req.body)
-    const {email, password} = req.body
-
-    bcrypt
+  bcrypt
     .genSalt(saltRounds)
-    .then((salt)=>{
-        console.log(salt)
-        return bcrypt.hash(password,salt)
+    .then((salt) => {
+      return bcrypt.hash(password, salt);
     })
-    .then(hashedPassword=>{
-      console.log (hashedPassword)
+    .then((hashedPassword) => {
       return User.create({
-        email:email,
-        encryptedPassword:hashedPassword
-      })
+        username: username,
+        encryptedPassword: hashedPassword,
+      });
     })
+    .then(res.redirect("/"))
 
-    .then(newUser => {
-      console.log(newUser)
-      if (email && password) {
-        console.log("move to main page")
-        res.redirect("/mainpage")
+    // .then(newUser => {
+    //   console.log("newUser: ", newUser)
+    //   if (email && password) {
+    //     console.log("move to main page")
+    //     res.redirect("/mainpage")
+    //   }
+    // })
+
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+  console.log("SESSION =====>", req.session);
+
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.render("auth/login", {
+      errorMessage: "please enter an email or password",
+    });
+    return;
+  }
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage:
+            "User not found please sign up. No account associated with email",
+        });
+      } else if (bcrypt.compareSync(password, user.encryptedPassword)) {
+        req.session.currentUser = user;
+        res.redirect("/");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect Password" });
       }
     })
 
-    .catch((error)=>{
-      console.log(error)
-  })
+    .then(res.redirect("/"))
 
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
-
-
-
-})
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
