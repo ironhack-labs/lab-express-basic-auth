f9863e780f64683250b7a192f757e250d7dedc06
Daniel Olea <oleaonize@gmail.com>
2023-01-25 21:25:01 +0100

    Ejercicio OK con modificaciones de clase

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,6 @@
 const User = require("../models/User.model");
-const bcryptjs = require('bcryptjs');
-const saltRounds = 10;
+// const bcryptjs = require('bcryptjs');
+// const saltRounds = 10;
 
 module.exports.create = (req, res, next) => {
 res.render("user/form")
@@ -11,26 +11,28 @@ res.render("user/profile")
 }
 
 module.exports.doCreate = (req, res, next) => {
-const { username, password } = req.body;
+//const { username, password } = req.body;
 
-//const hashedPassword = bcryptjs.hashSync(password, salt);
-
-//console.log(`Password hash: ${hashedPassword}`);
-
-bcryptjs.genSalt(saltRounds)
-.then(salt => bcryptjs.hash(password, salt))
-.then(hashedPassword => {
-    return User.create({
-        username,
-        password: hashedPassword
-    });
-})
-.then(( )=>{
-    //console.log('Newly created user is: ', user);
-    res.redirect("/profile")
-})
+User.create(req.body)
+.then((user) => res.redirect("/profile"))
 .catch(error => next(error));
 }
+
+// bcryptjs.genSalt(saltRounds)
+// .then(salt => bcryptjs.hash(password, salt))
+// .then(hashedPassword => {
+// //     return 
+//     User.create({
+//         username,
+//         password: hashedPassword
+//     });
+// })
+// .then(( )=>{
+//     //console.log('Newly created user is: ', user);
+//     res.redirect("/profile")
+// })
+// .catch(error => next(error));
+// }
 
 
 
--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,9 @@
 const { Schema, model } = require("mongoose");
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+const EMAIL_PATTERN =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
@@ -9,7 +14,28 @@ const userSchema = new Schema({
   },
   password: {
   type: String,
-  required: [true, 'Password is required.']
+  required: [true, 'Password is required.'],
+  minlength: [8, 'Your password must have at least 8 characters']
+  },
+  email: {
+    type: String,
+    match: EMAIL_PATTERN,
+    required: [true, 'Email is required'],
+    unique: [true, 'Email is already in use'],
+  },
+});
+
+userSchema.pre('save', function(next) {
+  const rawPassword = this.password;
+  if (this.isModified('password')) {
+    bcryptjs.hash(rawPassword, saltRounds)
+      .then(hash => {
+        this.password = hash;
+        next()
+      })
+      .catch(err => next(err))
+  } else {
+    next();
   }
 });
 
