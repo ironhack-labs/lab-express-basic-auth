751e6fd92de200553eb15309ff8df385916f7599
Barbara Rodrigues <barbara.rodrigues@cabify.com>
2023-01-28 13:25:29 +0100

    it1 progress

--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,48 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 10; // Cuantas veces se hashea el codigo (esta bien entre 10-14)
+const EMAIL_PATTERN =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
+const userSchema = new mongoose.Schema( 
+  {
+    name: {
     type: String,
-    unique: true
+    unique: true,
+    required: [true, "Name is required"]
   },
-  password: String
+
+    email: {
+      type: String,
+      match: EMAIL_PATTERN,
+      required: [true, "Email is required"], 
+      unique: [true, "Email is already in use"],
+    },
+
+  password: {
+    type: String,
+    required: [true, "password is required"],
+    minlength: [8, "Your password must have at least 8 characters"],
+  }
+
 });
 
-const User = model("User", userSchema);
+userSchema.pre("save", function(next){
+  const rawPassword = this.password;
+  if ( this.isModified("password")){
+    bcrypt.hash(rawPassword, SALT_ROUNDS)
+    .then(hash => {
+      this.password= hash;
+      next()
+    })
+    .catch (err => next(err))
+  }
+  else{
+    next();
+  }
+})
+
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
