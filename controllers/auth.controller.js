6ae1ea728c4b0caacabe8289adce835084218da2
silvi-hdez <silviahdez.b@gmail.com>
2023-01-29 18:46:21 +0100

    iteration 1 done

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,5 +3,37 @@ const User = require ('../models/User.model')
 
 
 module.exports.signup = (req, res) => {
-    res.render('../views/auth/signup.hbs')
+    res.render('auth/signup.hbs')
 }
+
+module.exports.doSignup = (req, res, next) => {
+    const renderWithErrors = (errors) => {
+      res.render('auth/signup', {
+        user: {
+          email: req.body.email,
+          username: req.body.username
+        },
+        errors
+      })
+    }
+  
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+         
+          return User.create(req.body)
+            .then(user => {
+              res.redirect('/')
+            })
+        } else {
+          renderWithErrors({ email: 'Email already in use' })
+        }
+      })
+      .catch(err => {
+        if (err instanceof mongoose.Error.ValidationError) {
+          renderWithErrors(err.errors)
+        } else {
+          next(err)
+        }
+      })
+  }
