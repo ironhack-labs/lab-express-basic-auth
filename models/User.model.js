c4c42fdc069003f839e602b5cbda6559d8980ef3
gulsahbayazit <bayazitgulsahbayazit@gmail.com>
2023-02-08 17:42:28 +0100

    [FTWD]Gülsah Bayazit

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,41 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true
+// const userSchema = new Schema({
+//   username: {
+//     type: String,
+//     unique: true
+//   },
+//   password: String
+// });
+
+// const User = model("User", userSchema);
+
+// module.exports = User;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: false,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
-  password: String
-});
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const router = require("express").Router();
+const express = require("express");
+const { isLoggedIn } = require("../middleware/route-guard");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+// Profile page
+router.get("/profile", isLoggedIn, (req, res, next) => {
+  // When you want to use the user info in a view
+  // Access logged in user
+  const user = req.session.user;
+  res.render("profile", { user: user });
 });
 
 module.exports = router;
