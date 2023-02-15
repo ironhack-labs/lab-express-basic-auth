346e72e3f1a36752a0e1efb7c0e79b2f4aa4ef93
waggnerl <wagnermflira@gmail.com>
2023-02-15 16:12:14 +0000

    feat:Include changes at model

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,20 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      require: true,
+    },
+    password: String,
+    require: true,
   },
-  password: String,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = model("User", userSchema);
 
