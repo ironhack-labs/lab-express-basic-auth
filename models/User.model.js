a876019ff41c45f5b43e7840292daed7d8c0cc02
Pedro Leal <pmclleal@gmail.com>
2023-02-15 20:46:29 +0000

    Lab done

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,21 @@
 const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
+const userSchema = new Schema(
+  {username: {
     type: String,
-    unique: true
+    unique: true,
+    trim: true,
   },
-  password: String
-});
+  password: {
+    type: String,
+    require: true
+  }}, 
+  {
+    timestamps: true
+  }
+
+);
 
 const User = model("User", userSchema);
 
