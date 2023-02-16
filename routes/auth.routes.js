213a15544d4de35ae7af3fa7847d98e685389172
Rafaela Umbelino <rafaelaaumbelino@gmail.com>
2023-02-16 08:49:01 +0000

    Fixed issue with button to home page

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = require("../models/User.model");
-const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard');
+const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard");
 
 router.get("/signup", (req, res) => res.render("auth/signup"));
 
@@ -27,61 +27,57 @@ router.post("/signup", async (req, res, next) => {
         errorMessage: error.message,
       });
 
-    console.log(error);
-    next(error);
+      console.log(error);
+      next(error);
+    }
   }
- }
 });
 
-router.get("/login", (req, res) => res.render("auth/login")); // Se mantiver isLoggedOut,
+// Se incluir o isLoggedOut a página de login não abre, não consegui resolver este problema.
+router.get("/login", (req, res) => res.render("auth/login"));
 
 router.post("/login", async (req, res, next) => {
   try {
+    let { username, password } = req.body;
 
-    let {username, password} = req.body;
-
-    if (!username || !password){
-        res.render('auth/login', { errorMessage: 'Please input all the fields' });
+    if (!username || !password) {
+      res.render("auth/login", { errorMessage: "Please input all the fields" });
     }
 
     let user = await User.findOne({ username });
 
-    if(!user){
-        res.render('auth/login', { errorMessage: 'Account does not exist' });
-    } else if (bcrypt.compareSync(password, user.password)){
-        req.session.user = user;
-        console.log(user);
-    res.redirect('/main');
-
+    if (!user) {
+      res.render("auth/login", { errorMessage: "Account does not exist" });
+    } else if (bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
+      console.log(user);
+      res.redirect("/main");
     } else {
-        res.render('auth/login', { errorMessage: 'Wrong credentials' });
-    } 
-
+      res.render("auth/login", { errorMessage: "Wrong credentials" });
+    }
   } catch (error) {
     console.log(error);
     next(error);
   }
 });
 
-router.get('/main', isLoggedIn, (req, res) => {
-    let user = req.session.user;
+router.get("/main", isLoggedIn, (req, res) => {
+  let user = req.session.user;
 
-    res.render('main', user);
-
+  res.render("main", user);
 });
 
-router.get('/private', isLoggedIn, (req, res) => {
-    let user = req.session.user;
+router.get("/private", isLoggedIn, (req, res) => {
+  let user = req.session.user;
 
-    res.render('private', user);
-
+  res.render("private", user);
 });
 
-router.post('/logout', (req, res, next) => {
-    req.session.destroy((err) => {
-      if (err) next(err);
-      else res.redirect('/');
-    });
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    else res.redirect("/");
   });
+});
 
 module.exports = router;
