79c4b61f7c727dc572c2a63711fd005bf8531eda
Wilkins Morales <wilkinscom@gmail.com>
2023-02-23 22:56:44 +0200

    fix iteration 1

--- db/index.js
+++ db/index.js
@@ -5,12 +5,16 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth";
+const MONGO_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/lab-express-basic-auth";
 
 mongoose
+  .set("strictQuery", false)
   .connect(MONGO_URI)
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,16 +3,20 @@ const router = new Router();
 const User = require("../models/User.model");
 
 const bcrypt = require("bcrypt");
-// what those 10 mean is
+// what those 10 mean is - difficulty
 const saltRounds = 10;
 
 //render signup page
-router.get("/signup", (req, res, next) => {
-    res.render('/auth/signup/')
+router.get("/sign-up", (req, res, next) => {
+  res.render("auth/sign-up");
 });
 
 router.post("/signup", (req, res, next) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body;
+});
+
+router.get("/user-profile", (req, res, next) => {
+  res.render("auth/user-profile");
 });
 
 module.exports = router;
