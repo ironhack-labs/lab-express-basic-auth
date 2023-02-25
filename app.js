424a59c2d006a84c2b98c4477f997b8066793d23
LKTOKB <theodiamanttt@gmail.com>
2023-02-25 09:49:03 +0200

    First iteration done

--- app.js
+++ app.js
@@ -31,5 +31,7 @@ app.use('/', index);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
+
+
 module.exports = app;
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,67 @@
-const router = require("express").Router();
+const { Router } = require('express');
+const router = new Router();
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+const User = require('../models/User.model');
+const mongoose = require('mongoose'); // <== has to be added
+
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-module.exports = router;
+// GET route ==> to display the signup form to users
+router.get('/signup', (req, res) => res.render('auth/signup'));
+
+// POST route ==> to process form data
+router.post('/signup', (req, res, next) => {
+    // console.log("The form data: ", req.body);
+   
+    const { username, email, password } = req.body;
+     // make sure users fill all mandatory fields:
+  if (!username || !email || !password) {
+    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+    return;
+  }
+   // make sure passwords are strong:
+   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+   if (!regex.test(password)) {
+     res
+       .status(500)
+       .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+     return;
+   }
+    bcryptjs
+      .genSalt(saltRounds)
+      .then(salt => bcryptjs.hash(password, salt))
+      .then(hashedPassword => {
+        return User.create({
+          // username: username
+          username,
+          email,
+          // passwordHash => this is the key from the User model
+          //     ^
+          //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
+          passwordHash: hashedPassword
+        });
+      })
+      .then(userFromDB => {
+        console.log('Newly created user is: ', userFromDB);
+        res.redirect('/userProfile');
+      })
+      .catch(error => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          res.status(500).render('auth/signup', { errorMessage: error.message });
+        } else if (error.code === 11000) {
+          res.status(500).render('auth/signup', {
+             errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+          });
+        } else {
+          next(error);
+        }
+      }); // close .catch()
+  }) // close .post()
+
+module.exports = router
