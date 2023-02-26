18c27d5181fd0976de42937d193a8816d4524ea9
Wilkins Morales <wilkinscom@gmail.com>
2023-02-26 18:34:32 +0200

    iteration 1 - sign-up

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,18 +1,50 @@
 const { Router } = require("express");
 const router = new Router();
 const User = require("../models/User.model");
+const mongoose = require("mongoose");
 
-const bcrypt = require("bcrypt");
-// what those 10 mean is - difficulty
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 //render signup page
 router.get("/sign-up", (req, res, next) => {
-  res.render("auth/sign-up");
+  res.render("auth/sign-up").catch((err) => next(err));
 });
 
-router.post("/signup", (req, res, next) => {
-  const { username, password } = req.body;
+router.post("/sign-up", (req, res, next) => {
+  const { username, pass } = req.body;
+
+  // check if user is already taken
+  if (!username || !pass) {
+    res.render("auth/sign-up", { errorMessage: "Please fill in all fields." });
+  } else {
+    bcrypt
+      .genSalt(saltRounds)
+      .then((salt) => bcrypt.hash(pass, salt))
+      .then((hashPass) => {
+        return User.create({
+          username: username.toLowerCase(),
+          password: hashPass,
+        });
+      })
+      .then((newUser) => {
+        console.log(newUser);
+        res.redirect("/user-profile");
+      })
+      .catch(error => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          res
+            .status(500)
+            .render("auth/sign-up", { errorMessage: error.message });
+        } else if (error.code === 11000) {
+          res.status(500).render("auth/sign-up", {
+            errorMessage: `Username ${username} is already used.`,
+          });
+        } else {
+          next(error);
+        }
+      });
+  }
 });
 
 router.get("/user-profile", (req, res, next) => {
--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`🏃running on http://localhost:${PORT}`);
 });
