64c46ce35b480f87901727b818d46ba330dd3196
ApollineStev <apollinecanard@gmail.com>
2023-02-27 17:35:06 +0100

    finish iteration 3

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,9 +7,11 @@ const saltRounds = 10
 const User = require('../models/User.model')
 const mongoose = require('mongoose')
 
-router.get('/signup', (req, res) => res.render('auth/signup'))
+const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js")
 
+router.get('/signup', isLoggedOut, (req, res) => res.render('auth/signup'))
 
+// Sign up routes
 
 router.post('/signup',(req, res, next) => {
      const { username, email, password } = req.body
@@ -51,6 +53,61 @@ router.post('/signup',(req, res, next) => {
      })
 })
 
-router.get('/userProfile', (req, res) => res.render('auth/user-profile'))
+////////////////////////////////LOGIN////////////////////////////
+// login routes
+
+router.get('/login', (req, res, next) => res.render('auth/login'))
+
+router.post('/login', (req, res, next) =>{
+   console.log('SESSION ===> ', req.session)
+
+   const { email, password } = req.body
+
+   if( email === '' || password === "" ){
+      res.render('/auth/login', {errorMessage: 'Please enter both, email and password to log in'})
+      return
+   }
+
+   User.findOne({ email })
+   .then(user => {
+      if(!user){
+         res.render('auth/login', {errorMessage: 'Email is not registered'})
+         return
+      } else if (bcryptjs.compareSync(password, user.passwordHash)){
+         //res.render('auth/user-profile', {user})
+         req.session.currentUser = user;
+         res.redirect('/userProfile');
+      } else {
+         res.render('auth/login', {errorMessage: 'incorect password'})
+      }
+   })
+   .catch(error => next(error))
+
+ })
+
+router.get('/userProfile',isLoggedIn, (req, res) => {
+   res.render('auth/user-profile', {userInSession: req.session.currentUser})
+})
+
+
+//////////////////////// Log Out //////////////////////
+
+router.post('/logout', (req,res,next) => {
+   req.session.destroy(err => {
+      if (err) next(err)
+      res.redirect('/')
+   })
+})
+
+
+////////////////////// Main routes ///////////////////
+
+router.get('/main', isLoggedIn, (req, res, next) => {
+   res.render('auth/main' , {userInSession: req.session.currentUser})
+})
+
+router.get('/private', isLoggedIn, (req, res, next) => {
+   res.render('auth/private' , {userInSession: req.session.currentUser})
+})
 
 module.exports = router
