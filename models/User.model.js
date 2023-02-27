d35bdd39fc6303d4132c0ae77ff1a77a097fe6d0
ApollineStev <apollinecanard@gmail.com>
2023-02-27 11:04:17 +0100

    user authentification

--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+var uniqueValidator = require('mongoose-unique-validator')
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
@@ -11,16 +12,22 @@ const userSchema = new Schema({
   email: {
     type: String,
     required: [true, 'email is required'],
+    match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
     unique: true,
     lowercase: true,
     trim: true
   },
-  password: {
+  passwordHash: {
     type: String,
     required: [true, 'Password is required']
   }
   
+},
+{
+  timestamps: true
 });
+
+userSchema.plugin(uniqueValidator)
 
 const User = model("User", userSchema);
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const router = new Router()
-const User = require('../models/User.model')
 
 const bcryptjs = require('bcryptjs')
 const saltRounds = 10
+
+const User = require('../models/User.model')
+const mongoose = require('mongoose')
 
 router.get('/signup', (req, res) => res.render('auth/signup'))
 
@@ -11,6 +13,17 @@ router.get('/signup', (req, res) => res.render('auth/signup'))
 
 router.post('/signup',(req, res, next) => {
      const { username, email, password } = req.body
+     if (!username || !email || !password) {
+      res.render('auth/signup', {errorMessage: 'All fields are mandatory. Please provide name, email and password.'})
+     return
+     }
+     const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+     if (!regex.test(password)) {
+      res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to be stronger!'})
+      return
+     }
 
      bcryptjs
      .genSalt(saltRounds)
@@ -19,14 +32,23 @@ router.post('/signup',(req, res, next) => {
         return User.create({
             username,  
             email,
-            password: hashedPassword
+            passwordHash: hashedPassword
         })
      })
      .then((userFromDB) => {
         console.log('New user created: ', userFromDB)
         res.redirect('/userProfile')
      })
-     .catch(error => next(error))
+     .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError){
+         res.status(500).render('auth/signup', {errorMessage: error.message})
+      }else if(error.code === 11000 ) {
+         res.status(500).render('auth/signup', { errorMessage: 'Username and email need to be unique!'})
+
+      } else {
+         next(error)
+      }
+     })
 })
 
 router.get('/userProfile', (req, res) => res.render('auth/user-profile'))
