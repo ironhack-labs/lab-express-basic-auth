ae84ab018047652a8bcc1235f7a7bb604515aa02
YinongLi <yinong.li97@gmail.com>
2023-02-27 21:29:43 +0000

    iteration1 -3

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
 require('dotenv/config');
 
 // ℹ️ Connects to the database
 require('./db');
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require('express');
 
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
+require('./config/session.config')(app);
 require('./config')(app);
 
 // default value for title local
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,26 +4,92 @@ const User = require('../models/User.model');
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const mongoose = require('mongoose');
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
 
-router.get('/signup', (req, res) => {
+
+router.get('/signup', isLoggedOut,(req, res) => {
     res.render('auth/signup');
     }
 );
 
 router.post('/signup', (req, res) => {
     const {username, password}  = req.body;
+    const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+    if (!regex.test(password)) {
+        res
+        .status(500)
+        .render('auth/signup', { error: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+        return;
+    }
+
+
     bcrypt
         .genSalt(saltRounds)
         .then(salt => bcrypt.hash(password, salt))
         .then(hashedPassword => {
+            res.render('auth/login')
             return User.create({
                 username,
                 password: hashedPassword
             });
         })
         .catch(error => {
-                next(error);
+            if (error instanceof mongoose.Error.ValidationError) {
+              res.status(500).render('auth/signup', { errorMessage: error.message });
+            } else if (error.code === 11000) {
+              res.status(500).render('auth/signup', {
+                 errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+              });
+            } else {
+              next(error);
             }
-        );
+        }); 
     }
 );
+
+router.get('/login', (req, res) => {
+    res.render('auth/login');
+    }
+);
+router.post('/login', (req, res) => {
+    console.log('SESSION =====> ', req.session);
+    const {username, password} = req.body;
+    if (username === '' || password === '') {
+        res.render('auth/login', { error: 'Please enter both, username and password to login.' });
+        return;
+    }
+    User.findOne({ username })
+        .then(user => {
+            if (!user) {
+                res.render('auth/login', { error: 'Username is not registered. Try with other username.' });
+                return;
+            } else if (bcrypt.compareSync(password, user.password)) {
+                req.session.currentUser = user;
+                res.redirect('/userProfile');
+            } else {
+                res.render('auth/login', { error: 'Incorrect password.' });
+            }
+        })
+        .catch(error => next(error));
+    }
+);
+
+router.get('/userProfile', isLoggedIn,(req, res) => {
+    res.render('users/user-profile', { userInSession: req.session.currentUser });
+    }
+);
+
+router.post('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) next(err);
+        res.redirect('/');
+      });
+    }
+);
+
+
+
+
+
+
+module.exports = router;
