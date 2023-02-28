68427e52d9c972a179b409c290863de8429b88d9
michael07200 <mickmhtong@gmail.com>
2023-02-28 18:46:36 +0100

    build: Iteration 1

---
+++ routes/auth.routes.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+const bcrypt = require("bcryptjs");
+const router = new Router();
+const User = require("../models/User.model");
+
+const saltRounds = 10;
+
+router.get("/signup", (req, res) => res.render("auth/signup"));
+
+router.get("/userProfile", (req, res) => res.render("auth/user-profile"));
+
+//POST
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      res.render("auth/signup", {
+        errorMessage: "All fields are mandatory. Please provide your username and password.",
+      });
+      return;
+    }
+
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    await User.create({
+      username,
+      password: hashedPassword,
+    });
+
+    res.redirect("/userProfile");
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
