2281f9d5bd6e0e0a69b3632275dab50d230335b0
Noa Leon <n.leon.carrasco@gmail.com>
2023-03-01 16:41:03 +0100

    e.e'

--- config/session.config.js
+++ config/session.config.js
@@ -11,8 +11,6 @@ module.exports = app => {
             resave: true,
             saveUninitialized: false,
             cookie: {
-                sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-                secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
                 maxAge: 60000
             },
