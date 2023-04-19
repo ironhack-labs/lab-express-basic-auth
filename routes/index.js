dfc9ee4f1be719311801674c0ca2c77ab3b2b46e
José Antonio Escudero García <jaeg90@gmail.com>
2023-04-19 21:13:52 +0200

    Iteration 1 in progress

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,21 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+/* Sign up */
+router.get("/sign-up", (req, res, next) => {
+  res.render("sign-up");
+});
+
+router.post("/sign-up", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.render("sign-up", { errorMessage: "Por favor completa los campos" });
+      return;
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
