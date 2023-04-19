eec02d5dbf60d6222e9917e65f6f7f6f450e2d73
Lena Imdahl <mail@lenaimdahl.de>
2023-04-19 08:40:15 +0200

    fixed re.direct routes, added catch with err

--- routes/auth.js
+++ routes/auth.js
@@ -12,19 +12,25 @@ router.get("/profile", (req, res) => {
 });
 
 router.post("/signup", async (req, res, next) => {
-  // const{email,password} = req.body;
+  try {
+    const salt = await bcryptjs.genSalt(saltRounds);
+    console.log(salt);
 
-  const salt = await bcryptjs.genSalt(saltRounds);
-  console.log(salt);
+    const hash = await bcryptjs.hash(req.body.password, salt);
+    console.log(hash);
 
-  const hash = await bcryptjs.hash(req.body.password, salt);
-  console.log(hash);
+    // also a way of creating and saving new user:
+    // const newUser = new User({ username: req.body.username, password: hash });
+    // await newUser.save();
 
-  const newUser = new User({ username: req.body.username, password: hash });
-  console.log(newUser);
-  await newUser.save();
+    //short way to create and save new user:
+    await User.create({ username: req.body.username, password: hash });
 
-  res.redirect("/profile");
+    res.redirect("/auth/profile");
+  } catch (err) {
+    console.log("there was an error", err);
+    res.redirect("/auth/profile");
+  }
 });
 
 module.exports = router;
