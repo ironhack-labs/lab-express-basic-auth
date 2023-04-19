b5ba74539f1b299dcace311e6f8042ccf76ff8c8
Diana Kalstein <dianakalstein@gmail.com>
2023-04-19 10:41:59 +0200

    Create lab

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
+const User = require("..models/User.model")
+const bcrypt = require("bcryptjs") 
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/auth/signup", (req, res, next) => {
+  res.render("signup");
+});
+
+router.post("/auth/signup", (req, res, next) => {
+const { username, password} = req.body
+
 });
 
 module.exports = router;
