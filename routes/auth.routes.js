810cf7ec4b7674205288505bf29e41b86d9650d1
Ayoze Martin <ayozemt@gmail.com>
2023-04-21 01:35:46 +0200

    express-session

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,9 +9,22 @@ router.get("/signup", (req, res) => res.render("auth/signup"));
 
 // POST route ==> to process form data
 router.post("/signup", (req, res, next) => {
-  // console.log('The form data: ', req.body);
-
   const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.render("auth/signup", { errorMessage: "All fields are mandatory." });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+  if (!regex.test(password)) {
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.",
+    });
+    return;
+  }
 
   bcryptjs
     .genSalt(saltRounds)
@@ -26,9 +39,55 @@ router.post("/signup", (req, res, next) => {
       //   console.log("Newly created user is: ", userFromDB);
       res.redirect("/userProfile");
     })
+    .catch((error) => {
+      if (error.code === 11000) {
+        res.status(500).render("auth/signup", {
+          errorMessage:
+            "Username needs to be unique. Username is already used.",
+        });
+      } else {
+        next(error);
+      }
+    });
+});
+
+// GET route ==> to display the login form to users
+router.get("/login", (req, res) => res.render("auth/login"));
+
+// POST login route ==> to process form data
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+  const { username, password } = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, username and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Username is not registered.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // res.render("users/user-profile", { user });
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        res.render("auth/login", {
+          errorMessage: "Your password is incorrect",
+        });
+      }
+    })
     .catch((error) => next(error));
 });
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get("/userProfile", (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
 
 module.exports = router;
