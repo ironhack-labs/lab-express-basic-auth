ffa762bafe23b13bcac4d609cf0aa4196adb2d62
Jon <jon_ander_23@protonmail.com>
2023-04-21 20:37:47 +0200

    Last version

--- config/session.config.js
+++ config/session.config.js
@@ -1,43 +1,11 @@
-// const session = require("express-session");
-// const MongoStore = require("connect-mongo");
-
-// module.exports = (app) => {
 //   // https://stackoverflow.com/questions/23413401/what-does-trust-proxy-actually-do-in-express-js-and-do-i-need-to-use-it
 //   // https://expressjs.com/en/guide/behind-proxies.html
-//   app.set("trust proxy", 1);
-//   app.use(
-//     session({
-//       secret: process.env.SESSION_SECRET, // String value used to sign session cookies
-//       resave: true, // Forces the session to be saved back to the session store, even if the session was never modified during the request.
-//       saveUninitialized: false, // Forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified. Choosing false is useful for implementing login sessions, reducing server storage usage
-//       cookie: {
-//         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // https://web.dev/samesite-cookies-explained/
-//         secure: process.env.NODE_ENV === "production",
-//         httpOnly: true,
-//         maxAge: 60000, // 1 min
-//       },
-//       // Save the session in the Database (sessions collection)
-//       store: MongoStore.create({
-//         mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/session",
-//       }),
-//     })
-//   );
-// };
 
-// require session
 const session = require("express-session");
 
-// since we are going to USE this middleware in the app.js,
-// let's export it and have it receive a parameter
 module.exports = (app) => {
-  // <== app is just a placeholder here
-  // but will become a real "app" in the app.js
-  // when this file gets imported/required there
-
-  // required for the app when deployed to Heroku (in production)
   app.set("trust proxy", 1);
 
-  // use session
   app.use(
     session({
       secret: process.env.SESS_SECRET,
@@ -47,7 +15,7 @@ module.exports = (app) => {
         sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         secure: process.env.NODE_ENV === "production",
         httpOnly: true,
-        maxAge: 60000, // 60 * 1000 ms === 1 min
+        maxAge: 60000,
       },
     })
   );
--- middlewares/route-guard.js
+++ middlewares/route-guard.js
@@ -1,24 +1,3 @@
-// const isLoggedIn = (req, res, next) => {
-//   if (req.session.currentUser) {
-//     next();
-//   } else {
-//     res.render("auth/login-form", {
-//       errorMessage: "Para acceder, inicia sesión",
-//     });
-//     return;
-//   }
-// };
-
-// const isLoggedOut = (req, res, next) => {
-//   if (req.session.currentUser) {
-//     res.redirect("/auth/profile");
-//   } else {
-//     next();
-//   }
-// };
-
-// module.exports = { isLoggedIn, isLoggedOut };
-
 const isLoggedIn = (req, res, next) => {
   if (!req.session.currentUser) {
     return res.redirect("/auth/log-in");
@@ -26,8 +5,6 @@ const isLoggedIn = (req, res, next) => {
   next();
 };
 
-// if an already logged in user tries to access the login page it
-// redirects the user to the home page
 const isLoggedOut = (req, res, next) => {
   if (req.session.currentUser) {
     return res.redirect("/");
