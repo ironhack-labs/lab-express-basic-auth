646ee8d6a9db4cf78666b37e85959973b38c2dcd
“Zuzana <zuzana@getyourguide.com>
2023-04-22 16:14:53 +0200

    adding form validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 // routes/auth.routes.js
-
+const mongoose = require('mongoose');
 const { Router } = require('express');
 const router = new Router();
 
@@ -15,19 +15,28 @@ router.get('/signup', (req, res) => res.render('auth/signup'));
 router.post('/signup', (req, res, next) => {
     // console.log("The form data: ", req.body);
     
-   
-    const { username, email, password } = req.body;
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username and password.' });
+      return;
+    }
+
+      // make sure passwords are strong:
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
    
     bcryptjs
       .genSalt(saltRounds)
       .then(salt => bcryptjs.hash(password, salt))
       .then(hashedPassword => {
         return User.create({
-          // username: username
           username,
-          // passwordHash => this is the key from the User model
-          //     ^
-          //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
           passwordHash: hashedPassword
         });
       })
@@ -35,8 +44,18 @@ router.post('/signup', (req, res, next) => {
         // console.log('Newly created user is: ', userFromDB);
         res.redirect('/userProfile');
       })
-      .catch(error => next(error));
-  });
+      .catch(error => {
+        if (error instanceof mongoose.Error.ValidationError) {
+          res.status(500).render('auth/signup', { errorMessage: error.message });
+        } else if (error.code === 11000) {
+          res.status(500).render('auth/signup', {
+             errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+          });
+        } else {
+          next(error);
+        }
+      }); // close .catch()
+  }) // close .post()
 
 router.get('/userProfile', (req, res) => res.render('users/user-profile'));
 
