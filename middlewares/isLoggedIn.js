fec9613462894f3161ce58b8b80c8353d07e34bc
Lena Imdahl <mail@lenaimdahl.de>
2023-04-22 21:16:46 +0200

    deleted commend, renamed environment variable

--- middlewares/isLoggedIn.js
+++ middlewares/isLoggedIn.js
@@ -1,20 +1,10 @@
 const isLoggedIn = (req, res, next) => {
-    if (req.session.user) {
-      next();
-      return;
-    }
-  
-    res.redirect("/login");
-  };
-  
-  // this and the one above are the same.
-  // function isLoggedIn(req, res, next) {
-  //   if (req.session.user) {
-  //     next();
-  //     return;
-  //   }
-  
-  //   res.redirect("/login");
-  // }
-  
-  module.exports = isLoggedIn;
+  if (req.session.user) {
+    next();
+    return;
+  }
+
+  res.redirect("/login");
+};
+
+module.exports = isLoggedIn;
