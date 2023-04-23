ae07400d20715f8ce80901b4bf26849884fe150e
“Zuzana <zuzana@getyourguide.com>
2023-04-23 15:37:24 +0200

    completed auth

--- config/session.config.js
+++ config/session.config.js
@@ -1,19 +1,17 @@
 // config/session.config.js
-
+ 
 // require session
 const session = require('express-session');
-
-// since we are going to USE this middleware in the app.js,
-// let's export it and have it receive a parameter
+ 
+// ADDED: require mongostore
+const MongoStore = require('connect-mongo');
+ 
+// ADDED: require mongoose
+const mongoose = require('mongoose');
+ 
 module.exports = app => {
-  // <== app is just a placeholder here
-  // but will become a real "app" in the app.js
-  // when this file gets imported/required there
-
-  // required for the app when deployed to Heroku (in production)
   app.set('trust proxy', 1);
-
-  // use session
+ 
   app.use(
     session({
       secret: process.env.SESS_SECRET,
@@ -23,8 +21,14 @@ module.exports = app => {
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         secure: process.env.NODE_ENV === 'production',
         httpOnly: true,
-        maxAge: 60000 // 60 * 1000 ms === 1 min
-      }
+        maxAge: 60000
+      }, // ADDED code below !!!
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/basic-auth'
+ 
+        // ttl => time to live
+        // ttl: 60 * 60 * 24 // 60sec * 60min * 24h => 1 day
+      })
     })
   );
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -95,7 +95,12 @@ router.get('/userProfile', (req, res) =>  {
   res.render('users/user-profile', { userInSession: req.session.currentUser });
 } )
 
-
+router.post('/logout', (req, res, next) => {
+  req.session.destroy(err => {
+    if (err) next(err);
+    res.redirect('/');
+  });
+});
 
 
 module.exports = router;
