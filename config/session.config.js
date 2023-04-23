d44dece861c263e704697a994fdfec9a73cfa8ba
“Zuzana <zuzana@getyourguide.com>
2023-04-23 15:31:21 +0200

    added cookie

---
+++ config/session.config.js
@@ -0,0 +1,30 @@
+// config/session.config.js
+
+// require session
+const session = require('express-session');
+
+// since we are going to USE this middleware in the app.js,
+// let's export it and have it receive a parameter
+module.exports = app => {
+  // <== app is just a placeholder here
+  // but will become a real "app" in the app.js
+  // when this file gets imported/required there
+
+  // required for the app when deployed to Heroku (in production)
+  app.set('trust proxy', 1);
+
+  // use session
+  app.use(
+    session({
+      secret: process.env.SESS_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        httpOnly: true,
+        maxAge: 60000 // 60 * 1000 ms === 1 min
+      }
+    })
+  );
+};
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -9,12 +9,13 @@ const saltRounds = 10;
 const User = require('../models/User.model');
 
 // GET route ==> to display the signup form to users
-router.get('/signup', (req, res) => res.render('auth/signup'));
+router.get('/signup', (req, res) => {
+  res.render('auth/signup')
+});
 
 // POST route ==> to process form data
 router.post('/signup', (req, res, next) => {
-    // console.log("The form data: ", req.body);
-    
+    console.log(req.body);
     const { username, password } = req.body;
 
     if (!username || !password) {
@@ -42,6 +43,7 @@ router.post('/signup', (req, res, next) => {
       })
       .then(userFromDB => {
         // console.log('Newly created user is: ', userFromDB);
+
         res.redirect('/userProfile');
       })
       .catch(error => {
@@ -49,7 +51,7 @@ router.post('/signup', (req, res, next) => {
           res.status(500).render('auth/signup', { errorMessage: error.message });
         } else if (error.code === 11000) {
           res.status(500).render('auth/signup', {
-             errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+             errorMessage: 'Usernames need to be unique. Either username or email is already used.'
           });
         } else {
           next(error);
@@ -57,7 +59,43 @@ router.post('/signup', (req, res, next) => {
       }); // close .catch()
   }) // close .post()
 
-router.get('/userProfile', (req, res) => res.render('users/user-profile'));
+
+// GET route ==> to display the login form to users
+router.get('/login', (req, res) => res.render('auth/login'));
+
+// POST login route ==> to process form data
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+  const { username, password } = req.body;
+ 
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'username is not registered. Try with other username.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+       // res.render('users/user-profile', { user });
+        req.session.currentUser = user;
+        res.redirect('/userProfile');
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
+router.get('/userProfile', (req, res) =>  {
+  res.render('users/user-profile', { userInSession: req.session.currentUser });
+} )
+
+
 
 
 module.exports = router;
