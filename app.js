ac47fc70a546043022c29040463eb527be31babc
MSiesto <miguelsiesto@gmail.com>
2023-05-08 22:18:07 +0200

    done

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
 require('dotenv/config');
-
-// ℹ️ Connects to the database
 require('./db');
+
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
@@ -13,10 +12,12 @@ const express = require('express');
 // https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 
-const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+const app = express();
+require("./config/session.config")(app)
+require("./config")(app)
+
 
 // default value for title local
 const projectName = 'lab-express-basic-auth';
@@ -27,6 +28,13 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const userRouter = require('./routes/user.routes')
+app.use('/', userRouter)
+
+const authRouter = require('./routes/auth.routes')
+app.use('/', authRouter)
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
