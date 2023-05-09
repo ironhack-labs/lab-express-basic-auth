0002aa8af72882174e730a827ddffc68c4da525c
Gan-Dev <60898077+Gan-dev@users.noreply.github.com>
2023-05-09 12:05:32 +0200

    hola

--- config/session.config.js
+++ config/session.config.js
@@ -13,7 +13,7 @@ module.exports = app => {
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
-                maxAge: 60000
+                maxAge: 60000000
             },
             store: MongoStore.create({
                 mongoUrl: process.env.MONGODB_URI || 'mongodb://localhost/basic-auth'
--- routes/private.routes.js
+++ routes/private.routes.js
@@ -3,10 +3,11 @@ const { isLoggedIn } = require("../middlewares/route.guard");
 const router = require("express").Router();
 
 router.get("/profile", isLoggedIn, (req, res, next) => {
-    res.render("user/profile", { user: req.session.currentUser })
+    const user = req.session.currentUser
+    res.render("user/profile", { user }) //mejorar con destructuracion
 })
 router.get("/main", isLoggedIn, (req, res, next) => {
-    res.render("user/main", { user: req.session.currentUser })
+    res.render("user/main", { user: req.session.currentUser }) //mejorar con destructurracion :)
 })
 module.exports = router
 
