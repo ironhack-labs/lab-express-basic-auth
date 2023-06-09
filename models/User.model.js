a7335c2aa405133203564adf7916f4f8386b152c
Patricia <marie.lou.trips@gmail.com>
2023-06-09 15:46:08 +0200

    iteration one

--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,23 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: [true, 'Username is required.'],
+      unique: true
+    },
+    passwordHash: {
+      type: String,
+      required: [true, 'Password is required.']
+    }
   },
-  password: String
-});
-
+  {
+    timestamps: true
+  }
+);
 const User = model("User", userSchema);
 
-module.exports = User;
+module.exports = model('User', userSchema);
---
+++ routes/auth.routes.js
@@ -0,0 +1,40 @@
+const bcryptjs = require('bcryptjs');
+const saltRounds = 10;
+
+const express = require('express');
+const router = express.Router(); 
+
+const User = require('../models/User.model');
+
+
+/* GET home page */
+router.get("/signup", (req, res, next) => {
+    res.render("auth/signup")
+});
+
+
+router.post("/signup", (req, res, next) => {
+    console.log("req.body", req.body)
+    const { username, password } = req.body;
+
+    bcryptjs
+    .genSalt(saltRounds)
+    .then(salt => bcryptjs.hash(password, salt))
+    .then(hashedPassword => {
+        return User.create({
+            username, 
+            password: hashedPassword
+        });
+    })
+});
+
+router.get("/profile/:username", (req, res, next) => {
+    User.findOne({ username: req.params.username })
+        .then(foundUser => {
+            console.log("foundUser", foundUser)
+            res.render("auth/profile", foundUser)
+        })
+        .catch(err => console.log(err)) 
+});
+
+module.export = router;
