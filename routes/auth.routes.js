12d8ca63b0985790c3d6feeae42caf33d5605848
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-06-09 19:38:34 +0100

    Iteration 1 Done

---
+++ routes/auth.routes.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/User.model');
+
+/* GET home page */
+router.get("/signup", (req, res, next) => {
+    res.render('auth/signup')
+});
+
+router.post("/signup", (req, res, next) => {
+    console.log('req.body', req.body)
+    const { username, email, password } = req.body;
+
+    bcrypt
+    .genSalt(saltRounds)
+    .then(salt => bcrypt.hash(password, salt))
+    .then(hashedPassword => {
+      return User.create({
+        username,
+        email,
+        passwordHash: hashedPassword
+      });
+    })
+    .then(userFromDB => {
+      console.log('Newly created user is: ', userFromDB);
+      res.redirect(`/auth/profile/${userFromDB.username}`)
+    })
+    .catch(error => next(error));
+
+});
+
+router.get("/profile/:username", (req, res, next) => {
+    User.findOne({ username: req.params.username })
+        .then(foundUser => {
+            console.log('foundUser', foundUser)
+            res.render('auth/profile', foundUser)
+        })
+        .catch(err => console.log(err))
+    
+});
+
+module.exports = router;
