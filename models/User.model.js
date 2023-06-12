2d5614b91a70e435692b36381db790f9b3f922b9
Diogo Parreira <diogomiguel.nunes@hotmail.com>
2023-06-12 10:33:29 +0100

    Iterations 2 & 3 Done

--- models/User.model.js
+++ models/User.model.js
@@ -1,6 +1,6 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
+// creating the model
 const userSchema = new Schema({
   username: {
     type: String,
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,5 @@
-const bcrypt = require('bcryptjs');
+
+const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 
 const express = require('express');
@@ -6,18 +7,20 @@ const router = express.Router();
 
 const User = require('../models/User.model');
 
-/* GET home page */
-router.get("/signup", (req, res, next) => {
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
+
+router.get("/signup", isLoggedOut, (req, res, next) => {
     res.render('auth/signup')
 });
 
 router.post("/signup", (req, res, next) => {
     console.log('req.body', req.body)
     const { username, email, password } = req.body;
-
-    bcrypt
+    
+// creating the User with hasshed password
+    bcryptjs
     .genSalt(saltRounds)
-    .then(salt => bcrypt.hash(password, salt))
+    .then(salt => bcryptjs.hash(password, salt))
     .then(hashedPassword => {
       return User.create({
         username,
@@ -25,22 +28,54 @@ router.post("/signup", (req, res, next) => {
         passwordHash: hashedPassword
       });
     })
-    .then(userFromDB => {
-      console.log('Newly created user is: ', userFromDB);
-      res.redirect(`/auth/profile/${userFromDB.username}`)
+    .then(() => {
+      res.redirect(`/auth/login`)
     })
     .catch(error => next(error));
 
 });
 
-router.get("/profile/:username", (req, res, next) => {
-    User.findOne({ username: req.params.username })
-        .then(foundUser => {
-            console.log('foundUser', foundUser)
-            res.render('auth/profile', foundUser)
-        })
-        .catch(err => console.log(err))
-    
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('auth/profile', { userInSession: req.session.currentUser });
 });
 
+router.get('/login', isLoggedOut, (req, res) => {
+  res.render('auth/login')
+})
+
+router.post('/login', (req, res, next) => {
+  console.log('SESSION =====> ', req.session);
+  console.log(req.body)
+  const { username, password } = req.body;
+
+// creating solutions for possible scenarios of correct/incorrect login
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.'
+    });
+    return;
+  }
+ 
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other username.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        req.session.currentUser = user;
+        res.redirect('/auth/profile');
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch(error => next(error));
+});
+
+router.get('/main', isLoggedIn, (req, res) => {
+    res.render('auth/main')
+});
+
+router.get('/private', isLoggedIn, (req, res) => {
+  res.render('auth/private')
+})
 module.exports = router;
