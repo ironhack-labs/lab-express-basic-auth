2b0477bd150c88d990945900405ed35c90b39f71
churrundo <churrundo@gmail.com>
2023-06-18 21:11:05 -0600

    finish iteration 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -35,18 +35,58 @@ router.post("/signup", (req, res, next) => {
       res.redirect("/userProfile");
     })
     .catch((error) => {
-        if (error instanceof mongoose.Error.ValidationError) {
-            res.status(500).render('auth/signup', { errorMessage: error.message });
-          } else if (error.code === 11000) {
-            res.status(500).render('auth/signup', {
-               errorMessage: 'Username and email need to be unique. Either username or email is already used.'
-            });
-          } else {
-            next(error);
-          }
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render("auth/signup", { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render("auth/signup", {
+          errorMessage:
+            "Username and email need to be unique. Either username or email is already used.",
+        });
+      } else {
+        next(error);
+      }
     });
 });
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get("/login", (req, res) => res.render("auth/login"));
+
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+  const { email, password } = req.body;
+
+  if (email === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, email and password to login.",
+    });
+    return;
+  }
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Email is not registered. Try with other email.",
+        });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        req.session.currentUser = user;
+        res.redirect("/userProfile");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+    .catch((error) => next(error));
+});
+
+router.get("/userProfile", (req, res) =>
+  res.render("users/user-profile", { userInSession: req.session.currentUser })
+);
+
+router.post('/logout', (req, res, next) => {
+    req.session.destroy(err => {
+      if (err) next(err);
+      res.redirect('/');
+    });
+  });
 
 module.exports = router;
