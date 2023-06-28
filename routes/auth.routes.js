c0a91d97f26187d81e3cd133afcf5ac63900a8b4
robopecha <julikolb@gmail.com>
2023-06-28 22:53:08 +0200

    bonus done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = new Router();
+const mongoose = require('mongoose');
 const User = require('../models/User.model');
 
 const bcryptjs = require('bcryptjs');
@@ -8,7 +9,18 @@ const saltRounds = 10;
 router.get('/signup', (req, res) => res.render('auth/signup'));
 
 router.post('/signup', (req, res, next) => {
-  const { username, email, password } = req.body;
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username and password.' });
+    return;
+  }
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res
+      .status(500)
+      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    return;
+  }
   bcryptjs
     .genSalt(saltRounds)
     .then(salt => bcryptjs.hash(password, salt))
@@ -21,8 +33,46 @@ router.post('/signup', (req, res, next) => {
     .then(userFromDB => {
       res.redirect('/userProfile');
     })
+    .catch(error => {
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: error.message });
+      } else if (error.code === 11000) {
+        res.status(500).render('auth/signup', {
+           errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+        });
+      } else {
+        next(error);
+      }
+    });
+});
+
+router.post('/login', (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.'
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        console.log("Username not registered. ");
+        res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        res.render('users/user-profile', { user });
+      } else {
+        console.log("Incorrect password. ");
+        res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+      }
+    })
     .catch(error => next(error));
 });
+
+router.get('/login', (req, res) => res.render('auth/login'));
 
 router.get('/userProfile', (req, res) => res.render('users/user-profile'));
 
