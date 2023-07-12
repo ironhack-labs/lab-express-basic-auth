03271cca91e4385dae19f829ee766a898e8cbdd7
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-07-12 21:18:03 +0200

    Iteration 1

--- models/User.model.js
+++ models/User.model.js
@@ -1,12 +1,38 @@
 const { Schema, model } = require("mongoose");
 
+const bcrypt = require("bcrypt");
+const SALT_ROUNDS = 10;
+
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: [true, "Username is required."],
+    },
+    password: {
+      type: String,
+      required: [true, "Username is required."],
+    },
   },
-  password: String
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    bcrypt
+      .hash(this.password, SALT_ROUNDS)
+      .then((hash) => {
+        this.password = hash;
+        next();
+      })
+      .catch((error) => next(error));
+  } else {
+    next();
+  }
 });
 
 const User = model("User", userSchema);
