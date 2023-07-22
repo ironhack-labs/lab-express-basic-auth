0e98319dfcfc86da6c5c27f30a9de17f28d42d5e
Biljana Kotevska <biljanakotev@gmail.com>
2023-07-22 10:06:19 +0200

    finished iteration 2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,12 +4,9 @@ const User = require('../models/User.model');
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 
+// SIGN UP ROUTES
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
-});
-
-router.get('/userProfile', (req, res) => {
-  res.render('users/user-profile');
 });
 
 router.post('/signup', (req, res) => {
@@ -30,6 +27,52 @@ router.post('/signup', (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.get('/userProfile', (req, res) => {
+  res.render('users/user-profile', { userInSession: req.session.currentUser });
+});
+
+// LOGIN ROUTES
+
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+router.post('/login', (req, res) => {
+  console.log('SESSION =====> ', req.session);
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.render('auth/login', {
+      errorMessage: 'Please enter both, username and password to login.',
+    });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other username.' });
+        return;
+      } else if (bcryptjs.compareSync(password, user.password)) {
+        // res.render('users/user-profile', { user });
+        req.session.currentUser = user;
+        res.redirect('/userProfile');
+      } else {
+        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+      }
+    })
+    .catch((error) => console.log(error));
+});
+
+// LOGOUT ROUTE
+
+router.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) console.log(err);
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
