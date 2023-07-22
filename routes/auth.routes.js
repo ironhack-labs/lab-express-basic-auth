d196b9e4fa7b44d5c6cb8fecf0e5766c55e9d152
Marcos Francesquini <francesquini.personal@gmail.com>
2023-07-22 09:50:15 +0200

    redirecting to user's page after registering

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -10,28 +10,35 @@ router.get("/signup", (req, res) => {
 
 router.post("/signup", (req, res) => {
     const { email, password } = req.body;
-  
+
     bcryptjs
-      .genSalt(saltRounds)
-      .then(salt => bcryptjs.hash(password, salt))
-      .then(hashedPassword => {
-        console.log(`Password hash: ${hashedPassword}`);
-  
-        // Agora que temos o hash da senha, podemos criar o usuário com a senha hash
-        User.create({ email, password: hashedPassword }) // <-- Use hashedPassword aqui
-          .then(() => {
-            console.log(email, hashedPassword);
-            res.redirect("/");
-          })
-          .catch(err => {
+        .genSalt(saltRounds)
+        .then(salt => bcryptjs.hash(password, salt))
+        .then(hashedPassword => {
+            console.log(`Password hash: ${hashedPassword}`);
+
+            // Agora que temos o hash da senha, podemos criar o usuário com a senha hash
+            User.create({ email, password: hashedPassword }) // <-- Use hashedPassword aqui
+                .then(() => {
+                    console.log(email, hashedPassword);
+                    res.redirect('userProfile');
+                })
+                .catch(err => {
+                    console.error(err);
+
+                });
+        })
+        .catch(err => {
             console.error(err);
 
-          });
-      })
-      .catch(err => {
-        console.error(err);
+        });
+});
 
-      });
-  });
-  
-  module.exports = router
+router.get('/userProfile', (req, res) => {
+    res.render('user-profile')
+
+});
+
+module.exports = router;
+
+module.exports = router
