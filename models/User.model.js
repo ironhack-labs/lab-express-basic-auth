9946f1bcc0950edbe1cfb8b15313fa4c0e591d64
Biljana Kotevska <biljanakotev@gmail.com>
2023-07-24 17:05:52 +0200

    finished iteration 2 added views for iteration 3

--- models/User.model.js
+++ models/User.model.js
@@ -2,12 +2,15 @@ const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
   {
-    username: {
+    email: {
       type: String,
-      trim: true,
-      required: [true, 'Username is required.'],
+      required: [true, 'Email is required.'],
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
       unique: true,
+      lowercase: true,
+      trim: true,
     },
+
     password: {
       type: String,
       required: [true, 'Password is required.'],
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,74 +3,96 @@ const router = new Router();
 const User = require('../models/User.model');
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
+const mongoose = require('mongoose');
+
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
 
 // SIGN UP ROUTES
-router.get('/signup', (req, res) => {
+router.get('/signup', isLoggedOut, (req, res) => {
   res.render('auth/signup');
 });
 
-router.post('/signup', (req, res) => {
-  const { username, password } = req.body;
+router.post('/signup', isLoggedOut, (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your email and password' });
+    return;
+  }
+
+  const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+  if (!regex.test(password)) {
+    res.status(500).render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter' });
+    return;
+  }
 
   bcryptjs
     .genSalt(saltRounds)
     .then((salt) => bcryptjs.hash(password, salt))
     .then((hashedPassword) => {
       return User.create({
-        username,
+        email,
         password: hashedPassword,
-      }).then((userFromDB) => {
-        console.log('Newly created user is:', userFromDB);
-        res.redirect('/userProfile');
       });
     })
+    .then(() => {
+      res.redirect('/userProfile');
+    })
     .catch((err) => {
-      console.log(err);
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(500).render('auth/signup', { errorMessage: err.message });
+      } else if (err.code === 11000) {
+        res.status(500).render('auth/signup', { errorMessage: 'Email is a already in use, signup with another email' });
+      } else {
+        next(err);
+      }
     });
 });
 
-router.get('/userProfile', (req, res) => {
+router.get('/userProfile', isLoggedIn, (req, res) => {
   res.render('users/user-profile', { userInSession: req.session.currentUser });
 });
 
 // LOGIN ROUTES
 
-router.get('/login', (req, res) => {
+router.get('/login', isLoggedOut, (req, res) => {
   res.render('auth/login');
 });
 
-router.post('/login', (req, res) => {
-  console.log('SESSION =====> ', req.session);
-  const { username, password } = req.body;
+router.post('/login', isLoggedOut, (req, res) => {
+  const { email, password } = req.body;
+  console.log('SESSION =====>', req.session);
 
-  if (username === '' || password === '') {
+  if (email === '' || password === '') {
     res.render('auth/login', {
-      errorMessage: 'Please enter both, username and password to login.',
+      errorMessage: 'Please enter both, email and password to login.',
     });
     return;
   }
 
-  User.findOne({ username })
+  User.findOne({ email })
     .then((user) => {
       if (!user) {
-        res.render('auth/login', { errorMessage: 'Username is not registered. Try with other username.' });
+        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email' });
         return;
       } else if (bcryptjs.compareSync(password, user.password)) {
         // res.render('users/user-profile', { user });
         req.session.currentUser = user;
         res.redirect('/userProfile');
       } else {
-        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        res.render('auth/login', { errorMessage: 'Incorrect password' });
       }
     })
-    .catch((error) => console.log(error));
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // LOGOUT ROUTE
 
-router.post('/logout', (req, res) => {
+router.post('/logout', isLoggedIn, (req, res, next) => {
   req.session.destroy((err) => {
-    if (err) console.log(err);
+    if (err) next(err);
     res.redirect('/');
   });
 });
