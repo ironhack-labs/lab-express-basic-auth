392692733860cd528aae464c29edcbe08a3fff25
nacho <nachosanson6@gmail.com>
2023-08-07 19:24:26 +0200

    done

---
+++ midllewares/route-guards.js
@@ -0,0 +1,11 @@
+const isLoggedIn = (req, res, next) => {
+    if (req.session.currentUser) {
+        next()
+    } else {
+        res.redirect('/login')
+    }
+}
+
+module.exports = {
+    isLoggedIn
+}
