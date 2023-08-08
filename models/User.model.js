aef383e82a37e6c2519a5bd2460ff42388896085
Lianshuang PUCHEU <lianshuang.pucheu@gmail.com>
2023-08-08 21:38:32 +0200

    done

--- models/User.model.js
+++ models/User.model.js
@@ -1,13 +1,30 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
+    required: true,
     unique: true
   },
-  password: String
+  password: 
+  {
+    type: String,
+    required: true
+  },
+
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  
+  next();
+});
+
 
 const User = model("User", userSchema);
 
