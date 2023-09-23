611282f6a5a7162607482cc4c87f807427f58e8d
Gaspar Bonari <89667053+GasparBonari@users.noreply.github.com>
2023-09-23 18:13:10 +0200

    almost done

--- app.js
+++ app.js
@@ -15,21 +15,19 @@ const hbs = require('hbs');
 
 const app = express();
 
+require('./config/session.config')(app);
+
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
-
-// default value for title local
-const projectName = 'lab-express-basic-auth';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
-
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
 
+const authRouter = require('./routes/auth.routes');
+app.use('/', authRouter );
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
 module.exports = app;
-
