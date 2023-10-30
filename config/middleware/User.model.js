29a72a209300ad10ce2f08d8518429339e3460d3
Alkim <alkim-atanasov@outlook.com>
2023-10-30 22:54:23 +0100

    done

---
+++ config/middleware/User.model.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: [true, 'username is required']
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: [true, 'password is required']
+    },
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = model("User", userSchema);
