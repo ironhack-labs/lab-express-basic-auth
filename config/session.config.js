6ebf5b7edd41e98c59c80855552940379f997321
Mohamed khamkham <mohamkham1999@gmail.com>
2023-11-07 12:05:19 +0100

    done

--- config/session.config.js
+++ config/session.config.js
@@ -15,10 +15,10 @@ module.exports = app => {
                 sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
                 secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
-                maxAge: 60000
+                maxAge: 60100
             },
             store: MongoStore.create({
-                mongoUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-basic-auth";
+                mongoUrl: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-basic-auth"
             })
         })
     );
