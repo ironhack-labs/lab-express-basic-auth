64e7a0fad5bc997367372c825142b707e1a6c576
jltorres29 <jltorres29@hotmail.com>
2023-12-09 13:46:43 +0100

    done

--- db/index.js
+++ db/index.js
@@ -1,17 +1,23 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
-
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lab-express-basic-auth";
+const DB_NAME = process.env.DB_NAME || "riderDB";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017";
 
 mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+  .connect(`${MONGO_URI}/${DB_NAME}`)
+  .then(() => console.log(`Connected to db: ${DB_NAME}`))
+  .catch((err) => console.error(`Error conecting to Mongo: ${err}`));
+
+process.on("SIGINT", () => {
+  mongoose.connection
+    .close()
+    .then(() => {
+      console.log(
+        "Mongoose default connection disconnected through app termination"
+      );
+      process.exit(0);
+    })
+    .catch((err) =>
+      console.log(`Mongoose default connection disconnection error: ${err}`)
+    );
+});
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
 const router = require("express").Router();
+const authController = require("../controllers/auth.controller");
+const usersController = require("../controllers/users.controller");
+const ridersController = require("../controllers/riders.controller");
+const authMiddleware = require("../middlewares/auth.middlewares");
 
-/* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get("/", authMiddleware.isAuthenticated, (req, res, next) => {
+  res.render("home");
 });
+
+// auth
+router.get("/login", authMiddleware.isNotAuthenticated, authController.login);
+router.post(
+  "/login",
+  authMiddleware.isNotAuthenticated,
+  authController.doLogin
+);
+router.get(
+  "/register",
+  authMiddleware.isNotAuthenticated,
+  authController.register
+);
+router.post(
+  "/register",
+  authMiddleware.isNotAuthenticated,
+  authController.doRegister
+);
+router.get("/logout", authMiddleware.isAuthenticated, authController.logout);
+router.get("/activate/:token", authController.activate);
+
+// users
+router.get("/profile", authMiddleware.isAuthenticated, usersController.profile);
+
+// riders
+router.get("/riders", authMiddleware.isAuthenticated, ridersController.list);
 
 module.exports = router;
