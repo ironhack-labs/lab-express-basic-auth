a56e802328388ee5d8a558bdac6c6c57f0d06e51
Ensha C Latheef <enshalatheef@gmail.com>
2023-12-18 22:04:07 +0100

    updated auth flows

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,10 @@ const router = new Router();
 const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 const User = require('../models/User.model');
+
+
 router.get('/signup', (req, res) => res.render('auth/signup'));
+
 router.post('/signup', (req, res, next) => {
     const { username, password } = req.body;
  
@@ -13,20 +16,57 @@ router.post('/signup', (req, res, next) => {
     .then(hashedPassword => {
       return User.create({
         username,
-        passwordHash: hashedPassword
+        password: hashedPassword
       });
 
     })
     .then(userFromDB => {
-        // console.log('Newly created user is: ', userFromDB);
-        res.redirect('/userProfile');   
+        res.redirect('/home');   
       })
     .catch(error => next(error));
 });
-router.get('/userProfile', (req, res) => res.render('users/user-profile'));
 
 
 
 
+//////////// L O G I N ///////////
+
+router.get('/login', (req, res) => res.render('auth/login'));
+router.post('/login', (req, res, next) => {
+    const { username, password } = req.body;
+  
+    if (username === '' || password === '') {
+      res.render('auth/login', {
+        errorMessage: 'Please enter both, username and password to login.'
+      });
+      return;
+    }
+  
+    User.findOne({ username })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.password)) {
+          res.redirect('/home');
+        } else {
+          console.log("Incorrect password. ");
+          res.render('auth/login', { errorMessage: 'User not found and/or incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
+  });
+  router.get('/home', (req, res) => {
+    res.render('index', { user: {} });
+  })
+  
+  router.get('/main', (req, res) => {
+    res.render('main', { user: {} });
+  })
+  
+  router.get('/private', (req, res) => {
+    res.render('private', { user: {} });
+  })
+  
 
   module.exports = router;
