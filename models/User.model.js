92ea894f1f70571dcc559eec3d8f71f0550868b6
Patricia Kroenert <p.kroenert98@gmail.com>
2023-12-18 17:03:27 +0100

    basic auth

--- models/User.model.js
+++ models/User.model.js
@@ -1,14 +1,26 @@
 const { Schema, model } = require("mongoose");
 
-// TODO: Please make sure you edit the user model to whatever makes sense in this case
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
   },
-  password: String
-});
+  {
+    timestamps: true,
+  }
+);
 
-const User = model("User", userSchema);
-
-module.exports = User;
+module.exports = model("User", userSchema);
